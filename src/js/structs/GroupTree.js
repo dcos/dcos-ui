b65cb698ccf33e8c4a189980d3bb6176ae1a5ce2
John Furrow <johnf@mesosphere.io>
2016-03-17 15:14:41 +0100

    Ensure item.labels exists and stop looking for null value

--- src/js/structs/GroupTree.js
+++ src/js/structs/GroupTree.js
@@ -62,8 +62,7 @@ module.exports = class GroupTree extends Tree {
       }
 
       // Check the label DCOS_PACKAGE_IS_FRAMEWORK which contains a string.
-      if (item.labels.DCOS_PACKAGE_IS_FRAMEWORK != null
-        && item.labels.DCOS_PACKAGE_IS_FRAMEWORK === 'true') {
+      if (item.labels && item.labels.DCOS_PACKAGE_IS_FRAMEWORK === 'true') {
         return new Framework(item);
       } else {
         return new Application(item);
