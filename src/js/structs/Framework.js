e8feed4cef2e87f09565daae5ddc29e62699ebba
Orlando Hohmeier <orlando@mesosphere.io>
2016-03-22 14:59:56 +0100

    Alphabetize framework getter

--- src/js/structs/Framework.js
+++ src/js/structs/Framework.js
@@ -2,26 +2,38 @@ import {
   ROUTE_ACCESS_PREFIX,
   SERVICE_ID_VALID_CHARACTERS
 } from '../constants/ServiceConstants';
-import HealthStatus from '../constants/HealthStatus';
 import Service from './Service';
+import ServiceUtil from '../utils/ServiceUtil';
+import Util from '../utils/Util';
 
 module.exports = class Framework extends Service {
-  getHealth() {
-    let meta = this.get('_meta');
-    if (!meta || !meta.marathon) {
-      return HealthStatus.NA;
+  getImages() {
+    return ServiceUtil.getServiceImages(this.getMetadata().images);
+  }
+
+  getMetadata() {
+    let labels = this.getLabels();
+    if (Util.findNestedPropertyInObject(labels, 'DCOS_PACKAGE_METADATA.length') == null) {
+      return {};
     }
-    return meta.marathon.health;
+
+    // extract content of the DCOS_PACKAGE_METADATA label
+    try {
+      var dataAsJsonString = global.atob(labels.DCOS_PACKAGE_METADATA);
+      return JSON.parse(dataAsJsonString);
+    } catch (error) {
+      return {};
+    }
+  }
+
+  getNodeIDs() {
+    return this.get('slave_ids');
   }
 
   getResourceID() {
     let regexp = new RegExp(`[^${SERVICE_ID_VALID_CHARACTERS}]`, 'g');
     // strip non-alphanumeric chars from name for safety
     return ROUTE_ACCESS_PREFIX + (this.get('name') || '').replace(regexp, '');
-  }
-
-  getNodeIDs() {
-    return this.get('slave_ids');
   }
 
   getUsageStats(resource) {
