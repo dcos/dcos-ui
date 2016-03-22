41a11a786353584cbbd71e7048900531c7716940
Orlando Hohmeier <orlando@mesosphere.io>
2016-03-22 19:04:35 +0100

    Remove application struct type getter

--- src/js/structs/Application.js
+++ src/js/structs/Application.js
@@ -1,7 +1,3 @@
 import Service from './Service';
 
-module.exports = class Application extends Service {
-  get isApplication() {
-    return !this.labels.DCOS_PACKAGE_IS_FRAMEWORK;
-  }
-};
+module.exports = class Application extends Service {};
