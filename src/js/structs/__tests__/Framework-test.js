a9ebf7c23cfc81204e728f99a86f39b008703d7b
Orlando Hohmeier <orlando@mesosphere.io>
2016-03-22 14:55:30 +0100

    Add framework image and metadata getter

--- src/js/structs/__tests__/Framework-test.js
+++ src/js/structs/__tests__/Framework-test.js
@@ -1,20 +1,61 @@
-let HealthStatus = require('../../constants/HealthStatus');
 let Framework = require('../Framework');
+let ServiceImages = require('../../constants/ServiceImages');
 
 describe('Framework', function () {
 
-  describe('#getHealth', function () {
+  beforeEach(function () {
+    this.instance = new Framework({
+      labels: {
+        DCOS_PACKAGE_METADATA: 'eyJuYW1lIjoic2VydmljZSIsImltYWdlcyI6eyJpY29u' +
+        'LXNtYWxsIjoiaWNvbi1zZXJ2aWNlLXNtYWxsLnBuZyIsImljb24tbWVkaXVtIjoiaWN' +
+        'vbi1zZXJ2aWNlLW1lZGl1bS5wbmciLCJpY29uLWxhcmdlIjoiaWNvbi1zZXJ2aWNlLW' +
+        'xhcmdlLnBuZyJ9fQ=='
+      }
+    });
+  });
 
-    it('returns NA health status', function () {
-      let framework = new Framework();
-      expect(framework.getHealth()).toEqual(HealthStatus.NA);
+  describe('#getImages', function () {
+
+    it('defaults to NA images', function () {
+      let framework = new Framework({});
+      expect(framework.getImages()).toEqual(ServiceImages.NA_IMAGES);
     });
 
-    it('returns correct health status', function () {
-      let service = new Framework({
-        _meta: {marathon: {health: HealthStatus.HEALTHY}}
+    it('get corrects images', function () {
+      expect(this.instance.getImages()).toEqual({
+        'icon-small': 'icon-service-small.png',
+        'icon-medium': 'icon-service-medium.png',
+        'icon-large': 'icon-service-large.png'
       });
-      expect(service.getHealth()).toEqual(HealthStatus.HEALTHY);
+    });
+
+  });
+
+  describe('#getMetadata', function () {
+
+    it('defaults to empty object', function () {
+      let framework = new Framework();
+      expect(framework.getMetadata()).toEqual({});
+    });
+
+    it('returns correct metadata', function () {
+      expect(this.instance.getMetadata()).toEqual({
+        name: 'service',
+        images: {
+          'icon-small': 'icon-service-small.png',
+          'icon-medium': 'icon-service-medium.png',
+          'icon-large': 'icon-service-large.png'
+        }
+      });
+    });
+
+  });
+
+  describe('#getNodeIDs', function () {
+
+    it('returns ids of nodes the service is running on', function () {
+      let framework = new Framework({slave_ids: [1, 2, 3]});
+      expect(framework.getNodeIDs()).toEqual([1, 2, 3]);
     });
 
   });
@@ -35,15 +76,6 @@ describe('Framework', function () {
       let framework = new Framework({name: 'foo-adsf-2'});
       expect(framework.getResourceID())
         .toEqual('dcos:adminrouter:service:foo-adsf-2');
-    });
-
-  });
-
-  describe('#getNodeIDs', function () {
-
-    it('returns ids of nodes the service is running on', function () {
-      let framework = new Framework({slave_ids: [1, 2, 3]});
-      expect(framework.getNodeIDs()).toEqual([1, 2, 3]);
     });
 
   });
