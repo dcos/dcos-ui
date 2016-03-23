bd895960e2fa47a61701b1aec87ed0a4b2c9fe39
Orlando Hohmeier <orlando@mesosphere.io>
2016-03-23 16:43:58 +0100

    s/ServiceConstants/FrameworkConstants/g
    Rename the `ServiceConstants` into `FrameworkConstants` as the respective
    constants are very framework specific and don't belong to all services.

---
+++ src/js/constants/FrameworkConstants.js
@@ -0,0 +1,10 @@
+let constants = {
+  ROUTE_ACCESS_PREFIX: 'dcos:adminrouter:service:',
+  FRAMEWORK_ID_VALID_CHARACTERS: '\\w-'
+};
+
+constants.FRAMEWORK_RESOURCE_ID_REGEXP = new RegExp(
+  `^${constants.ROUTE_ACCESS_PREFIX}[${constants.FRAMEWORK_ID_VALID_CHARACTERS}]+$`
+);
+
+module.exports = constants;
