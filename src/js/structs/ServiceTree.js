e4ca9fc2ee6428a4bf8e58aee55ecca015df0827
Orlando Hohmeier <orlando@mesosphere.io>
2016-03-24 19:28:27 +0100

    Improve ServiceTree input parsing

--- src/js/structs/ServiceTree.js
+++ src/js/structs/ServiceTree.js
@@ -24,33 +24,25 @@ module.exports = class ServiceTree extends Tree {
       this.id = options.id;
     }
 
-    // Replace groups with instances of (Marathon) ServiceTree and append them
-    // to item list/tree
+    // Append Marathon groups
     if (options.groups) {
-      this.list = this.list.concat(options.groups.map((item) => {
-        if (!(item instanceof ServiceTree)) {
-          return new this.constructor(
-            Object.assign({filterProperties: this.getFilterProperties()}, item)
-          );
-        }
-
-        return item;
-      }));
-
+      this.list = this.list.concat(options.groups);
     }
 
-    // Append apps to item list/tree
+    // Append applications
     if (options.apps) {
       this.list = this.list.concat(options.apps);
     }
 
-    // Replace group tree like items instances of ServiceTree and replace items
-    // with instance of Service
+    // Converts items into instances of ServiceTree, Application or Framework
+    // based on their properties.
     this.list = this.list.map((item) => {
       if (item instanceof ServiceTree) {
         return item;
       }
 
+      // Check item properties and convert items with an items array or an apps
+      // and groups array (Marathon group structure) into ServiceTree instances.
       if ((item.items != null && Util.isArray(item.items)) ||
           (item.groups != null && Util.isArray(item.groups) &&
           item.apps != null && Util.isArray(item.apps))) {
@@ -59,7 +51,8 @@ module.exports = class ServiceTree extends Tree {
         );
       }
 
-      // Check the label DCOS_PACKAGE_FRAMEWORK_NAME which contains a string.
+      // Check the DCOS_PACKAGE_FRAMEWORK_NAME label to determine if the item
+      // should be converted to an Application or Framework instance.
       if (item.labels && item.labels.DCOS_PACKAGE_FRAMEWORK_NAME != null) {
         return new Framework(item);
       } else {
