eb2fa3fcc871db982b0ccd1289ff52788d04d9e4
Orlando Hohmeier <orlando@mesosphere.io>
2016-04-04 12:08:10 +0200

    Integrate dcos store and structs into service page
    * Integrate dcos store and remove Marathon/Mesos store dependencies
    * Use the new service structs (ServiceTree, Framework, Applications)
    * Remove resource charts from service page
    * Remove filters from service page, as they will be replace with a new
      filter sidebar
    * Introduce custom service table sort functions

--- src/js/pages/ServicesPage.js
+++ src/js/pages/ServicesPage.js
@@ -1,55 +1,16 @@
 var _ = require('underscore');
 var React = require('react');
 import {RouteHandler} from 'react-router';
+import {StoreMixin} from 'mesosphere-shared-reactjs';
 
 var AlertPanel = require('../components/AlertPanel');
-import CompositeState from '../structs/CompositeState';
 import Config from '../config/Config';
-import EventTypes from '../constants/EventTypes';
-var FilterHealth = require('../components/FilterHealth');
-var FilterHeadline = require('../components/FilterHeadline');
-var FilterInputText = require('../components/FilterInputText');
+import DCOSStore from '../stores/DCOSStore';
 var InternalStorageMixin = require('../mixins/InternalStorageMixin');
-var Page = require('../components/Page');
-var MarathonStore = require('../stores/MarathonStore');
-var MesosSummaryStore = require('../stores/MesosSummaryStore');
-var ResourceBarChart = require('../components/charts/ResourceBarChart');
+import Page from '../components/Page';
 var ServicesTable = require('../components/ServicesTable');
 var SidebarActions = require('../events/SidebarActions');
 import SidePanels from '../components/SidePanels';
-
-function getCountByHealth(frameworks) {
-  return _.foldl(frameworks, function (acc, framework) {
-    let appHealth = MarathonStore.getServiceHealth(framework.name);
-    if (acc[appHealth.value] === undefined) {
-      acc[appHealth.value] = 1;
-    } else {
-      acc[appHealth.value]++;
-    }
-    return acc;
-  }, {});
-}
-
-function getMesosServices(state) {
-  let states = MesosSummaryStore.get('states');
-  let lastState = states.lastSuccessful();
-  let services = CompositeState.getServiceList();
-  let filteredServices = services.filter({
-    health: state.healthFilter,
-    name: state.searchString
-  }).getItems();
-  let serviceIDs = _.pluck(filteredServices, 'id');
-
-  return {
-    services: filteredServices,
-    totalServices: services.getItems().length,
-    countByHealth: getCountByHealth(services.getItems()),
-    statesProcessed: MesosSummaryStore.get('statesProcessed'),
-    refreshRate: Config.getRefreshRate(),
-    totalServiceResources: states.getResourceStatesForServiceIDs(serviceIDs),
-    totalResources: lastState.getSlaveTotalResources()
-  };
-}
 
 var DEFAULT_FILTER_OPTIONS = {
   healthFilter: null,
@@ -60,7 +21,7 @@ var ServicesPage = React.createClass({
 
   displayName: 'ServicesPage',
 
-  mixins: [InternalStorageMixin],
+  mixins: [InternalStorageMixin, StoreMixin],
 
   statics: {
     routeConfig: {
@@ -87,24 +48,15 @@ var ServicesPage = React.createClass({
   },
 
   componentWillMount: function () {
-    this.internalStorage_set(getMesosServices(this.state));
     this.internalStorage_update({
       openServicePanel: false,
       openTaskPanel: false
     });
+
+    this.store_listeners = [{name: 'dcos', events: ['change']}];
   },
 
   componentDidMount: function () {
-    MesosSummaryStore.addChangeListener(
-      EventTypes.MESOS_SUMMARY_CHANGE,
-      this.onMesosStateChange
-    );
-
-    MesosSummaryStore.addChangeListener(
-      EventTypes.MESOS_SUMMARY_REQUEST_ERROR,
-      this.onMesosStateChange
-    );
-
     this.internalStorage_update({
       openServicePanel: this.props.params.serviceName != null,
       openTaskPanel: this.props.params.taskID != null
@@ -118,97 +70,13 @@ var ServicesPage = React.createClass({
     });
   },
 
-  componentWillUnmount: function () {
-    MesosSummaryStore.removeChangeListener(
-      EventTypes.MESOS_SUMMARY_CHANGE,
-      this.onMesosStateChange
-    );
-
-    MesosSummaryStore.removeChangeListener(
-      EventTypes.MESOS_SUMMARY_REQUEST_ERROR,
-      this.onMesosStateChange
-    );
-  },
-
-  onMesosStateChange: function () {
-    this.internalStorage_update(getMesosServices(this.state));
-    this.forceUpdate();
-  },
-
-  handleHealthFilterChange: function (healthFilter) {
-    var stateChanges = _.clone(DEFAULT_FILTER_OPTIONS);
-    stateChanges.healthFilter = healthFilter;
-
-    this.internalStorage_update(getMesosServices(stateChanges));
-    this.setState(stateChanges);
-  },
-
-  onResourceSelectionChange: function (selectedResource) {
-    if (this.state.selectedResource !== selectedResource) {
-      this.setState({selectedResource: selectedResource});
-    }
-  },
-
-  handleSearchStringChange: function (searchString) {
-    var data = getMesosServices({
-      searchString: searchString,
-      healthFilter: this.state.healthFilter
-    });
-
-    this.internalStorage_update(data);
-    this.setState({searchString: searchString});
-  },
-
-  resetFilter: function () {
-    var state = _.clone(DEFAULT_FILTER_OPTIONS);
-    this.internalStorage_update(getMesosServices(state));
-    this.setState(state);
-  },
-
   getServicesPageContent: function () {
-    let state = this.state;
-    let data = this.internalStorage_get();
-    let appsProcessed = MarathonStore.hasProcessedApps();
+    let services = DCOSStore.serviceTree.getItems();
 
     return (
       <div>
-        <ResourceBarChart
-          itemCount={data.services.length}
-          resources={data.totalServiceResources}
-          totalResources={data.totalResources}
-          refreshRate={data.refreshRate}
-          resourceType="Services"
-          selectedResource={state.selectedResource}
-          onResourceSelectionChange={this.onResourceSelectionChange} />
-        <FilterHeadline
-          inverseStyle={true}
-          onReset={this.resetFilter}
-          name="Services"
-          currentLength={data.services.length}
-          totalLength={data.totalServices} />
-        <div className="filter-bar">
-          <div className="filter-bar-left">
-            <div className="filter-bar-item">
-              <FilterHealth
-                countByHealth={data.countByHealth}
-                healthFilter={state.healthFilter}
-                handleFilterChange={this.handleHealthFilterChange}
-                servicesLength={data.totalServices} />
-            </div>
-            <div className="filter-bar-item">
-              <div className="form-group flush-bottom">
-                <FilterInputText
-                  className="flush-bottom"
-                  searchString={state.searchString}
-                  handleFilterChange={this.handleSearchStringChange}
-                  inverseStyle={true} />
-              </div>
-            </div>
-          </div>
-        </div>
         <ServicesTable
-          services={data.services}
-          healthProcessed={appsProcessed} />
+          services={services}/>
         <SidePanels
           params={this.props.params}
           openedPage="services" />
@@ -238,8 +106,8 @@ var ServicesPage = React.createClass({
   },
 
   render: function () {
-    var data = this.internalStorage_get();
-    var isEmpty = data.statesProcessed && data.totalServices === 0;
+    let data = this.internalStorage_get();
+    let isEmpty = DCOSStore.statesProcessed && data.totalServices === 0;
 
     return (
       <Page title="Services">
