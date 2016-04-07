83acd360d4cc4ad148f18cc52b95992577712f7d
Orlando Hohmeier <orlando@mesosphere.io>
2016-04-07 20:33:33 +0200

    Refactor ServiceTableUtil to use private functions

--- src/js/utils/ServiceTableUtil.js
+++ src/js/utils/ServiceTableUtil.js
@@ -1,126 +1,124 @@
 import HealthSorting from '../constants/HealthSorting';
 
-class ServiceTableUtil {
+/**
+ * Compare number values
+ * @param {number} a
+ * @param {number} b
+ * @returns {number} a number indicating whether "a" comes before or after or
+ * is the same as "b" in sort order.
+ */
+function numberCompareFunction(a, b) {
+  let delta = a - b;
+  return (delta) / Math.abs(delta || 1);
+}
 
+/**
+ * Compare service names
+ * @param {Service} a
+ * @param {Service} b
+ * @returns {number} a number indicating whether "a" comes before or after or
+ * is the same as "b" in sort order.
+ */
+function nameCompareFunction(a, b) {
+  return a.getName().localeCompare(b.getName());
+}
+
+/**
+ * Compare number of running tasks
+ * @param {Service} a
+ * @param {Service} b
+ * @returns {number} a number indicating whether "a" comes before or after or
+ * is the same as "b" in sort order.
+ */
+function taskCompareFunction(a, b) {
+  return numberCompareFunction(
+    a.getTasksSummary().tasksRunning,
+    b.getTasksSummary().tasksRunning
+  );
+}
+
+/**
+ * Compare service health
+ * @param {Service} a
+ * @param {Service} b
+ * @returns {number} a number indicating whether "a" comes before or after or
+ * is the same as "b" in sort order.
+ */
+function healthCompareFunction(a, b) {
+  return numberCompareFunction(
+    HealthSorting[a.getHealth().key],
+    HealthSorting[b.getHealth().key]
+  );
+}
+
+/**
+ * Compare service cpus
+ * @param {Service} a
+ * @param {Service} b
+ * @returns {number} a number indicating whether "a" comes before or after or
+ * is the same as "b" in sort order.
+ */
+function cpusCompareFunction(a, b) {
+  return numberCompareFunction(
+    a.getResources().cpus,
+    b.getResources().cpus
+  );
+}
+
+/**
+ * Compare service memory
+ * @param {Service} a
+ * @param {Service} b
+ * @returns {number} a number indicating whether "a" comes before or after or
+ * is the same as "b" in sort order.
+ */
+function memCompareFunction(a, b) {
+  return numberCompareFunction(
+    a.getResources().mem,
+    b.getResources().mem
+  );
+}
+
+/**
+ * Compare service disk
+ * @param {Service} a
+ * @param {Service} b
+ * @returns {number} a number indicating whether "a" comes before or after or
+ * is the same as "b" in sort order.
+ */
+function diskCompareFunction(a, b) {
+  return numberCompareFunction(
+    a.getResources().disk,
+    b.getResources().disk
+  );
+}
+
+const ServiceTableUtil = {
   /**
    * Create service table prop compare functions
    * @param {string} prop
    * @returns {function} prop compare function
    */
-  static propCompareFunctionFactory(prop) {
+   propCompareFunctionFactory(prop) {
     switch (prop) {
       case 'name':
-        return ServiceTableUtil.nameCompareFunction;
+        return nameCompareFunction;
       case 'tasks':
-        return ServiceTableUtil.taskCompareFunction;
+        return taskCompareFunction;
       case 'health':
-        return ServiceTableUtil.healthCompareFunction;
+        return healthCompareFunction;
       case 'cpus':
-        return ServiceTableUtil.cpusCompareFunction;
+        return cpusCompareFunction;
       case 'mem':
-        return ServiceTableUtil.memCompareFunction;
+        return memCompareFunction;
       case 'disk':
-        return ServiceTableUtil.diskCompareFunction;
+        return diskCompareFunction;
       default:
         return function () {
           return 0;
         };
     }
   }
-
-  /**
-   * Compare number values
-   * @param {number} a
-   * @param {number} b
-   * @returns {number} a number indicating whether "a" comes before or after or
-   * is the same as "b" in sort order.
-   */
-  static numberCompareFunction(a, b) {
-    let delta = a - b;
-    return (delta) / Math.abs(delta || 1);
-  }
-
-  /**
-   * Compare service names
-   * @param {Service} a
-   * @param {Service} b
-   * @returns {number} a number indicating whether "a" comes before or after or
-   * is the same as "b" in sort order.
-   */
-  static nameCompareFunction(a, b) {
-    return a.getName().localeCompare(b.getName());
-  }
-
-  /**
-   * Compare number of running tasks
-   * @param {Service} a
-   * @param {Service} b
-   * @returns {number} a number indicating whether "a" comes before or after or
-   * is the same as "b" in sort order.
-   */
-  static taskCompareFunction(a, b) {
-    return ServiceTableUtil.numberCompareFunction(
-      a.getTasksSummary().tasksRunning,
-      b.getTasksSummary().tasksRunning
-    );
-  }
-
-  /**
-   * Compare service health
-   * @param {Service} a
-   * @param {Service} b
-   * @returns {number} a number indicating whether "a" comes before or after or
-   * is the same as "b" in sort order.
-   */
-  static healthCompareFunction(a, b) {
-    return ServiceTableUtil.numberCompareFunction(
-      HealthSorting[a.getHealth().key],
-      HealthSorting[b.getHealth().key]
-    );
-  }
-
-  /**
-   * Compare service cpus
-   * @param {Service} a
-   * @param {Service} b
-   * @returns {number} a number indicating whether "a" comes before or after or
-   * is the same as "b" in sort order.
-   */
-  static cpusCompareFunction(a, b) {
-    return ServiceTableUtil.numberCompareFunction(
-      a.getResources().cpus,
-      b.getResources().cpus
-    );
-  }
-
-  /**
-   * Compare service memory
-   * @param {Service} a
-   * @param {Service} b
-   * @returns {number} a number indicating whether "a" comes before or after or
-   * is the same as "b" in sort order.
-   */
-  static memCompareFunction(a, b) {
-    return ServiceTableUtil.numberCompareFunction(
-      a.getResources().mem,
-      b.getResources().mem
-    );
-  }
-
-  /**
-   * Compare service disk
-   * @param {Service} a
-   * @param {Service} b
-   * @returns {number} a number indicating whether "a" comes before or after or
-   * is the same as "b" in sort order.
-   */
-  static diskCompareFunction(a, b) {
-    return ServiceTableUtil.numberCompareFunction(
-      a.getResources().disk,
-      b.getResources().disk
-    );
-  }
-
-}
+};
 
 module.exports = ServiceTableUtil;
