f1c662cfb640d4298cc78c99e8d822220d280b48
Philipp Hinrichsen <philipph@mesosphere.io>
2016-04-11 17:19:21 +0200

    Change tests to be readable

--- src/js/components/__tests__/StatusBar-test.js
+++ src/js/components/__tests__/StatusBar-test.js
@@ -32,7 +32,7 @@ describe('#StatusBar', function () {
   });
 
   describe('PropTypes', function () {
-    it('Should throw an error if no data prop is provided', function () {
+    it('should throw an error if no data prop is provided', function () {
       spyOn(console, 'error');
       this.instance = ReactDOM.render(
         <StatusBar />,
@@ -45,33 +45,64 @@ describe('#StatusBar', function () {
         );
     });
 
-    it('Should throw an error if data is not in the right shape', function () {
-      spyOn(console, 'error');
-      this.instance = ReactDOM.render(
-        <StatusBar data={[{
-          value: 40,
+    it('should throw an error if a data item is missing a value',
+      function () {
+        spyOn(console, 'error');
+        this.instance = ReactDOM.render(
+          <StatusBar data={[{
           className: 'status'
         }]}/>,
-        this.container
-      );
-      expect(console.error)
-        .toHaveBeenCalledWith(
-          'Warning: Each child in an array or iterator should have a unique' +
-          ' "key" prop. Check the render method of `StatusBar`. See' +
-          ' https://fb.me/react-warning-keys for more information.'
+          this.container
         );
-    });
+        expect(console.error)
+          .toHaveBeenCalledWith(
+            'Warning: Failed propType: Required prop `data[0].value` was not' +
+            ' specified in `StatusBar`.'
+          );
+      });
+
+    it('should throw an error if one data item is missing a value',
+      function () {
+        spyOn(console, 'error');
+        this.instance = ReactDOM.render(
+          <StatusBar data={[{
+            className: 'status',
+            value: 10
+          }, {
+            className: 'unknown'
+          }]}/>,
+          this.container
+        );
+        expect(console.error)
+          .toHaveBeenCalledWith(
+            'Warning: Failed propType: Required prop `data[1].value` was not' +
+            ' specified in `StatusBar`.'
+          );
+      });
+
+    it('should not throw an error if data does only contain a value field',
+      function () {
+        spyOn(console, 'error');
+        this.instance = ReactDOM.render(
+          <StatusBar data={[{
+            value: 40
+          }]}/>,
+          this.container
+        );
+        expect(console.error)
+          .wasNotCalled();
+      });
   });
 
   describe('className', function () {
-    it('class name of the svg should contain status-bar (default)', function () {
+    it('should contain status-bar (default)', function () {
       expect(
         this.container.querySelector('svg')
           .classList.contains('status-bar')
       ).toBeTruthy();
     });
 
-    it('class name of the svg should contain test-bar (custom)', function () {
+    it('should contain test-bar (custom)', function () {
       this.instance = ReactDOM.render(
         <StatusBar
           data={testData}
@@ -86,14 +117,37 @@ describe('#StatusBar', function () {
   });
 
   describe('viewBox', function () {
-    it('viewBox of the svg should be `0 0 100 10` (default)', function () {
+    it('should have a viewBox of `0 0 100 10`', function () {
       expect(
         this.container.querySelector('svg')
           .getAttribute('viewBox')
       ).toEqual('0 0 100 10');
     });
 
-    it('viewBox of the svg should be `0 0 100 20`', function () {
+    it('should be `0 0 100 10` if a height is provided',
+      function () {
+        this.instance = ReactDOM.render(
+          <StatusBar
+            data={testData}
+            height={20}/>,
+          this.container
+        );
+        expect(
+          this.container.querySelector('svg')
+            .getAttribute('viewBox')
+        ).toEqual('0 0 100 10');
+      });
+  });
+
+  describe('height', function () {
+    it('should be 10 (default)', function () {
+      expect(
+        this.container.querySelector('svg')
+          .getAttribute('height')
+      ).toEqual('10');
+    });
+
+    it('should be 20 if 20 is provided', function () {
       this.instance = ReactDOM.render(
         <StatusBar
           data={testData}
@@ -102,65 +156,85 @@ describe('#StatusBar', function () {
       );
       expect(
         this.container.querySelector('svg')
-          .getAttribute('viewBox')
-      ).toEqual('0 0 100 20');
+          .getAttribute('height')
+      ).toEqual('20');
     });
   });
+
   describe('rectangles', function () {
-    it('should contain 2 Rectangles', function () {
+    it('should contain 2 rectangles', function () {
       expect(this.container.querySelectorAll('rect').length)
         .toEqual(testData.length);
     });
 
-    it('first rect should contain class name status', function () {
-      expect(
-        this.container.querySelector('rect:first-child')
-          .classList.contains('status')
-      ).toBeTruthy();
+    describe('First rect', function () {
+      it('should contain class name status', function () {
+        expect(
+          this.container.querySelector('rect:first-child')
+            .classList.contains('status')
+        ).toBeTruthy();
+      });
+
+      it('should have an offset of 0', function () {
+        expect(
+          this.container.querySelector('rect:first-child')
+            .getAttribute('x')
+        ).toEqual('0');
+      });
+
+      it('should have the class element-{index} if no classname is provided',
+        function () {
+          this.instance = ReactDOM.render(
+            <StatusBar data={[{
+            value: 40
+          }]}/>,
+            this.container
+          );
+          expect(this.container.querySelector('rect')
+            .classList.contains('element-0')
+          ).toBeTruthy();
+        });
+
     });
 
-    it('second rect should contain class name failed', function () {
-      expect(
-        this.container.querySelector('rect:nth-child(2)')
-          .classList.contains('failed')
-      ).toBeTruthy();
-    });
+    describe('Second rect', function () {
 
-    it('first rect should have an offset of 0', function () {
-      expect(
-        this.container.querySelector('rect:first-child')
-          .getAttribute('x')
-      ).toEqual('0');
-    });
+      it('should contain class name failed', function () {
+        expect(
+          this.container.querySelector('rect:nth-child(2)')
+            .classList.contains('failed')
+        ).toBeTruthy();
+      });
 
-    it('second rect should have an offset of 30', function () {
-      expect(
-        this.container.querySelector('rect:nth-child(2)')
-          .getAttribute('x')
-      ).toEqual('40');
-    });
+      it('should have an offset of 30', function () {
+        expect(
+          this.container.querySelector('rect:nth-child(2)')
+            .getAttribute('x')
+        ).toEqual('40');
+      });
 
-    it('second rect should have an offset of 60', function () {
-      let data = [
-        {
-          key: '#FFF',
-          value: 3,
-          className: 'status'
-        }, {
-          key: '#000',
-          value: 2,
-          className: 'failed'
-        }
-      ];
-      this.instance = ReactDOM.render(
-        <StatusBar
-          data={data}/>,
-        this.container
-      );
-      expect(
-        this.container.querySelector('rect:nth-child(2)')
-          .getAttribute('x')
-      ).toEqual('60');
+      it('should have an offset of 60', function () {
+        let data = [
+          {
+            key: '#FFF',
+            value: 3,
+            className: 'status'
+          }, {
+            key: '#000',
+            value: 2,
+            className: 'failed'
+          }
+        ];
+        this.instance = ReactDOM.render(
+          <StatusBar
+            data={data}/>,
+          this.container
+        );
+        expect(
+          this.container.querySelector('rect:nth-child(2)')
+            .getAttribute('x')
+        ).toEqual('60');
+      });
     });
   });
 });
