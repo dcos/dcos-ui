f8c795a974eb5dba76cdaef91b9c905ded76c835
Orlando Hohmeier <orlando@mesosphere.io>
2016-04-12 15:22:51 +0200

    Introduce a react router stub util
    Add a react router stub (`JestUtils.stubRouterContext(Component,
    props)`) to enable tests of components which reuquire
    (context) router.

--- src/js/utils/JestUtil.js
+++ src/js/utils/JestUtil.js
@@ -1,4 +1,5 @@
 const TestUtils = require('react-addons-test-utils');
+import React from 'react';
 
 let stores = {
   CosmosPackagesStore: '../stores/CosmosPackagesStore',
@@ -9,6 +10,25 @@ let stores = {
   MetadataStore: '../stores/MetadataStore',
   TaskDirectoryStore: '../stores/TaskDirectoryStore'
 };
+
+// Private router stub
+class RouterStub {
+  static makePath() {}
+  static makeHref() {}
+  static transitionTo() {}
+  static replaceWith() {}
+  static goBack() {}
+  static getCurrentPath() {}
+  static getCurrentRoutes() {}
+  static getCurrentPathname() {}
+  static getCurrentParams() {}
+  static getCurrentQuery() {}
+  static getLocation() {}
+  static isActive() {}
+  static getRouteAtDepth() {}
+  static setRouteComponentAtDepth() {}
+  static setRouteComponentAtDepth() {}
+}
 
 const JestUtil = {
   renderAndFindTag: function (instance, tag) {
@@ -29,7 +49,42 @@ const JestUtil = {
       jest.dontMock(stores[storeID]);
       return true;
     }
+  },
+
+  /**
+   * Helper to stub router context, based on
+   * https://github.com/reactjs/react-router/blob/0.13.x/docs/guides/testing.md
+   * @param {React.Component} Component
+   * @param {object} [props]
+   * @returns {React.Element} wrapped component element
+   */
+  stubRouterContext: function (Component, props={}) {
+    // Create wrapper component
+    class WrappedComponent extends React.Component {
+
+      static get childContextTypes() {
+        return {
+          router: React.PropTypes.func,
+          routeDepth: React.PropTypes.number
+        };
+      }
+
+      getChildContext() {
+        return {
+          router: RouterStub,
+          routeDepth: 0
+        };
+      }
+
+      render() {
+        return (<Component {...props} />);
+      }
+
+    }
+
+    return React.createElement(WrappedComponent);
   }
+
 };
 
 module.exports = JestUtil;
