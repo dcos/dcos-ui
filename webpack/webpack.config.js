afe1388908d334f13c424e2711337568a12d6fce
Mat Appelman <mat@mesosphere.io>
2016-04-25 14:40:50 -0700

    Update to use react hot loader and clean up webpack options

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,12 +1,29 @@
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var StringReplacePlugin = require('string-replace-webpack-plugin');
+var proxy;
+
+try {
+  // If this fails - you should create a proxy.dev.js by copying proxy.template.js
+  // and customizing the proxy as needed.
+  proxy = require('./proxy.dev.js');
+} catch (err) {
+  proxy = require('./proxy.template.js');
+}
 
 module.exports = require('./make-webpack-config')({
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/js/index.js'
+  ],
   devtool: '#eval-source-map',
   output: {
     path: './build',
     filename: 'index.js'
+  },
+  devServer: {
+    proxy: proxy
   },
   plugins: [
     new StringReplacePlugin(),
@@ -15,6 +32,9 @@ module.exports = require('./make-webpack-config')({
       template: './src/index.html',
     }),
 
-    new ExtractTextPlugin('[name].css')
-  ]
+    new ExtractTextPlugin('[name].css'),
+
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  hot: true
 });
