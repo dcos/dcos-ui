474a297bbdd882c4cc70d69c9524984c1701291a
Philipp Hinrichsen <philipph@mesosphere.io>
2016-04-26 14:15:26 +0200

    Add healthbar constants

---
+++ src/js/constants/HealthBar.js
@@ -0,0 +1,20 @@
+const HealthBarMap = {
+  tasksUnknown: {
+    cssName: 'unknown',
+    label: 'Unknown'
+  },
+  tasksHealthy: {
+    cssName: 'healthy',
+    label: 'Healthy'
+  },
+  tasksUnhealthy: {
+    cssName: 'unhealthy',
+    label: 'Unhealthy'
+  },
+  tasksStaged: {
+    cssName: 'staged',
+    label: 'Staged'
+  }
+};
+
+module.exports = HealthBarMap;
