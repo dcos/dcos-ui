9012b62693e05b2e0fe7a9a90d2026c059f15859
Christine Sun <chris@mesosphere.io>
2016-04-26 16:05:48 -0700

    changes from comments

--- src/js/components/ClickToSelect.js
+++ src/js/components/ClickToSelect.js
@@ -1,31 +1,20 @@
 import React from 'react';
 
-const METHODS_TO_BIND = [
-  'selectAll'
-];
-
 class ClickToSelect extends React.Component {
   constructor() {
     super();
-
-    METHODS_TO_BIND.forEach((method) => {
-      this[method] = this[method].bind(this);
-    });
+    this.selectAll = this.selectAll.bind(this);
   }
 
-  selectAll(event) {
-    event.preventDefault;
-    let selection = document.getSelection();
-    let node = this.target;
-
-    selection.selectAllChildren(node);
+  selectAll() {
+    document.getSelection().selectAllChildren(this.refs.node);
   }
 
   render() {
     return (
       <span
         onClick={this.selectAll}
-        ref={(node) => this.target = node}>
+        ref="node">
         {this.props.children}
       </span>
     );
