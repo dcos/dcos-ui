b107dc05fecd645192e2d1082a9ef18f71c4b6ed
philipnrmn <philip@mesosphere.io>
2016-04-27 13:42:02 -0700

    Add deployments tab to services page

--- src/js/routes/services.js
+++ src/js/routes/services.js
@@ -1,43 +1,57 @@
-import Router from 'react-router';
-let Route = Router.Route;
+import {Route, Redirect} from 'react-router';
 
+import DeploymentsTab from '../pages/services/DeploymentsTab';
 import ServiceOverlay from '../components/ServiceOverlay';
 import ServicesPage from '../pages/ServicesPage';
 import ServicesTab from '../pages/services/ServicesTab';
 
-let servicesRoutes = {
+let serviceRoutes = {
   type: Route,
   name: 'services',
-  path: 'services/?',
   handler: ServicesPage,
+  path: '/services/?',
   children: [
-    // Undocumented RR 0.13.5 behaviour: unnamed routes are treated as default
     {
       type: Route,
-      handler: ServicesTab
+      name: 'services-all',
+      path: 'all/',
+      handler: ServicesTab,
+      children: [
+        {
+          type: Route,
+          name: 'services-detail',
+          path: ':id/?'
+        },
+        {
+          type: Route,
+          name: 'service-ui',
+          path: 'ui/:serviceName/?',
+          handler: ServiceOverlay
+        },
+        {
+          type: Route,
+          name: 'services-panel',
+          path: 'service-detail/:serviceName/?'
+        },
+        {
+          type: Route,
+          name: 'services-task-panel',
+          path: 'task-detail/:taskID/?'
+        }
+      ]
     },
     {
       type: Route,
-      name: 'services-detail',
-      path: ':id/?'
+      name: 'services-deployments',
+      path: 'deployments/',
+      handler: DeploymentsTab
     },
     {
-      type: Route,
-      name: 'service-ui',
-      path: 'ui/:serviceName/?',
-      handler: ServiceOverlay
-    },
-    {
-      type: Route,
-      name: 'services-panel',
-      path: 'service-detail/:serviceName/?'
-    },
-    {
-      type: Route,
-      name: 'services-task-panel',
-      path: 'task-detail/:taskID/?'
+      type: Redirect,
+      from: '/services/?',
+      to: 'services-all'
     }
   ]
 };
 
-module.exports = servicesRoutes;
+module.exports = serviceRoutes;
