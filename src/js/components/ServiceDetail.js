be15d1bec9a16a63847427ffed5f2eff1de0059e
Orlando Hohmeier <orlando@mesosphere.io>
2016-04-28 14:40:39 +0200

    Move the service tasks view into it's own component
    Refactor the service detail and move the tasks view into it's own
    component -  this enables us to only poll the Mesos state, while a user
    is looking at the tasks list.

--- src/js/components/ServiceDetail.js
+++ src/js/components/ServiceDetail.js
@@ -1,18 +1,13 @@
 import mixin from 'reactjs-mixin';
 import React from 'react';
-import {StoreMixin} from 'mesosphere-shared-reactjs';
 
 import InternalStorageMixin from '../mixins/InternalStorageMixin';
-import MesosStateStore from '../stores/MesosStateStore';
 import Service from '../structs/Service';
+import ServiceDetailTaskTab from './ServiceDetailTaskTab';
 import ServiceInfo from './ServiceInfo';
 import TabsMixin from '../mixins/TabsMixin';
-import TaskView from './TaskView';
 
-const METHODS_TO_BIND = [];
-
-class ServiceDetail
-  extends mixin(InternalStorageMixin, TabsMixin, StoreMixin) {
+class ServiceDetail extends mixin(InternalStorageMixin, TabsMixin) {
 
   constructor() {
     super(...arguments);
@@ -27,14 +22,6 @@ class ServiceDetail
     this.state = {
       currentTab: Object.keys(this.tabs_tabs).shift()
     };
-
-    this.store_listeners = [
-      {name: 'state', events: ['success']}
-    ];
-
-    METHODS_TO_BIND.forEach((method) => {
-      this[method] = this[method].bind(this);
-    });
   }
 
   renderConfigurationTabView() {
@@ -50,11 +37,8 @@ class ServiceDetail
   }
 
   renderTasksTabView() {
-    let tasks = MesosStateStore.getTasksByServiceId(this.props.service.getId());
-
     return (
-      <TaskView tasks={tasks} inverseStyle={true}
-        parentRouter={this.context.router} />
+      <ServiceDetailTaskTab service={this.props.service}/>
     );
   }
 
