414622b23be8481fbac1192bcd5df5d266cb0204
Orlando Hohmeier <orlando@mesosphere.io>
2016-05-03 11:09:42 +0200

    Add marathon fetch groups test

--- src/js/events/__tests__/MarathonActions-test.js
+++ src/js/events/__tests__/MarathonActions-test.js
@@ -1,4 +1,5 @@
 jest.dontMock('../AppDispatcher');
+jest.dontMock('../UnitHealthActions');
 jest.dontMock('../../config/Config');
 jest.dontMock('../../constants/ActionTypes');
 
@@ -51,6 +52,51 @@ describe('MarathonActions', function () {
       });
 
       this.configuration.error({message: 'error'});
+    });
+
+  });
+
+  describe('#fetchGroups', function () {
+
+    beforeEach(function () {
+      spyOn(RequestUtil, 'json');
+      MarathonActions.fetchGroups();
+      this.configuration = RequestUtil.json.calls.mostRecent().args[0];
+    });
+
+    it('calls #json from the RequestUtil', function () {
+      expect(RequestUtil.json).toHaveBeenCalled();
+    });
+
+    it('fetches data from the correct URL', function () {
+      expect(this.configuration.url)
+        .toEqual(`${Config.rootUrl}/marathon/v2/groups`);
+    });
+
+    it('dispatches the correct action when successful', function () {
+      var id = AppDispatcher.register(function (payload) {
+        var action = payload.action;
+        AppDispatcher.unregister(id);
+        expect(action.type).toEqual(ActionTypes.REQUEST_MARATHON_GROUPS_SUCCESS);
+      });
+
+      this.configuration.success({
+        apps: [],
+        dependencies: [],
+        groups: [],
+        id: '/',
+        version: '2016-05-02T16:07:32.583Z'
+      });
+    });
+
+    it('dispatches the correct action when unsucessful', function () {
+      var id = AppDispatcher.register(function (payload) {
+        var action = payload.action;
+        AppDispatcher.unregister(id);
+        expect(action.type).toEqual(ActionTypes.REQUEST_MARATHON_GROUPS_ERROR);
+      });
+
+      this.configuration.error({message:'error'});
     });
 
   });
