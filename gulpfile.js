f28424d8a7f411a133c771bbde77ec0171cd21e7
Mat Appelman <mat@mesosphere.io>
2016-05-04 10:36:19 -0700

    Update gulp to copy over config template in scaffold

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,8 @@ gulp.task('ensureConfig', function () {
   // Make sure we have a Config.dev so we don't error on Config loading
   var configFilePath = './src/js/config/Config.dev.js';
   if (!fs.existsSync(configFilePath)) {
-    fs.writeFileSync(configFilePath, 'module.exports = {};', 'utf8');
+    var template = fs.readFileSync('./src/js/config/Config.template.js', 'utf8');
+    fs.writeFileSync(configFilePath, template, 'utf8');
   }
 });
 
