ec5d6b31a83faad2f2a3b0cae6de98b1e16d74a9
mlunoe <michael.lunoe@gmail.com>
2016-05-04 10:45:09 -0700

    Fix nodes side panel contents integration tests

--- tests/nodesPage/NodeSidePanelContents-cy.js
+++ tests/nodesPage/NodeSidePanelContents-cy.js
@@ -7,23 +7,23 @@ describe('Nodes Side Panel', function () {
     })
   });
 
-  context('Navigate to side panel', function () {
+  context('Navigate to node detail page', function () {
 
     it('navigates to node side panel', function () {
       cy.visitUrl({url: '/nodes', identify: true, fakeAnalytics: true});
       cy.get('tr a').contains('dcos-01').click();
 
-      cy.hash().should('match', /node-detail/);
-      cy.get('.side-panel-content-header h1').should(function ($title) {
+      cy.hash().should('match', /nodes\/[a-zA-Z0-9-]+/);
+      cy.get('.page-content h1').should(function ($title) {
         expect($title).to.contain('dcos-01');
       });
     });
 
     it('shows error in side panel when node is invalid [10a]', function () {
-      cy.visitUrl({url: '/nodes/list/node-detail/INVALID_NODE', identify: true, fakeAnalytics: true});
+      cy.visitUrl({url: '/nodes/INVALID_NODE', identify: true, fakeAnalytics: true});
 
-      cy.hash().should('match', /node-detail/);
-      cy.get('.side-panel-content h3').should(function ($title) {
+      cy.hash().should('match', /nodes\/INVALID_NODE/);
+      cy.get('.page-content h3').should(function ($title) {
         expect($title).to.contain('Error finding node');
       });
     });
