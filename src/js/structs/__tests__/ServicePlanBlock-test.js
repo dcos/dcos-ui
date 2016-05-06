0a516a401b1649e2ee66f59f814fd1a809214761
Mat Appelman <mat@mesosphere.io>
2016-05-06 15:02:43 -0700

    Add Block Struct

---
+++ src/js/structs/__tests__/ServicePlanBlock-test.js
@@ -0,0 +1,106 @@
+import ServicePlanBlock from '../ServicePlanBlock';
+import ServicePlanStatusTypes from '../../constants/ServicePlanStatusTypes';
+
+describe('ServicePlanBlock', function () {
+
+  describe('#getID', function () {
+
+    it('should return id', function () {
+      let Block = new ServicePlanBlock({id: 'block-1'});
+
+      expect(Block.getID()).toEqual('block-1');
+    });
+
+  });
+
+  describe('#getName', function () {
+
+    it('should return name', function () {
+      let Block = new ServicePlanBlock({name: 'block-2'});
+
+      expect(Block.getName()).toEqual('block-2');
+    });
+
+  });
+
+  describe('#isComplete', function () {
+
+    it('should return false', function () {
+      let Block = new ServicePlanBlock({
+        status: ServicePlanStatusTypes.IN_PROGRESS
+      });
+
+      expect(Block.isComplete()).toEqual(false);
+    });
+
+    it('should return true', function () {
+      let Block = new ServicePlanBlock({
+        status: ServicePlanStatusTypes.COMPLETE
+      });
+
+      expect(Block.isComplete()).toEqual(true);
+    });
+
+  });
+
+  describe('#isInProgress', function () {
+
+    it('should return false', function () {
+      let Block = new ServicePlanBlock({
+        status: ServicePlanStatusTypes.COMPLETE
+      });
+
+      expect(Block.isInProgress()).toEqual(false);
+    });
+
+    it('should return true', function () {
+      let Block = new ServicePlanBlock({
+        status: ServicePlanStatusTypes.IN_PROGRESS
+      });
+
+      expect(Block.isInProgress()).toEqual(true);
+    });
+
+  });
+
+  describe('#isPending', function () {
+
+    it('should return false', function () {
+      let Block = new ServicePlanBlock({
+        status: ServicePlanStatusTypes.COMPLETE
+      });
+
+      expect(Block.isPending()).toEqual(false);
+    });
+
+    it('should return true', function () {
+      let Block = new ServicePlanBlock({
+        status: ServicePlanStatusTypes.PENDING
+      });
+
+      expect(Block.isPending()).toEqual(true);
+    });
+
+  });
+
+  describe('#isWaiting', function () {
+
+    it('should return false', function () {
+      let Block = new ServicePlanBlock({
+        status: ServicePlanStatusTypes.COMPLETE
+      });
+
+      expect(Block.isWaiting()).toEqual(false);
+    });
+
+    it('should return true', function () {
+      let Block = new ServicePlanBlock({
+        status: ServicePlanStatusTypes.WAITING
+      });
+
+      expect(Block.isWaiting()).toEqual(true);
+    });
+
+  });
+
+});
