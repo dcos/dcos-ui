35631179b75cc92252a9d498405c339ea27221e2
John Furrow <johnf@mesosphere.io>
2016-05-16 12:46:51 -0700

    Separate icons

--- src/js/components/icons/IconUpgradeBlockDecisionPoint.js
+++ src/js/components/icons/IconUpgradeBlockDecisionPoint.js
@@ -7,7 +7,8 @@ class IconUpgradeBlock extends React.Component {
         className="icon icon-upgrade-block"
         viewBox="0 0 10 10"
         {...this.props}>
-        <rect width="10" height="10" />
+        <rect x="1.46" y="1.46" width="7.07" height="7.07"
+          transform="translate(5 -2.07) rotate(45)" />
       </svg>
     );
   }
