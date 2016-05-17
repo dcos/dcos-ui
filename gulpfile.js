96b9ef5da52f1a04668ec10845b8930d985ac2a7
Rafael Corral <me@rafaelcorral.com>
2016-05-17 11:06:02 -0700

    Test dependencies for licenses that we don't want.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
+var checker = require('license-checker');
 var connect = require('gulp-connect');
 var fs = require('fs');
 var gulp = require('gulp');
+var packageJSON = require('./package.json');
 var shrinkwrap = require('./npm-shrinkwrap.json');
 
 gulp.task('ensureConfig', function () {
@@ -19,6 +21,61 @@ gulp.task('ensureDevProxy', function () {
     var template = fs.readFileSync('./webpack/proxy.template.js', 'utf8');
     fs.writeFileSync(proxyFilePath, template, 'utf8');
   }
+});
+
+function buildDependenciesArray(dependencies) {
+  return Object.keys(dependencies).map(function (dependency) {
+    return dependency + '@' + dependencies[dependency];
+  });
+}
+
+gulp.task('checkDependencies', function () {
+  var dependencies = buildDependenciesArray(packageJSON.dependencies);
+  dependencies = dependencies.concat(
+    buildDependenciesArray(packageJSON.devDependencies)
+  );
+
+  checker.init({
+    start: './'
+  }, function(json, error) {
+    if (error) {
+      console.log(error);
+      process.exit(1);
+    }
+
+    var count = 0;
+    var found = [];
+    Object.keys(json).forEach(function (dependency) {
+      if (dependencies.indexOf(dependency) === -1) {
+        return;
+      }
+
+      count++;
+      found.push(dependency);
+
+      var licenses = json[dependency].licenses;
+      if (licenses === 'UNKNOWN') {
+        console.warn(
+          'Dependency has an unknown license', json[dependency]
+        );
+      }
+
+      if (licenses.indexOf('LGPL') === -1 && licenses.match(/GPL/gi)) {
+        console.log('Package has invalid license.', json[dependency]);
+        process.exit(1);
+      }
+    });
+
+    // Ensure we found them all
+    if (count !== dependencies.length) {
+      console.log('Dependency length doesn\'t match.');
+      var missing = dependencies.filter(function (dependency) {
+        return found.indexOf(dependency) === -1;
+      });
+      console.log(missing);
+      process.exit(1);
+    }
+  });
 });
 
 gulp.task('serve', function () {
