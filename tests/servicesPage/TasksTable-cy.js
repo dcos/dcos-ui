338af28d82db8222abf3bc4ff9b6797b5996a20d
Philip Norman <philip@mesosphere.io>
2016-05-17 15:05:14 +0200

    Added integration tests for task detail page files tab

--- tests/servicesPage/TasksTable-cy.js
+++ tests/servicesPage/TasksTable-cy.js
@@ -6,12 +6,35 @@ describe('Tasks Table', function () {
         mesos: '1-service-with-executor-task',
         nodeHealth: true
       });
-      cy.visitUrl({url: '/services/%2Fcassandra/'});
     });
 
     it('displays task detail page on task click', function () {
+      cy.visitUrl({url: '/services/%2Fcassandra/'});
       cy.get('a[title="server-0"]').click();
       cy.get('.page-content h1').should('to.have.text', 'server-0_10ab666b-cf9b-44eb-bc53-b6dba1b7c737');
+    });
+
+    context('Files tab', function () {
+
+      beforeEach(function () {
+        cy.visitUrl({
+          url: '/services/%2Fcassandra/task-detail/server-0_10ab666b-cf9b-44eb-bc53-b6dba1b7c737/'
+        });
+        cy.get('.page-content .tab-item-label').contains('Files').click();
+      });
+
+      it('shows the contents of the Mesos sandbox', function () {
+        cy.get('.page-content tbody tr:visible')
+          .should(function ($rows) {
+            expect($rows.length).to.equal(13);
+          });
+      });
+
+      it('shows directories as well as files', function () {
+        cy.get('.page-content tbody tr:visible:first')
+          .contains('jre1.7.0_76');
+      });
+
     });
 
   });
