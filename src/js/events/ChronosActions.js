2f018b3d12548fddb7ef241b21aa79f2ffdc86ad
Orlando Hohmeier <orlando@mesosphere.io>
2016-05-19 16:18:10 -0700

    Introduce action to fetch Chronos jobs

---
+++ src/js/events/ChronosActions.js
@@ -0,0 +1,66 @@
+import {
+  REQUEST_CHRONOS_JOBS_ERROR,
+  REQUEST_CHRONOS_JOBS_ONGOING,
+  REQUEST_CHRONOS_JOBS_SUCCESS
+} from '../constants/ActionTypes';
+import AppDispatcher from './AppDispatcher';
+import Config from '../config/Config';
+import RequestUtil from '../utils/RequestUtil';
+
+const ChronosActions = {
+  fetchJobs: RequestUtil.debounceOnError(
+    Config.getRefreshRate(),
+    function (resolve, reject) {
+      return function () {
+        RequestUtil.json({
+          url: `${Config.rootUrl}/chronos/jobs`,
+          data: [
+            {name: 'embed', value: 'activeJobs'},
+          ],
+          success: function (response) {
+            AppDispatcher.handleServerAction({
+              type: REQUEST_CHRONOS_JOBS_SUCCESS,
+              data: response
+            });
+            resolve();
+          },
+          error: function (e) {
+            AppDispatcher.handleServerAction({
+              type: REQUEST_CHRONOS_JOBS_ERROR,
+              data: e.message
+            });
+            reject();
+          },
+          hangingRequestCallback: function () {
+            AppDispatcher.handleServerAction({
+              type: REQUEST_CHRONOS_JOBS_ONGOING
+            });
+          }
+        });
+      };
+    },
+    {delayAfterCount: Config.delayAfterErrorCount}
+  ),
+};
+
+if (Config.useFixtures) {
+  const jobsFixture = require('../../../tests/_fixtures/chronos/jobs.json');
+
+  if (!global.actionTypes) {
+    global.actionTypes = {};
+  }
+
+  global.actionTypes.ChronosActions = {
+    fetchJobs: {
+      event: 'success', success: {response: jobsFixture}
+    }
+  };
+
+  Object.keys(global.actionTypes.ChronosActions).forEach(function (method) {
+    ChronosActions[method] = RequestUtil.stubRequest(
+      ChronosActions, 'ChronosActions', method
+    );
+  });
+}
+
+module.exports = ChronosActions;
