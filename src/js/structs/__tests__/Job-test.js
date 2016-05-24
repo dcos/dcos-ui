51b4a2ced29f3c7226f252d987e4a97e3f32eb8c
Orlando Hohmeier <orlando@mesosphere.io>
2016-05-23 19:03:27 -0700

    Introduce basic job struct

---
+++ src/js/structs/__tests__/Job-test.js
@@ -0,0 +1,29 @@
+let Job = require('../Job');
+
+describe('Job', function () {
+
+  describe('#getId', function () {
+
+    it('returns correct id', function () {
+      let job = new Job({
+        id: '/test/job'
+      });
+
+      expect(job.getId()).toEqual('/test/job');
+    });
+
+  });
+
+  describe('#getName', function () {
+
+    it('returns correct name', function () {
+      let job = new Job({
+        id: '/test/job'
+      });
+
+      expect(job.getName()).toEqual('job');
+    });
+
+  });
+
+});
