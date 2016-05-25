7e5b5837c9bfa00b2cfc5e80bdd6dc2e01617e00
mlunoe <michael.lunoe@gmail.com>
2016-05-25 11:33:05 -0700

    Spelling is hard

--- src/js/structs/Job.js
+++ src/js/structs/Job.js
@@ -6,7 +6,7 @@ module.exports = class Job extends Item {
 
     const id = this.getId();
     if (id !== '/' && (!id.startsWith('/') || id.endsWith('/'))) {
-      throw new Error(`Id (${id}) must start with a leading slash ("/") and should not end with a slash, except for root id wich is only a slash`);
+      throw new Error(`Id (${id}) must start with a leading slash ("/") and should not end with a slash, except for root id which is only a slash`);
     }
   }
 
