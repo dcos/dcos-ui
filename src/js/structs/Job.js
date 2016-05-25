963e066db334998b6c6713fc62f2eafa8f0e6474
mlunoe <michael.lunoe@gmail.com>
2016-05-25 10:45:55 -0700

    Handling not starts width and ends with slash

--- src/js/structs/Job.js
+++ src/js/structs/Job.js
@@ -1,6 +1,16 @@
 import Item from './Item';
 
 module.exports = class Job extends Item {
+  constructor(options = {}) {
+    super(...arguments);
+
+    const id = this.getId();
+    if (!id.startsWidth('/') || id.endsWidth('/')) {
+      throw new Error(
+        'Id must start with a leading slash ("/") and should not end with a slash.'
+      );
+    }
+  }
 
   getId() {
     return this.get('id') || '';
