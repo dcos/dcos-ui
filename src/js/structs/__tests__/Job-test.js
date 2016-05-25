beb817d29d1e324fb666683b22c2462c2ca2c152
mlunoe <michael.lunoe@gmail.com>
2016-05-25 10:58:50 -0700

    Adding tests to cover the new id handling

--- src/js/structs/__tests__/Job-test.js
+++ src/js/structs/__tests__/Job-test.js
@@ -5,9 +5,33 @@ describe('Job', function () {
   describe('#getId', function () {
 
     it('returns correct id', function () {
-      let job = new Job({
-        id: '/test/job'
-      });
+      let job = new Job({id: '/test/job'});
+
+      expect(job.getId()).toEqual('/test/job');
+    });
+
+    it('throws when id doesn\'t start with a slash', function () {
+      expect(function () { new Job({id: 'test/job'}); }).toThrow();
+    });
+
+    it('throws when id ends with a slash', function () {
+      expect(function () { new Job({id: 'test/job'}); }).toThrow();
+    });
+
+    it('doesn\'t throw when no id is provided', function () {
+      expect(function () { new Job({}); }).toThrow();
+    });
+
+    it('doesn\'t throw when no options are provided', function () {
+      expect(function () { new Job(); }).toThrow();
+    });
+
+    it('doesn\'t throw when id is slash (\'/\')', function () {
+      expect(function () { new Job({id: '/'}); }).toThrow();
+    });
+
+    it('returns correct id', function () {
+      let job = new Job({id: '/test/job'});
 
       expect(job.getId()).toEqual('/test/job');
     });
@@ -17,9 +41,7 @@ describe('Job', function () {
   describe('#getName', function () {
 
     it('returns correct name', function () {
-      let job = new Job({
-        id: '/test/job'
-      });
+      let job = new Job({id: '/test/job'});
 
       expect(job.getName()).toEqual('job');
     });
