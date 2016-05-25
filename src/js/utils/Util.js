6a3ce7a815da53f4a05b4fc7d358ca41e7151976
Orlando Hohmeier <orlando@mesosphere.io>
2016-05-24 17:56:46 -0700

    Remove Util.isArray

--- src/js/utils/Util.js
+++ src/js/utils/Util.js
@@ -57,14 +57,6 @@ const Util = {
     return -1;
   },
 
-  /**
-   * @param  {Object} arg to determine whether is an array or not
-   * @return {Boolean} returns whether given arg is an array or not
-   */
-  isArray: function (arg) {
-    return Object.prototype.toString.call(arg) === '[object Array]';
-  },
-
   getLocaleCompareSortFn: function (prop) {
     return function (a, b) {
       return a[prop].localeCompare(b[prop]);
