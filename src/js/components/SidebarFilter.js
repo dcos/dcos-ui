a333e47243d2c6087a4ec8624afd16430dca34b3
mlunoe <michael.lunoe@gmail.com>
2016-05-27 09:19:09 -0700

    Using comma (:pencil:) in stead of plus (:heavy_plus_sign:)

--- src/js/components/SidebarFilter.js
+++ src/js/components/SidebarFilter.js
@@ -144,8 +144,8 @@ class SidebarFilter extends mixin(QueryParamsMixin) {
       let checked = selectedNodes.indexOf(value) > -1;
 
       let labelClassSet = classNames(
-        'side-list-item form-row-element form-element-checkbox ' +
-        'inverse row row-flex flex-align-items-center vertical-center flush clickable',
+        'side-list-item form-row-element form-element-checkbox inverse',
+        'row row-flex flex-align-items-center vertical-center flush clickable',
         {
           'filter-active': this.getCountByValue(filterValue) > 0,
           'filter-checked': checked
