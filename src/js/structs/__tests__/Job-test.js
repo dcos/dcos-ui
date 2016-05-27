ca52006617f41e037cf8da5b3b1b4d7fba3be832
mlunoe <michael.lunoe@gmail.com>
2016-05-26 17:28:05 -0700

    Remove unecessary tests

--- src/js/structs/__tests__/Job-test.js
+++ src/js/structs/__tests__/Job-test.js
@@ -2,30 +2,6 @@ let Job = require('../Job');
 
 describe('Job', function () {
 
-  describe('#constructor', function () {
-
-    it('throws when id doesn\'t start with a slash', function () {
-      expect(function () { new Job({id: 'test/job'}); }).toThrow();
-    });
-
-    it('throws when id ends with a slash', function () {
-      expect(function () { new Job({id: '/test/job/'}); }).toThrow();
-    });
-
-    it('doesn\'t throw when no id is provided', function () {
-      expect(function () { new Job({}); }).not.toThrow();
-    });
-
-    it('doesn\'t throw when no options are provided', function () {
-      expect(function () { new Job(); }).not.toThrow();
-    });
-
-    it('doesn\'t throw when id is slash (\'/\')', function () {
-      expect(function () { new Job({id: '/'}); }).not.toThrow();
-    });
-
-  });
-
   describe('#getId', function () {
 
     it('returns correct id', function () {
