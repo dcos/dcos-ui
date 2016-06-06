0cc0c9945693797e6b66f736457c62252b01b345
Orlando Hohmeier <orlando@mesosphere.io>
2016-06-06 16:02:15 +0200

    Introduce tasks stats struct

---
+++ src/js/structs/__tests__/TaskStats-test.js
@@ -0,0 +1,97 @@
+const TaskStats = require('../TaskStats');
+const TaskStat = require('../TaskStat');
+
+describe('TaskStats', function () {
+
+  describe('#getStatsForTasksWithLatestConfig', function () {
+
+    it('returns task stat instance', function () {
+      let statistics = new TaskStats({withLatestConfig: {}})
+        .getStatsForTasksWithLatestConfig();
+
+      expect(statistics instanceof TaskStat).toBeTruthy();
+    });
+
+    it('pass correct data set to task stat struct', function () {
+      let statistics = new TaskStats({
+        withLatestConfig: {
+          stats: {
+            counts: {healthy: 1}
+          }
+        }
+      }).getStatsForTasksWithLatestConfig();
+
+      expect(statistics.getHealthyTaskCount()).toEqual(1);
+    });
+
+  });
+
+  describe('#getStatsForTasksStaredAfterLastScaling', function () {
+
+    it('returns task stat instance', function () {
+      let statistics = new TaskStats({startedAfterLastScaling: {}})
+        .getStatsForTasksStaredAfterLastScaling();
+
+      expect(statistics instanceof TaskStat).toBeTruthy();
+    });
+
+    it('pass correct data set to task stat struct', function () {
+      let statistics = new TaskStats({
+        startedAfterLastScaling: {
+          stats: {
+            counts: {healthy: 1}
+          }
+        }
+      }).getStatsForTasksStaredAfterLastScaling();
+
+      expect(statistics.getHealthyTaskCount()).toEqual(1);
+    });
+
+  });
+
+  describe('#getStatsForTasksWithOutdatedConfig', function () {
+
+    it('returns task stat instance', function () {
+      let statistics = new TaskStats({withOutdatedConfig: {}})
+        .getStatsForTasksWithOutdatedConfig();
+
+      expect(statistics instanceof TaskStat).toBeTruthy();
+    });
+
+    it('pass correct data set to task stat struct', function () {
+      let statistics = new TaskStats({
+        withOutdatedConfig: {
+          stats: {
+            counts: {healthy: 1}
+          }
+        }
+      }).getStatsForTasksWithOutdatedConfig();
+
+      expect(statistics.getHealthyTaskCount()).toEqual(1);
+    });
+
+  });
+
+  describe('#getStatsForAllTasks', function () {
+
+    it('returns task stat instance', function () {
+      let statistics = new TaskStats({totalSummary: {}}).getStatsForAllTasks();
+
+      expect(statistics instanceof TaskStat).toBeTruthy();
+    });
+
+    it('pass correct data set to task stat struct', function () {
+      let statistics = new TaskStats({
+        totalSummary: {
+          stats: {
+            counts: {healthy: 1}
+          }
+        }
+      }).getStatsForAllTasks();
+
+      expect(statistics.getHealthyTaskCount()).toEqual(1);
+    });
+
+  });
+
+});
