a755dc5d351ec36a6ed45cfa8889e2e5d63d049b
Pierluigi Cau <pierlo@mesosphere.io>
2016-06-07 16:23:15 +0200

    Sort service status types by relevance

--- src/js/constants/ServiceStatusTypes.js
+++ src/js/constants/ServiceStatusTypes.js
@@ -1,10 +1,10 @@
 var ServiceStatusTypes = {
-  RUNNING: 0,
-  DEPLOYING: 1,
-  SUSPENDED: 2,
-  NA: 4,
-  DELAYED: 5,
-  WAITING: 6
+  NA: 0,
+  RUNNING: 1,
+  DEPLOYING: 2,
+  SUSPENDED: 3,
+  DELAYED: 4,
+  WAITING: 5
 };
 
 module.exports = ServiceStatusTypes;
