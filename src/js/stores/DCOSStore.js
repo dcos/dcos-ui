5961518ff3bd71279a059ca8d56a94f929e20bb2
Pierluigi Cau <pierlo@mesosphere.io>
2016-06-08 15:02:14 +0200

    Expose Chronos jobs from DCOS Store

--- src/js/stores/DCOSStore.js
+++ src/js/stores/DCOSStore.js
@@ -137,6 +137,7 @@ class DCOSStore extends EventEmitter {
   }
 
   onChronosChange() {
+    this.data.chronos = ChronosStore.jobTree;
     this.emit(DCOS_CHANGE);
   }
 
@@ -197,7 +198,7 @@ class DCOSStore extends EventEmitter {
    * @type {JobTree}
    */
   get jobTree() {
-    return ChronosStore.jobTree;
+    return this.data.chronos;
   }
 
   /**
@@ -231,12 +232,6 @@ class DCOSStore extends EventEmitter {
       );
     });
 
-  }
-
-  get jobTree() {
-    let {chronos} = this.data;
-
-    return chronos;
   }
 
   get dataProcessed() {
