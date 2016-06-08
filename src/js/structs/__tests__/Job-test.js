fb1ab2951afd47f9d467c02082a36c0ba50a734a
John Furrow <johnf@mesosphere.io>
2016-06-08 13:22:11 -0700

    Fix Schedules getter

--- src/js/structs/__tests__/Job-test.js
+++ src/js/structs/__tests__/Job-test.js
@@ -271,6 +271,7 @@ describe('Job', function () {
   });
 
   describe('#getSchedules', function () {
+<<<<<<< HEAD
 
     it('returns the schedules', function () {
       let job = new Job({id: 'foo', schedules: ['bar']});
@@ -335,6 +336,13 @@ describe('Job', function () {
       });
 
       expect(job.getScheduleStatus()).toEqual('completed');
+=======
+
+    it('returns the schedules', function () {
+      let job = new Job({id: '/foo', schedules: ['bar']});
+
+      expect(job.getSchedules()).toEqual(['bar']);
+>>>>>>> 077d5dd... Fix Schedules getter
     });
 
   });
