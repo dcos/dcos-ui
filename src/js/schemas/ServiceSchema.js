eb9e03e3353f67575dfd2d050bab4a2008dc87d9
Philipp Hinrichsen <philipph@mesosphere.io>
2016-06-10 15:35:10 +0200

    Change order of the imports to be alphabetised

--- src/js/schemas/ServiceSchema.js
+++ src/js/schemas/ServiceSchema.js
@@ -2,10 +2,10 @@
 import React from 'react';
 /* eslint-enable no-unused-vars */
 
+import ContainerSettings from './service-schema/ContainerSettings';
 import General from './service-schema/General';
 import Labels from './service-schema/Labels';
 import Optional from './service-schema/Optional';
-import ContainerSettings from './service-schema/ContainerSettings';
 
 let ServiceSchema = {
   type: 'object',
