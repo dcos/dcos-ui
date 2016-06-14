b70f1946e0bd04339f82be914498f725b32b5b52
Orlando Hohmeier <orlando@mesosphere.io>
2016-06-14 17:22:51 +0200

    Introduce job util
    This util is used to create job specs from job structs as well as to
    create from models from jobs and respective form schemas.

---
+++ src/js/utils/JobUtil.js
@@ -0,0 +1,102 @@
+import Job from '../structs/Job';
+
+// TODO: DCOS-7747 Move this method as well as `createFormModelFromSchema` into
+// the SchemaUtil and refactor it accordingly.
+const getFindPropertiesRecursive = function (job, item) {
+
+  return Object.keys(item).reduce(function (memo, subItem) {
+    if (item[subItem].type === 'object') {
+      memo[subItem] = getFindPropertiesRecursive(job, item[subItem].properties);
+
+      return memo;
+    }
+
+    memo[subItem] = item[subItem].default;
+
+    if (item[subItem].getter) {
+      memo[subItem] = item[subItem].getter(job);
+    }
+
+    return memo;
+  }, {});
+};
+
+const JobUtil = {
+  createJobFromFormModel: function (formModel) {
+    if (formModel == null) {
+      return new Job();
+    }
+
+    let spec = {};
+    let {
+      general = {
+        id: null,
+        description: null
+      },
+      labels = {},
+      docker,
+      schedule
+    } = formModel;
+
+    spec.id = general.id;
+    spec.description = general.description;
+
+    if (Array.isArray(labels.items)) {
+      spec.labels = labels.items.reduce(function (memo, {key, value}) {
+        memo[key] = value || null;
+
+        return memo;
+      }, {});
+    }
+
+    spec.run = {
+      cmd: general.cmd,
+      cpus: general.cpus,
+      mem: general.mem,
+      disk: general.disk,
+      docker
+    };
+
+    spec.schedules = [schedule];
+
+    return new Job(spec);
+  },
+
+  createFormModelFromSchema: function (schema, service = new Job()) {
+
+    return getFindPropertiesRecursive(service, schema.properties);
+  },
+
+  createJobSpecFromJob: function (job) {
+    let spec = {};
+
+    spec.id = job.getId() || null;
+    spec.description = job.getDescription() || null;
+
+    spec.run = {
+      cmd: job.getCommand(),
+      cpus: job.getCpus(),
+      mem: job.getMem(),
+      disk: job.getDisk(),
+    };
+
+    let labels = job.getLabels();
+    if (Object.keys(labels).length > 0) {
+      spec.labels = labels;
+    }
+
+    let docker = job.getDocker();
+    if (docker.image) {
+      Object.assign(spec.run, {docker});
+    }
+
+    let [schedule = {}] = job.getSchedules();
+    if (schedule.id != null && schedule.cron != null) {
+      spec.schedules = [schedule];
+    }
+
+    return spec;
+  }
+};
+
+module.exports = JobUtil;
---
+++ src/js/utils/__tests__/JobUtil-test.js
@@ -0,0 +1,105 @@
+jest.dontMock('../ServiceUtil');
+jest.dontMock('../../structs/Service');
+
+var Job = require('../../structs/Job');
+var JobUtil = require('../JobUtil');
+
+describe('JobUtil', function () {
+
+  describe('#createJobFromFormModel', function () {
+
+    it('should return instance of Job', function () {
+      const job = JobUtil.createJobFromFormModel({
+        general: {
+          id: 'test'
+        }
+      });
+
+      expect(job).toEqual(jasmine.any(Job));
+    });
+
+    it('should return instance Job if null is provided', function () {
+      expect(JobUtil.createJobFromFormModel(null)).toEqual(jasmine.any(Job));
+    });
+
+    it('should return instance Job if empty object is provided', function () {
+      expect(JobUtil.createJobFromFormModel({})).toEqual(jasmine.any(Job));
+    });
+
+    it('should convert form model to the corresponding job', function () {
+      const job = JobUtil.createJobFromFormModel({
+        general: {
+          id: 'test',
+          cmd: 'sleep 1000;'
+        }
+      });
+
+      expect(job.getId()).toEqual('test');
+      expect(job.getCommand()).toEqual('sleep 1000;');
+    });
+
+  });
+
+  describe('#createFormModelFromSchema', function () {
+
+    it('should create the correct model', function () {
+      const schema = {
+        type: 'object',
+        properties: {
+          general: {
+            description: 'Configure your container',
+            type: 'object',
+            properties: {
+              id: {
+                default: 'job.id',
+                title: 'ID',
+                description: 'The id for the job',
+                type: 'string',
+                getter: function (job) {
+                  return job.getId();
+                }
+              }
+            }
+          }
+        },
+        required: [
+          'general'
+        ]
+      };
+
+      const job = new Job({
+        id: 'test',
+      });
+
+      expect(JobUtil.createFormModelFromSchema(schema, job)).toEqual({
+        general: {
+          id: 'test'
+        }
+      });
+    });
+  });
+
+  describe('#createJobSpecFromJob', function () {
+
+    it('should create the correct job', function () {
+      const job = new Job({
+        id: 'test',
+        run: {
+          cmd: 'sleep 1000;'
+        }
+      });
+
+      expect(JobUtil.createJobSpecFromJob(job)).toEqual(
+        {
+          id: 'test',
+          description: null,
+          run: {
+            cmd: 'sleep 1000;',
+            cpus: 0.01,
+            mem: 32,
+            disk: 0
+          }
+        });
+    });
+  });
+});
