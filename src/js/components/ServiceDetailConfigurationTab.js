308f98ffb3b2ed2e21bcb897a07687df0d28bf77
Felix Gertz <felix@mesosphere.io>
2016-06-17 13:50:19 +0200

    Enable apply button

--- src/js/components/ServiceDetailConfigurationTab.js
+++ src/js/components/ServiceDetailConfigurationTab.js
@@ -3,7 +3,9 @@ import React from 'react';
 
 import ConfigurationView from './ConfigurationView';
 import DCOSStore from '../stores/DCOSStore';
+import MarathonStore from '../stores/MarathonStore';
 import Service from '../structs/Service';
+import ServiceUtil from '../utils/ServiceUtil';
 
 const METHODS_TO_BIND = [
   'handleApplyButtonClick',
@@ -32,7 +34,14 @@ class ServiceDetailConfigurationTab extends React.Component {
   }
 
   handleApplyButtonClick() {
+    let serviceConfiguration =
+      this.props.service.getVersions().get(this.state.selectedVersionID);
 
+    MarathonStore.editService(
+      ServiceUtil.getAppDefinitionFromService(
+        new Service(serviceConfiguration)
+      )
+    );
   }
 
   handleEditButtonClick() {
