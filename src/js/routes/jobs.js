830a63fe9d02a59d3bc99a3e76e25a30283c2fff
Orlando Hohmeier <orlando@mesosphere.io>
2016-06-23 14:11:29 +0200

    Add breadcrumb configuration

--- src/js/routes/jobs.js
+++ src/js/routes/jobs.js
@@ -6,17 +6,47 @@ import React from 'react';
 import JobDetailPage from '../pages/jobs/JobDetailPage';
 import JobsPage from '../pages/JobsPage';
 import JobsTab from '../pages/jobs/JobsTab';
-
 import TaskDetail from '../pages/task-details/TaskDetail';
+import TaskDetailBreadcrumb from '../pages/nodes/breadcrumbs/TaskDetailBreadcrumb';
 import TaskDetailsTab  from '../pages/task-details/TaskDetailsTab';
 import TaskFilesTab from '../pages/task-details/TaskFilesTab';
 import TaskLogsTab from '../pages/task-details/TaskLogsTab';
+
+function buildJobCrumbs(router) {
+  let {id} = router.getCurrentParams();
+  let ids = id.split('.');
+  let aggregateIDs = '';
+
+  return ids.map(function (id) {
+    if (aggregateIDs !== '') {
+      aggregateIDs += '.';
+    }
+    aggregateIDs += id;
+
+    return {
+      label: id,
+      route: {to: 'jobs-page-detail', params: {id: aggregateIDs}}
+    };
+  });
+}
 
 let jobsRoutes = {
   type: Route,
   name: 'jobs-page',
   handler: JobsPage,
   path: '/jobs/?',
+  buildBreadCrumb: function () {
+    return {
+      getCrumbs: function () {
+        return [
+          {
+            label: 'Jobs',
+            route: {to: 'jobs-page'}
+          }
+        ];
+      }
+    }
+  },
   children: [
     {
       type: Route,
@@ -28,6 +58,12 @@ let jobsRoutes = {
           name: 'jobs-page-detail',
           path: ':id/?',
           hideHeaderNavigation: true,
+          buildBreadCrumb: function () {
+            return {
+              parentCrumb: 'jobs-page',
+              getCrumbs: buildJobCrumbs
+            }
+          },
           children:[
             {
               type: Route,
@@ -35,12 +71,30 @@ let jobsRoutes = {
               name: 'jobs-task-details',
               handler: TaskDetail,
               hideHeaderNavigation: true,
+              buildBreadCrumb: function () {
+                return {
+                  parentCrumb: 'jobs-page-detail',
+                  getCrumbs: function (router) {
+                    return [
+                      <TaskDetailBreadcrumb
+                        parentRouter={router}
+                        routeName="jobs-task-details" />
+                    ];
+                  }
+                }
+              },
               children: [
                 {
                   type: DefaultRoute,
                   name: 'jobs-task-details-tab',
                   handler: TaskDetailsTab,
                   hideHeaderNavigation: true,
+                  buildBreadCrumb: function () {
+                    return {
+                      parentCrumb: 'jobs-task-details',
+                      getCrumbs: function () { return []; }
+                    }
+                  },
                   title:'Details'
                 },
                 {
@@ -49,6 +103,12 @@ let jobsRoutes = {
                   path: 'files/?',
                   handler: TaskFilesTab,
                   hideHeaderNavigation: true,
+                  buildBreadCrumb: function () {
+                    return {
+                      parentCrumb: 'jobs-task-details',
+                      getCrumbs: function () { return []; }
+                    }
+                  },
                   title:'Files'
                 },
                 {
@@ -58,6 +118,12 @@ let jobsRoutes = {
                   path: 'logs/?',
                   handler: TaskLogsTab,
                   hideHeaderNavigation: true,
+                  buildBreadCrumb: function () {
+                    return {
+                      parentCrumb: 'jobs-task-details',
+                      getCrumbs: function () { return []; }
+                    }
+                  },
                   title:'Logs'
                 }
               ]
