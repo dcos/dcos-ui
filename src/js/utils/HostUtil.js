79489213d73b3bc96c70da92d251a0e9f342026c
Christine Sun <chris@mesosphere.io>
2016-06-29 15:18:47 -0700

    Add Host Util to parse valid hostnames

---
+++ src/js/utils/HostUtil.js
@@ -0,0 +1,29 @@
+const HostUtil = {
+  stringToHostname: function (string) {
+    const HOSTNAME_MAX_LENGTH = 63;
+
+    // Strip or convert illegal characters
+    let host = string.toLowerCase().replace(/[_\.]/g, '-').replace(/[^a-z0-9-]/g, '');
+
+    // Strip symbols from beginning
+    while (host.startsWith('-')) {
+      host = host.slice(1);
+    }
+
+    // Strip symbols from middle if greater than max length
+    while (host.length > HOSTNAME_MAX_LENGTH && host.charAt(HOSTNAME_MAX_LENGTH - 1) === '-') {
+      host = host.slice(0, HOSTNAME_MAX_LENGTH - 1).concat(host.slice(HOSTNAME_MAX_LENGTH));
+    }
+
+    host = host.slice(0, HOSTNAME_MAX_LENGTH);
+
+    // Strip symbols from end
+    while (host.endsWith('-')) {
+      host = host.slice(0, host.length - 1);
+    }
+
+    return host.slice(0, HOSTNAME_MAX_LENGTH);
+  }
+}
+
+module.exports = HostUtil;
---
+++ src/js/utils/__tests__/HostUtil-test.js
@@ -0,0 +1,72 @@
+jest.dontMock('../HostUtil');
+
+var HostUtil = require('../HostUtil');
+
+describe('HostUtil', function () {
+
+  describe('#stringToHostname', function () {
+
+    it('should strip out invalid characters from the middle', function () {
+      var result = HostUtil.stringToHostname('fd%gsf---gs7-f$gs--d7fddg-123');
+
+      expect(result).toEqual('fdgsf---gs7-fgs--d7fddg-123');
+    });
+
+    it('should strip out invalid characters from the start', function () {
+      var result = HostUtil.stringToHostname('-@4abc123');
+
+      expect(result).toEqual('4abc123');
+    });
+
+    it('should strip out invalid characters from the end', function () {
+      var result = HostUtil.stringToHostname('4abc123.');
+
+      expect(result).toEqual('4abc123');
+    });
+
+    it('should strip invalid characters from the start and end', function () {
+      var result = HostUtil.stringToHostname('##fdgsf---gs7-fgs--d7fddg123456789012345678901234567890123456789-');
+
+      expect(result).toEqual('fdgsf---gs7-fgs--d7fddg123456789012345678901234567890123456789');
+    });
+
+    it('should return valid hostnames', function () {
+      var result = HostUtil.stringToHostname('0-0');
+
+      expect(result).toEqual('0-0');
+    });
+
+    it('should allow dashes within the hostname', function () {
+      var result = HostUtil.stringToHostname('89fdgsf---gs7-fgs--d7fddg-123');
+
+      expect(result).toEqual('89fdgsf---gs7-fgs--d7fddg-123');
+    });
+
+    it('should strip invalid characters and trim to 63 characters and remove invalid end characters', function () {
+      var result = HostUtil.stringToHostname('fd%gsf---gs7-f$gs--d7fddg123456789012345678901234567890123456789-123');
+
+      expect(result).toEqual('fdgsf---gs7-fgs--d7fddg1234567890123456789012345678901234567891');
+    });
+
+    // TODO
+    it('should strip invalid start characters and trim to 63 characters and remove invalid end characters', function () {
+      var result = HostUtil.stringToHostname('%%fdgsf---gs7-fgs--d7fddg123456789012345678901234567890123456789---123');
+
+      expect(result).toEqual('fdgsf---gs7-fgs--d7fddg1234567890123456789012345678901234567891');
+    });
+
+    it('should return valid strings', function () {
+      var result = HostUtil.stringToHostname('a--------------------------------------------------------------b');
+
+      expect(result).toEqual('a-------------------------------------------------------------b');
+    });
+
+    it('should lowercase the string', function () {
+      var result = HostUtil.stringToHostname('XYZ');
+
+      expect(result).toEqual('xyz');
+    });
+
+  });
+
+});
