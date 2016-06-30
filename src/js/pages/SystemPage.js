c94941b7c34a3fe2324d2079053af7a140cbc860
John Ashenden <jaashenden@gmail.com>
2016-06-30 16:18:17 -0700

    Updated with icons

--- src/js/pages/SystemPage.js
+++ src/js/pages/SystemPage.js
@@ -173,7 +173,7 @@ SystemPage.contextTypes = {
 
 SystemPage.routeConfig = {
   label: 'System',
-  icon: <Icon id="gears" size="small" family="small" />,
+  icon: <Icon id="gear-inverse" size="small" family="small" />,
   matches: /^\/system/
 };
 
