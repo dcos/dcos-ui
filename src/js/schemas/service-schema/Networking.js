6c531b5e4bda23b076906fbcb713b75dff5e9bcd
John Furrow <johnf@mesosphere.io>
2016-07-01 13:35:52 -0700

    Change LB Port fieldtype to number when enabled

--- src/js/schemas/service-schema/Networking.js
+++ src/js/schemas/service-schema/Networking.js
@@ -108,11 +108,13 @@ const Networking = {
                   definition.value = null;
                   definition.disabled = false;
                   definition.className = 'form-control';
+                  definition.fieldType = 'number';
                 }
               } else {
                 // show as disabled
                 definition.value = disabledLBPortFieldValue;
                 definition.disabled = true;
+                definition.fieldType = 'text';
                 definition.className = 'form-control lb-port-input-field-disabled';
               }
 
@@ -124,7 +126,7 @@ const Networking = {
         properties: {
           lbPort: {
             title: 'LB Port',
-            type: 'number'
+            fieldType: 'number'
           },
           name: {
             title: 'Name',
