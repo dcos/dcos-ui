74d57af63c8617b67285c25e2dbde031bfe64caf
John Ashenden <jaashenden@gmail.com>
2016-07-03 21:15:36 -0700

    Ported Button Collection Docs from CNVS

--- src/js/pages/styles/components/ButtonCollectionsTabContent.js
+++ src/js/pages/styles/components/ButtonCollectionsTabContent.js
@@ -4,7 +4,217 @@ import SidebarActions from '../../../events/SidebarActions';
 class ButtonCollectionsTabContent extends React.Component {
 
   render() {
-    return null;
+
+    let codeBlockButtonCollection =
+`<!-- Button: Collection -->
+<div class="button-collection">
+  <a href="#" class="button button-primary">
+    Primary Button
+  </a>
+  <a href="#" class="button">
+    Button
+  </a>
+</div>`;
+    let codeBlockButtonCollectionVerticalAlignment =
+`<!-- Button Collection: Align Top-->
+<div class="button-collection button-collection-align-vertical-top">
+  …
+</div>
+
+<!-- Button Collection: Align Middle-->
+<div class="button-collection button-collection-align-vertical-middle">
+  …
+</div>
+
+<!-- Button Collection: Align Bottom-->
+<div class="button-collection button-collection-align-vertical-bottom">
+  …
+</div>`;
+
+    return (
+
+      <div>
+
+        <p>
+
+          Buttons render easily as objects inline with other elements.  When you have a group of buttons, use the <code>.button-collection</code> element to wrap a set of buttons.  A button collection defines simple rules governing the space between buttons both horizontally and vertically.
+
+        </p>
+
+        <div className="example-block flush-bottom">
+
+          <div className="example-block-content">
+
+            <div className="button-collection flush-bottom">
+
+              <a href="#" className="button button-primary">
+
+                Primary Button
+
+              </a>
+
+              <a href="#" className="button">
+
+                Button
+
+              </a>
+
+            </div>
+
+          </div>
+
+          <div className="example-block-footer example-block-footer-codeblock">
+
+            <pre className="prettyprint lang-html flush-bottom">
+
+              {codeBlockButtonCollection}
+
+            </pre>
+
+          </div>
+
+        </div>
+
+        <section id="button-collection-vertical-alignment">
+
+          <h2>
+
+            Button Collection Vertical Alignment
+
+          </h2>
+
+          <p>
+
+            When you have buttons of different sizes in the same <code>.button-collection</code>, you can use the button alignment properties <code>.button-collection-align-top</code>, <code>.button-collection-align-middle</code>, and <code>.button-collection-align-bottom</code> to position them along the top, middle, or baseline axis.
+
+          </p>
+
+          <div className="example-block flush-bottom">
+
+            <div className="example-block-content">
+
+              <div className="button-collection button-collection-align-vertical-top">
+
+                <a href="#" className="button button-jumbo">
+
+                  Jumbo
+
+                </a>
+
+                <a href="#" className="button button-large">
+
+                  Large
+
+                </a>
+
+                <a href="#" className="button">
+
+                  Default
+
+                </a>
+
+                <a href="#" className="button button-small">
+
+                  Small
+
+                </a>
+
+                <a href="#" className="button button-mini">
+
+                  Mini
+
+                </a>
+
+              </div>
+
+              <div className="button-collection button-collection-align-vertical-middle">
+
+                <a href="#" className="button button-jumbo">
+
+                  Jumbo
+
+                </a>
+
+                <a href="#" className="button button-large">
+
+                  Large
+
+                </a>
+
+                <a href="#" className="button">
+
+                  Default
+
+                </a>
+
+                <a href="#" className="button button-small">
+
+                  Small
+
+                </a>
+
+                <a href="#" className="button button-mini">
+
+                  Mini
+
+                </a>
+
+              </div>
+
+              <div className="button-collection button-collection-align-vertical-bottom flush-bottom">
+
+                <a href="#" className="button button-jumbo">
+
+                  Jumbo
+
+                </a>
+
+                <a href="#" className="button button-large">
+
+                  Large
+
+                </a>
+
+                <a href="#" className="button">
+
+                  Default
+
+                </a>
+
+                <a href="#" className="button button-small">
+
+                  Small
+
+                </a>
+
+                <a href="#" className="button button-mini">
+
+                  Mini
+
+                </a>
+
+              </div>
+
+            </div>
+
+            <div className="example-block-footer example-block-footer-codeblock">
+
+              <pre className="prettyprint lang-html flush-bottom">
+
+                {codeBlockButtonCollectionVerticalAlignment}
+
+              </pre>
+
+            </div>
+
+          </div>
+
+        </section>
+
+      </div>
+
+    );
+
   }
 }
 
