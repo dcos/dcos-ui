66a6d33451c3842a6b544e607829bdb00fa4024f
John Furrow <johnf@mesosphere.io>
2016-07-05 11:23:52 -0700

    Add DOMUtil#closest

--- src/js/utils/__tests__/DOMUtils-test.js
+++ src/js/utils/__tests__/DOMUtils-test.js
@@ -4,6 +4,60 @@ var DOMUtils = require('../DOMUtils');
 
 describe('DOMUtils', function () {
 
+  describe('#closest', function () {
+
+    it('should should return the parent element when provided a selector and ' +
+      'element where the element is a child of the selection', function () {
+      var el = {
+        parentElement: {
+          id: 'something-fake',
+          matches: function () {
+            return true;
+          }
+        },
+        matches: function () {
+          return false;
+        }
+      };
+      var match = DOMUtils.closest(el, '.fake-selector');
+
+      expect(match.id).toEqual('something-fake');
+    });
+
+    it('should should return null when provided a selector and element where ' +
+      'the element is not a child of the selection', function () {
+      var el = {
+        parentElement: null,
+        matches: function () {
+          return true;
+        }
+      };
+      var match = DOMUtils.closest(el, '.fake-selector');
+
+      expect(match).toEqual(null);
+    });
+
+    it('should should return the provided element when the provided element' +
+      'matches the selector AND has a parent element', function () {
+      var el = {
+        parentElement: {
+          id: 'something-fake',
+          matches: function () {
+            return false;
+          }
+        },
+        id: 'child-element',
+        matches: function () {
+          return true;
+        }
+      };
+      var match = DOMUtils.closest(el, '.fake-selector');
+
+      expect(match.id).toEqual('child-element');
+    });
+
+  });
+
   describe('#getComputedWidth', function () {
     function buildElement(style) {
       return `<div style="${style.join(';')}"></div>`;
