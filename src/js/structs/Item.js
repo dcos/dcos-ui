9a4a54b72fc75e319598550a673378aa947538cd
Mat Appelman <mat@mesosphere.io>
2016-07-05 13:01:13 -0700

    Move method to appropriate struct

--- src/js/structs/Item.js
+++ src/js/structs/Item.js
@@ -1,5 +1,3 @@
-import TaskStates from '../constants/TaskStates';
-
 module.exports = class Item {
   constructor(item = {}) {
     Object.keys(item).forEach(function (key) {
@@ -15,20 +13,5 @@ module.exports = class Item {
     }
 
     return this._itemData[key];
-  }
-
-  sumTaskTypesByState(state) {
-    let sum = 0;
-
-    Object.keys(TaskStates).forEach(function (taskType) {
-      if (TaskStates[taskType].stateTypes.indexOf(state) !== -1) {
-        // Make sure there's a value
-        if (this[taskType]) {
-          sum += this[taskType];
-        }
-      }
-    }, this);
-
-    return sum;
   }
 };
