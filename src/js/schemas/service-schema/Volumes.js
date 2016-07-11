5b7cbc3f0ccd6402cbebd3dee5543ec0c6dc1ca7
Suzanne Scala <suzanne@mesosphere.io>
2016-07-11 15:28:28 -0700

    Update Volumes.js

--- src/js/schemas/service-schema/Volumes.js
+++ src/js/schemas/service-schema/Volumes.js
@@ -6,7 +6,7 @@ const Volumes = {
       title: 'Persistent Local Volumes',
       description: (
         <span>
-        Specify a local volume or volumes to “pin” tasks and their associated data to the node they are first launched on. Tasks will be relaunched on that node if they terminate. <a href="https://dcos.io/docs/1.7/usage/storage/persistent-volume/">Learn more about creating stateful tasks with persistent volumes</a>.
+          Specify a local volume or volumes to “pin” tasks and their associated data to the node they are first launched on. Tasks will be relaunched on that node if they terminate. <a href="https://dcos.io/docs/1.7/usage/storage/persistent-volume/" target="_blank">Learn more about creating stateful tasks with persistent volumes</a>.
         </span>
       ),
       type: 'array',
@@ -45,7 +45,7 @@ const Volumes = {
       title: 'Docker Volumes',
       description: (
         <span>
-        Create a stateful application using Docker volumes. <a href="https://docs.docker.com/engine/tutorials/dockervolumes/">Learn more about Docker volumes.</a>
+          Create a stateful application using Docker volumes. <a href="https://docs.docker.com/engine/tutorials/dockervolumes/" target="_blank">Learn more about Docker volumes.</a>
         </span>
       ),
       type: 'array',
@@ -94,7 +94,7 @@ const Volumes = {
       title: 'External Volumes',
       description: (
         <span>
-        Create a stateful, fault-tolerant application with external volumes. <a href="https://docs.mesosphere.com/1.7/usage/storage/external-storage/">Learn more about external volumes.</a>
+          Create a stateful, fault-tolerant application with external volumes. <a href="https://docs.mesosphere.com/1.7/usage/storage/external-storage/" target="_blank">Learn more about external volumes.</a>
         </span>
       ),
       type: 'array',
