f930457e0c8aee476cb2ca5425bd172dc6ff57af
mlunoe <michael.lunoe@gmail.com>
2016-07-11 17:27:55 -0700

    Add tests

---
+++ src/js/components/__tests__/MarathonTaskDetailsList-test.js
@@ -0,0 +1,102 @@
+jest.dontMock('../MarathonTaskDetailsList');
+/* eslint-disable no-unused-vars */
+var React = require('react');
+/* eslint-enable no-unused-vars */
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+
+var MarathonTaskDetailsList = require('../MarathonTaskDetailsList');
+var MarathonStore = require('../../stores/MarathonStore');
+
+describe('MarathonTaskDetailsList', function () {
+
+  beforeEach(function () {
+    this.getServiceFromTaskID = MarathonStore.getServiceFromTaskID;
+    this.container = document.createElement('div');
+  });
+
+  afterEach(function () {
+    MarathonStore.getServiceFromTaskID = this.getServiceFromTaskID;
+    ReactDOM.unmountComponentAtNode(this.container);
+  });
+
+  describe('#getTaskEndpoints', function () {
+
+    it('returns None if ipAddresses, ports and host is not set', function () {
+      var instance = ReactDOM.render(
+        <MarathonTaskDetailsList taskID="foo" />,
+        this.container
+      );
+      expect(instance.getTaskEndpoints({})).toEqual('None');
+    });
+
+    it('returns a list of ipAddresses if ports is not defined', function () {
+      var instance = ReactDOM.render(
+        <MarathonTaskDetailsList taskID="foo" />,
+        this.container
+      );
+      expect(instance.getTaskEndpoints({
+        ipAddresses: [{ipAddress: 'foo'}, {ipAddress: 'bar'}]
+      })).toEqual('foo, bar');
+    });
+
+    it('returns host if ports and ipAddresses are not defined', function () {
+      var instance = ReactDOM.render(
+        <MarathonTaskDetailsList taskID="foo" />,
+        this.container
+      );
+      expect(instance.getTaskEndpoints({host: 'foo'}))
+        .toEqual('foo');
+    });
+
+    it('returns host with ports if ipAddresses are not defined', function () {
+      var instance = ReactDOM.render(
+        <MarathonTaskDetailsList taskID="foo" />,
+        this.container
+      );
+      var result = instance.getTaskEndpoints({host: 'foo', ports: [1, 2]});
+
+      expect(result.length).toEqual(2);
+      expect(result[0].props.children).toEqual('foo:1');
+    });
+
+    it('uses service ports if available', function () {
+      MarathonStore.getServiceFromTaskID = function () {
+        return {
+          ipAddress: {discovery : {ports: [{number: 3}]}}
+        };
+      };
+      var instance = ReactDOM.render(
+        <MarathonTaskDetailsList taskID="foo" />,
+        this.container
+      );
+      var result = instance.getTaskEndpoints({host: 'foo', ports: [1, 2]});
+
+      expect(result.length).toEqual(1);
+      expect(result[0].props.children).toEqual('foo:3');
+    });
+
+    it('defaults to ipAddresses and service ports', function () {
+      MarathonStore.getServiceFromTaskID = function () {
+        return {
+          ipAddress: {discovery : {ports: [{number: 3}]}}
+        };
+      };
+      var instance = ReactDOM.render(
+        <MarathonTaskDetailsList taskID="foo" />,
+        this.container
+      );
+      var result = instance.getTaskEndpoints({
+        host: 'foo',
+        ipAddresses: [{ipAddress: 'foo'}, {ipAddress: 'bar'}],
+        ports: [1, 2]
+      });
+
+      expect(result.length).toEqual(2);
+      expect(result[0].props.children).toEqual('foo:3');
+      expect(result[1].props.children).toEqual('bar:3');
+    });
+
+  })
+
+});
