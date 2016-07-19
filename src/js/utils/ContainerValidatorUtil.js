9e0dcfcf55fe974ba849cc5ff352989ccc01236d
Orlando Hohmeier <orlando@mesosphere.io>
2016-07-19 14:31:57 +0200

    Introduce container validator util

---
+++ src/js/utils/ContainerValidatorUtil.js
@@ -0,0 +1,13 @@
+const ContainerValidatorUtil = {
+  isValidDockerImage(dockerImage) {
+    if (typeof dockerImage !== 'string' || dockerImage === '') {
+      return false;
+    }
+
+    // The pattern is based on Dockers `validContainerName` regexp:
+    // https://github.com/docker/docker/blob/f63cdf0260/runtime.go#L33
+    return /^[a-zA-Z0-9_\-/:.]+$/.test(dockerImage);
+  }
+};
+
+module.exports = ContainerValidatorUtil;
