4727226f5d7c303b7004d68bf9044da324cfeaf0
John Furrow <johnf@mesosphere.io>
2016-07-25 12:35:08 -0700

    Audit and update existing table styles

--- src/js/components/ServicesTable.js
+++ src/js/components/ServicesTable.js
@@ -224,7 +224,7 @@ var ServicesTable = React.createClass({
 
     return (
       <Link to="services-detail"
-        className="headline"
+        className="table-cell-emphasized"
         params={{id}}>
         <span className="text-overflow">
           {service.getName()}
@@ -386,7 +386,7 @@ var ServicesTable = React.createClass({
     let isHeader = row == null;
 
     return classNames('flush-left text-align-right hidden-mini hidden-small', {
-      'highlight': prop === sortBy.prop,
+      'table-cell-sorted': prop === sortBy.prop,
       'clickable': isHeader
     });
   },
