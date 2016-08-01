4119ecc29495b0110ca8f6341d18eb30237de521
mlunoe <michael.lunoe@gmail.com>
2016-08-01 16:59:13 -0700

    Adding TODO

--- src/js/events/MesosSummaryActions.js
+++ src/js/events/MesosSummaryActions.js
@@ -17,6 +17,7 @@ var _historyServiceOnline = true;
 function testHistoryServerResponse(response) {
   // If the response is an empty object, that means something is whack
   // Fall back to making requests to Mesos
+  // TODO (DCOS-7764): This should be improved to validate against a schema
   if (response === {} ||
       Array.isArray(response.frameworks) ||
       Array.isArray(response.slaves)) {
