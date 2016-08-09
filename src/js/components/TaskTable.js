1c35614fcbc546c870bcf26bf3468992786bfd3f
John Furrow <johnf@mesosphere.io>
2016-08-09 15:55:04 -0700

    Remove console.log :no_mouth:

--- src/js/components/TaskTable.js
+++ src/js/components/TaskTable.js
@@ -334,8 +334,6 @@ class TaskTable extends React.Component {
       'danger': dangerState || unhealthy || failing
     });
 
-    console.log(this.getStatusValue(task));
-
     return (
       <div className="flex-box flex-box-align-vertical-center
         table-cell-flex-box">
