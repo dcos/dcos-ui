1ef04b6173accb41a612de2707e41a6450e73fb6
mlunoe <michael.lunoe@gmail.com>
2016-08-24 16:52:56 -0700

    Add help block for cron

--- src/js/schemas/job-schema/Schedule.js
+++ src/js/schemas/job-schema/Schedule.js
@@ -24,9 +24,9 @@ const Schedule = {
     },
     cron: {
       title: 'CRON Schedule',
-      description: (
+      helpBlock: (
         <span>
-          Use cron format to set your schedule, e.g. <i>0 0 20 * * *</i><br/><a href={MetadataStore.buildDocsURI('/usage/jobs/getting-started')} target="_blank">View documentation</a>.
+          Use cron format to set your schedule, e.g. <i>0 0 20 * * *</i>. <a href={MetadataStore.buildDocsURI('/usage/jobs/getting-started')} target="_blank">View documentation</a>.
         </span>
       ),
       type: 'string',
