5e9d74606d356ae1091f821376b3d43e2f9aa408
Ioannis Charalampidis <johnys2@gmail.com>
2016-08-29 16:35:38 +0200

    Moving mandatory functions to `Service`
    The functions exposed on the `Service` struct are the bare minimum
    required for a successful rendering on the summary display in the
    services table. Each type should implement them.

--- src/js/structs/Service.js
+++ src/js/structs/Service.js
@@ -1,8 +1,72 @@
 import Item from './Item';
+import HealthStatus from '../constants/HealthStatus';
+import ServiceStatus from '../constants/ServiceStatus';
+import VolumeList from './VolumeList';
+import ServiceImages from '../constants/ServiceImages';
 
 module.exports = class Service extends Item {
   getId() {
     return this.get('id') || '';
+  }
+
+  getName() {
+    return this.getId().split('/').pop();
+  }
+
+  getHealth() {
+    return HealthStatus.NA;
+  }
+
+  getLabels() {
+    return {};
+  }
+
+  getVolumes() {
+    return new VolumeList({items: []});
+  }
+
+  getStatus() {
+    const status = this.getServiceStatus();
+    if (status.displayName == null) {
+      return null;
+    }
+
+    return status.displayName;
+  }
+
+  getServiceStatus() {
+    return ServiceStatus.NA;
+  }
+
+  getImages() {
+    return ServiceImages.NA_IMAGES;
+  }
+
+  getWebURL() {
+    return null;
+  }
+
+  getInstancesCount() {
+    return 0;
+  }
+
+  getTasksSummary() {
+    return {
+      tasksHealthy: 0,
+      tasksStaged: 0,
+      tasksUnhealthy: 0,
+      tasksUnknown: 0,
+      tasksOverCapacity: 0,
+      tasksRunning: 0
+    };
+  }
+
+  getResources() {
+    return {
+      cpus: 0,
+      mem: 0,
+      disk: 0
+    };
   }
 
   toJSON() {
