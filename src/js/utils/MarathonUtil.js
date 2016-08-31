d2da9a635cdc1f3d28fc9eadfda16588c3dca4ed
Ioannis Charalampidis <johnys2@gmail.com>
2016-08-31 15:12:45 +0200

    Differentiating parsing of groups for pods

--- src/js/utils/MarathonUtil.js
+++ src/js/utils/MarathonUtil.js
@@ -3,6 +3,7 @@ import VolumeStatus from '../constants/VolumeStatus';
 
 function parseApp(app) {
   let {id, tasks = [], container = {}} = app;
+  app._type = 'app';
 
   if (id == null || !id.startsWith('/') || id.endsWith('/')) {
     throw new Error(`Id (${id}) must start with a leading slash ("/") ` +
@@ -64,8 +65,20 @@ function parseApp(app) {
   return Object.assign({volumes}, app);
 }
 
+function parsePod(pod) {
+  let {id} = pod;
+
+  if (id == null || !id.startsWith('/') || id.endsWith('/')) {
+    throw new Error(`Id (${id}) must start with a leading slash ("/") ` +
+      'and should not end with a slash.');
+  }
+
+  pod._type = 'pod';
+  return pod;
+}
+
 const MarathonUtil = {
-  parseGroups({id = '/', groups = [], apps = []}) {
+  parseGroups({id = '/', groups = [], apps = [], pods = []}) {
     if (id !== '/' && (!id.startsWith('/') || id.endsWith('/'))) {
       throw new Error(`Id (${id}) must start with a leading slash ("/") ` +
         'and should not end with a slash, except for root id which is only ' +
@@ -73,8 +86,10 @@ const MarathonUtil = {
     }
 
     // Parse items
-    let items = [].concat(groups.map(this.parseGroups.bind(this)),
-      apps.map(parseApp));
+    let items = [].concat(
+      groups.map(this.parseGroups.bind(this)),
+      apps.map(parseApp),
+      pods.map(parsePod));
 
     return {id, items};
   }
