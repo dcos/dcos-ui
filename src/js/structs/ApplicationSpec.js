d7f2e638c0c014dee1160e904395ece65f3dd0bb
Ioannis Charalampidis <johnys2@gmail.com>
2016-09-05 17:31:50 +0200

    Cleanly separating spec from service

---
+++ src/js/structs/ApplicationSpec.js
@@ -0,0 +1,8 @@
+import Application from './Application';
+
+module.exports = class ApplicationSpec extends Application {
+
+    // DCOS-9613: Should be properly implemented instead of
+    //            just extending application
+
+};
