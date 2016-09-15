fbd9397addc26749621a7e2458177aa7ec8eb485
Ioannis Charalampidis <johnys2@gmail.com>
2016-09-15 16:17:38 +0200

    Restructuring the describe in the tests

--- src/js/components/__tests__/PodInstancesTable-test.js
+++ src/js/components/__tests__/PodInstancesTable-test.js
@@ -40,286 +40,284 @@ describe('PodInstancesTable', function () {
 
   const pod = new Pod(fixture);
 
-  describe('#render (collapsed)', function () {
+  describe('#render', function () {
 
-    beforeEach(function () {
-      this.instance = TestUtils.renderIntoDocument(
-        <PodInstancesTable pod={pod} />
-      );
-    });
+    describe('collapsed table', function () {
+      beforeEach(function () {
+        this.instance = TestUtils.renderIntoDocument(
+          <PodInstancesTable pod={pod} />
+        );
+      });
 
-    it('should properly render the name column', function () {
-      var names = TestUtils.scryRenderedDOMComponentsWithClass(
-          this.instance, 'column-name')
-          .reduce(JestUtil.reduceTextContentOfSelector(
-            '.collapsing-string-full-string'), []);
+      it('should properly render the name column', function () {
+        var names = TestUtils.scryRenderedDOMComponentsWithClass(
+            this.instance, 'column-name')
+            .reduce(JestUtil.reduceTextContentOfSelector(
+              '.collapsing-string-full-string'), []);
 
-      expect(names).toEqual([
-        'instance-1',
-        'instance-2'
-      ]);
-    });
+        expect(names).toEqual([
+          'instance-1',
+          'instance-2'
+        ]);
+      });
 
-    it('should properly render the address column', function () {
-      var names = TestUtils.scryRenderedDOMComponentsWithClass(
-          this.instance, 'column-address')
-          .reduce(JestUtil.reduceTextContentOfSelector(
-            '.collapsing-string-full-string'), []);
+      it('should properly render the address column', function () {
+        var names = TestUtils.scryRenderedDOMComponentsWithClass(
+            this.instance, 'column-address')
+            .reduce(JestUtil.reduceTextContentOfSelector(
+              '.collapsing-string-full-string'), []);
 
-      expect(names).toEqual([
-        'agent-1',
-        'agent-2'
-      ]);
-    });
+        expect(names).toEqual([
+          'agent-1',
+          'agent-2'
+        ]);
+      });
 
-    it('should properly render the status column', function () {
-      var names = TestUtils.scryRenderedDOMComponentsWithClass(
-          this.instance, 'column-status')
-          .reduce(JestUtil.reduceTextContentOfSelector('.status-text'),
-            []);
+      it('should properly render the status column', function () {
+        var names = TestUtils.scryRenderedDOMComponentsWithClass(
+            this.instance, 'column-status')
+            .reduce(JestUtil.reduceTextContentOfSelector('.status-text'),
+              []);
 
-      expect(names).toEqual([
-        'Running',
-        'Running'
-      ]);
-    });
+        expect(names).toEqual([
+          'Running',
+          'Running'
+        ]);
+      });
 
-    it('should properly render the cpu column', function () {
-      var names = TestUtils.scryRenderedDOMComponentsWithClass(
-          this.instance, 'column-cpus')
-          .filter(JestUtil.filterByTagName('TD'))
-          .map(JestUtil.mapTextContent);
+      it('should properly render the cpu column', function () {
+        var names = TestUtils.scryRenderedDOMComponentsWithClass(
+            this.instance, 'column-cpus')
+            .filter(JestUtil.filterByTagName('TD'))
+            .map(JestUtil.mapTextContent);
 
-      expect(names).toEqual([
-        '1',
-        '1'
-      ]);
-    });
+        expect(names).toEqual([
+          '1',
+          '1'
+        ]);
+      });
 
-    it('should properly render the mem column', function () {
-      var names = TestUtils.scryRenderedDOMComponentsWithClass(
-          this.instance, 'column-mem')
-          .filter(JestUtil.filterByTagName('TD'))
-          .map(JestUtil.mapTextContent);
+      it('should properly render the mem column', function () {
+        var names = TestUtils.scryRenderedDOMComponentsWithClass(
+            this.instance, 'column-mem')
+            .filter(JestUtil.filterByTagName('TD'))
+            .map(JestUtil.mapTextContent);
 
-      expect(names).toEqual([
-        '128 MiB',
-        '128 MiB'
-      ]);
-    });
+        expect(names).toEqual([
+          '128 MiB',
+          '128 MiB'
+        ]);
+      });
 
-    it('should properly render the updated column', function () {
-      var names = TestUtils.scryRenderedDOMComponentsWithClass(
-          this.instance, 'column-updated')
-          .filter(JestUtil.filterByTagName('TD'))
-          .map(JestUtil.mapTextContent);
+      it('should properly render the updated column', function () {
+        var names = TestUtils.scryRenderedDOMComponentsWithClass(
+            this.instance, 'column-updated')
+            .filter(JestUtil.filterByTagName('TD'))
+            .map(JestUtil.mapTextContent);
 
-      expect(names).toEqual([
-        'a day ago',
-        '7 days ago'
-      ]);
-    });
+        expect(names).toEqual([
+          'a day ago',
+          '7 days ago'
+        ]);
+      });
 
-    it('should properly render the version column', function () {
-      var names = TestUtils.scryRenderedDOMComponentsWithClass(
-          this.instance, 'column-version')
-          .filter(JestUtil.filterByTagName('TD'))
-          .map(JestUtil.mapTextContent);
+      it('should properly render the version column', function () {
+        var names = TestUtils.scryRenderedDOMComponentsWithClass(
+            this.instance, 'column-version')
+            .filter(JestUtil.filterByTagName('TD'))
+            .map(JestUtil.mapTextContent);
 
-      expect(names).toEqual([
-        '2016-08-29T01:01:01.0012016-08-29T01:01:01.001',
-        '2016-08-29T01:01:01.0012016-08-29T01:01:01.001'
-      ]);
-    });
-
-  });
-
-  describe('#render (sort-ascending)', function () {
-
-    beforeEach(function () {
-      // Create a stub router context because when the items are expanded
-      // the are creating <Link /> instances.
-      let component = JestUtil.stubRouterContext(PodInstancesTable,
-        {pod}, {service: pod});
-      this.instance = TestUtils.renderIntoDocument(component);
-
-      // 1 click on the header (ascending)
-      let columnHeader = TestUtils.scryRenderedDOMComponentsWithClass(
-          this.instance, 'column-name')[0];
-      TestUtils.Simulate.click(columnHeader);
-    });
-
-    it('should properly sort the name column', function () {
-      var names = TestUtils.scryRenderedDOMComponentsWithClass(
-          this.instance, 'column-name')
-          .reduce(JestUtil.reduceTextContentOfSelector(
-            '.collapsing-string-full-string'), []);
-
-      expect(names).toEqual([
-        'instance-1',
-        'instance-2'
-      ]);
-    });
-  });
-
-  describe('#render (sort-descending)', function () {
-
-    beforeEach(function () {
-      // Create a stub router context because when the items are expanded
-      // the are creating <Link /> instances.
-      let component = JestUtil.stubRouterContext(PodInstancesTable,
-        {pod}, {service: pod});
-      this.instance = TestUtils.renderIntoDocument(component);
-
-      // 2 clicks on the header (descending)
-      let columnHeader = TestUtils.scryRenderedDOMComponentsWithClass(
-          this.instance, 'column-name')[0];
-      TestUtils.Simulate.click(columnHeader);
-      TestUtils.Simulate.click(columnHeader);
-    });
-
-    it('should properly sort the name column', function () {
-      var names = TestUtils.scryRenderedDOMComponentsWithClass(
-          this.instance, 'column-name')
-          .reduce(JestUtil.reduceTextContentOfSelector(
-            '.collapsing-string-full-string'), []);
-
-      expect(names).toEqual([
-        'instance-2',
-        'instance-1'
-      ]);
-    });
-  });
-
-  describe('#render (expanded)', function () {
-
-    beforeEach(function () {
-
-      // Create a stub router context because when the items are expanded
-      // the are creating <Link /> instances.
-      let component = JestUtil.stubRouterContext(PodInstancesTable,
-        {pod}, {service: pod});
-      this.instance = TestUtils.renderIntoDocument(component);
-
-      // Expand all table rows by clicking on them
-      TestUtils.scryRenderedDOMComponentsWithClass(
-          this.instance, 'column-name'
-      ).forEach(function (element) {
-        let target = element.querySelector('.is-expandable');
-        if (target) {
-          TestUtils.Simulate.click(target);
-        }
+        expect(names).toEqual([
+          '2016-08-29T01:01:01.0012016-08-29T01:01:01.001',
+          '2016-08-29T01:01:01.0012016-08-29T01:01:01.001'
+        ]);
       });
     });
 
-    it('should properly render the name column', function () {
-      var names = TestUtils.scryRenderedDOMComponentsWithClass(
-          this.instance, 'column-name')
-          .reduce(JestUtil.reduceTextContentOfSelector(
+    describe('collapsed table, sorted ascending by name', function () {
+      beforeEach(function () {
+        // Create a stub router context because when the items are expanded
+        // the are creating <Link /> instances.
+        let component = JestUtil.stubRouterContext(PodInstancesTable,
+          {pod}, {service: pod});
+        this.instance = TestUtils.renderIntoDocument(component);
+
+        // 1 click on the header (ascending)
+        let columnHeader = TestUtils.scryRenderedDOMComponentsWithClass(
+            this.instance, 'column-name')[0];
+        TestUtils.Simulate.click(columnHeader);
+      });
+
+      it('should properly sort the name column', function () {
+        var names = TestUtils.scryRenderedDOMComponentsWithClass(
+            this.instance, 'column-name')
+            .reduce(JestUtil.reduceTextContentOfSelector(
+              '.collapsing-string-full-string'), []);
+
+        expect(names).toEqual([
+          'instance-1',
+          'instance-2'
+        ]);
+      });
+    });
+
+    describe('collapsed table, sorted descending by name', function () {
+      beforeEach(function () {
+        // Create a stub router context because when the items are expanded
+        // the are creating <Link /> instances.
+        let component = JestUtil.stubRouterContext(PodInstancesTable,
+          {pod}, {service: pod});
+        this.instance = TestUtils.renderIntoDocument(component);
+
+        // 2 clicks on the header (descending)
+        let columnHeader = TestUtils.scryRenderedDOMComponentsWithClass(
+            this.instance, 'column-name')[0];
+        TestUtils.Simulate.click(columnHeader);
+        TestUtils.Simulate.click(columnHeader);
+      });
+
+      it('should properly sort the name column', function () {
+        var names = TestUtils.scryRenderedDOMComponentsWithClass(
+            this.instance, 'column-name')
+            .reduce(JestUtil.reduceTextContentOfSelector(
+              '.collapsing-string-full-string'), []);
+
+        expect(names).toEqual([
+          'instance-2',
+          'instance-1'
+        ]);
+      });
+    });
+
+    describe('expanded table', function () {
+      beforeEach(function () {
+
+        // Create a stub router context because when the items are expanded
+        // the are creating <Link /> instances.
+        let component = JestUtil.stubRouterContext(PodInstancesTable,
+          {pod}, {service: pod});
+        this.instance = TestUtils.renderIntoDocument(component);
+
+        // Expand all table rows by clicking on them
+        TestUtils.scryRenderedDOMComponentsWithClass(
+            this.instance, 'column-name'
+        ).forEach(function (element) {
+          let target = element.querySelector('.is-expandable');
+          if (target) {
+            TestUtils.Simulate.click(target);
+          }
+        });
+      });
+
+      it('should properly render the name column', function () {
+        var names = TestUtils.scryRenderedDOMComponentsWithClass(
+            this.instance, 'column-name')
+            .reduce(JestUtil.reduceTextContentOfSelector(
+              '.collapsing-string-full-string'), []);
+
+        expect(names).toEqual([
+          'instance-1',
+          'container-1',
+          'container-2',
+          'instance-2',
+          'container-1',
+          'container-2'
+        ]);
+      });
+
+      it('should properly render the address column', function () {
+        var columns = TestUtils.scryRenderedDOMComponentsWithClass(
+            this.instance, 'column-address');
+        let agents = columns.reduce(JestUtil.reduceTextContentOfSelector(
             '.collapsing-string-full-string'), []);
+        let ports = columns.reduce(JestUtil.reduceTextContentOfSelector('a'), []);
 
-      expect(names).toEqual([
-        'instance-1',
-        'container-1',
-        'container-2',
-        'instance-2',
-        'container-1',
-        'container-2'
-      ]);
-    });
+        expect(agents).toEqual([
+          'agent-1',
+          'agent-2'
+        ]);
+        expect(ports).toEqual([
+          ':31001',
+          ':31002',
+          ':31011',
+          ':31012'
+        ]);
+      });
 
-    it('should properly render the address column', function () {
-      var columns = TestUtils.scryRenderedDOMComponentsWithClass(
-          this.instance, 'column-address');
-      let agents = columns.reduce(JestUtil.reduceTextContentOfSelector(
-          '.collapsing-string-full-string'), []);
-      let ports = columns.reduce(JestUtil.reduceTextContentOfSelector('a'), []);
+      it('should properly render the status column', function () {
+        var names = TestUtils.scryRenderedDOMComponentsWithClass(
+            this.instance, 'column-status')
+            .reduce(JestUtil.reduceTextContentOfSelector('.status-text'), []);
 
-      expect(agents).toEqual([
-        'agent-1',
-        'agent-2'
-      ]);
-      expect(ports).toEqual([
-        ':31001',
-        ':31002',
-        ':31011',
-        ':31012'
-      ]);
-    });
+        expect(names).toEqual([
+          'Running',
+          'Running',
+          'Running',
+          'Running',
+          'Running',
+          'Running'
+        ]);
+      });
 
-    it('should properly render the status column', function () {
-      var names = TestUtils.scryRenderedDOMComponentsWithClass(
-          this.instance, 'column-status')
-          .reduce(JestUtil.reduceTextContentOfSelector('.status-text'), []);
+      it('should properly render the cpu column', function () {
+        var names = TestUtils.scryRenderedDOMComponentsWithClass(
+            this.instance, 'column-cpus')
+            .filter(JestUtil.filterByTagName('TD'))
+            .reduce(JestUtil.reduceTextContentOfSelector('div > div > span'), []);
 
-      expect(names).toEqual([
-        'Running',
-        'Running',
-        'Running',
-        'Running',
-        'Running',
-        'Running'
-      ]);
-    });
+        expect(names).toEqual([
+          '1',
+          '0.5',
+          '0.5',
+          '1',
+          '0.5',
+          '0.5'
+        ]);
+      });
 
-    it('should properly render the cpu column', function () {
-      var names = TestUtils.scryRenderedDOMComponentsWithClass(
-          this.instance, 'column-cpus')
-          .filter(JestUtil.filterByTagName('TD'))
-          .reduce(JestUtil.reduceTextContentOfSelector('div > div > span'), []);
+      it('should properly render the mem column', function () {
+        var names = TestUtils.scryRenderedDOMComponentsWithClass(
+            this.instance, 'column-mem')
+            .filter(JestUtil.filterByTagName('TD'))
+            .reduce(JestUtil.reduceTextContentOfSelector('div > div > span'), []);
 
-      expect(names).toEqual([
-        '1',
-        '0.5',
-        '0.5',
-        '1',
-        '0.5',
-        '0.5'
-      ]);
-    });
+        expect(names).toEqual([
+          '128 MiB',
+          '64 MiB',
+          '64 MiB',
+          '128 MiB',
+          '64 MiB',
+          '64 MiB'
+        ]);
+      });
 
-    it('should properly render the mem column', function () {
-      var names = TestUtils.scryRenderedDOMComponentsWithClass(
-          this.instance, 'column-mem')
-          .filter(JestUtil.filterByTagName('TD'))
-          .reduce(JestUtil.reduceTextContentOfSelector('div > div > span'), []);
+      it('should properly render the updated column', function () {
+        var names = TestUtils.scryRenderedDOMComponentsWithClass(
+            this.instance, 'column-updated')
+            .filter(JestUtil.filterByTagName('TD'))
+            .reduce(JestUtil.reduceTextContentOfSelector('time'), []);
 
-      expect(names).toEqual([
-        '128 MiB',
-        '64 MiB',
-        '64 MiB',
-        '128 MiB',
-        '64 MiB',
-        '64 MiB'
-      ]);
-    });
+        expect(names).toEqual([
+          'a day ago',
+          '3 days ago',
+          '5 days ago',
+          '7 days ago',
+          '9 days ago',
+          '11 days ago'
+        ]);
+      });
 
-    it('should properly render the updated column', function () {
-      var names = TestUtils.scryRenderedDOMComponentsWithClass(
-          this.instance, 'column-updated')
-          .filter(JestUtil.filterByTagName('TD'))
-          .reduce(JestUtil.reduceTextContentOfSelector('time'), []);
+      it('should properly render the version column', function () {
+        var names = TestUtils.scryRenderedDOMComponentsWithClass(
+            this.instance, 'column-version')
+            .filter(JestUtil.filterByTagName('TD'))
+            .map(JestUtil.mapTextContent);
 
-      expect(names).toEqual([
-        'a day ago',
-        '3 days ago',
-        '5 days ago',
-        '7 days ago',
-        '9 days ago',
-        '11 days ago'
-      ]);
-    });
-
-    it('should properly render the version column', function () {
-      var names = TestUtils.scryRenderedDOMComponentsWithClass(
-          this.instance, 'column-version')
-          .filter(JestUtil.filterByTagName('TD'))
-          .map(JestUtil.mapTextContent);
-
-      expect(names).toEqual([
-        '2016-08-29T01:01:01.0012016-08-29T01:01:01.001',
-        '2016-08-29T01:01:01.0012016-08-29T01:01:01.001'
-      ]);
+        expect(names).toEqual([
+          '2016-08-29T01:01:01.0012016-08-29T01:01:01.001',
+          '2016-08-29T01:01:01.0012016-08-29T01:01:01.001'
+        ]);
+      });
     });
 
   });
