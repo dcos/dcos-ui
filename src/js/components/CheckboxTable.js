6d709f3809fa499df8d147f8edbcf4c7ebbf3f76
Dmitrii Rozhkov <drozhkov@mesosphere.io>
2016-09-16 13:08:41 +0200

    Take into account disabled items when rendering a heading checkbox

--- src/js/components/CheckboxTable.js
+++ src/js/components/CheckboxTable.js
@@ -52,7 +52,9 @@ class CheckboxTable extends React.Component {
 
   bulkCheck(isChecked) {
     let checkedIDs = [];
-    let {data, onCheckboxChange, uniqueProperty} = this.props;
+    let {data, onCheckboxChange, uniqueProperty, disabledItemsMap} = this.props;
+
+    data = data.filter((datum) => !disabledItemsMap[datum[uniqueProperty]]);
 
     if (isChecked) {
       data.forEach(function (datum) {
@@ -75,13 +77,14 @@ class CheckboxTable extends React.Component {
   renderHeadingCheckbox() {
     let checked = false;
     let indeterminate = false;
-    let {allowMultipleSelect, checkedItemsMap, data} = this.props;
+    let {allowMultipleSelect, checkedItemsMap, disabledItemsMap, data} = this.props;
 
     if (!allowMultipleSelect) {
       return null;
     }
 
     let checkedItemsCount = Object.keys(checkedItemsMap).length;
+    let disabledItemsCount = Object.keys(disabledItemsMap).length;
 
     if (checkedItemsCount > 0) {
       indeterminate = true;
@@ -89,7 +92,7 @@ class CheckboxTable extends React.Component {
       checked = false;
     }
 
-    if (checkedItemsCount === data.length && checkedItemsCount !== 0) {
+    if (checkedItemsCount + disabledItemsCount >= data.length && checkedItemsCount !== 0) {
       checked = true;
       indeterminate = false;
     }
