d2a3096c1cf8a0da6234d79f618b9a25a3de9da3
Ioannis Charalampidis <johnys2@gmail.com>
2016-09-20 17:21:49 +0200

    Created respetcive jira ticket

--- src/js/constants/PodContainerState.js
+++ src/js/constants/PodContainerState.js
@@ -3,7 +3,7 @@
 // https://github.com/mesosphere/marathon/blob/feature/pods/docs/docs/rest-api/public/api/v2/types/podStatus.raml#L36
 //
 // TODO: Note that the SPEC currently states that this is not yet well-defiend,
-//       the current values are assumed to be correct:
+//       the current values are assumed to be correct (DCOS-9852).
 //
 module.exports = {
   RUNNING: 'RUNNING',
