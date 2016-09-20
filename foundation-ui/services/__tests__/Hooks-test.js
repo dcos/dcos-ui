d2534b9677180258d2498b3b1af72f5865be25bf
mlunoe <michael.lunoe@gmail.com>
2016-09-19 17:39:12 -0700

    Fixing tests

--- foundation-ui/services/__tests__/Hooks-test.js
+++ foundation-ui/services/__tests__/Hooks-test.js
@@ -1,9 +1,4 @@
 jest.dontMock('../Hooks');
-jest.dontMock('../../config/Config');
-jest.dontMock('../../constants/EventTypes');
-jest.dontMock('../../events/AppDispatcher');
-jest.dontMock('../../events/ConfigActions');
-jest.dontMock('../../stores/ConfigStore');
 
 const Hooks = require('../Hooks');
 
@@ -17,9 +12,10 @@ describe('HooksAPI', function () {
         return value.replace('bar', 'baz');
       });
 
-      Hooks.addFilter('foo', this.fakeFilter);
+      this.hooks = new Hooks();
+      this.hooks.addFilter('foo', this.fakeFilter);
 
-      this.filteredContent = Hooks.applyFilter('foo', 'foo bar', 'qux');
+      this.filteredContent = this.hooks.applyFilter('foo', 'foo bar', 'qux');
     });
 
     it('should receive the arguments that we defined', function () {
@@ -47,10 +43,10 @@ describe('HooksAPI', function () {
         return value.replace('bar', 'qux');
       });
 
-      Hooks.addFilter('corge', lowPriorityFilter, 20);
-      Hooks.addFilter('corge', highPriorityFilter, 1);
+      this.hooks.addFilter('corge', lowPriorityFilter, 20);
+      this.hooks.addFilter('corge', highPriorityFilter, 1);
 
-      var filteredContent = Hooks.applyFilter('corge', 'foo bar');
+      var filteredContent = this.hooks.applyFilter('corge', 'foo bar');
 
       expect(filteredContent).toEqual('foo qux');
     });
@@ -60,33 +56,34 @@ describe('HooksAPI', function () {
   describe('#doAction', function () {
 
     beforeEach(function () {
+      this.hooks = new Hooks();
       this.fakeAction = jest.genMockFunction();
-      Hooks.addAction('foo', this.fakeAction);
+      this.hooks.addAction('foo', this.fakeAction);
     });
 
     it('should be called only once when an action is performed', function () {
-      Hooks.doAction('foo', 'bar');
+      this.hooks.doAction('foo', 'bar');
       expect(this.fakeAction.mock.calls.length).toEqual(1);
     });
 
     it('should receive arguments when an action is performed', function () {
-      Hooks.doAction('foo', 'bar');
+      this.hooks.doAction('foo', 'bar');
       expect(this.fakeAction.mock.calls[0][0]).toEqual('bar');
     });
 
     it('should not receive arguments when arguments are not passed',
       function () {
         this.noArgumentsAction = jest.genMockFunction();
-        Hooks.addAction('qux', this.noArgumentsAction);
-        Hooks.doAction('qux');
+        this.hooks.addAction('qux', this.noArgumentsAction);
+        this.hooks.doAction('qux');
         expect(this.noArgumentsAction.mock.calls[0].length).toEqual(0);
       });
 
     it('should receive two arguments when two arguments are passed',
       function () {
         this.twoArgumentsAction = jest.genMockFunction();
-        Hooks.addAction('quux', this.twoArgumentsAction);
-        Hooks.doAction('quux', 'baz', 'bar');
+        this.hooks.addAction('quux', this.twoArgumentsAction);
+        this.hooks.doAction('quux', 'baz', 'bar');
         expect(this.twoArgumentsAction.mock.calls[0].length).toEqual(2);
       });
 
