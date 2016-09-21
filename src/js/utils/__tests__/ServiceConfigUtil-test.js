a5a7aaffdf44dc446c995c1c58a6e16be071f194
Dmitrii Rozhkov <drozhkov@mesosphere.io>
2016-09-21 16:25:14 +0200

    add tests for getPortDefinitionGroups

--- src/js/utils/__tests__/ServiceConfigUtil-test.js
+++ src/js/utils/__tests__/ServiceConfigUtil-test.js
@@ -47,4 +47,64 @@ describe('ServiceConfigUtil', function () {
 
   });
 
+  describe('#getPortDefinitionGroups', function () {
+    describe('port definitions with no name', function () {
+      it('creates index-based headline', function () {
+        const portDefinitions = [{}];
+        const {headline} =
+          ServiceConfigUtil.getPortDefinitionGroups(1, portDefinitions)[0];
+
+        expect(headline).toEqual('Port Definition 1');
+      });
+
+    });
+
+    describe('named port definitions', function () {
+      it('adds the name to the headline', function () {
+        const portDefinitions = [{
+          name: 'portName'
+        }];
+        const {headline} =
+          ServiceConfigUtil.getPortDefinitionGroups('1234', portDefinitions)[0];
+
+        expect(headline).toEqual('Port Definition 1 (portName)');
+      });
+
+    });
+
+    describe('load-balanced ports', function () {
+      it('adds a service address to the definition', function () {
+        const portDefinitions = [{
+          port: 80,
+          labels: {
+            'VIP_0': '1.2.3.4:80'
+          }
+        }];
+        const {hash} =
+          ServiceConfigUtil.getPortDefinitionGroups('1234', portDefinitions)[0];
+
+        expect(hash['Service Address'])
+          .toEqual('1234.marathon.l4lb.thisdcos.directory:80');
+      });
+
+    });
+
+    describe('generic ports', function () {
+      it('doesn\'t add a service address to the definition', function () {
+        const portDefinitions = [{
+          port: 80,
+          labels: {
+            'SOME': 'LABEL'
+          }
+        }];
+        const {hash} =
+          ServiceConfigUtil.getPortDefinitionGroups('1234', portDefinitions)[0];
+
+        expect(hash['Service Address']).toBeUndefined();
+      });
+
+    });
+
+  });
+
 });
