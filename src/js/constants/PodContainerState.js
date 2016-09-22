a102936922514740ab8d9f52ccdcfc62df260af3
Ioannis Charalampidis <ioannis.charalampidis@cern.ch>
2016-09-22 14:18:25 +0300

    Fixing multiline comment lint

--- src/js/constants/PodContainerState.js
+++ src/js/constants/PodContainerState.js
@@ -1,10 +1,10 @@
-//
-// The possible values of ContainerState enum for Pods, according to:
-// https://github.com/mesosphere/marathon/blob/feature/pods/docs/docs/rest-api/public/api/v2/types/podStatus.raml#L36
-//
-// TODO: Note that the SPEC currently states that this is not yet well-defiend,
-//       the current values are assumed to be correct (DCOS-9852).
-//
+/**
+ * The possible values of ContainerState enum for Pods, according to:
+ * https://github.com/mesosphere/marathon/blob/feature/pods/docs/docs/rest-api/public/api/v2/types/podStatus.raml#L36
+ *
+ * TODO: Note that the SPEC currently states that this is not yet well-defiend,
+ *       the current values are assumed to be correct (DCOS-9852).
+ */
 module.exports = {
   RUNNING: 'TASK_RUNNING',
   ERROR: 'TASK_ERROR',
--- src/js/constants/PodState.js
+++ src/js/constants/PodState.js
@@ -1,7 +1,7 @@
-//
-// The possible values of PodState enum, according to:
-// https://github.com/mesosphere/marathon/blob/feature/pods/docs/docs/rest-api/public/api/v2/types/podStatus.raml#L98
-//
+/**
+ * The possible values of PodState enum, according to:
+ * https://github.com/mesosphere/marathon/blob/feature/pods/docs/docs/rest-api/public/api/v2/types/podStatus.raml#L98
+ */
 module.exports = {
   DEGRADED: 'degraded',
   STABLE: 'stable',
