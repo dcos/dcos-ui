b955e468b65569ae0a53a6d16d17739d846b6bdc
Orlando Hohmeier <orlando@mesosphere.io>
2016-09-22 13:32:55 +0100

    Adjust VIP labels

--- tests/_fixtures/pods/PodFixture.js
+++ tests/_fixtures/pods/PodFixture.js
@@ -16,7 +16,7 @@ module.exports = {
             'hostPort': 0,
             'protocol': 'http',
             'labels': {
-              'VIP0': '1.2.3.4:80'
+              'VIP_0': '1.2.3.4:80'
             }
           }
         ],
@@ -32,7 +32,7 @@ module.exports = {
             'hostPort': 0,
             'protocol': 'http',
             'labels': {
-              'VIP0': '1.2.3.4:80'
+              'VIP_0': '1.2.3.4:80'
             }
           }
         ],
