71a299179d5ebaabd18a3251157139f3d6f2760a
John Furrow <johnf@mesosphere.io>
2016-09-23 11:08:36 -0700

    Fix TaskTable test from erroneous merge

--- src/js/components/__tests__/TaskTable-test.js
+++ src/js/components/__tests__/TaskTable-test.js
@@ -50,8 +50,11 @@ describe('TaskTable', function () {
       this.taskTable = new TaskTable();
     });
 
-    it('returns a map of disabled items', function () {
-      var tasks = [{id: '1', scheduler: 'marathon'}, {id: '2'}];
+    it('treats tasks started not by Marathon as disabled', function () {
+      var tasks = [
+        {id: '1', state: 'TASK_STARTING', isStartedByMarathon: true},
+        {id: '2', state: 'TASK_STARTING'}
+      ];
       expect(this.taskTable.getDisabledItemsMap(tasks)).toEqual({'2': true});
     });
 
