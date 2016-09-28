fc89c28e0d2386461aeec9f80978644b59e0a75f
mlunoe <michael.lunoe@gmail.com>
2016-09-28 16:30:09 -0700

    Adding JSDoc for Service

--- foundation-ui/service/Service.js
+++ foundation-ui/service/Service.js
@@ -1,3 +1,23 @@
+/**
+ * Service is a factory for creating services that relies on a single instance
+ * of Hooks. It utilses the hooks actions to notify subscribers of changes and
+ * filters to allow external packages to change output from the service
+ * @param  {Hooks} hooks, an instance of Hooks
+ * @returns {Service} service, an instance of Service
+ *
+ * @example
+ * User of service would listen for updates and possibly change it
+ * service.on('myID', function (value) {
+ *   return value.replace('my', 'myChanged');
+ * });
+ *
+ * Creater of service would instantiate the Hooks and apply filter when necessary
+ * let hooks = Hooks();
+ * let service = Service(hooks);
+ * hooks.applyFilter('myID', function (value) {
+ *   return 'myValue';
+ * });
+ */
 function Service(hooks) {
   return {
     on(id, callback, priority) {
