21c8510ff5fba529817533fea45dd2421d192e60
mlunoe <michael.lunoe@gmail.com>
2016-09-28 15:46:32 -0700

    Referencing Hooks correctly

--- foundation-ui/index.js
+++ foundation-ui/index.js
@@ -1,7 +1,9 @@
 import DCOSStore from './stores/DCOSStore';
+import Hooks from './hooks';
 import ResourceTableUtil from './utils/ResourceTableUtil';
 
 module.exports = {
   DCOSStore,
+  Hooks,
   ResourceTableUtil
 };
