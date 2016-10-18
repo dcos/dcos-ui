d02306c2928414dba0585915f6b5f7c64ee991a4
John Furrow <johnf@mesosphere.io>
2016-10-18 15:29:33 +0200

    Add tests for Tabs component family

---
+++ src/js/components/__tests__/TabButton-test.js
@@ -0,0 +1,52 @@
+jest.dontMock('../TabButton');
+
+const React = require('react');
+const ReactDOM = require('react-dom');
+const TestUtils = require('react-addons-test-utils');
+
+const TabButton = require('../TabButton');
+
+describe('TabButton', function () {
+
+  beforeEach(function () {
+    this.clickHandler = jasmine.createSpy('click handler');
+  });
+
+  it('should call the onClick prop with ID when clicked', function () {
+    this.instance = TestUtils.renderIntoDocument(
+      <TabButton label="foo" onClick={this.clickHandler} id="foo" />
+    );
+
+    // Click the TabButton's label
+    TestUtils.Simulate.click(ReactDOM.findDOMNode(this.instance).querySelector('.menu-tabbed-item-label'));
+
+    expect(this.clickHandler).toHaveBeenCalledWith('foo');
+  });
+
+  it('should clone nested TabButton instances with onClick and activeTab props', function () {
+    this.instance = TestUtils.renderIntoDocument(
+      <TabButton activeTab="foo" label="foo" onClick={this.clickHandler} id="foo">
+        <TabButton label="bar" id="bar" />
+      </TabButton>
+    );
+
+    let nestedInstance = TestUtils.scryRenderedComponentsWithType(this.instance, TabButton)[0];
+
+    expect(nestedInstance.props.activeTab).toEqual('foo');
+    expect(nestedInstance.props.onClick).toEqual(this.clickHandler);
+  });
+
+  it('should call the parent onClick when clicking a nested TabButton', function () {
+    this.instance = TestUtils.renderIntoDocument(
+      <TabButton activeTab="foo" label="foo" onClick={this.clickHandler} id="foo">
+        <TabButton label="bar" id="bar" />
+      </TabButton>
+    );
+
+    // Click the TabButton's label
+    TestUtils.Simulate.click(ReactDOM.findDOMNode(this.instance).querySelector('.menu-tabbed-item .menu-tabbed-item .menu-tabbed-item-label'));
+
+    expect(this.clickHandler).toHaveBeenCalledWith('bar');
+  });
+
+});
