9b3a9c42d951cbfef030bd2ce5979a0eeebb73b5
John Furrow <johnf@mesosphere.io>
2016-10-18 16:59:43 +0200

    Introduce the collapsing panel component

---
+++ src/js/components/CollapsingPanelHeader.js
@@ -0,0 +1,43 @@
+import classNames from 'classnames/dedupe';
+import React from 'react';
+
+import Icon from './Icon';
+
+class CollapsingPanelHeader extends React.Component {
+  getStateIndicator() {
+    let iconID = 'caret-down';
+
+    if (this.props.isExpanded) {
+      iconID = 'caret-up';
+    }
+
+    return <Icon id={iconID} color="neutral" family="small" size="mini" />;
+  }
+
+  render() {
+    let classes = classNames(
+      'panel-cell panel-cell-header panel-cell-short clickable',
+      this.props.className
+    );
+
+    return (
+      <div className={classes} onClick={this.props.onClick}>
+        {this.props.children}
+        {this.getStateIndicator()}
+      </div>
+    );
+  }
+}
+
+CollapsingPanelHeader.propTypes = {
+  children: React.PropTypes.node,
+  className: React.PropTypes.oneOfType([
+    React.PropTypes.array,
+    React.PropTypes.object,
+    React.PropTypes.string
+  ]),
+  isExpanded: React.PropTypes.bool,
+  onClick: React.PropTypes.func
+};
+
+module.exports = CollapsingPanelHeader;
