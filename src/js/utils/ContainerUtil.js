947d6e703f30b4dd9b67751cd9805b9c18c9745e
Mat Appelman <mat@mesosphere.io>
2016-10-19 13:08:55 +0200

    Factoring pendingActions and actionError assignment into a Util

---
+++ src/js/utils/ContainerUtil.js
@@ -0,0 +1,32 @@
+const ContainerUtil = {
+  /**
+   * Sets or clears error for actionType
+   * @param {Object} actionErrors
+   * @param  {String} actionType
+   * @param  {Any} error
+   * @return {Object} updated action errors
+   */
+  adjustActionErrors(actionErrors, actionType, error) {
+    return Object.assign(
+      {},
+      actionErrors,
+      {[actionType]: error}
+    );
+  },
+  /**
+   * Sets pending action to true/false
+   * @param {Object} pendingActions
+   * @param  {String}  actionType
+   * @param  {Boolean} isPending
+   * @return {Object} updated pending actions
+   */
+  adjustPendingActions(pendingActions, actionType, isPending) {
+    return Object.assign(
+      {},
+      pendingActions,
+      {[actionType]: isPending}
+    );
+  }
+};
+
+module.exports = ContainerUtil;
---
+++ src/js/utils/__tests__/ContainerUtil-test.js
@@ -0,0 +1,55 @@
+jest.dontMock('../ContainerUtil');
+
+const ContainerUtil = require('../ContainerUtil');
+
+describe('#adjustActionErrors', function () {
+
+  let actionErrors = ContainerUtil.adjustActionErrors({}, 'foo', 'error');
+
+  it('should correctly set error key', function () {
+    expect(actionErrors).toEqual({foo: 'error'});
+  });
+
+  it('should correctly add to error keys', function () {
+    actionErrors = ContainerUtil.adjustActionErrors(
+      actionErrors, 'bar', 'newError'
+    );
+
+    expect(actionErrors).toEqual({foo: 'error', bar: 'newError'});
+  });
+
+  it('should correctly alter existing keys', function () {
+    actionErrors = ContainerUtil.adjustActionErrors(
+      actionErrors, 'foo', null
+    );
+
+    expect(actionErrors).toEqual({foo: null, bar: 'newError'});
+  });
+
+});
+
+describe('#adjustPendingActions', function () {
+
+  let pendingActions = ContainerUtil.adjustPendingActions({}, 'foo', true);
+
+  it('should correctly set pending action key', function () {
+    expect(pendingActions).toEqual({foo: true});
+  });
+
+  it('should correctly add to pending action keys', function () {
+    pendingActions = ContainerUtil.adjustPendingActions(
+      pendingActions, 'bar', false
+    );
+
+    expect(pendingActions).toEqual({foo: true, bar: false});
+  });
+
+  it('should correctly alter existing keys', function () {
+    pendingActions = ContainerUtil.adjustPendingActions(
+      pendingActions, 'foo', false
+    );
+
+    expect(pendingActions).toEqual({foo: false, bar: false});
+  });
+
+});
