fc11985d4e16d27f30b3913aff12f31fd6b8505c
Philipp Hinrichsen <philipph@mesosphere.io>
2016-10-20 18:29:10 +0200

    Remove SetFormActions

--- src/js/structs/SetFormAction.js
+++
@@ -1,9 +0,0 @@
-import FormAction from './FormAction';
-
-class SetFormAction extends FormAction {
-  constructor(path, value) {
-    super(path, value, 'SET');
-  }
-}
-
-module.exports = SetFormAction;
