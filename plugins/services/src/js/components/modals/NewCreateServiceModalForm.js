0d50957e14d3767455bb222818b055acd0f1bd3c
mlunoe <michael.lunoe@gmail.com>
2016-10-27 13:31:40 -0700

    First working example

--- plugins/services/src/js/components/modals/NewCreateServiceModalForm.js
+++ plugins/services/src/js/components/modals/NewCreateServiceModalForm.js
@@ -2,20 +2,33 @@ import Ace from 'react-ace';
 import classNames from 'classnames';
 import React from 'react';
 
+import Action from '../../../../../../src/js/structs/FormAction';
+import Batch from '../../../../../../src/js/structs/Batch';
+import GeneralSection from '../../../../../../src/js/components/form/GeneralSection';
+import ReducerUtil from '../../../../../../src/js/utils/ReducerUtil';
 import Tabs from '../../../../../../src/js/components/Tabs';
 import TabButton from '../../../../../../src/js/components/TabButton';
 import TabButtonList from '../../../../../../src/js/components/TabButtonList';
 import TabView from '../../../../../../src/js/components/TabView';
 import TabViewList from '../../../../../../src/js/components/TabViewList';
 
-const METHODS_TO_BIND = ['handleJSONChange'];
+const METHODS_TO_BIND = [
+  'handleJSONChange',
+  'handleAddEvent',
+  'handleAddReducer'
+];
 
 class NewCreateServiceModalForm extends React.Component {
   constructor() {
     super(...arguments);
 
+    let batch = new Batch();
+    batch.add({action: 'INIT'});
+
     this.state = {
-      jsonValue: ''
+      appConfig: {},
+      batch,
+      reducerConfig: {}
     };
 
     METHODS_TO_BIND.forEach((method) => {
@@ -24,43 +37,71 @@ class NewCreateServiceModalForm extends React.Component {
   }
 
   handleJSONChange(jsonValue) {
-    this.setState({jsonValue});
+    try {
+      let appConfig = JSON.parse(jsonValue);
+      let batch = new Batch();
+      batch.add({action: 'INIT'});
+      this.setState({appConfig, batch});
+    } catch (event) {
+      // TODO: handle error
+      alert('JSON value is not valid json. Could not store it', event);
+    }
+  }
+
+  handleAddEvent(event) {
+    let {batch} = this.state;
+    let value = event.target.value;
+    let path = event.target.getAttribute('name');
+    batch.add(new Action(path.split(','), value));
+
+    this.setState({batch});
+  }
+
+  handleAddReducer(reducerConfig) {
+    this.setState({
+      reducerConfig: Object.assign({}, this.state.reducerConfig, reducerConfig)
+    });
   }
 
   render() {
-    let {props} = this;
+    let {isJSONModeActive} = this.props;
+
+    let {appConfig, batch, reducerConfig} = this.state;
+    let data = batch.reduce(
+      ReducerUtil.combineReducers(reducerConfig),
+      appConfig
+    );
+
     let jsonEditorPlaceholderClasses = classNames(
       'modal-full-screen-side-panel-placeholder',
-      {
-        'is-visible': props.isJSONModeActive
-      }
+      {'is-visible': isJSONModeActive}
     );
     let jsonEditorClasses = classNames('modal-full-screen-side-panel', {
-      'is-visible': props.isJSONModeActive
+      'is-visible': isJSONModeActive
     });
 
     return (
       <div className="flex flex-item-grow-1">
         <div className="container flex flex-direction-top-to-bottom">
-          <Tabs vertical={true}>
-            <TabButtonList>
-              <TabButton id="services" label="Services">
-                <TabButton id="container-1" label="Container 1" />
-                <TabButton id="container-2" label="Container 2" />
-              </TabButton>
-            </TabButtonList>
-            <TabViewList>
-              <TabView id="services">
-                Services Content
-              </TabView>
-              <TabView id="container-1">
-                Container 1 Content
-              </TabView>
-              <TabView id="container-2">
-                Container 2 Content
-              </TabView>
-            </TabViewList>
-          </Tabs>
+          <form onChange={this.handleAddEvent}>
+            <Tabs vertical={true}>
+              <TabButtonList>
+                <TabButton id="services" label="Services">
+                  <TabButton id="container-1" label="Container 1" />
+                  <TabButton id="container-2" label="Container 2" />
+                </TabButton>
+              </TabButtonList>
+                <TabViewList>
+                  <GeneralSection data={data} onAddReducer={this.handleAddReducer} />
+                  <TabView id="container-1">
+                    Container 1 Content
+                  </TabView>
+                  <TabView id="container-2">
+                    Container 2 Content
+                  </TabView>
+                </TabViewList>
+            </Tabs>
+          </form>
         </div>
         <div className={jsonEditorPlaceholderClasses} />
         <div className={jsonEditorClasses}>
@@ -71,12 +112,24 @@ class NewCreateServiceModalForm extends React.Component {
             showPrintMargin={false}
             theme="monokai"
             height="100%"
-            value={this.state.jsonValue}
-            width="100%"/>
+            value={JSON.stringify(data, null, 2)}
+            width="100%" />
         </div>
       </div>
     );
   }
 }
+
+NewCreateServiceModalForm.defaultProps = {
+  data: {},
+  onAddReducer() {},
+  onChange() {}
+};
+
+NewCreateServiceModalForm.propTypes = {
+  data: React.PropTypes.object,
+  onAddReducer: React.PropTypes.func,
+  onChange: React.PropTypes.func
+};
 
 module.exports = NewCreateServiceModalForm;
--- src/js/components/form/GeneralSection.js
+++ src/js/components/form/GeneralSection.js
@@ -1,109 +1,161 @@
 import React, {Component} from 'react';
 
+import TabView from '../TabView';
+import ReducerUtil  from '../../utils/ReducerUtil';
+
 class GeneralSection extends Component {
+  componentWillMount() {
+    this.props.onAddReducer({
+      id: ReducerUtil.simpleReducer('id', '/'),
+      cpus: ReducerUtil.simpleReducer('cpus', 1),
+      mem: ReducerUtil.simpleReducer('mem', 128),
+      disk: ReducerUtil.simpleReducer('disk', 0),
+      instances: ReducerUtil.simpleReducer('instances', 1),
+      cmd: ReducerUtil.simpleReducer('cmd', '')
+    });
+  }
+
   render() {
-    let data = this.props.data;
+    let {data} = this.props;
+
     return (
+      <TabView id="services">
         <div className="form flush-bottom">
-          <div className="form-row-element"><h2
-              className="form-header flush-top short-bottom">
-            General</h2><p className="flush-bottom"><span><span>Configure your container service here or </span><a
-              href="#/universe">install from Universe</a><span>.</span></span>
-          </p></div>
+          <div className="form-row-element">
+            <h2 className="form-header flush-top short-bottom">
+              General
+            </h2>
+            <p className="flush-bottom">
+                <span>Configure your container service here or </span>
+                <a href="#/universe">install from Universe</a>.
+            </p>
+          </div>
           <div className="row flex-box general">
             <div className="form-row-element column-12 form-row-input">
               <div className="form-group flush">
-                <label><span
-                    className="media-object-spacing-wrapper media-object-spacing-narrow"><div
-                    className="media-object media-object-inline"><span
-                    className="media-object-item">ID *</span><div
-                    className="tooltip-wrapper media-object-item"><svg
-                    className="icon icon-grey icon-mini"><use
-                    href="#icon-mini--ring-question"/></svg><noscript></noscript></div></div></span></label><input
-                  className="form-control" name="id" placeholder="" type="text"
+                <label>
+                  <span className="media-object-spacing-wrapper media-object-spacing-narrow">
+                    <div className="media-object media-object-inline">
+                      <span className="media-object-item">ID *</span>
+                      <div className="tooltip-wrapper media-object-item">
+                        <svg className="icon icon-grey icon-mini">
+                          <use href="#icon-mini--ring-question" />
+                        </svg>
+                      </div>
+                    </div>
+                  </span>
+                </label>
+                <input
+                  className="form-control"
+                  name="id"
+                  placeholder=""
+                  type="text"
                   onChange={()=>{}}
-                  value={data.id}/>
+                  value={data.id} />
               </div>
             </div>
           </div>
           <div className="row flex-box general">
             <div className="form-row-element column-3 form-row-input">
               <div className="form-group flush">
-                <label><span
-                    className="media-object-spacing-wrapper media-object-spacing-narrow"><div
-                    className="media-object media-object-inline"><span
-                    className="media-object-item">CPUs</span><div
-                    className="tooltip-wrapper media-object-item"><svg
-                    className="icon icon-grey icon-mini"><use
-                    href="#icon-mini--ring-question"/></svg><noscript></noscript></div></div></span></label><input
-                  className="form-control" name="cpus" placeholder=""
+                <label>
+                  <span className="media-object-spacing-wrapper media-object-spacing-narrow">
+                    <div className="media-object media-object-inline">
+                      <span className="media-object-item">CPUs</span>
+                      <div className="tooltip-wrapper media-object-item">
+                        <svg className="icon icon-grey icon-mini">
+                          <use href="#icon-mini--ring-question" />
+                        </svg>
+                      </div>
+                    </div>
+                  </span>
+                </label>
+                <input
+                  className="form-control"
+                  name="cpus"
+                  placeholder=""
                   type="number"
                   onChange={()=>{}}
-                  value={data.cpus}/>
+                  value={data.cpus} />
               </div>
             </div>
             <div className="form-row-element column-3 form-row-input">
               <div className="form-group flush">
-                <label className="">Memory
-                  (MiB)</label><input className="form-control" name="mem"
-                  placeholder="" type="number"
+                <label className="">
+                  Memory (MiB)
+                </label>
+                <input
+                  className="form-control"
+                  name="mem"
+                  placeholder=""
+                  type="number"
                   onChange={()=>{}}
-                  value={data.mem}/>
+                  value={data.mem} />
               </div>
             </div>
             <div className="form-row-element column-3 form-row-input">
               <div className="form-group flush">
-                <label className=""
-                >Disk
-                  (MiB)</label><input className="form-control" name="disk"
+                <label className="">Disk (MiB)</label>
+                <input className="form-control"
+                  name="disk"
                   placeholder="" type="number"
                   onChange={()=>{}}
-                  value={data.disk}/>
+                  value={data.disk} />
               </div>
             </div>
-            <div className="form-row-element column-3 form-row-input"
-            >
-              <div className="form-group flush"
-              >
-                <label className=""
-                >Instances</label><input
-                  className="form-control" name="instances" placeholder=""
+            <div className="form-row-element column-3 form-row-input">
+              <div className="form-group flush">
+                <label className="">Instances</label>
+                <input
+                  className="form-control"
+                  name="instances"
+                  placeholder=""
                   type="number"
-                  onChange={()=>{console.log('first level');}}
-                  value={data.instances}/>
+                  onChange={()=>{}}
+                  value={data.instances} />
               </div>
             </div>
           </div>
           <div className="row flex-box general">
             <div className="form-row-element column-12 form-row-input">
               <div className="form-group flush">
-                <label><span
-                    className="media-object-spacing-wrapper media-object-spacing-narrow"><div
-                    className="media-object media-object-inline"><span
-                    className="media-object-item">Command</span><div
-                    className="tooltip-wrapper media-object-item"><svg
-                    className="icon icon-grey icon-mini"><use
-                    href="#icon-mini--ring-question"/></svg><noscript></noscript></div></div></span></label>
+                <label>
+                  <span className="media-object-spacing-wrapper media-object-spacing-narrow">
+                    <div className="media-object media-object-inline">
+                      <span className="media-object-item">Command</span>
+                      <div className="tooltip-wrapper media-object-item">
+                        <svg className="icon icon-grey icon-mini">
+                          <use href="#icon-mini--ring-question" />
+                        </svg>
+                      </div>
+                    </div>
+                  </span>
+                </label>
                 <div className="content-editable-wrapper" style={{height:'100px'}}>
-                  <textarea className="content-editable form-control" name="cmd"
-                      placeholder="" type="textarea"
-                      value={data.cmd}
-                      onChange={()=>{}}
-                      style={{minHeight:'100px'}}/>
+                  <textarea
+                    className="content-editable form-control"
+                    name="cmd"
+                    placeholder=""
+                    type="textarea"
+                    value={data.cmd}
+                    onChange={()=>{}}
+                    style={{minHeight:'100px'}} />
                 </div>
               </div>
             </div>
           </div>
         </div>
+      </TabView>
     );
   }
 }
 
 GeneralSection.defaultProps = {
-  data: {id: ''}
+  onAddReducer: (a) => a
 };
 
 GeneralSection.propTypes = {
+  onAddReducer: React.PropTypes.func,
   data: React.PropTypes.object
 };
 
