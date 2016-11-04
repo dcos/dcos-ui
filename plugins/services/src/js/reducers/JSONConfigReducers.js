0e74372a257e0edeaed25ac9fc0afbd12717632d
Philipp Hinrichsen <philipph@mesosphere.io>
2016-11-04 18:42:53 +0100

    Add JSON reducers for environment var and labels

--- plugins/services/src/js/reducers/JSONConfigReducers.js
+++ plugins/services/src/js/reducers/JSONConfigReducers.js
@@ -3,6 +3,11 @@ import {
   simpleIntReducer,
   simpleReducer
 } from '../../../../../src/js/utils/ReducerUtil';
+import {
+  SET,
+  ADD_ROW,
+  REMOVE_ROW
+} from '../../../../../src/js/constants/TransactionTypes';
 
 module.exports = {
   id: simpleReducer('id', '/'),
@@ -10,5 +15,115 @@ module.exports = {
   mem: simpleIntReducer('mem', 128),
   disk: simpleIntReducer('disk', 0),
   instances: simpleIntReducer('instances', 1),
-  cmd: simpleReducer('cmd', '')
+  cmd: simpleReducer('cmd', ''),
+  env(state, {type, path, value}) {
+    if (this.env == null) {
+      this.env = [];
+
+      if (state != null) {
+        this.env = Object.keys(state).reduce((memo, key) => {
+          memo.push({
+            key: key.toUpperCase(),
+            value: state[key]
+          });
+          return memo;
+        }, []);
+      }
+    }
+
+    if (path != null && path.join('.').search('env') !== -1) {
+      if (path.join('.') === 'env') {
+        switch (type) {
+          case ADD_ROW:
+            this.env.push({key: null, value: null});
+            break;
+          case REMOVE_ROW:
+            this.env = this.env.filter((item, index) => {
+              return index !== value;
+            });
+            if (this.env.length === 0) {
+              this.env.push({key: null, value: null});
+            }
+            break;
+        }
+
+        return this.env.reduce((memo, item) => {
+          if (item.key != null || item.value != null) {
+            memo[item.key] = item.value;
+          }
+          return memo;
+        }, {});
+      }
+
+      const joinedPath = path.join('.');
+      const index = joinedPath.match(/\d+/)[0];
+      switch (true) {
+        case (type === SET && `env.${index}.key` === joinedPath):
+          this.env[index].key = value;
+          break;
+        case (type === SET && `env.${index}.value` === joinedPath):
+          this.env[index].value = value;
+          break;
+      }
+    }
+    return this.env.reduce((memo, item) => {
+      memo[item.key] = item.value;
+      return memo;
+    }, {});
+  },
+  labels(state, {type, path, value}) {
+    if (this.labels == null) {
+      this.labels = [];
+
+      if (state != null) {
+        this.labels = Object.keys(state).reduce((memo, key) => {
+          memo.push({
+            key: key.toUpperCase(),
+            value: state[key]
+          });
+          return memo;
+        }, []);
+      }
+    }
+
+    if (path != null && path.join('.').search('labels') !== -1) {
+      if (path.join('.') === 'labels') {
+        switch (type) {
+          case ADD_ROW:
+            this.labels.push({key: null, value: null});
+            break;
+          case REMOVE_ROW:
+            this.labels = this.labels.filter((item, index) => {
+              return index !== value;
+            });
+            if (this.labels.length === 0) {
+              this.labels.push({key: null, value: null});
+            }
+            break;
+        }
+
+        return this.labels.reduce((memo, item) => {
+          if (item.key != null || item.value != null) {
+            memo[item.key] = item.value;
+          }
+          return memo;
+        }, {});
+      }
+
+      const joinedPath = path.join('.');
+      const index = joinedPath.match(/\d+/)[0];
+      switch (true) {
+        case (type === SET && `labels.${index}.key` === joinedPath):
+          this.labels[index].key = value;
+          break;
+        case (type === SET && `labels.${index}.value` === joinedPath):
+          this.labels[index].value = value;
+          break;
+      }
+    }
+    return this.labels.reduce((memo, item) => {
+      memo[item.key] = item.value;
+      return memo;
+    }, {});
+  }
 };
