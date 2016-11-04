3738c45b8509d816810a29c7a3eeb1da07216c92
Philipp Hinrichsen <philipph@mesosphere.io>
2016-11-04 18:42:16 +0100

    Add parsers for labels and environment variables

--- plugins/services/src/js/reducers/JSONParserReducers.js
+++ plugins/services/src/js/reducers/JSONParserReducers.js
@@ -1,4 +1,10 @@
 import {simpleParser} from '../../../../../src/js/utils/ParserUtil';
+import {
+  ADD_ROW,
+  SET
+} from '../../../../../src/js/constants/TransactionTypes';
+
+import Transaction from '../../../../../src/js/structs/Transaction';
 
 module.exports = [
   simpleParser(['id']),
@@ -6,5 +12,39 @@ module.exports = [
   simpleParser(['mem']),
   simpleParser(['disk']),
   simpleParser(['instances']),
-  simpleParser(['cmd'])
+  simpleParser(['cmd']),
+  function (state) {
+    return Object.keys(state.env).reduce(function (memo, key, index) {
+      memo.push(new Transaction(['env'], index, ADD_ROW));
+      memo.push(new Transaction([
+        'env',
+        index,
+        'key'
+      ], key, SET));
+      memo.push(new Transaction([
+        'env',
+        index,
+        'value'
+      ], state.env[key], SET));
+
+      return memo;
+    }, []);
+  },
+  function (state) {
+    return Object.keys(state.labels).reduce(function (memo, key, index) {
+      memo.push(new Transaction(['labels'], index, ADD_ROW));
+      memo.push(new Transaction([
+        'labels',
+        index,
+        'key'
+      ], key, SET));
+      memo.push(new Transaction([
+        'labels',
+        index,
+        'value'
+      ], state.labels[key], SET));
+
+      return memo;
+    }, []);
+  }
 ];
