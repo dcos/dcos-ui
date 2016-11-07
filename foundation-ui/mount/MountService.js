628ca2684f6adf91c3d10bfeb003335a00ab73fe
Orlando Hohmeier <orlando@mesosphere.io>
2016-11-07 16:15:12 +0100

    Simplify mount service implementation

--- foundation-ui/mount/MountService.js
+++ foundation-ui/mount/MountService.js
@@ -1,34 +1,25 @@
 import {EventEmitter} from 'events';
-
-let componentStore = {};
+import {CHANGE} from './MountEvent';
 
 /**
- * MountService relies on a single instance of Hooks.
- * It utilses the hooks actions to notify MountPoints of changes and
- * filters to allow external packages to register for and change the content
- * output in the MountPoint
- *
- * @example
- * User of service would listen for updates and possibly change it
- * import {MountService} from 'foundation-ui/mount';
- * MountService.register('myID', 'myNewContent');
- *
- * MountPoint would register itself with the MountService to be able to get
- * updates on registered components
- * import {MountService} from 'foundation-ui/mount';
- * function updateContent(components) {
- *   // Do something with components
- * }
- * MountService.addListener('myID', updateContent);
+ * List of component descriptors
+ * @type {Array.<{component:function, priority:Number, role:String}>}
+ */
+const components = [];
+
+/**
+ * MountService
  */
 class MountService extends EventEmitter {
 
   /**
-   * Registers package for updates on this MountPoint id
-   * @param  {String} role of MointPoint to register package for
-   * @param  {React.Component|function} component to call when content is being fetched
-   * @param  {Number} priority to register package callback with
-   * [-Infinity; Infinity]
+   * Register component with role and optional priority
+   *
+   * @param {String} role - String which is used to match components with the
+   * respective mounts
+   * @param {function} component - React component which will be
+   * mounted based on the provided role
+   * @param {Number} priority - Number which is used to sort the components
    */
   registerComponent(role, component, priority) {
     if (typeof component !== 'function') {
@@ -48,60 +39,30 @@ class MountService extends EventEmitter {
       return;
     }
 
-    let components = componentStore[role];
-    if (!components) {
-      components = componentStore[role] = [];
-    }
-    components.push({priority, component});
-    components.sort(function (a, b) {
-      return a.priority - b.priority;
-    });
-    // Call all actions registered for id.
-    // This will call an action for all MountPoints with this id
-    // and have them update state by running a new filter
-    // (which will include the newly added filter)
-    let elements = components.map(function (item) {
-      return item.component;
-    });
-    this.emit(role, elements);
+    // Add component descriptor and sort list of components
+    components.push({component, priority, role});
+    components.sort((a, b) => a.priority - b.priority);
+
+    this.emit(CHANGE, role);
   }
 
   /**
-   * Unregisters package for updates on this MountPoint id
-   * @param  {String} role listener is registered with
-   * @param  {React.Component} component registered with listener
+   * Unregisters component by matching role and component
+   *
+   * @param  {String} role
+   * @param  {React.Component} component
    */
   unregisterComponent(role, component) {
-    let components = componentStore[role];
-    if (!components) {
-      return;
+    let i = components.length;
+    while (--i >= 0) {
+      if (components[i].component === component &&
+          components[i].role === role) {
+        components.splice(i, 1);
+        break;
+      }
     }
 
-    // Find and remove component from component list at given mount id
-    let {length} = components;
-    let index = components.findIndex(function (item) {
-      return item.component === component;
-    });
-    while (index > -1) {
-      components.splice(index, 1);
-      index = components.indexOf(listener, index);
-    }
-
-    // Check if there was a change
-    if (components.length === length) {
-      return;
-    }
-
-    // Re-sort the components
-    components.sort(function (a, b) {
-      return a.priority - b.priority;
-    });
-
-    // Have MountPoints run filter again so removed filter will be excluded
-    let elements = components.map(function (item) {
-      return item.component;
-    });
-    this.emit(role, elements);
+    this.emit(CHANGE, role);
   }
 }
 
