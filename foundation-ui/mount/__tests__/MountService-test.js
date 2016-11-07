140de0e192feaadf24c61528c7fc0eefc9bc3d31
Orlando Hohmeier <orlando@mesosphere.io>
2016-11-07 14:21:16 +0100

    Add test to verify component type conditions

--- foundation-ui/mount/__tests__/MountService-test.js
+++ foundation-ui/mount/__tests__/MountService-test.js
@@ -3,5 +3,49 @@ jest.dontMock('../MountService');
 /* eslint-disable no-unused-vars */
 const React = require('react');
 /* eslint-enable no-unused-vars */
+const MountService = require('../MountService');
 
-describe('MountService', function () {});
+describe('MountService', function () {
+  class ReactComponent extends React.Component {};
+  const FunctionalComponent = function () {};
+
+  describe('registerComponent', function () {
+
+    it('should not throw if a valid React.Component is provided', function () {
+      expect(function () {
+        MountService.registerComponent('register-test', ReactComponent, 0);
+      }).not.toThrow();
+    });
+
+    it('should not throw if a valid stateless functional component is provided',
+        function () {
+          expect(function () {
+            MountService
+                .registerComponent('register-test', FunctionalComponent, 0);
+          }).not.toThrow();
+        }
+    );
+
+    it('should throw if an object instead of a component was provided',
+        function () {
+          expect(function () {
+            MountService.registerComponent('register-test', {}, 0);
+          }).toThrow();
+        }
+    );
+
+    it('should throw if null instead of a component was provided', function () {
+      expect(function () {
+        MountService.registerComponent('register-test', null, 0);
+      }).toThrow();
+    });
+
+    it('should throw if component is undefined', function () {
+      expect(function () {
+        MountService.registerComponent('register-test', undefined, 0);
+      }).toThrow();
+    });
+
+  });
+
+});
