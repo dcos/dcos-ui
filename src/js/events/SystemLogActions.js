14140a50d058290fef23a3a07c910f2142b8b6dc
mlunoe <michael.lunoe@gmail.com>
2016-11-08 10:09:27 -0800

    Determining top loaded correctly

--- src/js/events/SystemLogActions.js
+++ src/js/events/SystemLogActions.js
@@ -7,6 +7,7 @@ import {
   REQUEST_PREVIOUS_SYSTEM_LOG_ERROR
 } from '../constants/ActionTypes';
 import AppDispatcher from './AppDispatcher';
+import CookieUtils from '../utils/CookieUtils';
 import SystemLogUtil from '../utils/SystemLogUtil';
 
 /**
@@ -19,20 +20,21 @@ let sources = {};
 const SystemLogActions = {
   subscribe(nodeID, options = {}) {
     let {
-      subscriptionID,
-      withCredentials = true
+      subscriptionID
     } = options;
 
     let url = SystemLogUtil.getUrl(nodeID, options);
     subscriptionID = subscriptionID || global.btoa(url);
-    // TODO: check user for credentials?
-    let source = new EventSource(url, {withCredentials});
+    let source = new EventSource(url, {
+      withCredentials: Boolean(CookieUtils.getUserMetadata())
+    });
 
-    source.addEventListener('message', function ({data, origin, target} = {}) {
-      if (origin !== global.location.origin && target.readyState !== EventSource.OPEN) {
+    source.addEventListener('message', function ({data, origin, eventPhase} = {}) {
+      if (origin !== global.location.origin && eventPhase !== EventSource.OPEN) {
         // Event is not from same origin, or is not open anymore
         return false;
       }
+
       let parsedData = JSON.parse(data);
       AppDispatcher.handleServerAction({
         type: REQUEST_SYSTEM_LOG_SUCCESS,
@@ -42,8 +44,6 @@ const SystemLogActions = {
     }, false);
 
     source.addEventListener('error', (event = {}) => {
-      let {target} = event;
-      let {cursor} = sources[subscriptionID];
       AppDispatcher.handleServerAction({
         type: REQUEST_SYSTEM_LOG_ERROR,
         data: RequestUtil.getErrorFromXHR(event),
@@ -67,45 +67,54 @@ const SystemLogActions = {
 
   fetchLogRange(nodeID, options = {}) {
     let {
-      limit = 0,
+      cursor,
       subscriptionID,
-      withCredentials = true
+      skip_prev
     } = options;
 
     let url = SystemLogUtil.getUrl(nodeID, options, false);
     subscriptionID = subscriptionID || global.btoa(url);
-    // TODO: check user for credentials?
-    let source = new EventSource(url, {withCredentials});
+    let source = new EventSource(url, {
+      withCredentials: Boolean(CookieUtils.getUserMetadata())
+    });
 
     let items = [];
+    let hasReachedTop = false;
 
-    source.addEventListener('message', function ({data, origin, target} = {}) {
-      if (origin !== global.location.origin) {
+    source.addEventListener('message', function ({data, origin, eventPhase} = {}) {
+      if (origin !== global.location.origin && eventPhase !== EventSource.OPEN) {
         // Event is not from same origin, or is not open anymore
         return false;
       }
-      if (target.readyState === EventSource.CLOSED) {
-        AppDispatcher.handleServerAction({
-          type: REQUEST_PREVIOUS_SYSTEM_LOG_SUCCESS,
-          data: items,
-          limit,
-          subscriptionID
-        });
-
-        source.close();
-      }
 
       let parsedData = JSON.parse(data);
-      items.push(parsedData);
+      if (!hasReachedTop && skip_prev > 0 && parsedData.cursor === cursor) {
+        hasReachedTop = true;
+      }
+      if (!hasReachedTop) {
+        items.push(parsedData);
+      }
     }, false);
 
     source.addEventListener('error', (event = {}) => {
-      source.close();
-      AppDispatcher.handleServerAction({
-        type: REQUEST_PREVIOUS_SYSTEM_LOG_ERROR,
-        data: RequestUtil.getErrorFromXHR(event),
-        subscriptionID
-      });
+      let {eventPhase} = event;
+      if (eventPhase === EventSource.CLOSED) {
+        source.close();
+        AppDispatcher.handleServerAction({
+          type: REQUEST_PREVIOUS_SYSTEM_LOG_SUCCESS,
+          data: items,
+          subscriptionID,
+          hasReachedTop
+        });
+      } else {
+        source.close();
+        AppDispatcher.handleServerAction({
+          type: REQUEST_PREVIOUS_SYSTEM_LOG_ERROR,
+          data: RequestUtil.getErrorFromXHR(event),
+          subscriptionID
+        });
+      }
+
     });
   }
 };
