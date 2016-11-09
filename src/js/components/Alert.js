1d79400cd8a25fb0726dc8ea45b40c8e9177a2b8
John Furrow <johnf@mesosphere.io>
2016-11-09 09:34:06 -0800

    Remove switch statement

--- src/js/components/Alert.js
+++ src/js/components/Alert.js
@@ -10,28 +10,14 @@ const Alert = ({children, showIcon, type}) => {
   let icon = null;
 
   if (showIcon) {
-    const className = 'alert-icon';
-    const family = 'mini';
-    const size = 'mini';
+    const ids = {
+      danger: 'ring-exclamation',
+      success: 'checkmark'
+    };
 
-    switch (type) {
-      case 'danger':
-        icon = (
-          <Icon className={className}
-            family={family}
-            id="ring-exclamation"
-            size={size} />
-          );
-        break;
-      case 'successs':
-        icon = (
-          <Icon className={className}
-            family={family}
-            id="checkmark"
-            size={size} />
-          );
-        break;
-    }
+    icon = (
+      <Icon className="alert-icon" family="mini" id={ids[type]} size="mini" />
+    );
   }
 
   return (
@@ -52,7 +38,7 @@ Alert.defaultProps = {
 Alert.propTypes = {
   children: React.PropTypes.node.isRequired,
   showIcon: React.PropTypes.bool,
-  type: React.PropTypes.oneOf(['danger', 'success'])
+  type: React.PropTypes.oneOf(['danger', 'success']).isRequired
 };
 
 module.exports = Alert;
