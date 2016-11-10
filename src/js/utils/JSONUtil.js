4ab026b00c664de82870714e1a9619d4f86b265f
Ioannis Charalampidis <ioannis.charalampidis@cern.ch>
2016-11-10 23:35:41 +0100

    JSONUtil: Fixing line spacing & Removed try/catch

--- src/js/utils/JSONUtil.js
+++ src/js/utils/JSONUtil.js
@@ -31,7 +31,7 @@ const TOKENIZER_TYPES = Object.keys( TOKENIZER_TOKENS );
  * @returns {array} - Returns an array with [ tokenType, tokenOffset ]
  */
 function nextJSONToken( chunk, offset ) {
-  return TOKENIZER_TYPES.reduce(function ( lastMatch, tokenName ) {
+  return TOKENIZER_TYPES.reduce(function (lastMatch, tokenName) {
     let tokenRx = TOKENIZER_TOKENS[tokenName];
 
     // Search for a token at given offset
@@ -104,229 +104,195 @@ module.exports = {
    */
   getObjectInformation(source) {
     let keyLines = [];
+    // Keeps track of the current line number
+    let lineNo = 1;
+    // The current path in the object trace
+    let path = [];
+    // Keeps track of last literal (number, string, null, boolean)
+    let lastLiteral;
+    // This variable is used only when the parser is within an array [ ],
+    // pointing to the current array index. Otherwise it's -1
+    let arrayIndex = -1;
+    // This variable keeps track of the current object key. It's used only
+    // when in an object { } and is populated only when a colon ':'' is encoutered
+    let objectKey = null;
 
-    // Get the javascript model for easier processing
-    try {
+    // First validate sanity of the JSON source, in order for us
+    // to safely asume that this is a legit JSON source
+    // (This will throw an exception)
+    JSON.parse(source);
 
-      // Keeps track of the current line number
-      let lineNo = 1;
-
-      // The current path in the object trace
-      let path = [];
-
-      // Keeps track of last literal (number, string, null, boolean)
-      let lastLiteral;
-
-      // This variable is used only when the parser is within an array [ ],
-      // pointing to the current array index. Otherwise it's -1
-      let arrayIndex = -1;
-
-      // This variable keeps track of the current object key. It's used only
-      // when in an object { } and is populated only when a colon ':'' is encoutered
-      let objectKey = null;
-
-      // First validate sanity of the JSON source, in order for us
-      // to safely asume that this is a legit JSON source
-      JSON.parse(source);
-
-      // Process tokens
-      for (let i=0, l=source.length; i<l; ) {
-
-        // Get next token or exit if there are no more
-        let [token, match, offset] = nextJSONToken( source, i );
-        if (!token) {
-          break;
-        }
-
-        // Keep current position and forward to next token
-        let curr = i;
-        i = offset + match.length;
-
-        // Count how many new lines we encountered since the
-        // last index and forward line number accordingly
-        lineNo += countNewLines( source, curr, i );
-
-        // Process token
-        switch (token) {
-
-          //
-          // The last literal before '{' is the object name.
-          // This action should add a level in the path
-          //
-          case 'begin-object':
-
-            // Keep track of keyed objects
-            if (objectKey) {
-              keyLines.push({
-                path: [].concat(path, objectKey),
-                line: lineNo,
-                type: 'object'
-              });
-
-              // Also track path
-              path.push(objectKey);
-              objectKey = null;
-            }
-
-            // Keep track of indexed objects
-            if (arrayIndex !== -1) {
-              keyLines.push({
-                path: [].concat(path, arrayIndex),
-                line: lineNo,
-                type: 'object'
-              });
-
-              // Also track path
-              path.push(arrayIndex);
-            }
-
-            // Wipe array index
-            arrayIndex = -1;
-
-            break;
-
-          //
-          // When we start an array, push a numeric component on path
-          //
-          case 'begin-array':
-
-            // Keep track of keyed objects
-            if (objectKey) {
-              keyLines.push({
-                path: [].concat(path, objectKey),
-                line: lineNo,
-                type: 'array'
-              });
-
-              // Also track path
-              path.push(objectKey);
-              objectKey = null;
-            }
-
-            // Keep track of indexed objects
-            if (arrayIndex !== -1) {
-              keyLines.push({
-                path: [].concat(path, arrayIndex),
-                line: lineNo,
-                type: 'array'
-              });
-
-              // Also track path
-              path.push(arrayIndex);
-            }
-
-            // Start array index
-            arrayIndex = 0;
-
-            break;
-
-          //
-          // When objects or arrays are ended, pop the last item
-          // and keep track of the array information
-          //
-          case 'end-object':
-          case 'end-array':
-
-            // Pop path and resume possible array tracking
-            let lastValue = path.pop();
-            if (typeof lastValue === 'number') {
-              arrayIndex = lastValue;
-            } else {
-              arrayIndex = -1;
-            }
-
-            break;
-
-          //
-          // A comma is handled only if we have an open array
-          // on the last path component. It should advance the
-          // index by one.
-          //
-          case 'comma':
-
-            // Advance array index
-            if (arrayIndex !== -1) {
-              arrayIndex++;
-            }
-
-            break;
-
-          //
-          // The moment we find a label ':', the last literal
-          // is the key to a property. Keep track of it in the path.
-          //
-          case 'end-label':
-
-            // Label after a literal defines an object key
-            if (lastLiteral) {
-              objectKey = lastLiteral;
-            }
-
-            break;
-
-          //
-          // String literals include their quotes,
-          // everything else is kept as-is
-          //
-          case 'string':
-            match = match.slice(1, -1);
-
-          /* eslint-disable no-fallthrough */
-          case 'maybe-string':
-          case 'number':
-          case 'boolean':
-          case 'null':
-          /* eslint-enable no-fallthrough */
-
-            // Cast tokens to their correct type
-            if (token === 'number') {
-              match = Number(match);
-            } else if (token === 'boolean') {
-              match = (match === 'true');
-            } else if (token === 'null') {
-              match = null;
-            }
-
-            // Keep last literal
-            lastLiteral = match;
-
-            // Keep track of keyed literals
-            if (objectKey) {
-              keyLines.push({
-                path: [].concat(path, objectKey),
-                line: lineNo,
-                value: match,
-                type: 'literal'
-              });
-              objectKey = null;
-            }
-
-            // Keep track of indexed literals
-            if (arrayIndex !== -1) {
-              keyLines.push({
-                path: [].concat(path, arrayIndex),
-                line: lineNo,
-                value: match,
-                type: 'literal'
-              });
-            }
-
-            break;
-
-        }
-
+    // Process tokens
+    for (let i=0, l=source.length; i<l; ) {
+      // Get next token or exit if there are no more
+      let [token, match, offset] = nextJSONToken( source, i );
+      if (!token) {
+        break;
       }
 
-    } catch (e) {
+      // Count how many new lines we encountered since the
+      // last index and forward line number accordingly
+      let curr = i;
+      i = offset + match.length;
+      lineNo += countNewLines( source, curr, i );
 
-      // Convert all exceptions to syntax errors
-      throw SyntaxError(`Parsing error: ${e.toString()}`);
+      // Process token
+      switch (token) {
+        //
+        // The last literal before '{' is the object name.
+        // This action should add a level in the path
+        //
+        case 'begin-object':
+          // Keep track of keyed objects
+          if (objectKey) {
+            keyLines.push({
+              path: [].concat(path, objectKey),
+              line: lineNo,
+              type: 'object'
+            });
+
+            path.push(objectKey);
+            objectKey = null;
+          }
+
+          // Keep track of indexed objects
+          if (arrayIndex !== -1) {
+            keyLines.push({
+              path: [].concat(path, arrayIndex),
+              line: lineNo,
+              type: 'object'
+            });
+
+            path.push(arrayIndex);
+          }
+
+          // Wipe array index
+          arrayIndex = -1;
+          break;
+
+        //
+        // When we start an array, push a numeric component on path
+        //
+        case 'begin-array':
+          // Keep track of keyed arrays
+          if (objectKey) {
+            keyLines.push({
+              path: [].concat(path, objectKey),
+              line: lineNo,
+              type: 'array'
+            });
+
+            path.push(objectKey);
+            objectKey = null;
+          }
+
+          // Keep track of indexed arrays
+          if (arrayIndex !== -1) {
+            keyLines.push({
+              path: [].concat(path, arrayIndex),
+              line: lineNo,
+              type: 'array'
+            });
+
+            path.push(arrayIndex);
+          }
+
+          // Start array index
+          arrayIndex = 0;
+          break;
+
+        //
+        // When objects or arrays are ended, pop the last item
+        // and keep track of the array information
+        //
+        case 'end-object':
+        case 'end-array':
+          // Pop path and resume possible array tracking
+          let lastValue = path.pop();
+          if (typeof lastValue === 'number') {
+            arrayIndex = lastValue;
+          } else {
+            arrayIndex = -1;
+          }
+          break;
+
+        //
+        // A comma is handled only if we have an open array
+        // on the last path component. It should advance the
+        // index by one.
+        //
+        case 'comma':
+          // Advance array index
+          if (arrayIndex !== -1) {
+            arrayIndex++;
+          }
+          break;
+
+        //
+        // The moment we find a label ':', the last literal
+        // is the key to a property. Keep track of it in the path.
+        //
+        case 'end-label':
+          // Label after a literal defines an object key
+          if (lastLiteral) {
+            objectKey = lastLiteral;
+          }
+          break;
+
+        //
+        // String literals include their quotes,
+        // everything else is kept as-is
+        //
+        case 'string':
+          match = match.slice(1, -1);
+
+        /* eslint-disable no-fallthrough */
+        case 'maybe-string':
+        case 'number':
+        case 'boolean':
+        case 'null':
+        /* eslint-enable no-fallthrough */
+
+          // Cast tokens to their correct type
+          if (token === 'number') {
+            match = Number(match);
+          } else if (token === 'boolean') {
+            match = (match === 'true');
+          } else if (token === 'null') {
+            match = null;
+          }
+
+          // Keep last literal
+          lastLiteral = match;
+
+          // Keep track of keyed literals
+          if (objectKey) {
+            keyLines.push({
+              path: [].concat(path, objectKey),
+              line: lineNo,
+              value: match,
+              type: 'literal'
+            });
+            objectKey = null;
+          }
+
+          // Keep track of indexed literals
+          if (arrayIndex !== -1) {
+            keyLines.push({
+              path: [].concat(path, arrayIndex),
+              line: lineNo,
+              value: match,
+              type: 'literal'
+            });
+          }
+          break;
+
+      }
 
     }
 
     // Return the key line numbers
     return keyLines;
-
   }
 
 };
