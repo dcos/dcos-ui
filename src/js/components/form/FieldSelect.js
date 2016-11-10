7e30a4ea49c8239bb41d38e1e636bfb965b7257e
Philipp Hinrichsen <philipph@mesosphere.io>
2016-11-10 17:13:52 +0100

    Add the FieldSelect Component

---
+++ src/js/components/form/FieldSelect.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const FieldSelect = (props) => {
+  return (
+    <span className="form-control form-control-select">
+      <select {...props}>
+        {props.children}
+      </select>
+    </span>
+  );
+};
+
+FieldSelect.defaultProps = {
+  name: '',
+  onChange() {},
+  value: ''
+};
+
+FieldSelect.propTypes = {
+  name: React.PropTypes.string,
+  onChange: React.PropTypes.func,
+  value: React.PropTypes.oneOfType([
+    React.PropTypes.number,
+    React.PropTypes.string
+  ])
+};
+
+module.exports = FieldSelect;
