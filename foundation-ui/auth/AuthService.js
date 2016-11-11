48ab0313d33fc7666c5f033861acfc7aa21bed2b
Orlando Hohmeier <orlando@mesosphere.io>
2016-11-11 17:00:14 +0100

    Create new auth service

---
+++ foundation-ui/auth/AuthService.js
@@ -0,0 +1,8 @@
+import {EventEmitter} from 'events';
+
+/**
+ * AuthService
+ */
+class AuthService extends EventEmitter {}
+
+module.exports = new AuthService();
---
+++ foundation-ui/auth/__tests__/AuthService-test.js
@@ -0,0 +1,5 @@
+describe('AuthService', function () {
+
+  it('init', function () {});
+
+});
