49ee477665fb5156d62d7bcf9fdc85193cc9508c
Orlando Hohmeier <orlando@mesosphere.io>
2016-11-11 17:07:53 +0100

    Add a method to register authorizer

--- foundation-ui/auth/AuthService.js
+++ foundation-ui/auth/AuthService.js
@@ -1,8 +1,36 @@
 import {EventEmitter} from 'events';
+import Authorizer from './Authorizer';
+
+/**
+ * List of authorizers
+ *
+ * @type {Array.<Authorizer>}
+ */
+const authorizers = [];
 
 /**
  * AuthService
  */
-class AuthService extends EventEmitter {}
+class AuthService extends EventEmitter {
+
+  /**
+   * Register authorizer
+   *
+   * @param {Authorizer} authorizer
+   */
+  registerAuthorizer(authorizer) {
+    if (!authorizer || !(authorizer instanceof Authorizer)) {
+      if (global.__DEV__) {
+        throw new TypeError('Provided component must be a ' +
+            'React.Component constructor or a stateless functional component.');
+      }
+
+      return;
+    }
+
+    authorizers.push(authorizer);
+  }
+
+}
 
 module.exports = new AuthService();
--- foundation-ui/auth/__tests__/AuthService-test.js
+++ foundation-ui/auth/__tests__/AuthService-test.js
@@ -1,5 +1,41 @@
-describe('AuthService', function () {
+jest.dontMock('../AuthService');
 
-  it('init', function () {});
+const AuthService = require('../AuthService');
+const Authorizer = require('../Authorizer');
+
+describe('AuthService', function () {
+  const authorizer = new Authorizer();
+
+  describe('registerAuthorizer', function () {
+
+    it('should not throw if a instance of Authorizer is provided',
+        function () {
+          expect(function () {
+            AuthService.registerAuthorizer(authorizer);
+          }).not.toThrow();
+        }
+    );
+
+    it('should throw if an object instead of a authorizer was provided',
+        function () {
+          expect(function () {
+            AuthService.registerAuthorizer({});
+          }).toThrow();
+        }
+    );
+
+    it('should throw if null instead of a authorizer was provided', function () {
+      expect(function () {
+        AuthService.registerAuthorizer(null);
+      }).toThrow();
+    });
+
+    it('should throw if authorizer is undefined', function () {
+      expect(function () {
+        AuthService.registerAuthorizer(undefined);
+      }).toThrow();
+    });
+
+  });
 
 });
