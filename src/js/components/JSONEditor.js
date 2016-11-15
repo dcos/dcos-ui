b962c510999ea9833d8239f5bc94509dcb1a3e68
Ioannis Charalampidis <johnys2@gmail.com>
2016-11-15 16:27:32 +0100

    JSONEditor: Introducing high-level component

---
+++ src/js/components/JSONEditor.js
@@ -0,0 +1,484 @@
+import AceEditor from 'react-ace';
+import React from 'react';
+import deepEqual from 'deep-equal';
+
+import {omit} from '../utils/Util';
+import JSONUtil from '../utils/JSONUtil';
+import JSONEditorUtil from '../utils/JSONEditorUtil';
+
+const METHODS_TO_BIND = [
+  'handleBlur',
+  'handleChange',
+  'handleEditorLoad',
+  'handleWindowResize',
+  'handleFocus'
+];
+
+/**
+ * How long to wait before clearing the `isTyping` flag, used internally to
+ * prohibit external updates to the editor while the user is typing.
+ *
+ * @const {number}
+ */
+const ISTYPING_TIMEOUT = 2000;
+
+/**
+ * This is a high-order component on top of `AceEditor` that abstracts the JSON
+ * string manipulation and allows object-level access to the underlaying data.
+ *
+ * It's features are:
+ *
+ * - Solve some bugs and peculiarities of AceEditor component
+ * - Maintain visual coherence between object updates
+ * - Callback on per-property updates as the user types
+ * - Receive errors through properties and show them in the gutter
+ *
+ * @example <caption>How to use the JSONEditor</caption>
+ *
+ * handleChange(newObject) {
+ *   console.log('New object:', newObject);
+ * }
+ *
+ * handlePropertyChange(path, value) {
+ *   console.log('Property /'+path.join('/'), 'changed to', value);
+ * }
+ *
+ * render() {
+ *   let value = {
+ *     some: {value: 'object'}
+ *   };
+ *   let errors = [
+ *     {path: ['some'], message: 'An error on object `some`'}
+ *   ];
+ *
+ *   return (
+ *     <JSONEditor
+ *       value={value}
+ *       errors={errors}
+ *       onChange={this.handleChange}
+ *       onPropertyChange={this.handlePropertyChange}
+ *       />
+ *   );
+ * };
+ *
+ */
+class JSONEditor extends React.Component {
+  constructor() {
+    super(...arguments);
+
+    // We are using the react-way of updating the component **only** when we
+    // need to define a new text to work upon (ex. when the owner component has
+    // updated the value).
+    //
+    // Updating the AceEditor on every render cycle seems to cause some trouble
+    // to it's internals, that I couldn't pinpoint yet.
+    this.state = {
+      initialText: ''
+    };
+
+    //
+    // The following properties are part of the `internal`, non-react state
+    // and is synchronized with the react through `componentWillReceiveProps`
+    //
+    this.externalErrors = [];
+    this.jsonError = null;
+    this.jsonMeta = [];
+    this.jsonText = '{}';
+    this.jsonValue = {};
+
+    //
+    // The following properties are flags and references used for other purposes
+    //
+    this.aceEditor = null;
+    this.isFocused = false;
+    this.isTyping = false;
+    this.timerIsTyping = null;
+
+    METHODS_TO_BIND.forEach((method) => {
+      this[method] = this[method].bind(this);
+    });
+  }
+
+  /**
+   * @override
+   */
+  componentWillReceiveProps(nextProps) {
+    // Synchronise error updates
+    if (!deepEqual(this.externalErrors, nextProps.errors)) {
+      this.externalErrors = nextProps.errors;
+      this.updateEditorState();
+    }
+
+    // Ignore invalid values
+    if (typeof nextProps.value !== 'object') {
+      return;
+    }
+
+    // Ignore property changes if the user is currently using the editor field
+    if (this.isTyping && this.isFocused) {
+      return;
+    }
+
+    // If this update originates from an onChange -> prop={value} loop, the
+    // object defined in the `internalValue` property should be equal to the
+    // `value` property we just received. In this case, don't update anything.
+    if (deepEqual(this.jsonValue, nextProps.value)) {
+      return;
+    }
+
+    // If we reached this point, the owning component has actually sent us a new
+    // json object that we must display. For visual coherence we must try our
+    // best to properly display the new values without breaking the JSON text
+    // arrangement.
+
+    // Align the order the properties appear & calculate new JSON text
+    let value = JSONEditorUtil.sortObjectKeys(this.jsonValue, nextProps.value);
+    let composedText = JSON.stringify(value, null, '\t');
+
+    // Update local state with the new, computed text
+    this.updateLocalJsonState(composedText);
+    this.setState({initialText: composedText});
+  }
+
+  /**
+   * Since ACEEditor component does not behave nicely when we update the text
+   * as a response to an `onChange` event, we must be very careful never to
+   * render the ACEeditor when the user types.
+   *
+   * This way, the component is updated *only* when:
+   *
+   * - Editor-related properties change (ex. editorProps, width, height)
+   * - The text in the editor has to be replaced due to an external value change
+   *
+   * @override
+   */
+  shouldComponentUpdate(nextProps, nextState) {
+    // Update if ACE editor properties are changed
+    if (!deepEqual(nextProps.editorProps, this.props.editorProps) ||
+        (nextProps.height !== this.props.height) ||
+        (nextProps.width !== this.props.width)) {
+      return true;
+    }
+
+    // Otherwise update ONLY when initial value changes in state
+    return nextState.initialText !== this.state.initialText;
+  }
+
+  /**
+   * @override
+   */
+  componentDidUpdate() {
+    this.updateEditorState();
+  }
+
+  // -- BEGIN HACK -------------------------------------------------------------
+  //
+  // This tries to undo an issue caused by a `transform` attribute in the CSS
+  // chain, that makes Ace editor miscalculate the viewport origin and
+  // therefore render the tooltip outside the viewable region.
+  //
+  // TODO: Properly solve this in CSS
+  //
+  /**
+   * @override
+   */
+  componentDidMount() {
+    window.addEventListener('resize', this.handleWindowResize);
+  }
+
+  /**
+   * @override
+   */
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowResize);
+  }
+
+  handleWindowResize() {
+    if (!this.aceEditor) {
+      return;
+    }
+
+    let editorRect = this.aceEditor.container.getBoundingClientRect();
+    let tooltip = this.aceEditor.container.querySelector('.ace_tooltip');
+    if (!tooltip) {
+      return;
+    }
+
+    tooltip.style.transform =
+      tooltip.style.webkitTransform =
+      tooltip.style.mozTransform =
+      tooltip.style.msTransform =
+      `translateX(-${editorRect.left}px) translateY(-${editorRect.top}px)`;
+  }
+  // -- END HACK ---------------------------------------------------------------
+
+  handleEditorLoad(editor) {
+    this.aceEditor = editor;
+    window.editor = editor;
+
+    // Disable syntax highlighing worker, since we are responsible for feeding
+    // the correct syntax error + provided error markers
+    editor.getSession().setUseWorker(false);
+
+    // Synchronise editor state
+    this.updateEditorState();
+
+    // -- BEGIN HACK -----------------------------------------------------------
+    //
+    // This tries to undo an issue caused by a `transform` attribute in the CSS
+    // chain, that makes Ace editor miscalculate the viewport origin and
+    // therefore render the tooltip outside the viewable region.
+    //
+    // TODO: Properly solve this in CSS
+    //
+    let tooltipPoller = setInterval(() => {
+      let tooltip = this.aceEditor.container
+        .getElementsByClassName('ace_tooltip');
+      if (tooltip.length === 0) return;
+
+      // When the toltip appears in DOM, stop the poller and re-align it
+      clearInterval(tooltipPoller);
+      this.handleWindowResize();
+    }, 10);
+    //
+    // -- END HACK -------------------------------------------------------------
+
+  }
+
+  /**
+   * `onBlur` handler for the AceEditor
+   *
+   * @param {DOMEvent} event - The event object
+   */
+  handleBlur(event) {
+    this.props.onBlur(event);
+    this.isFocused = false;
+  }
+
+  /**
+   * `onChange` handler for the AceEditor
+   *
+   * @param {string} jsonText - The new JSON string
+   */
+  handleChange(jsonText) {
+    let lastValue = this.jsonValue;
+    let lastError = this.jsonError;
+
+    // Calculate what the next state is going to be
+    this.updateLocalJsonState(jsonText);
+
+    // Handle errors
+    if (lastError !== this.jsonError) {
+      this.props.onErrorStateChange(this.jsonError);
+    }
+
+    // Update the `isTyping` flag
+    this.isTyping = true;
+    this.scheduleIsTypingReset(ISTYPING_TIMEOUT);
+
+    // If we have errors don't continue with updating the local structures
+    if (this.jsonError) {
+      return;
+    }
+
+    // Calculate differences in the JSON and trigger `onPropertyChange`
+    // event for every property that changed in the JSON
+    let diff = JSONEditorUtil.deepObjectDiff(lastValue, this.jsonValue);
+    diff.forEach((diff) => {
+      this.props.onPropertyChange(diff.path, diff.value);
+    });
+
+    // Trigger change with the latest json object
+    this.props.onChange(this.jsonValue);
+  }
+
+  /**
+   * `onFocus` handler for the AceEditor
+   *
+   * @param {DOMEvent} event - The event object
+   */
+  handleFocus(event) {
+    this.props.onFocus(event);
+    this.isFocused = true;
+  }
+
+  /**
+   * Apply pending state updates to the ACE editor instance.
+   *
+   * This function operates purely on the `aceEditor` instance, created by the
+   * `AceEditor` composed and exposed through the `onLoad` event. Since the
+   * `AceEditor` component does a pretty bad job updating only what's needed,
+   * we had to use this approach and avoid re-rendering the component as much
+   * as possible.
+   */
+  updateEditorState() {
+    if (!this.aceEditor) {
+      return;
+    }
+
+    // Prepare markers
+    let markers = [];
+
+    // Extract syntax errors, or other errors that refer to line
+    if (this.jsonError) {
+
+      // Strip out the 'at line xxx' message, and keep track of that line
+      let errorLine = 0;
+      let errorMsg = this.jsonError.replace(/at line ([\d:]+)/g, function (m, line) {
+        errorLine = parseInt(line.split(':')[0]);
+        return '';
+      });
+
+      // Push the error marker
+      markers.push({
+        row: errorLine,
+        text: errorMsg,
+        type: 'error'
+      });
+
+    } else {
+
+      // Append annotation markers from external errors
+      markers = this.externalErrors.reduce((memo, error) => {
+        let errorPath = error.path.join('.');
+        let token = this.jsonMeta.find(function (token) {
+          return token.path.join('.') === errorPath;
+        });
+
+        if (!token) {
+          return memo;
+        }
+
+        memo.push({
+          row: token.line-1,
+          text: error.message,
+          type: 'error'
+        });
+
+        return memo;
+      }, markers);
+
+    }
+
+    // Merge current annotations with the annotations we are going to show
+    this.aceEditor.getSession().setAnnotations(markers);
+  }
+
+  /**
+   * This function assigns the local state of the JSON text, outside the React
+   * state, since this does not nesseceraly trigger an update.
+   *
+   * This is useful in order to have the latest available metainformation when
+   * the user changes the text (on the `handleChange` handler), but we don't
+   * want to re-do the processing when this will eventually trigger an update
+   * to the `value` property with the same data.
+   *
+   * @param {String} jsonText - The JSON buffer to extract metadata from
+   */
+  updateLocalJsonState(jsonText) {
+    // Do not perform heavyweight calculations, such as `getObjectInformation`
+    // if the json text hasn't really changed.
+    if (this.jsonText === jsonText) {
+      return;
+    }
+
+    // Reset some properties
+    this.jsonText = jsonText;
+    this.jsonError = null;
+
+    // Try to parse and extract metadata
+    try {
+      this.jsonValue = JSON.parse(jsonText);
+      this.jsonMeta = JSONUtil.getObjectInformation(jsonText).reverse();
+    } catch (e) {
+      // Prettify the error message by resolving the line/column instead of
+      // just keeping the offset in the string
+      let errorStr = e.toString();
+      this.jsonError = errorStr.replace(/at position (\d+)/g, (match, offset) => {
+        let cursor = JSONEditorUtil.cursorFromOffset(parseInt(offset), jsonText);
+        return `at line ${cursor.row}:${cursor.column}`;
+      });
+    }
+
+    // Sync editor without a render cycle
+    this.updateEditorState();
+  }
+
+  /**
+   * Schedule the `isTyping` state property to set back to `false` if this
+   * function is not called within the given timeout.
+   *
+   * @param {number} timeout - The timeout (in millisecond) on which to clear
+   *                 the flag
+   */
+  scheduleIsTypingReset(timeout) {
+    if (this.timerIsTyping) {
+      clearTimeout(this.timerIsTyping);
+    }
+    this.timerIsTyping = setTimeout(() => {
+      this.isTyping = false;
+    }, timeout);
+  }
+
+  /**
+   * @override
+   */
+  render() {
+    let {width, height, editorProps} = this.props;
+    let {initialText} = this.state;
+
+    let omitKeys = [].concat(
+      Object.keys(JSONEditor.propTypes),
+      'mode'
+    );
+
+    return (
+      <AceEditor
+        {...omit(this.props, omitKeys)}
+        width={width}
+        height={height}
+        editorProps={editorProps}
+        mode="json"
+        onBlur={this.handleBlur}
+        onChange={this.handleChange}
+        onFocus={this.handleFocus}
+        onLoad={this.handleEditorLoad}
+        value={initialText}
+        />
+    );
+  }
+};
+
+JSONEditor.defaultProps = {
+  errors: [],
+  editorProps: {$blockScrolling: Infinity},
+  height: '100%',
+  onBlur() {},
+  onChange() {},
+  onErrorStateChange() {},
+  onFocus() {},
+  onPropertyChange() {},
+  value: {},
+  width: '100%'
+};
+
+JSONEditor.propTypes = {
+  errors: React.PropTypes.array,
+  editorProps: React.PropTypes.object,
+  height: React.PropTypes.oneOfType([
+    React.PropTypes.string,
+    React.PropTypes.number
+  ]),
+  onBlur: React.PropTypes.func,
+  onChange: React.PropTypes.func,
+  onErrorStateChange: React.PropTypes.func,
+  onFocus: React.PropTypes.func,
+  onPropertyChange: React.PropTypes.func,
+  value: React.PropTypes.object,
+  width: React.PropTypes.oneOfType([
+    React.PropTypes.string,
+    React.PropTypes.number
+  ])
+};
+
+module.exports = JSONEditor;
---
+++ src/js/utils/__tests__/JSONEditorUtil-test.js
@@ -0,0 +1,301 @@
+jest.dontMock('../JSONEditorUtil');
+
+const JSONEditorUtil = require('../JSONEditorUtil');
+
+describe('JSONEditorUtil', function () {
+
+  describe('#cursorFromOffset', function () {
+
+    it('should properly resolve beginning edge', function () {
+      let text = 'some oneline text';
+      expect(JSONEditorUtil.cursorFromOffset(0, text)).toEqual({
+        row: 0, column: 0
+      });
+    });
+
+    it('should properly resolve ending edge', function () {
+      let text = 'some oneline text';
+      expect(JSONEditorUtil.cursorFromOffset(16, text)).toEqual({
+        row: 0, column: 16
+      });
+    });
+
+    it('should ignore negative offsets', function () {
+      let text = 'some oneline text';
+      expect(JSONEditorUtil.cursorFromOffset(-1, text)).toEqual({
+        row: 0, column: 0
+      });
+    });
+
+    it('should ignore offsets exceeding string length', function () {
+      let text = 'some oneline text';
+      expect(JSONEditorUtil.cursorFromOffset(20, text)).toEqual({
+        row: 0, column: 17
+      });
+    });
+
+    it('should properly resolve offset in multiline text', function () {
+      let text = 'first line\nsecond line\nthird line';
+      expect(JSONEditorUtil.cursorFromOffset(13, text)).toEqual({
+        row: 1, column: 2
+      });
+    });
+
+  });
+
+  describe('#deepObjectDiff', function () {
+
+    it('should detect added values', function () {
+      let a = {};
+      let b = {a: 'foo'};
+
+      expect(JSONEditorUtil.deepObjectDiff(a, b)).toEqual([
+        {path: ['a'], value: 'foo', previous: undefined},
+        {path: [], value: {a: 'foo'}, previous: {}}
+      ]);
+    });
+
+    it('should detect removed values', function () {
+      let a = {a: 'foo'};
+      let b = {};
+
+      expect(JSONEditorUtil.deepObjectDiff(a, b)).toEqual([
+        {path: ['a'], value: undefined, previous: 'foo'},
+        {path: [], value: {}, previous: {a: 'foo'}}
+      ]);
+    });
+
+    it('should detect modified values', function () {
+      let a = {a: 'foo'};
+      let b = {a: 'bar'};
+
+      expect(JSONEditorUtil.deepObjectDiff(a, b)).toEqual([
+        {path: ['a'], value: 'bar', previous: 'foo'},
+        {path: [], value: {a: 'bar'}, previous: {a: 'foo'}}
+      ]);
+    });
+
+    it('should detect added values in arrays', function () {
+      let a = {a: []};
+      let b = {a: ['foo']};
+
+      expect(JSONEditorUtil.deepObjectDiff(a, b)).toEqual([
+        {path: ['a', '0'], value: 'foo', previous: undefined},
+        {path: ['a'], value: ['foo'], previous: []},
+        {path: [], value: {a: ['foo']}, previous: {a: []}}
+      ]);
+    });
+
+    it('should detect removed values in arrays', function () {
+      let a = {a: ['foo']};
+      let b = {a: []};
+
+      expect(JSONEditorUtil.deepObjectDiff(a, b)).toEqual([
+        {path: ['a', '0'], value: undefined, previous: 'foo'},
+        {path: ['a'], value: [], previous: ['foo']},
+        {path: [], value: {a: []}, previous: {a: ['foo']}}
+      ]);
+    });
+
+    it('should detect modified values in arrays', function () {
+      let a = {a: ['foo']};
+      let b = {a: ['bar']};
+
+      expect(JSONEditorUtil.deepObjectDiff(a, b)).toEqual([
+        {path: ['a', '0'], value: 'bar', previous: 'foo'},
+        {path: ['a'], value: ['bar'], previous: ['foo']},
+        {path: [], value: {a: ['bar']}, previous: {a: ['foo']}}
+      ]);
+    });
+
+    it('should detect added values in objects', function () {
+      let a = {a: {}};
+      let b = {a: {b: 'foo'}};
+
+      expect(JSONEditorUtil.deepObjectDiff(a, b)).toEqual([
+        {path: ['a', 'b'], value: 'foo', previous: undefined},
+        {path: ['a'], value: {b: 'foo'}, previous: {}},
+        {path: [], value: {a: {b: 'foo'}}, previous: {a: {}}}
+      ]);
+    });
+
+    it('should detect removed values in objects', function () {
+      let a = {a: {b: 'foo'}};
+      let b = {a: {}};
+
+      expect(JSONEditorUtil.deepObjectDiff(a, b)).toEqual([
+        {path: ['a', 'b'], value: undefined, previous: 'foo'},
+        {path: ['a'], value: {}, previous: {b: 'foo'}},
+        {path: [], value: {a: {}}, previous: {a: {b: 'foo'}}}
+      ]);
+    });
+
+    it('should detect modified values in objects', function () {
+      let a = {a: {b: 'foo'}};
+      let b = {a: {b: 'bar'}};
+
+      expect(JSONEditorUtil.deepObjectDiff(a, b)).toEqual([
+        {path: ['a', 'b'], value: 'bar', previous: 'foo'},
+        {path: ['a'], value: {b: 'bar'}, previous: {b: 'foo'}},
+        {path: [], value: {a: {b: 'bar'}}, previous: {a: {b: 'foo'}}}
+      ]);
+    });
+
+    it('should handle object-to-array mutations', function () {
+      let a = {a: {b: 'foo'}};
+      let b = {a: ['foo']};
+
+      expect(JSONEditorUtil.deepObjectDiff(a, b)).toEqual([
+        {path: ['a', 'b'], value: undefined, previous: 'foo'},
+        {path: ['a', '0'], value: 'foo', previous: undefined},
+        {path: ['a'], value: ['foo'], previous: {b: 'foo'}},
+        {path: [], value: {a: ['foo']}, previous: {a: {b: 'foo'}}}
+      ]);
+    });
+
+    it('should handle object-to-scalar mutations', function () {
+      let a = {a: {b: 'foo'}};
+      let b = {a: 'bar'};
+
+      expect(JSONEditorUtil.deepObjectDiff(a, b)).toEqual([
+        {path: ['a'], value: 'bar', previous: {b: 'foo'}},
+        {path: [], value: {a: 'bar'}, previous: {a: {b: 'foo'}}}
+      ]);
+    });
+
+    it('should handle array-to-object mutations', function () {
+      let a = {a: ['foo']};
+      let b = {a: {b: 'foo'}};
+
+      expect(JSONEditorUtil.deepObjectDiff(a, b)).toEqual([
+        {path: ['a', '0'], value: undefined, previous: 'foo'},
+        {path: ['a', 'b'], value: 'foo', previous: undefined},
+        {path: ['a'], value: {b: 'foo'}, previous: ['foo']},
+        {path: [], value: {a: {b: 'foo'}}, previous: {a: ['foo']}}
+      ]);
+    });
+
+    it('should handle scalar-to-object mutations', function () {
+      let a = {a: 'bar'};
+      let b = {a: {b: 'foo'}};
+
+      expect(JSONEditorUtil.deepObjectDiff(a, b)).toEqual([
+        {path: ['a'], value: {b: 'foo'}, previous: 'bar'},
+        {path: [], value: {a: {b: 'foo'}}, previous: {a: 'bar'}}
+      ]);
+    });
+  });
+
+  describe('#sortObjectKeys', function () {
+
+    it('should order keys according to old ones', function () {
+      let a = {a:0, b:1, c:2};
+      let b = {c:3, a:4, b:5};
+
+      let value = JSONEditorUtil.sortObjectKeys(a, b);
+      expect(Object.keys(value)).toEqual(['a', 'b', 'c']);
+      expect(value).toEqual({a:4, b:5, c:3});
+    });
+
+    it('should order less than previous keys in the same order', function () {
+      let a = {a:0, b:1, c:2};
+      let b = {c:3, a:4};
+
+      let value = JSONEditorUtil.sortObjectKeys(a, b);
+      expect(Object.keys(value)).toEqual(['a', 'c']);
+      expect(value).toEqual({a:4, c:3});
+    });
+
+    it('should add new keys at the end of the object', function () {
+      let a = {a:0, b:1, c:2};
+      let b = {c:3, a:4, d:5};
+
+      let value = JSONEditorUtil.sortObjectKeys(a, b);
+      expect(Object.keys(value)).toEqual(['a', 'c', 'd']);
+      expect(value).toEqual({a:4, c:3, d:5});
+    });
+
+    it('should order keys according to old ones (nested)', function () {
+      let a = {o: {a:0, b:1, c:2}};
+      let b = {o: {c:3, a:4, b:5}};
+
+      let value = JSONEditorUtil.sortObjectKeys(a, b);
+      expect(Object.keys(value.o)).toEqual(['a', 'b', 'c']);
+      expect(value).toEqual({o: {a:4, b:5, c:3}});
+    });
+
+    it('should order less than previous keys in the same order (nested)', function () {
+      let a = {o: {a:0, b:1, c:2}};
+      let b = {o: {c:3, a:4}};
+
+      let value = JSONEditorUtil.sortObjectKeys(a, b);
+      expect(Object.keys(value.o)).toEqual(['a', 'c']);
+      expect(value).toEqual({o: {a:4, c:3}});
+    });
+
+    it('should add new keys at the end of the object (nested)', function () {
+      let a = {o: {a:0, b:1, c:2}};
+      let b = {o: {c:3, a:4, d:5}};
+
+      let value = JSONEditorUtil.sortObjectKeys(a, b);
+      expect(Object.keys(value.o)).toEqual(['a', 'c', 'd']);
+      expect(value).toEqual({o: {a:4, c:3, d:5}});
+    });
+
+    it('should order array values', function () {
+      let a = {a: ['a', 'b', 'c']};
+      let b = {a: ['b', 'a', 'c']};
+
+      let value = JSONEditorUtil.sortObjectKeys(a, b);
+      expect(value).toEqual({a: ['a', 'b', 'c']});
+    });
+
+    it('should add new array values at the end', function () {
+      let a = {a: ['a', 'b', 'c']};
+      let b = {a: ['d', 'b', 'a']};
+
+      let value = JSONEditorUtil.sortObjectKeys(a, b);
+      expect(value).toEqual({a: ['a', 'b', 'd']});
+    });
+
+    it('should order objects in array values', function () {
+      let a = {a: [
+        {a:1, b:2, c:3},
+        {b:1, a:2, c:3}
+      ]};
+      let b = {a: [
+        {a:4, c:6, b:5},
+        {c:6, b:4, a:5}
+      ]};
+
+      let value = JSONEditorUtil.sortObjectKeys(a, b);
+      expect(Object.keys(value.a[0])).toEqual(['a', 'b', 'c']);
+      expect(Object.keys(value.a[1])).toEqual(['b', 'a', 'c']);
+      expect(value).toEqual({a: [
+        {a:4, b:5, c:6},
+        {b:4, a:5, c:6}
+      ]});
+    });
+
+    it('should order new keys at the end of objects in array values', function () {
+      let a = {a: [
+        {a:1, b:2, c:3},
+        {b:1, a:2, c:3}
+      ]};
+      let b = {a: [
+        {a:4, d:6, c:3, b:5},
+        {d:6, b:4, a:5, c:8}
+      ]};
+
+      let value = JSONEditorUtil.sortObjectKeys(a, b);
+      expect(Object.keys(value.a[0])).toEqual(['a', 'b', 'c', 'd']);
+      expect(Object.keys(value.a[1])).toEqual(['b', 'a', 'c', 'd']);
+      expect(value).toEqual({a: [
+        {a:4, b:5, c:3, d:6},
+        {b:4, a:5, c:8, d:6}
+      ]});
+    });
+
+  });
+
+});
