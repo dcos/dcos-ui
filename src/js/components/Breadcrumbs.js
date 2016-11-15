21b0901fdf1357f1ca7656cfe037c84bcea179ce
Dmitriy Rozhkov <drozhkov@mesosphere.io>
2016-11-15 17:38:46 +0100

    Recursively identify routes for the Breadcrumbs

--- src/js/components/Breadcrumbs.js
+++ src/js/components/Breadcrumbs.js
@@ -1,4 +1,5 @@
 import React, {PropTypes} from 'react';
+import {Redirect} from 'react-router';
 
 import ManualBreadcrumbs from './ManualBreadcrumbs';
 
@@ -10,15 +11,87 @@ class Breadcrumbs extends React.Component {
     let crumbs = crumbConfiguration.getCrumbs(params, routes);
 
     if (crumbConfiguration.parentCrumb) {
-      let parentCrumb = routes.find(function (eachRoute) {
-        return eachRoute.path === crumbConfiguration.parentCrumb;
-      });
-      if (parentCrumb && parentCrumb.buildBreadCrumb) {
-        crumbs = this.buildCrumbs(parentCrumb).concat(crumbs);
+      let parentRoute = this.findParentRoute(crumbConfiguration.parentCrumb);
+
+      if (parentRoute && parentRoute.buildBreadCrumb) {
+        crumbs = this.buildCrumbs(parentRoute).concat(crumbs);
       }
     }
 
     return crumbs;
+  }
+
+  findParentRoute(parentCrumbPath) {
+    let segments = parentCrumbPath.split('/');
+
+    // remove the split('/') artifact - an empty string
+    segments.shift(0, 1);
+
+    return this.findRoute(segments, this.getIndexRoute().childRoutes);
+  }
+
+  findRoute(segments, routes) {
+    const segment = segments[0];
+
+    // Nothing to match
+    if (!segment) {
+      return null;
+    }
+
+    for (var i = 0; i < routes.length; i++) {
+      const route = routes[i];
+
+      const isRedirect = route.component === Redirect;
+      const matchesPath = route.path && route.path.startsWith(segment);
+      const hasPath = !!route.path;
+      const hasChildren = route.childRoutes && route.childRoutes.length > 0;
+
+      if (!isRedirect && matchesPath) {
+        // Can't go deeper, return current route
+        if (segments.length === 1) {
+          return route;
+        }
+
+        // There are children and we still have untested segments
+        if (hasChildren) {
+          let foundRoute = this.findRoute(segments.slice(1), route.childRoutes);
+
+          if (foundRoute) {
+            return foundRoute;
+          }
+        }
+
+        // No children, still have segments.
+        // Meaning a route declared something like task/:taskID to be it's path
+        // So we need to concatinate two first segments and try again this set of routes
+        // We will be growing the segment until we exhaust all segments
+        if (segments.length > 1) {
+          const newSegment = `${segments[0]}/${segments[1]}`;
+          const newSegments = [newSegment].concat(segments.slice(2));
+
+          return this.findRoute(newSegments, routes);
+        }
+
+        return route;
+      }
+
+      // There're route definitions without a path that are being used for aggregation
+      // i.e. nodes/NodesOverview
+      if (!hasPath && hasChildren) {
+        let foundRoute = this.findRoute(segments, route.childRoutes);
+
+        if (foundRoute) {
+          return foundRoute;
+        }
+      }
+    }
+
+    return null;
+  }
+
+  getIndexRoute() {
+    // In our routing structure the route with id='index' always goes 2
+    return this.props.routes[1];
   }
 
   getCrumbsFromRoute() {
