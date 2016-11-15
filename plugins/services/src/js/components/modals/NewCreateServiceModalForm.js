58c181d697fdb4ca50195e02bd562b87a0170417
Ioannis Charalampidis <johnys2@gmail.com>
2016-11-15 18:15:51 +0100

    NewCreateServiceModalForm: Per-field validation

--- plugins/services/src/js/components/modals/NewCreateServiceModalForm.js
+++ plugins/services/src/js/components/modals/NewCreateServiceModalForm.js
@@ -126,7 +126,7 @@ class NewCreateServiceModalForm extends Component {
     this.setState(newState);
   }
 
-  handleFormBlur() {
+  handleFormBlur(event) {
     // Create temporary finalized appConfig
     let appConfig = this.getAppConfig();
 
@@ -135,7 +135,13 @@ class NewCreateServiceModalForm extends Component {
       AppValidators.App
     ]);
 
-    let errors = DataValidatorUtil.errorArrayToMap( errorList );
+    // [Case F2] Update errors only on the current field
+    let path = event.target.getAttribute('name').split('.');
+    let errors = DataValidatorUtil.updateOnlyMapPath(
+      this.state.errors,
+      DataValidatorUtil.errorArrayToMap( errorList ),
+      path
+    );
 
     // Create new jsonValue
     let jsonValue = JSON.stringify(appConfig, null, 2);
@@ -161,6 +167,12 @@ class NewCreateServiceModalForm extends Component {
         2
       );
     }
+
+    // [Case F1] Reset errors only on the current field
+    newState.errors = DataValidatorUtil.resetOnlyMapPath(
+      this.state.errors,
+      path
+    );
 
     this.setState(newState);
   }
