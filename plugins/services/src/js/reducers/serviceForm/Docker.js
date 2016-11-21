6efa099bee9a2a922a8d578b01626598e55c97c1
John Furrow <johnf@mesosphere.io>
2016-11-21 13:41:19 -0800

    Assign the network type in the container key

--- plugins/services/src/js/reducers/serviceForm/Docker.js
+++ plugins/services/src/js/reducers/serviceForm/Docker.js
@@ -1,7 +1,15 @@
 import {combineReducers, simpleReducer} from '../../../../../../src/js/utils/ReducerUtil';
+import Networking from '../../../../../../src/js/constants/Networking';
 
 module.exports = combineReducers({
   privileged: simpleReducer('container.docker.privileged', null),
   forcePullImage: simpleReducer('container.docker.forcePullImage', null),
-  image: simpleReducer('container.docker.image', '')
+  image: simpleReducer('container.docker.image', ''),
+  network(state, {type, path, value}) {
+    if (type === SET && joinedPath === 'networking.type') {
+      return Networking.type[value];
+    }
+
+    return state;
+  }
 });
