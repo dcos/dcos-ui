700bd402fa9e9e159ca75655d49c05584399aa32
MatApple <mat@mesosphere.com>
2016-11-30 13:03:42 -0800

    fix grammar typo

--- plugins/services/src/js/service-configuration/PodContainerArtifactsConfigSection.js
+++ plugins/services/src/js/service-configuration/PodContainerArtifactsConfigSection.js
@@ -25,7 +25,7 @@ module.exports = ({artifacts}) => {
         }}
         columns={[
           {
-            heading: 'Articaft URI',
+            heading: 'Artifact URI',
             prop: 'uri'
           },
           {
