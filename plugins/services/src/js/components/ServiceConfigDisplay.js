3232450dfaab2d9a5bc658f1c47dd13047305bfa
MatApple <mat@mesosphere.com>
2016-11-30 12:51:28 -0800

    ABC's

--- plugins/services/src/js/components/ServiceConfigDisplay.js
+++ plugins/services/src/js/components/ServiceConfigDisplay.js
@@ -2,22 +2,22 @@ import React from 'react';
 import {MountService} from 'foundation-ui';
 
 import ConfigurationMap from '../../../../../src/js/components/ConfigurationMap';
-import ServiceConfigGeneralSectionDisplay from './ServiceConfigGeneralSectionDisplay';
-import ServiceConfigPlacementConstraintsSectionDisplay from './ServiceConfigPlacementConstraintsSectionDisplay';
-import ServiceConfigNetworkingSectionDisplay from './ServiceConfigNetworkingSectionDisplay';
-import ServiceConfigStorageSectionDisplay from './ServiceConfigStorageSectionDisplay';
-import ServiceConfigEnvironmentVariablesSectionDisplay from './ServiceConfigEnvironmentVariablesSectionDisplay';
-import ServiceConfigLabelsSectionDisplay from './ServiceConfigLabelsSectionDisplay';
-import ServiceConfigHealthChecksSectionDisplay from './ServiceConfigHealthChecksSectionDisplay';
-import PodSpec from '../structs/PodSpec';
-import PodGeneralConfigSection from '../service-configuration/PodGeneralConfigSection';
 import PodContainersConfigSection from '../service-configuration/PodContainersConfigSection';
-import PodPlacementConstraintsConfigSection from '../service-configuration/PodPlacementConstraintsConfigSection';
-import PodNetworkConfigSection from '../service-configuration/PodNetworkConfigSection';
-import PodStorageConfigSection from '../service-configuration/PodStorageConfigSection';
 import PodEnvironmentVariablesConfigSection from '../service-configuration/PodEnvironmentVariablesConfigSection';
-import PodLabelsConfigSection from '../service-configuration/PodLabelsConfigSection';
+import PodGeneralConfigSection from '../service-configuration/PodGeneralConfigSection';
 import PodHealthChecksConfigSection from '../service-configuration/PodHealthChecksConfigSection';
+import PodLabelsConfigSection from '../service-configuration/PodLabelsConfigSection';
+import PodNetworkConfigSection from '../service-configuration/PodNetworkConfigSection';
+import PodPlacementConstraintsConfigSection from '../service-configuration/PodPlacementConstraintsConfigSection';
+import PodSpec from '../structs/PodSpec';
+import PodStorageConfigSection from '../service-configuration/PodStorageConfigSection';
+import ServiceConfigEnvironmentVariablesSectionDisplay from './ServiceConfigEnvironmentVariablesSectionDisplay';
+import ServiceConfigGeneralSectionDisplay from './ServiceConfigGeneralSectionDisplay';
+import ServiceConfigHealthChecksSectionDisplay from './ServiceConfigHealthChecksSectionDisplay';
+import ServiceConfigLabelsSectionDisplay from './ServiceConfigLabelsSectionDisplay';
+import ServiceConfigNetworkingSectionDisplay from './ServiceConfigNetworkingSectionDisplay';
+import ServiceConfigPlacementConstraintsSectionDisplay from './ServiceConfigPlacementConstraintsSectionDisplay';
+import ServiceConfigStorageSectionDisplay from './ServiceConfigStorageSectionDisplay';
 
 const PRIORITIES_PAD_NUMBER = 100;
 const DEFAULT_DISPLAY_COMPONENTS = [
