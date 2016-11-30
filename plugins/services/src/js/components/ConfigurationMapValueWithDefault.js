aa7651183143fd8dcc778ed470bb72c1ae47f1fc
Ioannis Charalampidis <johnys2@gmail.com>
2016-11-30 19:18:40 +0100

    Introducing <ConfigurationMapValueWithDefault />

---
+++ plugins/services/src/js/components/ConfigurationMapValueWithDefault.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import ConfigurationMapValue from '../../../../../src/js/components/ConfigurationMapValue';
+import ValidatorUtil from '../../../../../src/js/utils/ValidatorUtil.js';
+
+/**
+ * Render a defaultValue value if the value is empty or falsy.
+ */
+class ConfigurationMapValueWithDefault extends React.Component {
+  render() {
+    let {defaultValue, value} = this.props;
+
+    // Bail early with default if empty
+    if (ValidatorUtil.isEmpty(value)) {
+      return <ConfigurationMapValue>{defaultValue}</ConfigurationMapValue>;
+    }
+
+    return (
+      <ConfigurationMapValue>
+        {value}
+      </ConfigurationMapValue>
+    );
+  }
+};
+
+ConfigurationMapValueWithDefault.defaultProps = {
+  value: undefined,
+  defaultValue: <span>&mdash;</span>
+};
+
+ConfigurationMapValueWithDefault.propTypes = {
+  value: React.PropTypes.any,
+  defaultValue: React.PropTypes.oneOfType([
+    React.PropTypes.string,
+    React.PropTypes.node
+  ])
+};
+
+module.exports = ConfigurationMapValueWithDefault;
