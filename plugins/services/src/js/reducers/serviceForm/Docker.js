063eab99a20d9b17215f133fb6f68a2653c39042
mlunoe <michael.lunoe@gmail.com>
2016-11-30 16:40:14 -0800

    Fix rebase errors

--- plugins/services/src/js/reducers/serviceForm/Docker.js
+++ plugins/services/src/js/reducers/serviceForm/Docker.js
@@ -1,11 +1,14 @@
 import {combineReducers, simpleReducer} from '../../../../../../src/js/utils/ReducerUtil';
 import Networking from '../../../../../../src/js/constants/Networking';
+import {SET} from '../../../../../../src/js/constants/TransactionTypes';
 
 module.exports = combineReducers({
   privileged: simpleReducer('container.docker.privileged', null),
   forcePullImage: simpleReducer('container.docker.forcePullImage', null),
   image: simpleReducer('container.docker.image', ''),
-  network(state, {type, path, value}) {
+  network(state, {type, path = [], value}) {
+    let joinedPath = path.join('.');
+
     if (type === SET && joinedPath === 'networking.type') {
       return Networking.type[value];
     }
