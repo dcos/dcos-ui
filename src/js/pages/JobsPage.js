0838a375806fd365782dfb5bfada48dbd44a858a
Philip Norman <philip@mesosphere.io>
2016-12-05 02:24:23 -0800

    Roll out new headers across job specific pages

--- src/js/pages/JobsPage.js
+++ src/js/pages/JobsPage.js
@@ -2,19 +2,12 @@ import React from 'react';
 import {routerShape} from 'react-router';
 
 import Icon from '../components/Icon';
-import Page from '../components/Page';
 import SidebarActions from '../events/SidebarActions';
 
 class JobsPage extends React.Component {
   render() {
-    let {routes, children} = this.props;
-    return (
-      <Page
-        dontScroll={routes[routes.length - 1].dontScroll}
-        title="Jobs">
-        {children}
-      </Page>
-    );
+    let {children} = this.props;
+    return children;
   }
 }
 
