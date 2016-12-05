3bcb119f124fa992b8b2a2054249704c0dae4451
Ioannis Charalampidis <johnys2@gmail.com>
2016-12-05 16:55:14 +0100

    Adding ServiceAttribIsFilter

---
+++ plugins/services/src/js/filters/ServiceAttribIsFilter.js
@@ -0,0 +1,45 @@
+import DSLFilterTypes from '../../../../../src/js/constants/DSLFilterTypes';
+import DSLFilter from '../../../../../src/js/structs/DSLFilter';
+import ServiceStatus from '../constants/ServiceStatus';
+
+const TEXT_TO_INSTANCE = {
+  delayed: ServiceStatus.DELAYED,
+  deploying: ServiceStatus.DEPLOYING,
+  na: ServiceStatus.NA,
+  running: ServiceStatus.RUNNING,
+  suspended: ServiceStatus.SUSPENDED,
+  waiting: ServiceStatus.WAITING
+};
+
+/**
+ * This filter handles the `is:status` for services using `getServiceStatus`
+ */
+class ServiceAttribIsFilter extends DSLFilter {
+
+  /**
+   * Handle all `is:XXXX` attrib filters that we can handle.
+   *
+   * @override
+   */
+  filterCanHandle(filterType, filterArguments) {
+    return filterType === DSLFilterTypes.ATTRIB &&
+           filterArguments.label === 'is' &&
+           TEXT_TO_INSTANCE[filterArguments.text] != null;
+  }
+
+  /**
+   * Keep only services whose status
+   *
+   * @override
+   */
+  filterApply(resultset, filterType, filterArguments) {
+    let testStatus = TEXT_TO_INSTANCE[filterArguments.text];
+
+    return resultset.filterItems((service) => {
+      return service.getServiceStatus() === testStatus;
+    });
+  }
+
+}
+
+module.exports = ServiceAttribIsFilter;
