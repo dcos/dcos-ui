378e46ff5830feab43dce3a6d845062f0943e1d3
Ioannis Charalampidis <johnys2@gmail.com>
2016-12-05 19:14:56 +0100

    Adding ServiceNameTextFilter

---
+++ plugins/services/src/js/filters/ServiceNameTextFilter.js
@@ -0,0 +1,32 @@
+import DSLFilterTypes from '../../../../../src/js/constants/DSLFilterTypes';
+import DSLFilter from '../../../../../src/js/structs/DSLFilter';
+
+/**
+ * This filter handles the `text` attributes against service's `getName` value
+ */
+class ServiceNameTextFilter extends DSLFilter {
+
+  /**
+   * Handle all `name` attrib filters that we can handle.
+   *
+   * @override
+   */
+  filterCanHandle(filterType) {
+    return filterType === DSLFilterTypes.EXACT ||
+           filterType === DSLFilterTypes.FUZZY;
+  }
+
+  /**
+   * Keep only services whose status
+   *
+   * @override
+   */
+  filterApply(resultset, filterType, filterArguments) {
+    return resultset.filterItems((service) => {
+      return service.getName().indexOf(filterArguments.text) !== -1;
+    });
+  }
+
+}
+
+module.exports = ServiceNameTextFilter;
