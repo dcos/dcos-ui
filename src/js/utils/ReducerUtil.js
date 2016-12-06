1c1c5196cf4d56b4890b70be2f91bcfb4c417346
Philipp Hinrichsen <philipph@mesosphere.io>
2016-12-06 15:46:39 +0100

    Fix reducerUtil to allow falsy values

--- src/js/utils/ReducerUtil.js
+++ src/js/utils/ReducerUtil.js
@@ -60,11 +60,8 @@ module.exports = {
         // reducer.bind(context.get(reducer))(state[key], action);
         // but it will not copy the function, which in this case is a huge
         // increase in performance.
-        let value =
-            reducer.call(context.get(reducer), localState[key], action, index);
-        if (value) {
-          localState[key] = value;
-        }
+        localState[key] =
+          reducer.call(context.get(reducer), localState[key], action, index);
       }
 
       return localState;
