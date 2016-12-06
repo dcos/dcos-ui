35bcb8b81decd9c6715e64be76cd4f72f86118ac
Ioannis Charalampidis <johnys2@gmail.com>
2016-12-06 19:38:24 +0100

    Introducing DSLExpression

---
+++ src/js/structs/DSLExpression.js
@@ -0,0 +1,63 @@
+import SearchDSL from '../../resources/grammar/SearchDSL.jison';
+
+/**
+ * This is a high-level compnent that wraps
+ */
+class DSLExpression {
+
+  /**
+   * Construct an immutable DSL expression
+   *
+   * @param {String} value - The expression string
+   */
+  constructor(value='') {
+    let ast = null;
+    let filter = (filters, resultset) => resultset;
+    let hasErrors = false;
+
+    if (value) {
+      // Try to parse the expression
+      try {
+        let result = SearchDSL.parse(value);
+        if (result.ast && result.filter) {
+          ast = result.ast;
+          filter = result.filter;
+        }
+      } catch (e) {
+        hasErrors = true;
+      }
+    }
+
+    // Define properties
+    Object.defineProperties(this, {
+      ast: {
+        value: ast,
+        writable: false,
+        enumerable: true
+      },
+      defined: {
+        value: Boolean(value),
+        writable: false,
+        enumerable: true
+      },
+      filter: {
+        value: filter,
+        writable: false,
+        enumerable: true
+      },
+      hasErrors: {
+        value: hasErrors,
+        writable: false,
+        enumerable: true
+      },
+      value: {
+        value,
+        writable: false,
+        enumerable: true
+      }
+    });
+  }
+
+};
+
+module.exports = DSLExpression;
