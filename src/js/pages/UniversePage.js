4d8f8502dd10538f255d23de6c1d83c13fb850a5
John Furrow <johnf@mesosphere.io>
2016-12-06 13:42:46 -0800

    Remove unused code from UniversePage

--- src/js/pages/UniversePage.js
+++ src/js/pages/UniversePage.js
@@ -1,72 +1,17 @@
-import mixin from 'reactjs-mixin';
-/* eslint-disable no-unused-vars */
 import React from 'react';
-/* eslint-enable no-unused-vars */
-import {routerShape} from 'react-router';
 
 import Icon from '../components/Icon';
-import RouterUtil from '../utils/RouterUtil';
-import SidebarActions from '../events/SidebarActions';
-import TabsMixin from '../mixins/TabsMixin';
 
-class UniversePage extends mixin(TabsMixin) {
-  constructor() {
-    super();
-
-    this.tabs_tabs = {};
-    this.state = {};
-  }
-
-  componentWillMount() {
-    this.updateCurrentTab();
-  }
-
-  componentWillReceiveProps(nextProps) {
-    this.updateCurrentTab(nextProps);
-  }
-
-  updateCurrentTab(nextProps) {
-    let {routes} = nextProps || this.props;
-    let currentTab = RouterUtil.reconstructPathFromRoutes(routes);
-
-    // Universe Tabs
-    this.tabs_tabs = {
-      '/universe/packages': 'Packages',
-      '/universe/installed-packages': 'Installed'
-    };
-
-    this.setState({currentTab});
-  }
-
-  getNavigation() {
-    if (RouterUtil.shouldHideNavigation(this.props.routes)) {
-      return null;
-    }
-
-    return (
-      <ul className="menu-tabbed">
-        {this.tabs_getRoutedTabs()}
-      </ul>
-    );
-  }
-
+class UniversePage extends React.Component {
   render() {
     return this.props.children;
   }
 }
 
-UniversePage.contextTypes = {
-  router: routerShape
-};
-
 UniversePage.routeConfig = {
   label: 'Packages',
   icon: <Icon id="packages-inverse" size="small" family="product" />,
   matches: /^\/universe/
-};
-
-UniversePage.willTransitionTo = function () {
-  SidebarActions.close();
 };
 
 module.exports = UniversePage;
