94bc7ec0e1c7b67d26c62bc7c9b9f5768f265468
Rafael Corral <me@rafaelcorral.com>
2016-12-06 10:29:41 -0800

    Implement page header in Users Management

--- src/js/pages/OrganizationPage.js
+++ src/js/pages/OrganizationPage.js
@@ -1,15 +1,10 @@
 import React from 'react';
 
 import Icon from '../components/Icon';
-import Page from '../components/Page';
 
 class OrganizationPage extends React.Component {
   render() {
-    return (
-      <Page title="Organization">
-        {this.props.children}
-      </Page>
-    );
+    return this.props.children;
   }
 }
 
