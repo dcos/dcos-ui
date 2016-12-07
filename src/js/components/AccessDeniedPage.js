41b49d8d25ec8a10ae2e0ded21502b6454d99b0c
John Furrow <johnf@mesosphere.io>
2016-12-07 10:07:25 -0800

    Fix access denied page styling

--- src/js/components/AccessDeniedPage.js
+++ src/js/components/AccessDeniedPage.js
@@ -35,16 +35,16 @@ module.exports = class AccessDeniedPage extends React.Component {
 
   render() {
     return (
-      <div className="page flex flex-direction-top-to-bottom flex-item-grow-1">
-        <div className="page-body-content flex flex-direction-top-to-bottom
-          flex-item-grow-1 horizontal-center vertical-center">
-          <AlertPanel
-            footer={this.getFooter()}
-            title="Access denied">
-            <p className="tall">
-              You do not have access to this service. Please contact your {Config.productName} administrator or see <a href={MetadataStore.buildDocsURI('/administration/id-and-access-mgt/')} target="_blank">security documentation</a> for more information.
-            </p>
-          </AlertPanel>
+      <div className="application-wrapper">
+        <div className="page">
+          <div className="page-body-content vertical-center">
+            <AlertPanel title="Access denied">
+              <p className="tall">
+                You do not have access to this service. Please contact your {Config.productName} administrator or see <a href={MetadataStore.buildDocsURI('/administration/id-and-access-mgt/')} target="_blank">security documentation</a> for more information.
+              </p>
+              {this.getFooter()}
+            </AlertPanel>
+          </div>
         </div>
       </div>
     );
