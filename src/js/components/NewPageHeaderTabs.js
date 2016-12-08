3666aaf378cc9310e7ba39380d4b3447ffd87eba
Philip Norman <philip@mesosphere.io>
2016-12-07 18:38:09 -0800

    Show active class on routed tabs

--- src/js/components/NewPageHeaderTabs.js
+++ src/js/components/NewPageHeaderTabs.js
@@ -24,7 +24,7 @@ class PageHeaderTabs extends React.Component {
 
       if (tab.callback == null) {
         link = (
-          <Link className={linkClasses} to={tab.routePath}>
+          <Link className={linkClasses} to={tab.routePath} activeClassName="active">
             {innerLinkSpan}
           </Link>
         );
