42007df5f15af0a778e13ab785206364c3d66844
Ioannis Charalampidis <johnys2@gmail.com>
2016-12-08 11:53:28 +0100

    Renaming DSLPropTypes to DSLExpressionPart

--- src/js/structs/DSLExpressionPart.js
+++ src/js/structs/DSLExpressionPart.js
@@ -2,31 +2,47 @@ import DSLFilterTypes from '../constants/DSLFilterTypes';
 import {FilterNode} from './DSLASTNodes';
 
 /**
- * The DSL PropTypes are used by components inserted in a `DSLInputForm` in
- * order to specify the AST nodes they are managing.
+ * The DSL Expression Parts are used by components inserted in a `DSLInputForm`
+ * in order to specify the expression's AST nodes they are targeting.
  *
  * The functions and getters in this file are just shorthands to create virtual
  * AST nodes (without any position information) that are used as comparision
  * targets for selecting the correct nodes from the paresed AST nodes.
  *
- * @example <caption>How to use DSLPropTypes</caption>
+ * @example <caption>How to use DSLExpressionPart</caption>
  *
- * MyFormComponent.dslPropTypes = {
+ * const EXPRESSION_PARTS = {
  *
  *   // Create a boolean property that is defined when the attribute is:healthy
  *   // exists in the parsed DSL
- *   is_healthy: DSLPropTypes.attrib('is', 'healthy'),
+ *   is_healthy: DSLExpressionPart.attrib('is', 'healthy'),
  *
  *   // Create a string property that contains the first exact-match node
- *   text: DSLPropTypes.exact,
+ *   text: DSLExpressionPart.exact,
  *
  *   // Create a string property that contains all the fuzzy-match nodes,
  *   // concatenated with spaces
- *   text: DSLPropTypes.fuzzy
+ *   text: DSLExpressionPart.fuzzy
  *
  * }
+ *
+ * class MyComponent {
+ *   render() {
+ *      const {expression} = this.props;
+ *      const data = DSLUtil.getExpressionParts(expression, EXPRESSION_PARTS);
+ *
+ *      return (
+ *         <div>
+ *           <p>
+ *             Is healthy: <input type="checkbox" checked={data.is_healthy} />
+ *           </p>
+ *         </div>
+ *      );
+ *   }
+ * }
+ *
  */
-class DSLPropTypes {
+class DSLExpressionPart {
 
   static attrib(label, text=undefined) {
     return new FilterNode(0, 0, DSLFilterTypes.ATTRIB, {label, text});
@@ -42,4 +58,4 @@ class DSLPropTypes {
 
 };
 
-module.exports = DSLPropTypes;
+module.exports = DSLExpressionPart;
