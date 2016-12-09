3742ae77bbb5f24f4d821d53a782cdfd378f3379
Ioannis Charalampidis <ioannis.charalampidis@cern.ch>
2016-12-09 17:01:44 +0100

    Adding DSL filters for pod, universe and volumes

---
+++ plugins/services/src/js/filters/ServiceHasVolumesAttribFilter.js
@@ -0,0 +1,37 @@
+import DSLFilterTypes from '../../../../../src/js/constants/DSLFilterTypes';
+import DSLFilter from '../../../../../src/js/structs/DSLFilter';
+
+const LABEL = 'has';
+const LABEL_TEXT = 'volumes';
+
+/**
+ * This filter handles the `is:status` for services using `getServiceStatus`
+ */
+class ServiceIsUniverseAttribFilter extends DSLFilter {
+
+  /**
+   * Handle all `has:volumes` attrib filters that we can handle.
+   *
+   * @override
+   */
+  filterCanHandle(filterType, filterArguments) {
+    return filterType === DSLFilterTypes.ATTRIB &&
+      filterArguments.label === LABEL &&
+      filterArguments.text.toLowerCase() === LABEL_TEXT;
+  }
+
+  /**
+   * Keep only services that contain more than 1 volume
+   *
+   * @override
+   */
+  filterApply(resultset) {
+    return resultset.filterItems((service) => {
+      let volumes = service.getVolumes();
+      return (volumes.list && volumes.list.length > 0);
+    });
+  }
+
+}
+
+module.exports = ServiceIsUniverseAttribFilter;
---
+++ plugins/services/src/js/filters/ServiceIsUniverseAttribFilter.js
@@ -0,0 +1,37 @@
+import DSLFilterTypes from '../../../../../src/js/constants/DSLFilterTypes';
+import DSLFilter from '../../../../../src/js/structs/DSLFilter';
+import Framework from '../structs/Framework';
+
+const LABEL = 'is';
+const LABEL_TEXT = 'universe';
+
+/**
+ * This filter handles the `is:status` for services using `getServiceStatus`
+ */
+class ServiceIsUniverseAttribFilter extends DSLFilter {
+
+  /**
+   * Handle all `is:universe` attrib filters that we can handle.
+   *
+   * @override
+   */
+  filterCanHandle(filterType, filterArguments) {
+    return filterType === DSLFilterTypes.ATTRIB &&
+      filterArguments.label === LABEL &&
+      filterArguments.text.toLowerCase() === LABEL_TEXT;
+  }
+
+  /**
+   * Keep only services whose instance is of type `Framework`
+   *
+   * @override
+   */
+  filterApply(resultset) {
+    return resultset.filterItems((service) => {
+      return service instanceof Framework;
+    });
+  }
+
+}
+
+module.exports = ServiceIsUniverseAttribFilter;
