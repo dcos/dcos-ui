6b4625d3f4ec21cfd3e2e06be96a9952fd7b0774
John Furrow <johnf@mesosphere.io>
2016-12-09 13:16:22 -0800

    Allow disabling Gemini in FullScreenModal

--- src/js/components/modals/FullScreenModal.js
+++ src/js/components/modals/FullScreenModal.js
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import {Modal} from 'reactjs-components';
 import React from 'react';
 
@@ -5,24 +6,38 @@ import Util from '../../utils/Util';
 
 class FullScreenModal extends React.Component {
   render() {
-    let {props} = this;
+    const {props} = this;
+    const geminiClasses = classNames({
+      'gm-scrollbar-container-flex': props.useGemini !== false
+    });
+
+    let modalHeight = null;
+
+    if (props.useGemini) {
+      modalHeight = '100%';
+    }
 
     return (
-      <Modal geminiClass="gm-scrollbar-container-flex"
-        modalHeight="100%"
+      <Modal geminiClass={geminiClasses}
         modalClass="modal modal-full-screen"
+        modalHeight={modalHeight}
         showHeader={true}
         showFooter={false}
         transitionNameModal="modal-full-screen"
-        {...Util.omit(props, Object.keys(FullScreenModal.propTypes))}>
+        {...Util.omit(props, ['children'])}>
         {props.children}
       </Modal>
     );
   }
 }
 
+FullScreenModal.defaultProps = {
+  useGemini: true
+};
+
 FullScreenModal.propTypes = {
-  children: React.PropTypes.node
+  children: React.PropTypes.node,
+  useGemini: React.PropTypes.bool
 };
 
 module.exports = FullScreenModal;
