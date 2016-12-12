8d55b04b7630b6b76c563ff64bea629c5288a95c
Orlando Hohmeier <orlando@mesosphere.io>
2016-12-12 16:24:43 +0100

    Adjust container reducer to set resource defaults
    Change the pod default to include `mem` and adjust the container reducer
    (`ADD_ITEM` transition) to properly define resource defaults.

--- plugins/services/src/js/constants/DefaultPod.js
+++ plugins/services/src/js/constants/DefaultPod.js
@@ -1,4 +1,4 @@
-const DEFAULT_POD_RESOURCES = {cpus: 0.001};
+const DEFAULT_POD_RESOURCES = {cpus: 0.001, mem: 32};
 const DEFAULT_POD_CONTAINER =
     {name: 'container-1', resources: DEFAULT_POD_RESOURCES};
 const DEFAULT_POD_SPEC = {containers: [DEFAULT_POD_CONTAINER]};
