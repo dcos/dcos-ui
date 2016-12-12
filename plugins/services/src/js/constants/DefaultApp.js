87fe82be163c83fa40e05510f22353bf306c06d1
Orlando Hohmeier <orlando@mesosphere.io>
2016-12-12 15:27:06 +0100

    Reorganize app and pod defaults
    Rename the app and pod default constants and files to allow the export
    of additional defaults.

---
+++ plugins/services/src/js/constants/DefaultApp.js
@@ -0,0 +1,7 @@
+const DEFAULT_APP_RESOURCES = {cpus: 0.001, mem: 128};
+const DEFAULT_APP_SPEC = Object.assign({instances: 1}, DEFAULT_APP_RESOURCES);
+
+module.exports = {
+  DEFAULT_APP_RESOURCES,
+  DEFAULT_APP_SPEC
+};
