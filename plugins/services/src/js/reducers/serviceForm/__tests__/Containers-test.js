dc786d0df995d7a84ab373a9ec2b2e984fc40d4a
Philipp Hinrichsen <philipph@mesosphere.io>
2016-12-13 14:31:02 +0100

    Add Endpoint tests to containers test

--- plugins/services/src/js/reducers/serviceForm/__tests__/Containers-test.js
+++ plugins/services/src/js/reducers/serviceForm/__tests__/Containers-test.js
@@ -12,7 +12,7 @@ describe('Containers', function () {
       let batch = new Batch();
 
       expect(batch.reduce(Containers.JSONReducer.bind({}), []))
-        .toEqual([]);
+      .toEqual([]);
     });
 
     it('returns an array as default with a container path Transaction', function () {
@@ -23,6 +23,720 @@ describe('Containers', function () {
         .toEqual([DEFAULT_POD_CONTAINER]);
     });
 
+    describe('endpoints', function () {
+      describe('Host Mode', function () {
+
+        it('should have one endpoint', function () {
+          let batch = new Batch();
+
+          batch = batch.add(new Transaction(['containers'], 0, ADD_ITEM));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints'
+              ], 0, ADD_ITEM));
+
+          expect(batch.reduce(Containers.JSONReducer.bind({})))
+          .toEqual([
+            {
+              name: 'container 1',
+              endpoints: [
+                {
+                  name: null,
+                  hostPort: 0,
+                  protocol: [
+                    'tcp'
+                  ]
+                }
+              ]
+            }
+          ]);
+        });
+
+        it('should have one endpoint with name', function () {
+          let batch = new Batch();
+
+          batch = batch.add(new Transaction(['containers'], 0, ADD_ITEM));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints'
+              ], 0, ADD_ITEM));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints',
+                0,
+                'name'
+              ], 'foo'));
+
+          expect(batch.reduce(Containers.JSONReducer.bind({})))
+          .toEqual([
+            {
+              name: 'container 1',
+              endpoints: [
+                {
+                  name: 'foo',
+                  hostPort: 0,
+                  protocol: [
+                    'tcp'
+                  ]
+                }
+              ]
+            }
+          ]);
+        });
+
+        it('should have one endpoint with name and a hostport', function () {
+          let batch = new Batch();
+
+          batch = batch.add(new Transaction(['containers'], 0, ADD_ITEM));
+
+          batch = batch.add(new Transaction(['network'], 'HOST'));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints'
+              ], 0, ADD_ITEM));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints',
+                0,
+                'name'
+              ], 'foo'));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints',
+                0,
+                'automaticPort'
+              ], false));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints',
+                0,
+                'hostPort'
+              ], 8080));
+
+          expect(batch.reduce(Containers.JSONReducer.bind({})))
+          .toEqual([
+            {
+              name: 'container 1',
+              endpoints: [
+                {
+                  name: 'foo',
+                  hostPort: 8080,
+                  protocol: [
+                    'tcp'
+                  ]
+                }
+              ]
+            }
+          ]);
+        });
+
+        it('should set the protocol right', function () {
+          let batch = new Batch();
+
+          batch = batch.add(new Transaction(['containers'], 0, ADD_ITEM));
+
+          batch = batch.add(new Transaction(['network'], 'HOST'));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints'
+              ], 0, ADD_ITEM));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints',
+                0,
+                'protocol'
+              ], 'udp'));
+
+          expect(batch.reduce(Containers.JSONReducer.bind({})))
+          .toEqual([
+            {
+              name: 'container 1',
+              endpoints: [
+                {
+                  name: null,
+                  hostPort: 0,
+                  protocol: [
+                    'udp'
+                  ]
+                }
+              ]
+            }
+          ]);
+        });
+
+        it('should set protocol to unknown value', function () {
+          let batch = new Batch();
+
+          batch = batch.add(new Transaction(['containers'], 0, ADD_ITEM));
+
+          batch = batch.add(new Transaction(['network'], 'HOST'));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints'
+              ], 0, ADD_ITEM));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints',
+                0,
+                'protocol'
+              ], 'foo'));
+
+          expect(batch.reduce(Containers.JSONReducer.bind({})))
+          .toEqual([
+            {
+              name: 'container 1',
+              endpoints: [
+                {
+                  name: null,
+                  hostPort: 0,
+                  protocol: [
+                    'foo'
+                  ]
+                }
+              ]
+            }
+          ]);
+        });
+
+      });
+
+      describe('container Mode', function () {
+
+        it('should have one endpoint', function () {
+          let batch = new Batch();
+
+          batch = batch.add(new Transaction(['containers'], 0, ADD_ITEM));
+
+          batch = batch.add(new Transaction(['network'], 'CONTAINER.foo'));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints'
+              ], 0, ADD_ITEM));
+
+          expect(batch.reduce(Containers.JSONReducer.bind({})))
+          .toEqual([
+            {
+              name: 'container 1',
+              endpoints: [
+                {
+                  name: null,
+                  hostPort: 0,
+                  containerPort: null,
+                  labels: null,
+                  protocol: [
+                    'tcp'
+                  ]
+                }
+              ]
+            }
+          ]);
+        });
+
+        it('should have one endpoint with name', function () {
+          let batch = new Batch();
+
+          batch = batch.add(new Transaction(['containers'], 0, ADD_ITEM));
+
+          batch = batch.add(new Transaction(['network'], 'CONTAINER.foo'));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints'
+              ], 0, ADD_ITEM));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints',
+                0,
+                'name'
+              ], 'foo'));
+
+          expect(batch.reduce(Containers.JSONReducer.bind({})))
+          .toEqual([
+            {
+              name: 'container 1',
+              endpoints: [
+                {
+                  name: 'foo',
+                  containerPort: null,
+                  labels: null,
+                  hostPort: 0,
+                  protocol: [
+                    'tcp'
+                  ]
+                }
+              ]
+            }
+          ]);
+        });
+
+        it('should have one endpoint with name and a hostport', function () {
+          let batch = new Batch();
+
+          batch = batch.add(new Transaction(['containers'], 0, ADD_ITEM));
+
+          batch = batch.add(new Transaction(['network'], 'CONTAINER.foo'));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints'
+              ], 0, ADD_ITEM));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints',
+                0,
+                'name'
+              ], 'foo'));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints',
+                0,
+                'automaticPort'
+              ], false));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints',
+                0,
+                'hostPort'
+              ], 8080));
+
+          expect(batch.reduce(Containers.JSONReducer.bind({})))
+          .toEqual([
+            {
+              name: 'container 1',
+              endpoints: [
+                {
+                  name: 'foo',
+                  containerPort: null,
+                  labels: null,
+                  hostPort: 8080,
+                  protocol: [
+                    'tcp'
+                  ]
+                }
+              ]
+            }
+          ]);
+        });
+
+        it('should set the protocol right', function () {
+          let batch = new Batch();
+
+          batch = batch.add(new Transaction(['containers'], 0, ADD_ITEM));
+
+          batch = batch.add(new Transaction(['network'], 'CONTAINER.foo'));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints'
+              ], 0, ADD_ITEM));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints',
+                0,
+                'protocol'
+              ], 'udp'));
+
+          expect(batch.reduce(Containers.JSONReducer.bind({})))
+          .toEqual([
+            {
+              name: 'container 1',
+              endpoints: [
+                {
+                  name: null,
+                  containerPort: null,
+                  labels: null,
+                  hostPort: 0,
+                  protocol: [
+                    'udp'
+                  ]
+                }
+              ]
+            }
+          ]);
+        });
+
+        it('should set protocol to unknown value', function () {
+          let batch = new Batch();
+
+          batch = batch.add(new Transaction(['containers'], 0, ADD_ITEM));
+
+          batch = batch.add(new Transaction(['network'], 'CONTAINER.foo'));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints'
+              ], 0, ADD_ITEM));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints',
+                0,
+                'protocol'
+              ], 'foo'));
+
+          expect(batch.reduce(Containers.JSONReducer.bind({})))
+          .toEqual([
+            {
+              name: 'container 1',
+              endpoints: [
+                {
+                  name: null,
+                  containerPort: null,
+                  labels: null,
+                  hostPort: 0,
+                  protocol: [
+                    'foo'
+                  ]
+                }
+              ]
+            }
+          ]);
+        });
+
+        it('should set the right contianer Port', function () {
+          let batch = new Batch();
+
+          batch = batch.add(new Transaction(['containers'], 0, ADD_ITEM));
+
+          batch = batch.add(new Transaction(['network'], 'CONTAINER.foo'));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints'
+              ], 0, ADD_ITEM));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints',
+                0,
+                'containerPort'
+              ], '8080'));
+
+          expect(batch.reduce(Containers.JSONReducer.bind({})))
+          .toEqual([
+            {
+              name: 'container 1',
+              endpoints: [
+                {
+                  name: null,
+                  containerPort: 8080,
+                  labels: null,
+                  hostPort: 0,
+                  protocol: [
+                    'tcp'
+                  ]
+                }
+              ]
+            }
+          ]);
+        });
+
+        it('should set the right vip', function () {
+          let batch = new Batch();
+
+          batch = batch.add(new Transaction(['containers'], 0, ADD_ITEM));
+
+          batch = batch.add(new Transaction(['network'], 'CONTAINER.foo'));
+          batch = batch.add(new Transaction(['id'], '/foobar'));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints'
+              ], 0, ADD_ITEM));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints',
+                0,
+                'containerPort'
+              ], '8080'));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints',
+                0,
+                'loadBalanced'
+              ], true));
+
+          expect(batch.reduce(Containers.JSONReducer.bind({})))
+          .toEqual([
+            {
+              name: 'container 1',
+              endpoints: [
+                {
+                  name: null,
+                  containerPort: 8080,
+                  labels: {
+                    'VIP_0': '/foobar:8080'
+                  },
+                  hostPort: 0,
+                  protocol: [
+                    'tcp'
+                  ]
+                }
+              ]
+            }
+          ]);
+        });
+
+        it('should set the right custom vip', function () {
+          let batch = new Batch();
+
+          batch = batch.add(new Transaction(['containers'], 0, ADD_ITEM));
+
+          batch = batch.add(new Transaction(['network'], 'CONTAINER.foo'));
+          batch = batch.add(new Transaction(['id'], '/foobar'));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints'
+              ], 0, ADD_ITEM));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints',
+                0,
+                'containerPort'
+              ], '8080'));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints',
+                0,
+                'loadBalanced'
+              ], true));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints',
+                0,
+                'vip'
+              ], '1.3.3.7:8080'));
+
+          expect(batch.reduce(Containers.JSONReducer.bind({})))
+          .toEqual([
+            {
+              name: 'container 1',
+              endpoints: [
+                {
+                  name: null,
+                  containerPort: 8080,
+                  labels: {
+                    'VIP_0': '1.3.3.7:8080'
+                  },
+                  hostPort: 0,
+                  protocol: [
+                    'tcp'
+                  ]
+                }
+              ]
+            }
+          ]);
+        });
+
+        it('should set the right vip after id change', function () {
+          let batch = new Batch();
+
+          batch = batch.add(new Transaction(['containers'], 0, ADD_ITEM));
+
+          batch = batch.add(new Transaction(['network'], 'CONTAINER.foo'));
+          batch = batch.add(new Transaction(['id'], '/foobar'));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints'
+              ], 0, ADD_ITEM));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints',
+                0,
+                'containerPort'
+              ], '8080'));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints',
+                0,
+                'loadBalanced'
+              ], true));
+
+          batch = batch.add(new Transaction(['id'], '/barfoo'));
+
+          expect(batch.reduce(Containers.JSONReducer.bind({})))
+          .toEqual([
+            {
+              name: 'container 1',
+              endpoints: [
+                {
+                  name: null,
+                  containerPort: 8080,
+                  labels: {
+                    'VIP_0': '/barfoo:8080'
+                  },
+                  hostPort: 0,
+                  protocol: [
+                    'tcp'
+                  ]
+                }
+              ]
+            }
+          ]);
+        });
+
+        it('should set the right custom vip even after id change', function () {
+          let batch = new Batch();
+
+          batch = batch.add(new Transaction(['containers'], 0, ADD_ITEM));
+
+          batch = batch.add(new Transaction(['network'], 'CONTAINER.foo'));
+          batch = batch.add(new Transaction(['id'], '/foobar'));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints'
+              ], 0, ADD_ITEM));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints',
+                0,
+                'containerPort'
+              ], '8080'));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints',
+                0,
+                'loadBalanced'
+              ], true));
+
+          batch =
+              batch.add(new Transaction([
+                'containers',
+                0,
+                'endpoints',
+                0,
+                'vip'
+              ], '1.3.3.7:8080'));
+
+          batch = batch.add(new Transaction(['id'], '/barfoo'));
+
+          expect(batch.reduce(Containers.JSONReducer.bind({})))
+          .toEqual([
+            {
+              name: 'container 1',
+              endpoints: [
+                {
+                  name: null,
+                  containerPort: 8080,
+                  labels: {
+                    'VIP_0': '1.3.3.7:8080'
+                  },
+                  hostPort: 0,
+                  protocol: [
+                    'tcp'
+                  ]
+                }
+              ]
+            }
+          ]);
+        });
+
+      });
+    });
   });
+
   // FormReducer is the same as JSONReducer
 });
