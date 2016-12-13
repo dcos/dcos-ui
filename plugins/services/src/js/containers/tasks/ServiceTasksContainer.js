4057989faa694f4d13c74c18dc92ed057d9eceb1
Mat Appelman <mat@mesosphere.io>
2016-12-13 11:21:15 -0800

    Adding filter for mesosXHRError

--- plugins/services/src/js/containers/tasks/ServiceTasksContainer.js
+++ plugins/services/src/js/containers/tasks/ServiceTasksContainer.js
@@ -1,3 +1,4 @@
+import {Hooks} from 'PluginSDK';
 import mixin from 'reactjs-mixin';
 import React from 'react';
 import {StoreMixin} from 'mesosphere-shared-reactjs';
@@ -19,6 +20,7 @@ class ServiceTasksContainer extends mixin(StoreMixin) {
     super(...arguments);
 
     this.state = {
+      errorMessage: null,
       lastUpdate: 0,
       mesosStateErrorCount: 0
     };
@@ -38,19 +40,33 @@ class ServiceTasksContainer extends mixin(StoreMixin) {
       || this.state.mesosStateErrorCount !== 0) {
 
       this.setState({
+        errorMessage: null,
         lastUpdate: Date.now(),
         mesosStateErrorCount: 0
       });
     }
   }
 
-  onStateStoreError() {
+  onStateStoreError(xhr) {
+    const errorMessage = Hooks.applyFilter(
+      'mesosStateErrorMessage',
+      xhr.status
+    );
+
+    if (errorMessage !== xhr.status) {
+      this.setState({errorMessage});
+
+      return;
+    }
+
     this.setState({
       mesosStateErrorCount: this.state.mesosStateErrorCount + 1
     });
   }
 
   render() {
+    const {errorMessage} = this.state;
+
     const isLoading = Object.keys(
       MesosStateStore.get('lastMesosState')
     ).length === 0;
@@ -61,8 +77,14 @@ class ServiceTasksContainer extends mixin(StoreMixin) {
       return <Loader />;
     }
 
+    // General Error
     if (hasError) {
       return <RequestErrorMsg />;
+    }
+
+    // Specific Mesos Error
+    if (errorMessage) {
+      return errorMessage;
     }
 
     let tasks = MesosStateStore.getTasksByService(this.props.service);
