a5b04ca6830e88cf4a5f74386b7aa7ff79a5613f
Philipp Hinrichsen <philipph@mesosphere.io>
2016-12-16 17:17:01 +0100

    Add Default scaling to pods

--- plugins/services/src/js/constants/DefaultPod.js
+++ plugins/services/src/js/constants/DefaultPod.js
@@ -1,10 +1,15 @@
 const DEFAULT_POD_RESOURCES = {cpus: 0.001, mem: 32};
+const DEFAULT_POD_SCALING = {kind: 'fixed', instances: 1};
 const DEFAULT_POD_CONTAINER =
-    {name: 'container-1', resources: DEFAULT_POD_RESOURCES};
-const DEFAULT_POD_SPEC = {containers: [DEFAULT_POD_CONTAINER]};
+  {name: 'container-1', resources: DEFAULT_POD_RESOURCES};
+const DEFAULT_POD_SPEC = {
+  containers: [DEFAULT_POD_CONTAINER],
+  scaling: DEFAULT_POD_SCALING
+};
 
 module.exports = {
   DEFAULT_POD_CONTAINER,
   DEFAULT_POD_RESOURCES,
+  DEFAULT_POD_SCALING,
   DEFAULT_POD_SPEC
 };
