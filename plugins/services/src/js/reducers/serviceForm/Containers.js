e230cafd40f669984acf276b7434c771138d5eb0
Philipp Hinrichsen <philipph@mesosphere.io>
2016-12-16 15:47:06 +0100

    Add usage of first networks item

--- plugins/services/src/js/reducers/serviceForm/Containers.js
+++ plugins/services/src/js/reducers/serviceForm/Containers.js
@@ -234,7 +234,7 @@ function containersParser(state) {
           )
         ]);
 
-        if (state.network.mode === Networking.type.CONTAINER.toLowerCase()) {
+        if (state.networks[0].mode === Networking.type.CONTAINER.toLowerCase()) {
           memo.push(new Transaction(
             ['containers', index, 'endpoints', endpointIndex, 'containerPort'],
             endpoint.containerPort
@@ -269,7 +269,7 @@ function containersParser(state) {
           ));
         }
 
-        if (state.network.mode === Networking.type.HOST.toLowerCase()) {
+        if (state.networks[0].mode === Networking.type.HOST.toLowerCase()) {
           memo.push(new Transaction(
             ['containers', index, 'endpoints', endpointIndex, 'protocol'],
             endpoint.protocol.join()
