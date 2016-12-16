bc475552f7103f4f0cab3e24f5eda8cec21b6c7f
Philipp Hinrichsen <philipph@mesosphere.io>
2016-12-16 18:15:08 +0100

    Add parseInt to check if index is 0 string or int

--- plugins/services/src/js/reducers/serviceForm/Containers.js
+++ plugins/services/src/js/reducers/serviceForm/Containers.js
@@ -317,7 +317,7 @@ module.exports = {
       this.appState.id = value;
     }
 
-    if (base === 'networks' && index === 0 && type === SET) {
+    if (base === 'networks' && parseInt(index) === 0 && type === SET) {
       const valueSplit = value.split('.');
 
       this.networkType = valueSplit[0];
