a127b1c7218b7651d722da8a46393c38ed1f75ec
Rafael Corral <me@rafaelcorral.com>
2016-12-19 16:28:46 -0800

    Fixes two bugs with task health and status
    1. We separated task health and status, but didn’t think about how other parts of the application use task health. This caused an issue where all tasks were shown as healthy.
    2. We weren’t polling marathon in the Nodes page, which caused task to show as “Unkown” health status until we went to a page that polled marathon.

--- plugins/nodes/src/js/pages/nodes/NodeDetailTaskTab.js
+++ plugins/nodes/src/js/pages/nodes/NodeDetailTaskTab.js
@@ -1,12 +1,52 @@
+import {DCOSStore} from 'foundation-ui';
 import React from 'react';
 import {routerShape} from 'react-router';
+
+import {
+  DCOS_CHANGE
+} from '../../../../../../src/js/constants/EventTypes';
 
 import MesosStateStore from '../../../../../../src/js/stores/MesosStateStore';
 import TasksContainer from '../../../../../services/src/js/containers/tasks/TasksContainer';
 
-class NodeDetailTaskTab extends React.Component {
-  render() {
+const METHODS_TO_BIND = [
+  'onStoreChange'
+];
 
+class NodeDetailTaskTab extends React.Component {
+  constructor() {
+    super(...arguments);
+
+    this.state = {
+      isLoading: true,
+      lastUpdate: 0
+    };
+
+    METHODS_TO_BIND.forEach((method) => {
+      this[method] = this[method].bind(this);
+    });
+  }
+
+  componentDidMount() {
+    DCOSStore.addChangeListener(DCOS_CHANGE, this.onStoreChange);
+  }
+
+  componentWillUnmount() {
+    DCOSStore.removeChangeListener(DCOS_CHANGE, this.onStoreChange);
+  }
+
+  onStoreChange() {
+    // Throttle updates from DCOSStore
+    if (Date.now() - this.state.lastUpdate > 1000
+      || (!!DCOSStore.dataProcessed && this.state.isLoading)) {
+      this.setState({
+        isLoading: !(!!DCOSStore.dataProcessed),
+        lastUpdate: Date.now()
+      });
+    }
+  }
+
+  render() {
     const {nodeID} = this.props.params;
     let tasks = MesosStateStore.getTasksFromNodeID(nodeID);
 
