637f091496ec095a7acec939a5e1f277bac25142
John Furrow <johnf@mesosphere.io>
2016-12-19 16:47:04 -0800

    Extract delay logic to utility

--- plugins/services/src/js/components/ServiceStatusWarning.js
+++ plugins/services/src/js/components/ServiceStatusWarning.js
@@ -2,12 +2,11 @@ import React from 'react';
 import {Tooltip} from 'reactjs-components';
 
 import DateUtil from '../../../../../src/js/utils/DateUtil';
+import DeclinedOffersUtil from '../utils/DeclinedOffersUtil';
 import Icon from '../../../../../src/js/components/Icon';
 import Pod from '../structs/Pod';
 import Service from '../structs/Service';
 import ServiceTree from '../structs/ServiceTree';
-
-const DEPLOYMENT_WARNING_DELAY_MS = 1000 * 60 * 5;
 
 const getTooltipContent = (timeWaiting, showDebugInstruction) => {
   const additionalCopy = showDebugInstruction
@@ -20,23 +19,20 @@ const getTooltipContent = (timeWaiting, showDebugInstruction) => {
 const ServiceStatusWarning = ({item, showDebugInstruction}) => {
   const queue = item.getQueue();
 
-  if (queue != null) {
+  if (queue != null
+    && DeclinedOffersUtil.shouldDisplayDeclinedOffersWarning(queue)) {
     const waitingSince = DateUtil.strToMs(queue.since);
     const timeWaiting = Date.now() - waitingSince;
 
-    // If the item has been waiting for less than five minutes, we don't
-    // display the warning.
-    if (timeWaiting >= DEPLOYMENT_WARNING_DELAY_MS) {
-      return (
-        <Tooltip
-          content={getTooltipContent(timeWaiting, showDebugInstruction)}
-          width={250}
-          wrapText={true}
-          wrapperClassName="tooltip-wrapper status-waiting-indicator">
-          <Icon color="red" id="yield" size="mini" />
-        </Tooltip>
-      );
-    }
+    return (
+      <Tooltip
+        content={getTooltipContent(timeWaiting, showDebugInstruction)}
+        width={250}
+        wrapText={true}
+        wrapperClassName="tooltip-wrapper status-waiting-indicator">
+        <Icon color="red" id="yield" size="mini" />
+      </Tooltip>
+    );
   }
 
   return <noscript />;
