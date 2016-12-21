13a2e729dde8b7b7a77fc160b1da2f0e270fd089
Dmitriy Rozhkov <drozhkov@mesosphere.io>
2016-12-21 17:28:16 +0100

    Add requirePorts reducer

---
+++ plugins/services/src/js/reducers/serviceForm/RequirePorts.js
@@ -0,0 +1,17 @@
+import {SET} from '../../../../../../src/js/constants/TransactionTypes';
+
+module.exports = {
+  JSONReducer(state, {path, type, value}) {
+    const [_base, index, field] = path;
+
+    if (!this.requriePorts) {
+      this.requriePorts = [];
+    }
+
+    if (field === 'hostPort' && type === SET) {
+      this.requriePorts[index] = value > 0;
+    }
+
+    return this.requriePorts.find((entry) => entry);
+  }
+};
---
+++ plugins/services/src/js/reducers/serviceForm/__tests__/RequirePorts-test.js
@@ -0,0 +1,49 @@
+const RequirePorts = require('../RequirePorts');
+const Batch = require('../../../../../../../src/js/structs/Batch');
+const Transaction = require('../../../../../../../src/js/structs/Transaction');
+const {SET} =
+  require('../../../../../../../src/js/constants/TransactionTypes');
+
+describe('RequirePorts', function () {
+  describe('#JSONReducer', function () {
+    it('it should return undefined as default', function () {
+      let batch = new Batch();
+
+      batch = batch.add(new Transaction(['id'], 'foo'));
+
+      expect(batch.reduce(RequirePorts.JSONReducer.bind({}))).toEqual(undefined);
+    });
+
+    it('should return true if there is an endpoint with requested host port', function () {
+      let batch = new Batch();
+
+      batch = batch.add(new Transaction(['portDefinitions', 0, 'hostPort'], 80, SET));
+
+      expect(batch.reduce(RequirePorts.JSONReducer.bind({})))
+      .toEqual(true);
+    });
+
+    it('should return true if there is at least one endpoint with requested host port', function () {
+      let batch = new Batch();
+
+      batch = batch.add(new Transaction(['portDefinitions', 0, 'hostPort'], 80, SET));
+      batch = batch.add(new Transaction(['portDefinitions', 0, 'hostPort'], 0, SET));
+      batch = batch.add(new Transaction(['portDefinitions', 1, 'hostPort'], 8080, SET));
+
+      expect(batch.reduce(RequirePorts.JSONReducer.bind({})))
+      .toEqual(true);
+    });
+
+    it('should return undefined if all of the endpoints do not request host port', function () {
+      let batch = new Batch();
+
+      batch = batch.add(new Transaction(['portDefinitions', 0, 'hostPort'], 0, SET));
+      batch = batch.add(new Transaction(['portDefinitions', 1, 'hostPort'], 0, SET));
+      batch = batch.add(new Transaction(['portDefinitions', 2, 'hostPort'], 0, SET));
+
+      expect(batch.reduce(RequirePorts.JSONReducer.bind({})))
+      .toEqual(undefined);
+    });
+
+  });
+});
