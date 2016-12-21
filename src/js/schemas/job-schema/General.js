e1aa6e4baa955e06790568dd47d0fba28ec06ac6
Orlando Hohmeier <orlando@mesosphere.io>
2016-12-21 15:17:39 +0100

    Use job defaults instead of Mesos constants
    Use the new job defaults instead of Mesos constants to properly define
    job defaults that match the API.
    
    Closes DCOS-9310

--- src/js/schemas/job-schema/General.js
+++ src/js/schemas/job-schema/General.js
@@ -5,6 +5,7 @@ import React from 'react';
 import JobValidatorUtil from '../../utils/JobValidatorUtil';
 import MesosConstants from '../../constants/MesosConstants';
 import ValidatorUtil from '../../utils/ValidatorUtil';
+import JobResources from '../../constants/JobResources';
 
 const General = {
   title: 'General',
@@ -44,7 +45,7 @@ const General = {
       properties: {
         cpus: {
           title: 'CPUs',
-          default: MesosConstants.MIN_CPUS,
+          default: JobResources.DEFAULT_CPUS,
           description: 'The amount of CPUs the job requires',
           type:'number',
           getter(job) {
@@ -64,7 +65,7 @@ const General = {
         },
         mem: {
           title: 'Mem (MiB)',
-          default: MesosConstants.MIN_MEM,
+          default: JobResources.DEFAULT_MEM,
           type: 'number',
           getter(job) {
             return `${job.getMem()}`;
@@ -83,7 +84,7 @@ const General = {
         },
         disk: {
           title: 'Disk (MiB)',
-          default: 0,
+          default: JobResources.DEFAULT_DISK,
           type: 'number',
           getter(job) {
             return `${job.getDisk()}`;
