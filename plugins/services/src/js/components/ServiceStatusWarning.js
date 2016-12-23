c997d194b5968f3ca2172dc3b458bf97e25c654e
Philipp Hinrichsen <philipph@mesosphere.io>
2016-12-23 16:35:51 +0100

    Change to Inheritance

--- plugins/services/src/js/components/ServiceStatusWarning.js
+++ plugins/services/src/js/components/ServiceStatusWarning.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Tooltip} from 'reactjs-components';
 
 import DateUtil from '../../../../../src/js/utils/DateUtil';
@@ -8,30 +8,32 @@ import Pod from '../structs/Pod';
 import Service from '../structs/Service';
 import ServiceTree from '../structs/ServiceTree';
 
-const getTooltipContent = (timeWaiting) => {
-  return `DC/OS has been waiting for resources and unable to complete this deployment for ${DateUtil.getDuration(timeWaiting, null)}.`;
-};
-
-const ServiceStatusWarning = ({item}) => {
-  const queue = item.getQueue();
-
-  if (queue != null
-    && DeclinedOffersUtil.shouldDisplayDeclinedOffersWarning(queue)) {
-    const waitingSince = DateUtil.strToMs(queue.since);
-    const timeWaiting = Date.now() - waitingSince;
-
-    return (
-      <Tooltip
-        content={getTooltipContent(timeWaiting)}
-        width={250}
-        wrapText={true}
-        wrapperClassName="tooltip-wrapper status-waiting-indicator">
-        <Icon color="red" id="yield" size="mini" />
-      </Tooltip>
-    );
+class ServiceStatusWarning extends Component {
+  getTooltipContent(timeWaiting) {
+    return `DC/OS has been waiting for resources and unable to complete this deployment for ${DateUtil.getDuration(timeWaiting, null)}.`;
   }
+  render() {
+    const {item} = this.props;
+    const queue = item.getQueue();
 
-  return <noscript />;
+    if (queue != null
+      && DeclinedOffersUtil.shouldDisplayDeclinedOffersWarning(queue)) {
+      const waitingSince = DateUtil.strToMs(queue.since);
+      const timeWaiting = Date.now() - waitingSince;
+
+      return (
+        <Tooltip
+          content={this.getTooltipContent(timeWaiting)}
+          width={250}
+          wrapText={true}
+          wrapperClassName="tooltip-wrapper status-waiting-indicator">
+          <Icon color="red" id="yield" size="mini" />
+        </Tooltip>
+      );
+    }
+
+    return <noscript />;
+  }
 };
 
 ServiceStatusWarning.propTypes = {
--- plugins/services/src/js/components/ServiceStatusWarningWithDebugInstruction.js
+++ plugins/services/src/js/components/ServiceStatusWarningWithDebugInstruction.js
@@ -1,47 +1,12 @@
-import React from 'react';
-import {Tooltip} from 'reactjs-components';
-
 import DateUtil from '../../../../../src/js/utils/DateUtil';
-import DeclinedOffersUtil from '../utils/DeclinedOffersUtil';
-import Icon from '../../../../../src/js/components/Icon';
-import Pod from '../structs/Pod';
-import Service from '../structs/Service';
-import ServiceTree from '../structs/ServiceTree';
+import ServiceStatusWarning from './ServiceStatusWarning';
 
-const getTooltipContent = (timeWaiting) => {
-  const additionalCopy = ' See more information in the debug tab.';
+class ServiceStatusWarningWithDebugInstruction extends ServiceStatusWarning {
+  getTooltipContent(timeWaiting) {
+    const additionalCopy = ' See more information in the debug tab.';
 
-  return `DC/OS has been waiting for resources and unable to complete this deployment for ${DateUtil.getDuration(timeWaiting, null)}.${additionalCopy}`;
-};
-
-const ServiceStatusWarningWithDebugInstruction = ({item}) => {
-  const queue = item.getQueue();
-
-  if (queue != null
-    && DeclinedOffersUtil.shouldDisplayDeclinedOffersWarning(queue)) {
-    const waitingSince = DateUtil.strToMs(queue.since);
-    const timeWaiting = Date.now() - waitingSince;
-
-    return (
-      <Tooltip
-        content={getTooltipContent(timeWaiting)}
-        width={250}
-        wrapText={true}
-        wrapperClassName="tooltip-wrapper status-waiting-indicator">
-        <Icon color="red" id="yield" size="mini" />
-      </Tooltip>
-    );
+    return `DC/OS has been waiting for resources and unable to complete this deployment for ${DateUtil.getDuration(timeWaiting, null)}.${additionalCopy}`;
   }
-
-  return <noscript />;
-};
-
-ServiceStatusWarningWithDebugInstruction.propTypes = {
-  item: React.PropTypes.oneOfType([
-    React.PropTypes.instanceOf(Service),
-    React.PropTypes.instanceOf(ServiceTree),
-    React.PropTypes.instanceOf(Pod)
-  ])
-};
+}
 
 module.exports = ServiceStatusWarningWithDebugInstruction;
