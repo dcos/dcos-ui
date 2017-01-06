944856c0ef8d5275943ad901ed66f054d7164fe5
John Furrow <johnf@mesosphere.io>
2017-01-06 15:20:45 -0800

    Display declined offers warning for app groups

--- plugins/services/src/js/components/ServiceStatusWarning.js
+++ plugins/services/src/js/components/ServiceStatusWarning.js
@@ -7,27 +7,42 @@ import Icon from '../../../../../src/js/components/Icon';
 import Pod from '../structs/Pod';
 import Service from '../structs/Service';
 import ServiceTree from '../structs/ServiceTree';
+import StringUtil from '../../../../../src/js/utils/StringUtil';
 
 class ServiceStatusWarning extends Component {
-  getTooltipContent(timeWaiting) {
-    return `DC/OS has been waiting for resources and is unable to complete this deployment for ${DateUtil.getDuration(timeWaiting, null)}.`;
+  getTooltip(content) {
+    return (
+      <Tooltip
+        content={content}
+        width={250}
+        wrapText={true}
+        wrapperClassName="tooltip-wrapper status-waiting-indicator">
+        <Icon color="red" id="yield" size="mini" />
+      </Tooltip>
+    );
   }
+
   render() {
     const {item} = this.props;
-    const queue = item.getQueue();
 
-    if (DeclinedOffersUtil.shouldDisplayDeclinedOffersWarning(queue)) {
-      const timeWaiting = DeclinedOffersUtil.getTimeWaiting(queue);
+    if (item instanceof ServiceTree) {
+      const appsWithDeclinedOffers = DeclinedOffersUtil
+        .getAppsWithDeclinedOffersFromServiceTree(item);
+      const appCount = appsWithDeclinedOffers.length;
 
-      return (
-        <Tooltip
-          content={this.getTooltipContent(timeWaiting)}
-          width={250}
-          wrapText={true}
-          wrapperClassName="tooltip-wrapper status-waiting-indicator">
-          <Icon color="red" id="yield" size="mini" />
-        </Tooltip>
-      );
+      if (appCount > 0) {
+        const appsNoun = StringUtil.pluralize('service', appCount);
+
+        return this.getTooltip(`DC/OS is waiting for resources and is unable to complete the deployment of ${appCount} ${appsNoun} in this group.`);
+      }
+    } else {
+      const queue = item.getQueue();
+
+      if (DeclinedOffersUtil.shouldDisplayDeclinedOffersWarning(queue)) {
+        const timeWaiting = DeclinedOffersUtil.getTimeWaiting(queue);
+
+        return this.getTooltip(`DC/OS has been waiting for resources and is unable to complete this deployment for ${DateUtil.getDuration(timeWaiting, null)}.`);
+      }
     }
 
     return <noscript />;
