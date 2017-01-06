2da1d6b7fba9a1d98ff6f6475d2dc0e7619b102a
mlunoe <michael.lunoe@gmail.com>
2017-01-06 14:21:12 -0800

    Update download endpoint for logs

--- src/js/utils/SystemLogUtil.js
+++ src/js/utils/SystemLogUtil.js
@@ -3,9 +3,10 @@ import Config from '../config/Config';
 const paramOptions = [
   'cursor',
   'limit',
+  'postfix',
+  'read_reverse',
   'skip_prev',
-  'skip_next',
-  'read_reverse'
+  'skip_next'
 ];
 
 const SystemLogUtil = {
@@ -17,18 +18,20 @@ const SystemLogUtil = {
    * @param {String} [options.cursor] ID of the cursor to request entries from
    * @param {String} [options.subscriptionID] ID to emit events to,
    *   if omitted a new one will be created
+   * @param {String} [options.containerID] ID for container to retrieve logs from
+   * @param {String} [options.executorID] ID for executor to retrieve logs from
+   * @param {String} [options.frameworkID] ID for framework to retrieve logs from
+   * @param {Object} [options.filter] filter parameters to add to URL
+   * @param {Number} [options.limit] limit how many entries to receive
+   * @param {Number} [options.postfix] postifx to add to the download name
+   * @param {String} [options.read_reverse] will read events in reverse order if set to true
    * @param {Number} [options.skip_prev] how many entries backwards to look
    * @param {Number} [options.skip_next] how many entries to look ahead
-   * @param {Number} [options.limit] limit how many entries to receive
-   * @param {Object} [options.filter] filter parameters to add to URL
-   * @param {String} [options.frameworkID] ID for framework to retrieve logs from
-   * @param {String} [options.executorID] ID for executor to retrieve logs from
-   * @param {String} [options.containerID] ID for container to retrieve logs from
-   * @param {String} [options.read_reverse] will read events in reverse order if set to true
-   * @param {Boolean} [isStream = true] whether to use stream or range endpoint
+   * @param {Boolean} [isStream = true] whether to use stream or range base
+   * @param {String} [endpoint = ''] add aditional endpoint to the url
    * @returns {String} URL for system logs request
    */
-  getUrl(nodeID, options, isStream = true) {
+  getUrl(nodeID, options, isStream = true, endpoint = '') {
     const {filter = {}} = options;
 
     const range = paramOptions.reduce((memo, key) => {
@@ -49,9 +52,9 @@ const SystemLogUtil = {
       return memo;
     }, range);
 
-    let endpoint = 'range';
+    let base = 'range';
     if (isStream) {
-      endpoint = 'stream';
+      base = 'stream';
     }
 
     const idArray = ['framework', 'executor', 'container'].reduce(function (memo, key) {
@@ -63,7 +66,7 @@ const SystemLogUtil = {
       return memo;
     }, []);
 
-    return `${Config.logsAPIPrefix}/${nodeID}/logs/v1/${endpoint}/${idArray.join('/')}?${paramsArray.join('&')}`;
+    return `${Config.logsAPIPrefix}/${nodeID}/logs/v1/${base}/${idArray.join('/')}${endpoint}?${paramsArray.join('&')}`;
   }
 };
 
