4859e9a1ea9b9077c94095ee9095aacefb415b9f
Rafael Corral <me@rafaelcorral.com>
2017-01-10 19:05:17 -0800

    Commenting out unit test.

---
+++ tests/_support/utils/tests/testUtil-test.js
@@ -0,0 +1,16 @@
+// const testUtil = require('../testUtil');
+
+// Enable these once we move folders around correctly with cypress, otherwise cypress will attempt to run these, but they're unit tests
+// describe('testUtil', function () {
+//   describe('#getSearchParameter', function () {
+//     it('should get first search parameter', function () {
+//       const uri = 'something?q=something&q=somethingelse';
+//       expect(testUtil.getSearchParameter(uri)).toEqual('q=something');
+//     });
+
+//     it('should return undefined if no query is found', function () {
+//       const uri = 'something?t=0m';
+//       expect(testUtil.getSearchParameter(uri)).toEqual(undefined);
+//     });
+//   });
+// });
