6b0467082c2bfd1e07516724b68a33087f91161a
Rafael Corral <me@rafaelcorral.com>
2017-01-12 15:23:11 -0800

    Fixes junit reporter.

--- jest/setupTestFramework.js
+++ jest/setupTestFramework.js
@@ -1,7 +1,13 @@
+import path from 'path';
+
 if (process.env.TEAMCITY_VERSION != null) {
   var jasmineReporters = require('jasmine-reporters/src/teamcity_reporter');
   jasmine.getEnv().addReporter(new jasmineReporters.TeamCityReporter());
 } else if (process.env.JENKINS_VERSION != null) {
   var jasmineReporters = require('jasmine-reporters/src/junit_reporter');
-  jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter());
+  jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+    savePath: path.join(__dirname, 'testresults'),
+    consolidateAll: false,
+    consolidate: true
+  }));
 }
