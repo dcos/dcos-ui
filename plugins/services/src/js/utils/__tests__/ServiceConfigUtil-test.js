f23d1c8780353c0ef803249ec446455df8664c87
Dmitriy Rozhkov <drozhkov@mesosphere.io>
2017-01-12 16:39:08 +0100

    Extract VIP label related functions/constant

--- plugins/services/src/js/utils/__tests__/ServiceConfigUtil-test.js
+++ plugins/services/src/js/utils/__tests__/ServiceConfigUtil-test.js
@@ -3,6 +3,39 @@ jest.dontMock('../ServiceConfigUtil');
 const ServiceConfigUtil = require('../ServiceConfigUtil');
 
 describe('ServiceConfigUtil', function () {
+
+  describe('#matchVIPLabel', function () {
+
+    it('returns true', function () {
+      expect(
+        ServiceConfigUtil.matchVIPLabel('VIP_1')
+      ).toBeTruthy();
+    });
+
+    it('returns false', function () {
+      expect(
+        ServiceConfigUtil.matchVIPLabel('LABEL')
+      ).toBeFalsy();
+    });
+
+  });
+
+  describe('#findVIPLabel', function () {
+
+    it('returns label', function () {
+      expect(
+        ServiceConfigUtil.findVIPLabel({VIP_1: ''})
+      ).toEqual('VIP_1');
+    });
+
+    it('returns undefined', function () {
+      expect(
+        ServiceConfigUtil.findVIPLabel({LABEL: ''})
+      ).toEqual(undefined);
+    });
+
+  });
+
   describe('#hasVIPLabel', function () {
 
     it('returns true', function () {
