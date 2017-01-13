7cfd78a29bb1dbfbe22bab3ba405ea66c2553222
Orlando Hohmeier <orlando@mesosphere.io>
2017-01-13 12:29:30 +0100

    Rename test results directory

--- jest/setupTestFramework.js
+++ jest/setupTestFramework.js
@@ -6,7 +6,7 @@ if (process.env.TEAMCITY_VERSION != null) {
 } else if (process.env.JENKINS_VERSION != null) {
   var jasmineReporters = require('jasmine-reporters/src/junit_reporter');
   jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
-    savePath: path.join(__dirname, 'testresults'),
+    savePath: path.join(__dirname, 'test-results'),
     consolidateAll: false,
     consolidate: true
   }));
