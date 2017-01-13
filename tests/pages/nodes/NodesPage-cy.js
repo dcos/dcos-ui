ed931fc6c2f32602e2347ce5cf4df9ee8fad69b0
Rafael Corral <me@rafaelcorral.com>
2017-01-13 11:33:15 -0800

    Cleaned up tests to run more efficiently

--- tests/pages/nodes/NodesPage-cy.js
+++ tests/pages/nodes/NodesPage-cy.js
@@ -1,37 +1,41 @@
 describe('Nodes Page', function () {
 
-  beforeEach(function () {
-    cy.configureCluster({
-      mesos: '1-for-each-health',
-      nodeHealth: true
-    });
-    cy.visitUrl({url: '/nodes'});
-  });
-
   context('Filters nodes table', function () {
 
+    beforeEach(function () {
+      cy.configureCluster({
+        mesos: '1-for-each-health',
+        nodeHealth: true
+      });
+      cy.visitUrl({url: '/nodes'});
+      cy.get('tbody tr td:first-child').as('hostnames');
+    });
+
     it('should show all nodes', function () {
-      cy.get('tbody tr').should('contain', 'dcos-01');
-      cy.get('tbody tr').should('contain', '167.114.218.155');
-      cy.get('tbody tr').should('contain', '167.114.218.156');
+      cy.get('@hostnames')
+        .should('contain', 'dcos-01')
+        .should('contain', '167.114.218.155')
+        .should('contain', '167.114.218.156');
     });
 
     it('should only show healthy node', function () {
       cy.get('.filter-bar').as('filterBar');
       cy.get('@filterBar').contains('Healthy').click();
 
-      cy.get('tbody tr').should('not.contain', 'dcos-01');
-      cy.get('tbody tr').should('not.contain', '167.114.218.156');
-      cy.get('tbody tr').should('contain', '167.114.218.155');
+      cy.get('@hostnames')
+        .should('not.contain', 'dcos-01')
+        .should('not.contain', '167.114.218.156')
+        .should('contain', '167.114.218.155');
     });
 
     it('should only show unhealthy node', function () {
       cy.get('.filter-bar').as('filterBar');
       cy.get('@filterBar').contains('Unhealthy').click();
 
-      cy.get('tbody tr').should('not.contain', 'dcos-01');
-      cy.get('tbody tr').should('not.contain', '167.114.218.155');
-      cy.get('tbody tr').should('contain', '167.114.218.156');
+      cy.get('@hostnames')
+        .should('not.contain', 'dcos-01')
+        .should('not.contain', '167.114.218.155')
+        .should('contain', '167.114.218.156');
     });
 
     it('should only show nodes with service', function () {
@@ -39,59 +43,75 @@ describe('Nodes Page', function () {
       cy.get('@filterBar').contains('Filter by Service').click();
       cy.get('.dropdown-menu').contains('cassandra-healthy').click();
 
-      cy.get('tbody tr').should('not.contain', '167.114.218.156');
-      cy.get('tbody tr').should('not.contain', '167.114.218.155');
-      cy.get('tbody tr').should('contain', 'dcos-01');
+      cy.get('@hostnames')
+        .should('not.contain', '167.114.218.156')
+        .should('not.contain', '167.114.218.155')
+        .should('contain', 'dcos-01');
     });
 
   });
 
-  context('Filters nodes grid', function () {
+  context('Nodes grid', function () {
 
     beforeEach(function () {
-      cy.visitUrl({url: '/nodes/grid'});
+      cy.configureCluster({
+        mesos: '1-for-each-health',
+        nodeHealth: true
+      });
     });
 
-    it('should show all nodes', function () {
+    it('navigates to grid', function () {
       // Navigate to the grid using button
       cy.visitUrl({url: '/nodes'});
       cy.get('.filter-bar').as('filterBar');
       cy.get('@filterBar').contains('Grid').click();
 
-      cy.get('.nodes-grid-dials').should('contain', '3%');
-      cy.get('.nodes-grid-dials').should('contain', '5%');
-      cy.get('.nodes-grid-dials').should('contain', '19%');
+      cy.get('.nodes-grid-dials')
+        .should('contain', '3%')
+        .should('contain', '5%')
+        .should('contain', '19%');
     });
 
-    it('should only show cassandra-healthy nodes', function () {
-      cy.get('.filter-bar').as('filterBar');
-      cy.get('@filterBar').contains('Filter by Service').click();
-      cy.get('.dropdown-menu').contains('cassandra-healthy').click();
+    context('Filters nodes grid', function () {
 
-      cy.get('.nodes-grid-dials').should('contain', '3%');
-      cy.get('.nodes-grid-dials').should('not.contain', '5%');
-      cy.get('.nodes-grid-dials').should('not.contain', '19%');
-    });
+      beforeEach(function () {
+        cy.visitUrl({url: '/nodes/grid'});
+      });
 
-    it('should not display any nodes', function () {
-      cy.get('.filter-bar').as('filterBar');
-      cy.get('@filterBar').contains('Filter by Service').click();
-      cy.get('.dropdown-menu').contains('cassandra-na').click();
+      it('should only show cassandra-healthy nodes', function () {
+        cy.get('.filter-bar').as('filterBar');
+        cy.get('@filterBar').contains('Filter by Service').click();
+        cy.get('.dropdown-menu').contains('cassandra-healthy').click();
 
-      cy.get('.nodes-grid-dials').should('not.contain', '3%');
-      cy.get('.nodes-grid-dials').should('not.contain', '5%');
-      cy.get('.nodes-grid-dials').should('not.contain', '19%');
-    });
+        cy.get('.nodes-grid-dials')
+          .should('contain', '3%')
+          .should('not.contain', '5%')
+          .should('not.contain', '19%');
+      });
 
-    it('should only show unhealthy node', function () {
-      cy.get('.filter-bar').as('filterBar');
-      cy.get('@filterBar').contains('Filter by Service').click();
-      cy.get('.dropdown-menu').contains('cassandra-unhealthy').click();
-      cy.get('@filterBar').contains('Healthy').click();
+      it('should not display any nodes', function () {
+        cy.get('.filter-bar').as('filterBar');
+        cy.get('@filterBar').contains('Filter by Service').click();
+        cy.get('.dropdown-menu').contains('cassandra-na').click();
 
-      cy.get('.nodes-grid-dials').should('not.contain', '3%');
-      cy.get('.nodes-grid-dials').should('contain', '5%');
-      cy.get('.nodes-grid-dials').should('not.contain', '19%');
+        cy.get('.nodes-grid-dials')
+          .should('not.contain', '3%')
+          .should('not.contain', '5%')
+          .should('not.contain', '19%');
+      });
+
+      it('should only show unhealthy node', function () {
+        cy.get('.filter-bar').as('filterBar');
+        cy.get('@filterBar').contains('Filter by Service').click();
+        cy.get('.dropdown-menu').contains('cassandra-unhealthy').click();
+        cy.get('@filterBar').contains('Healthy').click();
+
+        cy.get('.nodes-grid-dials')
+          .should('not.contain', '3%')
+          .should('contain', '5%')
+          .should('not.contain', '19%');
+      });
+
     });
 
   });
