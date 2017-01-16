34f6ebc5f5b6edfdfc6604a39216f4dbd6405e61
Orlando Hohmeier <orlando@mesosphere.io>
2017-01-16 20:41:09 +0100

    Fix the string to hostname util
    Introduce a new `stringToLabel` util and change the way the
    `stringToHostname` util works to align them with the respective DC/OS
    component. The new implementation follows RFC-952 and mirrors the
    `dcos/mesos_state` implementation.
    
    Closes DCOS-11890

--- plugins/services/src/js/utils/HostUtil.js
+++ plugins/services/src/js/utils/HostUtil.js
@@ -1,32 +1,80 @@
 const HostUtil = {
+
+  /**
+   * Util to "transform" a string to a valid hostname following the RFC-952
+   * standard. It mirrors the `dcos/mesos_state` implementation.
+   * The util  will remove empty labels and use the string to label util
+   * to ensure that labels don't include invalid characters and don't exceed
+   * the character limit.
+   *
+   * @param {string} string
+   * @return {string} hostname
+   */
   stringToHostname(string) {
-    const HOSTNAME_MAX_LENGTH = 63;
+    if (string == null) {
+
+      return null;
+    }
+
+    return string
+      .split('.')
+      .filter(function (string) {
+        return string !== '';
+      })
+      .map(HostUtil.stringToLabel)
+      .join('.');
+  },
+
+  /**
+   * Util to "convert" a given string to a RFC-952 compliant DNS label.
+   * It mirrors the respective `dcos/mesos_state` implementation.
+   * The util  will strip invalid characters and ensures that the label don't
+   * exceed the character limit.
+   *
+   * @param {string} string
+   * @return {string} label
+   */
+  stringToLabel(string) {
+    const LABEL_MAX_LENGTH = 63;
 
     if (string == null) {
-      return string;
+      return '';
     }
 
-    // Strip or convert illegal characters
-    let host = string.toLowerCase().replace(/[_\.]/g, '-').replace(/[^a-z0-9-]/g, '');
+    string = string.toLowerCase();
 
-    // Strip symbols from beginning
-    while (host.startsWith('-')) {
-      host = host.slice(1);
+    // Strip all invalid character including leading and trailing dashes
+    // or replace them with dashes.
+    string = string.replace(/(_)|^-+|-+$|[^a-z0-9-]/g, function (match, p1) {
+
+      // Replace underscores (first capture group) with dashes
+      if (p1 != null) {
+        return '-';
+      }
+
+      // Strip any other invalid character
+      return '';
+    });
+
+    // Trim labels if they exceed the allowed max length
+    if (string.length > LABEL_MAX_LENGTH) {
+
+      // Remove all dashes and groups of dashes that have an offset larger or
+      // equal to the allowed max length before trimming the label to ensure
+      // that no label ends with a dash.
+      string = string.replace(/[-]+/g, function (match, offset) {
+        if (offset + match.length >= LABEL_MAX_LENGTH) {
+          return '';
+        }
+
+        return match;
+      });
+
+      // Trim the label to the allowed max length
+      string = string.slice(0, LABEL_MAX_LENGTH);
     }
 
-    // Strip symbols from middle if greater than max length
-    while (host.length > HOSTNAME_MAX_LENGTH && host.charAt(HOSTNAME_MAX_LENGTH - 1) === '-') {
-      host = host.slice(0, HOSTNAME_MAX_LENGTH - 1).concat(host.slice(HOSTNAME_MAX_LENGTH));
-    }
-
-    host = host.slice(0, HOSTNAME_MAX_LENGTH);
-
-    // Strip symbols from end
-    while (host.endsWith('-')) {
-      host = host.slice(0, host.length - 1);
-    }
-
-    return host.slice(0, HOSTNAME_MAX_LENGTH);
+    return string;
   }
 };
 
