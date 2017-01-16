dec85ccc897b5138f9fa29f5b80c7285b4dc2033
Orlando Hohmeier <orlando@mesosphere.io>
2017-01-16 19:19:47 +0100

    Align tests with 80 char per line rule

--- src/js/utils/__tests__/HostUtil-test.js
+++ src/js/utils/__tests__/HostUtil-test.js
@@ -6,28 +6,32 @@ describe('HostUtil', function () {
 
   describe('#stringToHostname', function () {
 
-    it('should strip out invalid characters from the middle', function () {
+    it('should strip invalid chars', function () {
       const result = HostUtil.stringToHostname('fd%gsf---gs7-f$gs--d7fddg-123');
 
       expect(result).toEqual('fdgsf---gs7-fgs--d7fddg-123');
     });
 
-    it('should strip out invalid characters from the start', function () {
+    it('should strip invalid chars from the start', function () {
       const result = HostUtil.stringToHostname('-@4abc123');
 
       expect(result).toEqual('4abc123');
     });
 
-    it('should strip out invalid characters from the end', function () {
+    it('should strip invalid chars from the end', function () {
       const result = HostUtil.stringToHostname('4abc123.');
 
       expect(result).toEqual('4abc123');
     });
 
-    it('should strip invalid characters from the start and end', function () {
-      const result = HostUtil.stringToHostname('##fdgsf---gs7-fgs--d7fddg123456789012345678901234567890123456789-');
+    it('should strip invalid chars from the start and end', function () {
+      const result = HostUtil.stringToHostname(
+        '##fdgsf---gs7-fgs--d7fddg123456789012345678901234567890123456789-'
+      );
 
-      expect(result).toEqual('fdgsf---gs7-fgs--d7fddg123456789012345678901234567890123456789');
+      expect(result).toEqual(
+        'fdgsf---gs7-fgs--d7fddg123456789012345678901234567890123456789'
+      );
     });
 
     it('should return valid hostnames', function () {
@@ -42,22 +46,44 @@ describe('HostUtil', function () {
       expect(result).toEqual('89fdgsf---gs7-fgs--d7fddg-123');
     });
 
-    it('should strip invalid characters and trim to 63 characters and remove invalid end characters', function () {
-      const result = HostUtil.stringToHostname('fd%gsf---gs7-f$gs--d7fddg123456789012345678901234567890123456789-123');
+    it('should strip invalid chars and trim string to 63 chars', function () {
+      const result = HostUtil.stringToHostname(
+        'fd%gsf---gs7-f$gs--d7fddg123456789012345678901234567890123456789-123'
+      );
 
-      expect(result).toEqual('fdgsf---gs7-fgs--d7fddg1234567890123456789012345678901234567891');
+      expect(result).toEqual(
+        'fdgsf---gs7-fgs--d7fddg1234567890123456789012345678901234567891'
+      );
     });
 
-    it('should strip invalid start characters and trim to 63 characters and remove invalid end characters', function () {
-      const result = HostUtil.stringToHostname('%%fdgsf---gs7-fgs--d7fddg123456789012345678901234567890123456789---123');
+    it('should strip invalid chars from start and trim string', function () {
+      const result = HostUtil.stringToHostname(
+        '%%fdgsf---gs7-fgs--d7fddg123456789012345678901234567890123456789---123'
+      );
 
-      expect(result).toEqual('fdgsf---gs7-fgs--d7fddg1234567890123456789012345678901234567891');
+      expect(result).toEqual(
+        'fdgsf---gs7-fgs--d7fddg1234567890123456789012345678901234567891'
+      );
     });
 
-    it('should return valid strings', function () {
-      const result = HostUtil.stringToHostname('a--------------------------------------------------------------b');
+    it('should strip dashes from the end and trim string', function () {
+      const result = HostUtil.stringToHostname(
+        '%%fdgsf---gs7-fgs--d7fddg123456789012345678901234567890123456789---123'
+      );
 
-      expect(result).toEqual('a-------------------------------------------------------------b');
+      expect(result).toEqual(
+        'fdgsf---gs7-fgs--d7fddg1234567890123456789012345678901234567891'
+      );
+    });
+
+    it('should not alter valid hostnames', function () {
+      const result = HostUtil.stringToHostname(
+        'a--------------------------------------------------------------b'
+      );
+
+      expect(result).toEqual(
+        'a-------------------------------------------------------------b'
+      );
     });
 
     it('should lowercase the string', function () {
