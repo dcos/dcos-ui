51fa61104f5d576449c7d3ae2d7e8be9e4bd88eb
Orlando Hohmeier <orlando@mesosphere.io>
2017-01-16 20:41:09 +0100

    Fix the string to hostname util
    Introduce a new `stringToLabel` util and change the way the
    `stringToHostname` util works to align them with the respective DC/OS
    component. The new implementation follows RFC-952 and mirrors the
    `dcos/mesos_state` implementation.
    
    Closes DCOS-11890

--- src/js/utils/__tests__/HostUtil-test.js
+++ src/js/utils/__tests__/HostUtil-test.js
@@ -6,26 +6,87 @@ describe('HostUtil', function () {
 
   describe('#stringToHostname', function () {
 
+    it('should omit dots at the start', function () {
+      const result = HostUtil.stringToHostname('.fdgsf.4abc123');
+
+      expect(result).toEqual('fdgsf.4abc123');
+    });
+
+    it('should omit dots at the end', function () {
+      const result = HostUtil.stringToHostname('fdgsf.4abc123.');
+
+      expect(result).toEqual('fdgsf.4abc123');
+    });
+
+    it('should omit duplicated dots', function () {
+      const result = HostUtil.stringToHostname('fdgsf..4abc123');
+
+      expect(result).toEqual('fdgsf.4abc123');
+    });
+
+    it('should not replace dots midway', function () {
+      const result = HostUtil.stringToHostname('fdgsf.4abc123');
+
+      expect(result).toEqual('fdgsf.4abc123');
+    });
+
+    it('should allow 63 char labels', function () {
+      const hostname = [
+        '010203040506070809010111213141516171819202122232425262728293031',
+        '010203040506070809010111213141516171819202122232425262728293031'
+      ].join('.');
+
+      expect(HostUtil.stringToHostname(hostname)).toEqual(hostname);
+    });
+
+    it('should not alter valid strings', function () {
+      const result = HostUtil.stringToHostname('0-0');
+
+      expect(result).toEqual('0-0');
+    });
+
+  });
+
+  describe('#stringToLabel', function () {
+
     it('should strip invalid chars', function () {
-      const result = HostUtil.stringToHostname('fd%gsf---gs7-f$gs--d7fddg-123');
+      const result = HostUtil.stringToLabel('fd%gsf---gs7-f$gs--d7fddg-123');
 
       expect(result).toEqual('fdgsf---gs7-fgs--d7fddg-123');
     });
 
+    it('should replace underscores', function () {
+      const result = HostUtil.stringToLabel('fdgsf_4abc123');
+
+      expect(result).toEqual('fdgsf-4abc123');
+    });
+
+    it('should omit slashes', function () {
+      const result = HostUtil.stringToLabel('fdgsf/4abc123');
+
+      expect(result).toEqual('fdgsf4abc123');
+    });
+
+    it('should omit dots', function () {
+      const result = HostUtil.stringToLabel('.fdgsf.4abc123.');
+
+      expect(result).toEqual('fdgsf4abc123');
+    });
+
     it('should strip invalid chars from the start', function () {
-      const result = HostUtil.stringToHostname('-@4abc123');
+      const result = HostUtil.stringToLabel('-@4abc123');
 
       expect(result).toEqual('4abc123');
     });
 
     it('should strip invalid chars from the end', function () {
-      const result = HostUtil.stringToHostname('4abc123.');
+      const result = HostUtil.stringToLabel('4abc123-');
 
       expect(result).toEqual('4abc123');
     });
 
     it('should strip invalid chars from the start and end', function () {
-      const result = HostUtil.stringToHostname(
+      const result = HostUtil.stringToLabel(
         '##fdgsf---gs7-fgs--d7fddg123456789012345678901234567890123456789-'
       );
 
@@ -34,20 +95,14 @@ describe('HostUtil', function () {
       );
     });
 
-    it('should return valid hostnames', function () {
-      const result = HostUtil.stringToHostname('0-0');
-
-      expect(result).toEqual('0-0');
-    });
-
     it('should allow dashes within the hostname', function () {
-      const result = HostUtil.stringToHostname('89fdgsf---gs7-fgs--d7fddg-123');
+      const result = HostUtil.stringToLabel('89fdgsf---gs7-fgs--d7fddg-123');
 
       expect(result).toEqual('89fdgsf---gs7-fgs--d7fddg-123');
     });
 
     it('should strip invalid chars and trim string to 63 chars', function () {
-      const result = HostUtil.stringToHostname(
+      const result = HostUtil.stringToLabel(
         'fd%gsf---gs7-f$gs--d7fddg123456789012345678901234567890123456789-123'
       );
 
@@ -56,8 +111,18 @@ describe('HostUtil', function () {
       );
     });
 
+    it('should trim string to 63 chars', function () {
+      const result = HostUtil.stringToLabel(
+        '0102030405060708090101112131415161718192021222324252627282930313233'
+      );
+
+      expect(result).toEqual(
+        '010203040506070809010111213141516171819202122232425262728293031'
+      );
+    });
+
     it('should strip invalid chars from start and trim string', function () {
-      const result = HostUtil.stringToHostname(
+      const result = HostUtil.stringToLabel(
         '%%fdgsf---gs7-fgs--d7fddg123456789012345678901234567890123456789---123'
       );
 
@@ -67,7 +132,7 @@ describe('HostUtil', function () {
     });
 
     it('should strip dashes from the end and trim string', function () {
-      const result = HostUtil.stringToHostname(
+      const result = HostUtil.stringToLabel(
         '%%fdgsf---gs7-fgs--d7fddg123456789012345678901234567890123456789---123'
       );
 
@@ -76,18 +141,27 @@ describe('HostUtil', function () {
       );
     });
 
+    it('should strip all dashes that exceed the char limit', function () {
+      const result = HostUtil.stringToLabel(
+        'a--------------------------------------------------------------------b'
+      );
+
+      expect(result).toEqual('ab');
+
+    });
+
     it('should not alter valid hostnames', function () {
-      const result = HostUtil.stringToHostname(
-        'a--------------------------------------------------------------b'
+      const result = HostUtil.stringToLabel(
+        'a-----------------------------------------------b'
       );
 
       expect(result).toEqual(
-        'a-------------------------------------------------------------b'
+        'a-----------------------------------------------b'
       );
     });
 
     it('should lowercase the string', function () {
-      const result = HostUtil.stringToHostname('XYZ');
+      const result = HostUtil.stringToLabel('XYZ');
 
       expect(result).toEqual('xyz');
     });
