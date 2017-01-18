383a578dfc5f332e0e652b73e99bf2d9e05557cb
Ioannis Charalampidis <johnys2@gmail.com>
2017-01-18 14:51:50 +0100

    Converting error messages to flat list

--- plugins/services/src/js/constants/ServiceErrorMessages.js
+++ plugins/services/src/js/constants/ServiceErrorMessages.js
@@ -1,15 +1,22 @@
 import DefaultErrorMessages from '../../../../../src/js/constants/DefaultErrorMessages';
-import ErrorMessagesUtil from '../../../../../src/js/utils/ErrorMessagesUtil';
 
-const ServiceErrorMessages = ErrorMessagesUtil.extend({
-
-  'id': {
-    PROP_MISSING: 'Service ID is required',
-    ALREADY_EXISTS: 'A service with this ID already exists',
-    STRING_PATTERN: 'Service ID may only contain digits (0-9), dashes (-), ' +
+const ServiceErrorMessages = [
+  {
+    path: /^id$/,
+    type: 'PROP_MISSING',
+    message: 'Service ID is required'
+  },
+  {
+    path: /^id$/,
+    type: 'ALREADY_EXISTS',
+    message: 'A service with this ID already exists'
+  },
+  {
+    path: /^id$/,
+    type: 'STRING_PATTERN',
+    message: 'Service ID may only contain digits (0-9), dashes (-), ' +
       'dots (.),lowercase letters (a-z), and slashes (/) e.g. /group/my-service'
   }
-
-}, DefaultErrorMessages);
+].concat(DefaultErrorMessages);
 
 module.exports = ServiceErrorMessages;
