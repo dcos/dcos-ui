674f6b6ae7b0775ebb82b483f87e7b9cfa8ca343
Ioannis Charalampidis <johnys2@gmail.com>
2017-02-14 14:52:03 +0100

    Set 'Docker' as the default application spec runtime

--- plugins/services/src/js/constants/DefaultApp.js
+++ plugins/services/src/js/constants/DefaultApp.js
@@ -1,5 +1,11 @@
+import ContainerConstants from './ContainerConstants';
+
+const {DOCKER} = ContainerConstants.type;
+
 const DEFAULT_APP_RESOURCES = {cpus: 0.1, mem: 128};
-const DEFAULT_APP_SPEC = Object.assign({instances: 1}, DEFAULT_APP_RESOURCES);
+const DEFAULT_APP_CONTAINER = {container: {type: DOCKER}};
+const DEFAULT_APP_SPEC = Object.assign({instances: 1},
+  DEFAULT_APP_CONTAINER, DEFAULT_APP_RESOURCES);
 
 module.exports = {
   DEFAULT_APP_RESOURCES,
