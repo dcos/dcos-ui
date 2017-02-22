57616fb6ea89e1eeb6ed8039d76869a8a921ce13
John Furrow <johnf@mesosphere.io>
2017-02-22 12:56:35 -0800

    Add integration tests

--- tests/pages/services/ServiceActions-cy.js
+++ tests/pages/services/ServiceActions-cy.js
@@ -30,7 +30,7 @@ describe('Service Actions', function () {
       cy
         .route({
           method: 'PUT',
-          url: /marathon\/v2\/apps\/\/cassandra\-healthy/,
+          url: /marathon\/v2\/apps\/\/cassandra-healthy/,
           response: []
         });
       cy.get('.modal .modal-header .button')
@@ -160,7 +160,7 @@ describe('Service Actions', function () {
       cy
         .route({
           method: 'PUT',
-          url: /marathon\/v2\/apps\/\/cassandra\-healthy/,
+          url: /marathon\/v2\/apps\/\/cassandra-healthy/,
           response: []
         });
       cy.get('.modal-footer .button-collection .button-primary')
@@ -172,7 +172,7 @@ describe('Service Actions', function () {
       cy
         .route({
           method: 'PUT',
-          url: /marathon\/v2\/apps\/\/cassandra\-healthy/,
+          url: /marathon\/v2\/apps\/\/cassandra-healthy/,
           response: []
         });
       cy.get('.modal-footer .button-collection .button-primary').click();
@@ -184,7 +184,7 @@ describe('Service Actions', function () {
         .route({
           method: 'PUT',
           status: 409,
-          url: /marathon\/v2\/apps\/\/cassandra\-healthy/,
+          url: /marathon\/v2\/apps\/\/cassandra-healthy/,
           response: {
             message: 'App is locked by one or more deployments.'
           }
@@ -199,7 +199,7 @@ describe('Service Actions', function () {
         .route({
           method: 'PUT',
           status: 403,
-          url: /marathon\/v2\/apps\/\/cassandra\-healthy/,
+          url: /marathon\/v2\/apps\/\/cassandra-healthy/,
           response: {
             message: 'Not Authorized to perform this action!'
           }
@@ -213,7 +213,7 @@ describe('Service Actions', function () {
       cy
         .route({
           method: 'PUT',
-          url: /marathon\/v2\/apps\/\/cassandra\-healthy/,
+          url: /marathon\/v2\/apps\/\/cassandra-healthy/,
           response: [],
           delay: SERVER_RESPONSE_DELAY
         });
@@ -244,7 +244,7 @@ describe('Service Actions', function () {
       cy
         .route({
           method: 'PUT',
-          url: /marathon\/v2\/apps\/\/cassandra\-healthy/,
+          url: /marathon\/v2\/apps\/\/cassandra-healthy/,
           response: []
         });
       cy.get('.confirm-modal .button-collection .button-primary')
@@ -256,7 +256,7 @@ describe('Service Actions', function () {
       cy
         .route({
           method: 'PUT',
-          url: /marathon\/v2\/apps\/\/cassandra\-healthy/,
+          url: /marathon\/v2\/apps\/\/cassandra-healthy/,
           response: []
         });
       cy.get('.confirm-modal .button-collection .button-primary').click();
@@ -268,7 +268,7 @@ describe('Service Actions', function () {
         .route({
           method: 'PUT',
           status: 409,
-          url: /marathon\/v2\/apps\/\/cassandra\-healthy/,
+          url: /marathon\/v2\/apps\/\/cassandra-healthy/,
           response: {
             message: 'App is locked by one or more deployments.'
           }
@@ -283,7 +283,7 @@ describe('Service Actions', function () {
         .route({
           method: 'PUT',
           status: 403,
-          url: /marathon\/v2\/apps\/\/cassandra\-healthy/,
+          url: /marathon\/v2\/apps\/\/cassandra-healthy/,
           response: {
             message: 'Not Authorized to perform this action!'
           }
@@ -297,7 +297,7 @@ describe('Service Actions', function () {
       cy
         .route({
           method: 'PUT',
-          url: /marathon\/v2\/apps\/\/cassandra\-healthy/,
+          url: /marathon\/v2\/apps\/\/cassandra-healthy/,
           response: [],
           delay: SERVER_RESPONSE_DELAY
         });
@@ -311,6 +311,176 @@ describe('Service Actions', function () {
       cy.get('.confirm-modal .button-collection .button').contains('Cancel')
         .click();
       cy.get('.confirm-modal').should('to.have.length', 0);
+    });
+  });
+
+  context('Resume Action', function () {
+    function openDropdown() {
+      cy.get('.service-table-column-name')
+        .contains('sleep')
+        .get('.dropdown-toggle')
+        .click({force: true});
+    }
+
+    function clickResume() {
+      cy.get('.dropdown-menu-items')
+        .get('li')
+        .contains('Resume')
+        .click();
+    }
+
+    beforeEach(function () {
+      cy.configureCluster({
+        mesos: '1-service-suspended',
+        nodeHealth: true
+      });
+
+      cy.visitUrl({url: '/services/overview'});
+    });
+
+    it('hides the suspend option in the service action dropdown', function () {
+      openDropdown();
+
+      cy.get('.dropdown-menu-items')
+        .get('li')
+        .contains('Suspend')
+        .should('have.class', 'hidden');
+    });
+
+    it('shows the resume option in the service action dropdown', function () {
+      openDropdown();
+
+      cy.get('.dropdown-menu-items')
+        .get('li')
+        .contains('Resume')
+        .should('not.have.class', 'hidden');
+    });
+
+    it('opens the resume dialog', function () {
+      openDropdown();
+      clickResume();
+
+      cy.get('.modal-header')
+        .contains('Resume Service')
+        .should('have.length', 1);
+    });
+
+    it('opens the resume dialog with the instances textbox if the single app instance label does not exist', function () {
+      openDropdown();
+      clickResume();
+
+      cy.get('input[name="instances"]')
+        .should('have.length', 1);
+    });
+
+    it('opens the resume dialog without the instances textbox if the single app instance label exists', function () {
+      cy.configureCluster({
+        mesos: '1-service-suspended-single-instance',
+        nodeHealth: true
+      });
+
+      cy.visitUrl({url: '/services/overview'});
+
+      openDropdown();
+      clickResume();
+
+      cy.get('input[name="instances"]')
+        .should('have.length', 0);
+    });
+
+    it('disables button during API request', function () {
+      cy.route({
+        method: 'PUT',
+        url: /marathon\/v2\/apps\/\/sleep/,
+        response: []
+      });
+
+      openDropdown();
+      clickResume();
+
+      cy.get('.modal-footer .button-collection .button-primary')
+        .click()
+        .should('have.class', 'disabled');
+    });
+
+    it('closes dialog on successful API request', function () {
+      cy
+        .route({
+          method: 'PUT',
+          url: /marathon\/v2\/apps\/\/sleep/,
+          response: []
+        });
+
+      openDropdown();
+      clickResume();
+
+      cy.get('.modal-footer .button-collection .button-primary').click();
+      cy.get('.modal-body').should('to.have.length', 0);
+    });
+
+    it('shows error message on conflict', function () {
+      cy
+        .route({
+          method: 'PUT',
+          status: 409,
+          url: /marathon\/v2\/apps\/\/sleep/,
+          response: {
+            message: 'App is locked by one or more deployments.'
+          }
+        });
+
+      openDropdown();
+      clickResume();
+
+      cy.get('.modal-footer .button-collection .button-primary').click();
+      cy.get('.modal-body .text-danger')
+        .should('to.have.text', 'App is locked by one or more deployments.');
+    });
+
+    it('shows error message on not authorized', function () {
+      cy
+        .route({
+          method: 'PUT',
+          status: 403,
+          url: /marathon\/v2\/apps\/\/sleep/,
+          response: {
+            message: 'Not Authorized to perform this action!'
+          }
+        });
+
+      openDropdown();
+      clickResume();
+
+      cy.get('.modal-footer .button-collection .button-primary').click();
+      cy.get('.modal-body .text-danger')
+        .should('to.have.text', 'Not Authorized to perform this action!');
+    });
+
+    it('reenables button after faulty request', function () {
+      cy
+        .route({
+          method: 'PUT',
+          url: /marathon\/v2\/apps\/\/sleep/,
+          response: [],
+          delay: SERVER_RESPONSE_DELAY
+        });
+
+      openDropdown();
+      clickResume();
+
+      cy.get('.modal-footer .button-collection .button-primary')
+        .as('primaryButton').click();
+      cy.get('@primaryButton').should('have.class', 'disabled');
+      cy.get('@primaryButton').should('not.have.class', 'disabled');
+    });
+
+    it('closes dialog on secondary button click', function () {
+      openDropdown();
+      clickResume();
+
+      cy.get('.modal-footer .button-collection .button').contains('Cancel')
+        .click();
+      cy.get('.modal-body').should('to.have.length', 0);
     });
   });
 
