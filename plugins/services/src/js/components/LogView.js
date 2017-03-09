56ddbee585767efe3d7503be6b5aa74593706e17
Dmitriy Rozhkov <drozhkov@mesosphere.io>
2017-03-09 17:36:01 +0100

    fix(LogView): hide go down button when there is no logs

--- plugins/services/src/js/components/LogView.js
+++ plugins/services/src/js/components/LogView.js
@@ -219,8 +219,9 @@ class LogView extends React.Component {
       return this.getEmptyLogScreen();
     }
 
-    return (
+    return [
       <pre
+        key="log-container"
         className="flex-item-grow-1 flush-bottom prettyprint"
         ref={(ref) => { this.logContainer = ref; }}
         onScroll={this.handleLogContainerScroll}>
@@ -233,8 +234,18 @@ class LogView extends React.Component {
           watching={watching}>
           {fullLog}
         </Highlight>
-      </pre>
-    );
+      </pre>,
+      <ReactCSSTransitionGroup
+        key="log-go-down-button"
+        transitionAppear={true}
+        transitionName="button"
+        transitionAppearTimeout={350}
+        transitionEnterTimeout={350}
+        transitionLeaveTimeout={350}
+        component="div">
+        {this.getGoToBottomButton()}
+      </ReactCSSTransitionGroup>
+    ];
   }
 
   getGoToBottomButton() {
@@ -279,15 +290,6 @@ class LogView extends React.Component {
     return (
       <div className="log-view flex flex-direction-top-to-bottom flex-item-grow-1 flex-item-shrink-1">
         {this.getLog()}
-        <ReactCSSTransitionGroup
-          transitionAppear={true}
-          transitionName="button"
-          transitionAppearTimeout={350}
-          transitionEnterTimeout={350}
-          transitionLeaveTimeout={350}
-          component="div">
-          {this.getGoToBottomButton()}
-        </ReactCSSTransitionGroup>
       </div>
     );
   }
