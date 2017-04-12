392c9ec7f80167917af229eb5be2560de3108c67
Philipp Hinrichsen <philipph@mesosphere.io>
2017-04-12 18:01:25 +0200

    fix(localVolumes): Add Default size
    Local Volumes now will get a default size of 10 so
    if they get rendered into the JSON they will have
    a `persistent:{size:DEFAULT_SIZE}` and they can be
    parsed back.
    
    Closes DCOS-14756

--- plugins/services/src/js/reducers/serviceForm/LocalVolumes.js
+++ plugins/services/src/js/reducers/serviceForm/LocalVolumes.js
@@ -1,6 +1,9 @@
 import { ADD_ITEM, REMOVE_ITEM, SET } from "#SRC/js/constants/TransactionTypes";
 import { parseIntValue } from "#SRC/js/utils/ReducerUtil";
 import Transaction from "#SRC/js/structs/Transaction";
+import {
+  DEFAULT_SIZE as LOCAL_VOLUME_DEFAULT_SIZE
+} from "../../constants/LocalVolumes";
 
 module.exports = {
   JSONParser(state) {
@@ -12,20 +15,20 @@ module.exports = {
       .filter(item => item.external == null)
       .reduce(function(memo, item, index) {
         /**
-         * For the localVolumes we have a special case as all the volumes
-         * are present in the `container.volumes` But in this parser we only
-         * want to parse the local volumes. which means that we first filter
-         * those and only keep local volumes (decision based on if
-         * persistent is set). After that we do get all the values even
-         * stuff which we do not handle in the form yet. These steps are:
-         * 1) Add a new Item to the path with the index equal to index.
-         * 2) Set the size from `volume.persistent.size`on the path
-         *    `localVolumes.${index}.size`.
-         * 3) Set the containerPath from `volume.containerPath on the path
-         *    `localVolumes.${index}.containerPath`
-         * 4) Set the mode from `volume.mode` on the path
-         *    `localVolumes.${index}.mode`
-         */
+       * For the localVolumes we have a special case as all the volumes
+       * are present in the `container.volumes` But in this parser we only
+       * want to parse the local volumes. which means that we first filter
+       * those and only keep local volumes (decision based on if
+       * persistent is set). After that we do get all the values even
+       * stuff which we do not handle in the form yet. These steps are:
+       * 1) Add a new Item to the path with the index equal to index.
+       * 2) Set the size from `volume.persistent.size`on the path
+       *    `localVolumes.${index}.size`.
+       * 3) Set the containerPath from `volume.containerPath on the path
+       *    `localVolumes.${index}.containerPath`
+       * 4) Set the mode from `volume.mode` on the path
+       *    `localVolumes.${index}.mode`
+       */
         memo.push(new Transaction(["localVolumes"], index, ADD_ITEM));
 
         if (item.persistent != null && item.persistent.size != null) {
@@ -45,13 +48,15 @@ module.exports = {
             new Transaction(["localVolumes", index, "type"], "HOST", SET)
           );
 
-          memo.push(
-            new Transaction(
-              ["localVolumes", index, "hostPath"],
-              item.hostPath,
-              SET
-            )
-          );
+          if (item.hostPath != null) {
+            memo.push(
+              new Transaction(
+                ["localVolumes", index, "hostPath"],
+                item.hostPath,
+                SET
+              )
+            );
+          }
         }
 
         if (item.containerPath != null) {
@@ -85,7 +90,11 @@ module.exports = {
       if (joinedPath === "localVolumes") {
         switch (type) {
           case ADD_ITEM:
-            state.push({ containerPath: null, size: null, mode: "RW" });
+            state.push({
+              containerPath: null,
+              size: LOCAL_VOLUME_DEFAULT_SIZE,
+              mode: "RW"
+            });
             break;
           case REMOVE_ITEM:
             state = state.filter((item, index) => {
