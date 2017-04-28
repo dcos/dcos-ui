87e7fa32f5173a11a0e7f3d214419866f41ff3d3
mlunoe <michael.lunoe@gmail.com>
2017-04-28 16:30:48 -0700

    refactor(Service Actions): move table actions tests to its own file

---
+++ tests/pages/services/ServiceTable-cy.js
@@ -0,0 +1,207 @@
+import { SERVER_RESPONSE_DELAY } from "../../_support/constants/Timeouts";
+
+describe("Service Table", function() {
+  function openDropdown(serviceName) {
+    cy
+      .get(".service-table")
+      .contains(serviceName)
+      .closest("tr")
+      .find(".dropdown")
+      .click();
+  }
+
+  function clickDropdownAction(actionText) {
+    cy.get(".dropdown-menu-items").contains(actionText).click();
+  }
+
+  context("Open Service Action", function() {
+    beforeEach(function() {
+      cy.configureCluster({
+        mesos: "1-for-each-health",
+        nodeHealth: true
+      });
+
+      cy.visitUrl({ url: "/services/overview" });
+    });
+
+    it('displays the "Open Service" option for services that have a web UI', function() {
+      openDropdown("cassandra-healthy");
+
+      cy
+        .get(".dropdown-menu-items")
+        .contains("Open Service")
+        .should(function($menuItem) {
+          expect($menuItem.length).to.equal(1);
+        });
+    });
+
+    it('does not display the "Open Service" option for services that have a web UI', function() {
+      openDropdown("cassandra-unhealthy");
+
+      cy
+        .get(".dropdown-menu-items")
+        .contains("Open Service")
+        .should(function($menuItem) {
+          expect($menuItem.hasClass("hidden")).to.equal(true);
+        });
+    });
+  });
+
+  context("Resume Action", function() {
+    beforeEach(function() {
+      cy.configureCluster({
+        mesos: "1-service-suspended",
+        nodeHealth: true
+      });
+
+      cy.visitUrl({ url: "/services/overview" });
+    });
+
+    it("hides the suspend option in the service action dropdown", function() {
+      openDropdown("sleep");
+
+      cy
+        .get(".dropdown-menu-items li")
+        .contains("Suspend")
+        .should("have.class", "hidden");
+    });
+
+    it("shows the resume option in the service action dropdown", function() {
+      openDropdown("sleep");
+
+      cy
+        .get(".dropdown-menu-items li")
+        .contains("Resume")
+        .should("not.have.class", "hidden");
+    });
+
+    it("opens the resume dialog", function() {
+      openDropdown("sleep");
+      clickDropdownAction("Resume");
+
+      cy
+        .get(".modal-header")
+        .contains("Resume Service")
+        .should("have.length", 1);
+    });
+
+    it("opens the resume dialog with the instances textbox if the single app instance label does not exist", function() {
+      openDropdown("sleep");
+      clickDropdownAction("Resume");
+
+      cy.get('input[name="instances"]').should("have.length", 1);
+    });
+
+    it("opens the resume dialog without the instances textbox if the single app instance label exists", function() {
+      cy.configureCluster({
+        mesos: "1-service-suspended-single-instance",
+        nodeHealth: true
+      });
+
+      cy.visitUrl({ url: "/services/overview" });
+
+      openDropdown("sleep");
+      clickDropdownAction("Resume");
+
+      cy.get('input[name="instances"]').should("have.length", 0);
+    });
+
+    it("disables button during API request", function() {
+      cy.route({
+        method: "PUT",
+        url: /marathon\/v2\/apps\/\/sleep/,
+        response: []
+      });
+
+      openDropdown("sleep");
+      clickDropdownAction("Resume");
+
+      cy
+        .get(".modal-footer .button-collection .button-primary")
+        .click()
+        .should("have.class", "disabled");
+    });
+
+    it("closes dialog on successful API request", function() {
+      cy.route({
+        method: "PUT",
+        url: /marathon\/v2\/apps\/\/sleep/,
+        response: []
+      });
+
+      openDropdown("sleep");
+      clickDropdownAction("Resume");
+
+      cy.get(".modal-footer .button-collection .button-primary").click();
+      cy.get(".modal-body").should("to.have.length", 0);
+    });
+
+    it("shows error message on conflict", function() {
+      cy.route({
+        method: "PUT",
+        status: 409,
+        url: /marathon\/v2\/apps\/\/sleep/,
+        response: {
+          message: "App is locked by one or more deployments."
+        }
+      });
+
+      openDropdown("sleep");
+      clickDropdownAction("Resume");
+
+      cy.get(".modal-footer .button-collection .button-primary").click();
+      cy
+        .get(".modal-body .text-danger")
+        .should("to.have.text", "App is locked by one or more deployments.");
+    });
+
+    it("shows error message on not authorized", function() {
+      cy.route({
+        method: "PUT",
+        status: 403,
+        url: /marathon\/v2\/apps\/\/sleep/,
+        response: {
+          message: "Not Authorized to perform this action!"
+        }
+      });
+
+      openDropdown("sleep");
+      clickDropdownAction("Resume");
+
+      cy.get(".modal-footer .button-collection .button-primary").click();
+      cy
+        .get(".modal-body .text-danger")
+        .should("to.have.text", "Not Authorized to perform this action!");
+    });
+
+    it("reenables button after faulty request", function() {
+      cy.route({
+        method: "PUT",
+        url: /marathon\/v2\/apps\/\/sleep/,
+        response: [],
+        delay: SERVER_RESPONSE_DELAY
+      });
+
+      openDropdown("sleep");
+      clickDropdownAction("Resume");
+
+      cy
+        .get(".modal-footer .button-collection .button-primary")
+        .as("primaryButton")
+        .click();
+      cy.get("@primaryButton").should("have.class", "disabled");
+      cy.get("@primaryButton").should("not.have.class", "disabled");
+    });
+
+    it("closes dialog on secondary button click", function() {
+      openDropdown("sleep");
+      clickDropdownAction("Resume");
+
+      cy
+        .get(".modal-footer .button-collection .button")
+        .contains("Cancel")
+        .click();
+      cy.get(".modal-body").should("to.have.length", 0);
+    });
+  });
+});
