c69be36dc80357fcbc562d69b929839486d83f9f
mlunoe <michael.lunoe@gmail.com>
2017-04-28 16:31:52 -0700

    test(ServiceDetail): add tests for resume action for service detail

--- tests/pages/services/ServiceActions-cy.js
+++ tests/pages/services/ServiceActions-cy.js
@@ -388,4 +388,148 @@ describe("Service Actions", function() {
       cy.get(".confirm-modal").should("to.have.length", 0);
     });
   });
+
+  context("Resume Action", function() {
+    beforeEach(function() {
+      cy.configureCluster({
+        mesos: "1-service-suspended",
+        nodeHealth: true
+      });
+
+      cy.visitUrl({ url: "/services/detail/%2Fsleep" });
+    });
+
+    it("hides the suspend option in the service action dropdown", function() {
+      cy.get(".page-header-actions .dropdown").click();
+
+      cy.get(".dropdown-menu-items li").should("not.contain", "Suspend");
+    });
+
+    it("shows the resume option in the service action dropdown", function() {
+      cy.get(".page-header-actions .dropdown").click();
+
+      cy
+        .get(".dropdown-menu-items li")
+        .contains("Resume")
+        .should("not.have.class", "hidden");
+    });
+
+    it("opens the resume dialog", function() {
+      clickHeaderAction("Resume");
+
+      cy
+        .get(".modal-header")
+        .contains("Resume Service")
+        .should("have.length", 1);
+    });
+
+    it("opens the resume dialog with the instances textbox if the single app instance label does not exist", function() {
+      clickHeaderAction("Resume");
+
+      cy.get('input[name="instances"]').should("have.length", 1);
+    });
+
+    it("opens the resume dialog without the instances textbox if the single app instance label exists", function() {
+      cy.configureCluster({
+        mesos: "1-service-suspended-single-instance",
+        nodeHealth: true
+      });
+
+      clickHeaderAction("Resume");
+
+      cy.get('input[name="instances"]').should("have.length", 0);
+    });
+
+    it("disables button during API request", function() {
+      cy.route({
+        method: "PUT",
+        url: /marathon\/v2\/apps\/\/sleep/,
+        response: []
+      });
+
+      clickHeaderAction("Resume");
+
+      cy
+        .get(".modal-footer .button-collection .button-primary")
+        .click()
+        .should("have.class", "disabled");
+    });
+
+    it("closes dialog on successful API request", function() {
+      cy.route({
+        method: "PUT",
+        url: /marathon\/v2\/apps\/\/sleep/,
+        response: []
+      });
+
+      clickHeaderAction("Resume");
+
+      cy.get(".modal-footer .button-collection .button-primary").click();
+      cy.get(".modal-body").should("to.have.length", 0);
+    });
+
+    it("shows error message on conflict", function() {
+      cy.route({
+        method: "PUT",
+        status: 409,
+        url: /marathon\/v2\/apps\/\/sleep/,
+        response: {
+          message: "App is locked by one or more deployments."
+        }
+      });
+
+      clickHeaderAction("Resume");
+
+      cy.get(".modal-footer .button-collection .button-primary").click();
+      cy
+        .get(".modal-body .text-danger")
+        .should("to.have.text", "App is locked by one or more deployments.");
+    });
+
+    it("shows error message on not authorized", function() {
+      cy.route({
+        method: "PUT",
+        status: 403,
+        url: /marathon\/v2\/apps\/\/sleep/,
+        response: {
+          message: "Not Authorized to perform this action!"
+        }
+      });
+
+      clickHeaderAction("Resume");
+
+      cy.get(".modal-footer .button-collection .button-primary").click();
+      cy
+        .get(".modal-body .text-danger")
+        .should("to.have.text", "Not Authorized to perform this action!");
+    });
+
+    it("reenables button after faulty request", function() {
+      cy.route({
+        method: "PUT",
+        url: /marathon\/v2\/apps\/\/sleep/,
+        response: [],
+        delay: SERVER_RESPONSE_DELAY
+      });
+
+      clickHeaderAction("Resume");
+
+      cy
+        .get(".modal-footer .button-collection .button-primary")
+        .as("primaryButton")
+        .click();
+      cy.get("@primaryButton").should("have.class", "disabled");
+      cy.get("@primaryButton").should("not.have.class", "disabled");
+    });
+
+    it("closes dialog on secondary button click", function() {
+      clickHeaderAction("Resume");
+
+      cy
+        .get(".modal-footer .button-collection .button")
+        .contains("Cancel")
+        .click();
+      cy.get(".modal-body").should("to.have.length", 0);
+    });
+  });
 });
