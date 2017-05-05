6a713585add58f06b1418ad92743baeb3b00c2ec
mlunoe <michael.lunoe@gmail.com>
2017-05-05 15:25:45 -0700

    test(ServiceTable): add test for opening the edit modal

--- tests/pages/services/ServiceTable-cy.js
+++ tests/pages/services/ServiceTable-cy.js
@@ -519,6 +519,26 @@ describe("Service Table", function() {
       cy.get(".modal").should("not.exist");
     });
 
+    it.only("opens the edit dialog", function() {
+      openDropdown("sdk-sleep");
+      clickDropdownAction("Edit");
+
+      cy
+        .get(".modal-header")
+        .contains("Edit Service")
+        .should("to.have.length", 1);
+
+      cy
+        .get(".modal pre")
+        .contains(
+          "dcos test --name=/services/sdk-sleep update --options=test-options.json"
+        );
+
+      cy.get(".modal button").contains("Close").click();
+
+      cy.get(".modal").should("not.exist");
+    });
+
     it("opens the scale dialog", function() {
       openDropdown("sdk-sleep");
       clickDropdownAction("Scale");
