c727049d805d2c5ea98ad192e386fca6320507e2
tnguyen0 <tnguyen@mesosphere.com>
2017-05-22 17:28:10 -0700

    feat(DCOS-15919): Design system pages for Style and Patterns

--- plugins/design-system/hooks.js
+++ plugins/design-system/hooks.js
@@ -7,10 +7,24 @@ import { navigation, routing } from "foundation-ui";
 import ButtonsPage from "./pages/buttons/ButtonsPage";
 import IntroductionPage from "./pages/IntroductionPage";
 import ModalsPage from "./pages/ModalsPage";
+import TablesPage from "./pages/TablesPage";
+import ColorPage from "./pages/ColorPage";
+import CreateEditPage from "./pages/CreateEditPage";
+import EmptyStatesPage from "./pages/EmptyStatesPage";
+import FormsPage from "./pages/FormsPage";
+import HelpFeedbackPage from "./pages/HelpFeedbackPage";
+import IconsPage from "./pages/IconsPage";
+import LayoutPage from "./pages/LayoutPage";
+import LoadingPage from "./pages/LoadingPage";
+import MotionPage from "./pages/MotionPage";
+import NotificationsPage from "./pages/NotificationsPage";
+import TypographyPage from "./pages/TypographyPage";
+import ValidationPage from "./pages/ValidationPage";
+import WritingPage from "./pages/WritingPage";
+
 import OverviewTab from "./pages/buttons/OverviewTab";
 import CodeTab from "./pages/buttons/CodeTab";
 import StylesTab from "./pages/buttons/StylesTab";
-import TablesPage from "./pages/TablesPage";
 
 const SDK = require("./SDK").getSDK();
 
@@ -62,6 +76,18 @@ module.exports = {
       }
     );
 
+    navigation.NavigationService.registerPrimary("/ds-style", "Style", {
+      category: "design system",
+      icon: (
+        <Icon
+          className="sidebar-menu-item-icon icon icon-small"
+          id="services-inverse"
+          size="small"
+          family="product"
+        />
+      )
+    });
+
     navigation.NavigationService.registerPrimary(
       "/ds-components",
       "Components",
@@ -78,9 +104,60 @@ module.exports = {
       }
     );
 
+    navigation.NavigationService.registerPrimary("/ds-patterns", "Patterns", {
+      category: "design system",
+      icon: (
+        <Icon
+          className="sidebar-menu-item-icon icon icon-small"
+          id="services-inverse"
+          size="small"
+          family="product"
+        />
+      )
+    });
+
     // Secondary routes appear nested underneath their parent.
     // In the following call to #registerSecondary, we provide the parent route,
     // the child route, and the display label.
+
+    // Style
+    navigation.NavigationService.registerSecondary(
+      "/ds-style",
+      "color",
+      "Color"
+    );
+
+    navigation.NavigationService.registerSecondary(
+      "/ds-style",
+      "icons",
+      "Icons"
+    );
+
+    navigation.NavigationService.registerSecondary(
+      "/ds-style",
+      "layout",
+      "Layout"
+    );
+
+    navigation.NavigationService.registerSecondary(
+      "/ds-style",
+      "motion",
+      "Motion"
+    );
+
+    navigation.NavigationService.registerSecondary(
+      "/ds-style",
+      "typography",
+      "Typography"
+    );
+
+    navigation.NavigationService.registerSecondary(
+      "/ds-style",
+      "writing",
+      "Writing"
+    );
+
+    // Components
     navigation.NavigationService.registerSecondary(
       "/ds-components",
       "buttons",
@@ -99,12 +176,99 @@ module.exports = {
       "Tables"
     );
 
+    // Patterns
+    navigation.NavigationService.registerSecondary(
+      "/ds-patterns",
+      "create-edit",
+      "Create/Edit"
+    );
+
+    navigation.NavigationService.registerSecondary(
+      "/ds-patterns",
+      "empty-states",
+      "Empty States"
+    );
+
+    navigation.NavigationService.registerSecondary(
+      "/ds-patterns",
+      "help-feedback",
+      "Help & Feedback"
+    );
+
+    navigation.NavigationService.registerSecondary(
+      "/ds-patterns",
+      "forms",
+      "Forms"
+    );
+
+    navigation.NavigationService.registerSecondary(
+      "/ds-patterns",
+      "loading",
+      "Loading"
+    );
+
+    navigation.NavigationService.registerSecondary(
+      "/ds-patterns",
+      "notifications",
+      "Notifications"
+    );
+
+    navigation.NavigationService.registerSecondary(
+      "/ds-patterns",
+      "validation",
+      "Validation"
+    );
+
     // The following calls to #registerPage define which components should be
     // rendered for the given top-level routes.
     routing.RoutingService.registerPage("/ds-introduction", IntroductionPage);
+
+    routing.RoutingService.registerPage("/ds-style/color", ColorPage);
+
+    routing.RoutingService.registerPage("/ds-style/icons", IconsPage);
+
+    routing.RoutingService.registerPage("/ds-style/layout", LayoutPage);
+
+    routing.RoutingService.registerPage("/ds-style/motion", MotionPage);
+
+    routing.RoutingService.registerPage("/ds-style/typography", TypographyPage);
+
+    routing.RoutingService.registerPage("/ds-style/writing", WritingPage);
+
     routing.RoutingService.registerPage("/ds-components/buttons", ButtonsPage);
+
     routing.RoutingService.registerPage("/ds-components/modals", ModalsPage);
+
     routing.RoutingService.registerPage("/ds-components/tables", TablesPage);
+
+    routing.RoutingService.registerPage("/ds-patterns/forms", FormsPage);
+
+    routing.RoutingService.registerPage("/ds-patterns/loading", LoadingPage);
+
+    routing.RoutingService.registerPage(
+      "/ds-patterns/create-edit",
+      CreateEditPage
+    );
+
+    routing.RoutingService.registerPage(
+      "/ds-patterns/empty-states",
+      EmptyStatesPage
+    );
+
+    routing.RoutingService.registerPage(
+      "/ds-patterns/help-feedback",
+      HelpFeedbackPage
+    );
+
+    routing.RoutingService.registerPage(
+      "/ds-patterns/notifications",
+      NotificationsPage
+    );
+
+    routing.RoutingService.registerPage(
+      "/ds-patterns/validation",
+      ValidationPage
+    );
 
     // The following calls to #registerTab define child routes for each page.
     // We provide the parent route, the child route, and the component to be
@@ -153,6 +317,11 @@ module.exports = {
    *   beginning.
    */
   sidebarNavigation(routes) {
-    return ["/ds-introduction", "/ds-components"].concat(routes);
+    return [
+      "/ds-introduction",
+      "/ds-style",
+      "/ds-components",
+      "/ds-patterns"
+    ].concat(routes);
   }
 };
---
+++ plugins/design-system/pages/IconsPage.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+import DesignSystemBreadcrumbs from "../components/DesignSystemBreadcrumbs";
+
+const SDK = require("../SDK").getSDK();
+
+const { Page } = SDK.get(["Page"]);
+
+class IconsPage extends Component {
+  render() {
+    return (
+      <Page>
+        <Page.Header breadcrumbs={<DesignSystemBreadcrumbs />} />
+        <div>
+          Icons Page
+        </div>
+      </Page>
+    );
+  }
+}
+
+module.exports = IconsPage;
