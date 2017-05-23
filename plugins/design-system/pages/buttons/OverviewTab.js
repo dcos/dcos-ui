796671e78b22fd16fc0c669efaf8eb9e8b7cfcf7
Lee Munroe <lee@leemunroe.com>
2017-05-23 13:42:38 -0700

    Break up guidelines table

--- plugins/design-system/pages/buttons/OverviewTab.js
+++ plugins/design-system/pages/buttons/OverviewTab.js
@@ -11,15 +11,27 @@ class OverviewTab extends Component {
           Do not use Buttons as navigational elements. Instead, use Links because it takes the user to a new page and is not associated with an action. Each page will likely have one primary button. Any remaining calls-to-action are represented as default buttons.
         </p>
         <h2>Guidelines</h2>
+        <h3>Button States</h3>
         <table className="table">
           <thead>
             <tr>
-              <th width="20%">Button Type</th>
+              <th width="20%">State</th>
               <th>Purpose</th>
               <th width="20%">Preview</th>
             </tr>
           </thead>
           <tbody>
+            <tr>
+              <th>Default</th>
+              <td>
+                Most buttons on a page are likely default.
+              </td>
+              <td>
+                <button className="button" type="button">
+                  Default
+                </button>
+              </td>
+            </tr>
             <tr>
               <th>Primary</th>
               <td>
@@ -32,13 +44,62 @@ class OverviewTab extends Component {
               </td>
             </tr>
             <tr>
-              <th>Default</th>
+              <th>Destructive</th>
               <td>
-                Most buttons on a page are likely default.
+                Destructive buttons are used to highlight something dangerous e.g. "Delete"
+              </td>
+              <td>
+                <button className="button button-danger" type="button">
+                  Destructive
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <th>Disabled</th>
+              <td>
+                Disable a button when the action is not applicable but we want to show the user that it may be possible. Disabled buttons should have a tooltip.
+              </td>
+              <td>
+                <button
+                  className="button button-disabled"
+                  type="button"
+                  disabled
+                >
+                  Disabled
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <h3>Button Types</h3>
+        <table className="table">
+          <thead>
+            <tr>
+              <th width="20%">Type</th>
+              <th>Purpose</th>
+              <th width="20%">Preview</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>Filled</th>
+              <td>
+                ...
               </td>
               <td>
                 <button className="button" type="button">
-                  Default
+                  Filled
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <th>Outline</th>
+              <td>
+                ...
+              </td>
+              <td>
+                <button className="button button-outline" type="button">
+                  Outline
                 </button>
               </td>
             </tr>
@@ -53,47 +114,75 @@ class OverviewTab extends Component {
                 </button>
               </td>
             </tr>
+          </tbody>
+        </table>
+        <h3>Button Sizes</h3>
+        <table className="table">
+          <thead>
             <tr>
-              <th>Outline</th>
+              <th width="20%">Size</th>
+              <th>Purpose</th>
+              <th width="20%">Preview</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>Default</th>
               <td>
-                Outline buttons are available to use when ...
+                ...
               </td>
               <td>
-                <button className="button button-outline" type="button">
-                  Outline
+                <button className="button" type="button">
+                  Default
                 </button>
               </td>
             </tr>
             <tr>
-              <th>Danger</th>
-              <td>
-                Danger buttons are used to highlight something destructive e.g. "Delete"
-              </td>
-              <td>
-                <button className="button button-danger" type="button">
-                  Danger
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <th>Disabled button</th>
-              <td>
-                Disable a button when the action is not applicable but we want to show the user that it may be possible. Disabled buttons should have a tooltip.
-              </td>
-              <td>
-                <button className="button button-disabled" type="button">
-                  Disabled
-                </button>
-              </td>
-            </tr>
-            <tr>
-              <th>Small button</th>
+              <th>Small</th>
               <td>
                 Use when in confined spaces.
               </td>
               <td>
                 <button className="button button-small" type="button">
                   Small
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <h3>Button with Icons</h3>
+        <table className="table">
+          <thead>
+            <tr>
+              <th width="20%">Style</th>
+              <th>Purpose</th>
+              <th width="20%">Preview</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>Icon Only</th>
+              <td>
+                ...
+              </td>
+              <td>
+                <button className="button button-link" type="button">
+                  <svg className="icon icon-mini">
+                    <use xlinkHref="#icon-system--plus" />
+                  </svg>
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <th>Icon with Label</th>
+              <td>
+                ...
+              </td>
+              <td>
+                <button className="button button-primary" type="button">
+                  <svg className="icon icon-mini">
+                    <use xlinkHref="#icon-system--plus" />
+                  </svg> Action
                 </button>
               </td>
             </tr>
