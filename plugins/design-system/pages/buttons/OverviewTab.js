9f9a3e2ded5ce10b171e5570b6b77d98fb6198cd
Lee Munroe <lee@leemunroe.com>
2017-05-23 11:45:21 -0700

    Initial button commit

--- plugins/design-system/pages/buttons/OverviewTab.js
+++ plugins/design-system/pages/buttons/OverviewTab.js
@@ -10,31 +10,13 @@ class OverviewTab extends Component {
         <p>
           Do not use Buttons as navigational elements. Instead, use Links because it takes the user to a new page and is not associated with an action. Each page will likely have one primary button. Any remaining calls-to-action are represented as default buttons.
         </p>
-        <div className="panel">
-          <div className="panel-cell text-align-center">
-            <div className="button-collection">
-              <button className="button button-primary" type="button">
-                Primary Button
-              </button>
-              <button className="button" type="button">Default Button</button>
-              <button className="button" type="button" disabled>
-                Primary Button
-              </button>
-              <button className="button button-primary-link" type="button">
-                Link Button
-              </button>
-              <button className="button button-danger" type="button">
-                Danger Button
-              </button>
-            </div>
-          </div>
-        </div>
         <h2>Guidelines</h2>
         <table className="table">
           <thead>
             <tr>
               <th width="20%">Button Type</th>
               <th>Purpose</th>
+              <th width="20%">Preview</th>
             </tr>
           </thead>
           <tbody>
@@ -43,42 +25,104 @@ class OverviewTab extends Component {
               <td>
                 Primary call to action button. Should stand out above all other actions. Examples include Save, Deploy.
               </td>
+              <td>
+                <button className="button-primary" type="button">
+                  Primary
+                </button>
+              </td>
             </tr>
             <tr>
-              <th>Secondary</th>
-              <td />
+              <th>Default</th>
+              <td>
+                Most buttons on a page are likely default.
+              </td>
+              <td>
+                <button className="button" type="button">
+                  Default
+                </button>
+              </td>
             </tr>
             <tr>
-              <th>Text</th>
-              <td />
+              <th>Link</th>
+              <td>
+                Link buttons are available to use if we want the action to not stand out as much e.g. "Cancel".
+              </td>
+              <td>
+                <button className="button button-link" type="button">
+                  Link
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <th>Outline</th>
+              <td>
+                Outline buttons are available to use when ...
+              </td>
+              <td>
+                <button className="button button-outline" type="button">
+                  Outline
+                </button>
+              </td>
             </tr>
             <tr>
               <th>Danger</th>
-              <td />
-            </tr>
-            <tr>
-              <th>Button with icon</th>
-              <td />
-            </tr>
-            <tr>
-              <th>Icon only button</th>
-              <td />
-            </tr>
-            <tr>
-              <th>Set of buttons</th>
-              <td />
+              <td>
+                Danger buttons are used to highlight something destructive e.g. "Delete"
+              </td>
+              <td>
+                <button className="button button-danger" type="button">
+                  Danger
+                </button>
+              </td>
             </tr>
             <tr>
               <th>Disabled button</th>
-              <td />
+              <td>
+                Disable a button when the action is not applicable but we want to show the user that it may be possible. Disabled buttons should have a tooltip.
+              </td>
+              <td>
+                <button className="button button-disabled" type="button">
+                  Disabled
+                </button>
+              </td>
             </tr>
             <tr>
               <th>Small button</th>
-              <td />
+              <td>
+                Use when in confined spaces.
+              </td>
+              <td>
+                <button className="button button-small" type="button">
+                  Small
+                </button>
+              </td>
             </tr>
           </tbody>
         </table>
-        <h2>Best Practices</h2>
+        <h2>Labels</h2>
+        <p>
+          Button labels tell users what will happen when they click the button.
+        </p>
+        <p>
+          Use verbs that describe the action. Acceptable labels include:
+        </p>
+        <ul>
+          <li>Create Object</li>
+          <li>Edit Object</li>
+          <li>Save Object</li>
+          <li>Delete Object</li>
+          <li>Deploy, Scale, Restart</li>
+        </ul>
+        <p>
+          Use sentence-style capitalization (only the first word in a phrase and any proper nouns capitalized) and no more than three words for button labels.
+        </p>
+        <p>
+          For Sets of Buttons, use specific labels, such as Save or Cancel, instead of using Yes and No. This is particularly helpful when the user is confirming an action.
+        </p>
+        <h2>Usage</h2>
+        <h2>Attribute Breakdown</h2>
+        <h2>Format</h2>
+        <h2>Interaction/Behavior</h2>
       </div>
     );
   }
--- plugins/design-system/pages/buttons/StylesTab.js
+++ plugins/design-system/pages/buttons/StylesTab.js
@@ -3,8 +3,16 @@ import React, { Component } from "react";
 class StylesTab extends Component {
   render() {
     return (
-      <div>
-        Styles Tab
+      <div className="container">
+        <p>
+          Ideally we don't hard code values here otherwise they will be out of date very quickly.
+        </p>
+        <p>
+          Documenting how we do hover/active/focus styles and colors would be useful.
+        </p>
+        <h2>Color</h2>
+        <h2>Type</h2>
+        <h2>Spacing</h2>
       </div>
     );
   }
