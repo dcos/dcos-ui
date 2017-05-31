e9b4bc4a0dc2a4c0b957c902a0b6bddc805f8455
tnguyen0 <tommy_nguyen_76@hotmail.com>
2017-05-30 18:33:42 -0700

    feat(DCOS-15919): Tabs for Design System UI Components

---
+++ plugins/design-system/pages/ui-components/radio-buttons/tabs/CodeTab.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class CodeTab extends Component {
+  render() {
+    return (
+      <div>
+        <h3 className="flush-top">Primary Button</h3>
+        <p>Here's a primary button</p>
+        <button className="button button-primary">Button</button>
+      </div>
+    );
+  }
+}
+
+module.exports = CodeTab;
