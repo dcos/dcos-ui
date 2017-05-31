4490170a15632d5609d8272afb5444fb478eb3aa
tnguyen0 <tommy_nguyen_76@hotmail.com>
2017-05-30 18:33:42 -0700

    feat(DCOS-15919): Tabs for Design System UI Components

---
+++ plugins/design-system/pages/ui-components/charts/tabs/StylesTab.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class StylesTab extends Component {
+  render() {
+    return (
+      <div>
+        Styles Tab
+      </div>
+    );
+  }
+}
+
+module.exports = StylesTab;
