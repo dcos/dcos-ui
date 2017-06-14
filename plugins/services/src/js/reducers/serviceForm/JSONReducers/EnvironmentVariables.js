ad664bc09a2b3cccfa861e7155dd97dfd4ef4ffc
Dmitriy Rozhkov <drozhkov@mesosphere.io>
2017-06-14 16:32:34 +0200

    fix(JSONParser): skip non string values in env var parsers

--- plugins/services/src/js/reducers/serviceForm/JSONReducers/EnvironmentVariables.js
+++ plugins/services/src/js/reducers/serviceForm/JSONReducers/EnvironmentVariables.js
@@ -63,20 +63,26 @@ module.exports = {
       return [];
     }
 
-    return Object.keys(state.env).reduce(function(memo, key, index) {
-      /**
-       * For the environment variables which are a key => value based object
-       * we want to create a new item and fill it with the key and the
-       * value. So we need 3 transactions for each key value pair.
-       * 1) Add a new Item to the path with the index equal to index.
-       * 2) Set the key on the path `env.${index}.key`
-       * 3) Set the value on the path `env.${index}.value`
-       */
-      memo.push(new Transaction(["env"], index, ADD_ITEM));
-      memo.push(new Transaction(["env", index, "key"], key, SET));
-      memo.push(new Transaction(["env", index, "value"], state.env[key], SET));
+    return Object.keys(state.env)
+      .filter(function(key) {
+        return typeof state.env[key] === "string";
+      })
+      .reduce(function(memo, key, index) {
+        /**
+         * For the environment variables which are a key => value based object
+         * we want to create a new item and fill it with the key and the
+         * value. So we need 3 transactions for each key value pair.
+         * 1) Add a new Item to the path with the index equal to index.
+         * 2) Set the key on the path `env.${index}.key`
+         * 3) Set the value on the path `env.${index}.value`
+         */
+        memo.push(new Transaction(["env"], index, ADD_ITEM));
+        memo.push(new Transaction(["env", index, "key"], key, SET));
+        memo.push(
+          new Transaction(["env", index, "value"], state.env[key], SET)
+        );
 
-      return memo;
-    }, []);
+        return memo;
+      }, []);
   }
 };
