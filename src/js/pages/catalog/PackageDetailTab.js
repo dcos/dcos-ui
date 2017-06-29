7af9ffd7c55295b8124412872b9f9cb0608d6fb4
Michael Lancaster <mlancaster@mesosphere.com>
2017-06-29 11:30:25 -0700

    feat(DCOS-16435): fix typo

--- src/js/pages/catalog/PackageDetailTab.js
+++ src/js/pages/catalog/PackageDetailTab.js
@@ -323,7 +323,7 @@ class PackageDetailTab extends mixin(StoreMixin) {
               <div className="media-object-item package-action-buttons">
                 {this.getInstallButtons(cosmosPackage)}
                 <small>
-                  By deploying you agree to {" "}
+                  By deploying you agree to the {" "}
                   <a
                     href={this.getTermsConditionUrl()}
                     target="_blank"
