237d59c8b955427e98f9684f8c6f8f6eb669093f
Andrew Hoskins <ahoskins@mesosphere.com>
2017-07-10 14:53:05 +0200

    test(DCOS-15059): new service shows in dashboard list + inc task count

--- system-tests/dashboard/test-dashboard.js
+++ system-tests/dashboard/test-dashboard.js
@@ -339,4 +339,213 @@ describe("Dashboard", function() {
       expect(newValue).to.eq(initialValue + 10);
     });
   });
+
+  it("new service shows in services list on dashboard when started", function() {
+    // First run a service
+    cy.visitUrl(`services/overview/%2F${Cypress.env("TEST_UUID")}/create`);
+
+    const serviceName = "test-service-appear-in-list";
+    const cmdline = "while true; do echo 'test' ; sleep 100 ; done";
+
+    // Select 'Single Container'
+    cy.contains("Single Container").click();
+
+    // Fill-in the input elements
+    cy
+      .root()
+      .getFormGroupInputFor("Service ID *")
+      .type(`{selectall}{rightarrow}${serviceName}`);
+
+    cy.root().getFormGroupInputFor("Memory (MiB) *").type("{selectall}10");
+    cy.root().getFormGroupInputFor("Command").type(cmdline);
+    cy.root().getFormGroupInputFor("Container Image").type("nginx");
+
+    cy.contains("More Settings").click();
+    cy.root().getFormGroupInputFor("Disk (MiB)").type("{selectall}10");
+
+    // Check JSON view
+    cy.contains("JSON Editor").click();
+
+    // Check contents of the JSON editor
+    cy.get("#brace-editor").contents().asJson().should("deep.equal", [
+      {
+        id: `/${Cypress.env("TEST_UUID")}/${serviceName}`,
+        cmd: cmdline,
+        cpus: 0.1,
+        mem: 10,
+        disk: 10,
+        instances: 1,
+        portDefinitions: [],
+        container: {
+          type: "DOCKER",
+          volumes: [],
+          docker: {
+            image: "nginx"
+          }
+        },
+        requirePorts: false,
+        networks: [],
+        healthChecks: [],
+        fetch: [],
+        constraints: []
+      }
+    ]);
+
+    // Click Review and Run
+    cy.contains("Review & Run").click();
+
+    // Verify the review screen
+    cy
+      .root()
+      .configurationSection("General")
+      .configurationMapValue("Service ID")
+      .contains(`/${Cypress.env("TEST_UUID")}/${serviceName}`);
+    cy
+      .root()
+      .configurationSection("General")
+      .configurationMapValue("CPU")
+      .contains("0.1");
+    cy
+      .root()
+      .configurationSection("General")
+      .configurationMapValue("Memory")
+      .contains("10 MiB");
+    cy
+      .root()
+      .configurationSection("General")
+      .configurationMapValue("Disk")
+      .contains("10 MiB");
+
+    cy.get("button.button-primary").contains("Run Service").click();
+
+    // Wait for the table and the service to appear
+    cy
+      .get(".page-body-content table", {
+        timeout: Timeouts.SERVICE_DEPLOYMENT_TIMEOUT
+      })
+      .contains(serviceName, { timeout: Timeouts.SERVICE_DEPLOYMENT_TIMEOUT })
+      .should("exist");
+
+    // Check that the dashboard is updated
+    cy.visitUrl("dashboard");
+
+    cy
+      .get(".panel-header")
+      .contains("Services Health")
+      .parents(".panel")
+      .find("li")
+      .contains(serviceName);
+  });
+
+  it("new service updates task count in dashboard", function() {
+    let tasksCountSpan = cy
+      .get(".panel-header")
+      .contains("Tasks")
+      .parents(".panel")
+      .find(".unit-primary");
+
+    let tasksCountInitial;
+    tasksCountSpan.should(function($label) {
+      tasksCountInitial = parseFloat($label.text());
+    });
+
+    // First run a service
+    cy.visitUrl(`services/overview/%2F${Cypress.env("TEST_UUID")}/create`);
+
+    const serviceName = "test-service-updates-task-count";
+    const cmdline = "while true; do echo 'test' ; sleep 100 ; done";
+
+    // Select 'Single Container'
+    cy.contains("Single Container").click();
+
+    // Fill-in the input elements
+    cy
+      .root()
+      .getFormGroupInputFor("Service ID *")
+      .type(`{selectall}{rightarrow}${serviceName}`);
+
+    cy.root().getFormGroupInputFor("Memory (MiB) *").type("{selectall}10");
+    cy.root().getFormGroupInputFor("Command").type(cmdline);
+    cy.root().getFormGroupInputFor("Container Image").type("nginx");
+
+    cy.contains("More Settings").click();
+    cy.root().getFormGroupInputFor("Disk (MiB)").type("{selectall}10");
+
+    // Check JSON view
+    cy.contains("JSON Editor").click();
+
+    // Check contents of the JSON editor
+    cy.get("#brace-editor").contents().asJson().should("deep.equal", [
+      {
+        id: `/${Cypress.env("TEST_UUID")}/${serviceName}`,
+        cmd: cmdline,
+        cpus: 0.1,
+        mem: 10,
+        disk: 10,
+        instances: 1,
+        portDefinitions: [],
+        container: {
+          type: "DOCKER",
+          volumes: [],
+          docker: {
+            image: "nginx"
+          }
+        },
+        requirePorts: false,
+        networks: [],
+        healthChecks: [],
+        fetch: [],
+        constraints: []
+      }
+    ]);
+
+    // Click Review and Run
+    cy.contains("Review & Run").click();
+
+    // Verify the review screen
+    cy
+      .root()
+      .configurationSection("General")
+      .configurationMapValue("Service ID")
+      .contains(`/${Cypress.env("TEST_UUID")}/${serviceName}`);
+    cy
+      .root()
+      .configurationSection("General")
+      .configurationMapValue("CPU")
+      .contains("0.1");
+    cy
+      .root()
+      .configurationSection("General")
+      .configurationMapValue("Memory")
+      .contains("10 MiB");
+    cy
+      .root()
+      .configurationSection("General")
+      .configurationMapValue("Disk")
+      .contains("10 MiB");
+
+    cy.get("button.button-primary").contains("Run Service").click();
+
+    // Wait for the table and the service to appear
+    cy
+      .get(".page-body-content table", {
+        timeout: Timeouts.SERVICE_DEPLOYMENT_TIMEOUT
+      })
+      .contains(serviceName, { timeout: Timeouts.SERVICE_DEPLOYMENT_TIMEOUT })
+      .should("exist");
+
+    // Check that the dashboard is updated
+    cy.visitUrl("dashboard");
+
+    tasksCountSpan = cy
+      .get(".panel-header")
+      .contains("Tasks")
+      .parents(".panel")
+      .find(".unit-primary");
+
+    tasksCountSpan.should(function($tasksCount) {
+      const newValue = parseFloat($tasksCount.text());
+      expect(newValue).to.eq(tasksCountInitial + 1);
+    });
+  });
 });
