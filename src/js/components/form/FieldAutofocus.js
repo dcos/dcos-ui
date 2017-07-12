52e2537b17a1160731c948d13ce937ff55efd6d8
Bill Stavroulakis <bstavroulakis@mesosphere.io>
2017-07-12 15:08:44 +0200

    fix(FieldAutofocusComponent): remove custom propTypes in FieldAutofocus
    Removed the custom propTypes in the FieldAutofocus component.

--- src/js/components/form/FieldAutofocus.js
+++ src/js/components/form/FieldAutofocus.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import DOMUtils from "#SRC/js/utils/DOMUtils";
 
 let lockFieldAutofocus = false;
-const SUPPORTED_TYPES = ["FieldInput", "FieldTextarea", "input", "textarea"];
 
 class FieldAutofocus extends React.Component {
   componentDidMount() {
@@ -11,7 +10,7 @@ class FieldAutofocus extends React.Component {
       return;
     }
     const input = DOMUtils.getInputElement(ReactDOM.findDOMNode(this));
-    if (!input || input === document.activeElement) {
+    if (!input) {
       return;
     }
     lockFieldAutofocus = true;
@@ -27,19 +26,5 @@ class FieldAutofocus extends React.Component {
     return this.props.children;
   }
 }
-
-FieldAutofocus.propTypes = {
-  children(props, propName, componentName) {
-    const prop = props[propName];
-    if (
-      React.Children.count(prop) !== 1 ||
-      !SUPPORTED_TYPES.includes(prop.type.name)
-    ) {
-      return new Error(
-        `${componentName} should have a single child of the following types: ${SUPPORTED_TYPES.join(", ")}.`
-      );
-    }
-  }
-};
 
 module.exports = FieldAutofocus;
