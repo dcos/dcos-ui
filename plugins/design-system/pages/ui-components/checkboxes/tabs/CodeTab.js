b04cdedc71f483e2be0a3ed17cc0cb318fd2397d
tnguyen0 <tommy_nguyen_76@hotmail.com>
2017-07-12 16:49:09 -0700

    feat(design-system): use ComponentExample component for design system checkbox
    Uses the ComponentExample code preview component to show the React and HTML code
    representation of the design system checkbox component

--- plugins/design-system/pages/ui-components/checkboxes/tabs/CodeTab.js
+++ plugins/design-system/pages/ui-components/checkboxes/tabs/CodeTab.js
@@ -1,12 +1,43 @@
 import React, { Component } from "react";
 
+import FieldInput from "#SRC/js/components/form/FieldInput";
+import FieldLabel from "#SRC/js/components/form/FieldLabel";
+import FormGroup from "#SRC/js/components/form/FormGroup";
+
+import ComponentExample
+  from "../../../../components/component-example/ComponentExample";
+
 class CodeTab extends Component {
   render() {
     return (
       <div>
-        <h3 className="flush-top">Primary Button</h3>
-        <p>Here's a primary button</p>
-        <button className="button button-primary">Button</button>
+        <h3 className="flush-top">Checkbox</h3>
+        <ComponentExample>
+          <div>
+            <FormGroup>
+              <FieldLabel>
+                <FieldInput
+                  checked={false}
+                  disabled={false}
+                  name="check_box_first"
+                  type="checkbox"
+                />
+                Check Me First
+              </FieldLabel>
+            </FormGroup>
+            <FormGroup>
+              <FieldLabel>
+                <FieldInput
+                  checked={false}
+                  disabled={false}
+                  name="check_box_second"
+                  type="checkbox"
+                />
+                Check Me Second
+              </FieldLabel>
+            </FormGroup>
+          </div>
+        </ComponentExample>
       </div>
     );
   }
