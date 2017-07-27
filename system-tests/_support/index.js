bea2c597bff71ee28fc64c929da9cbc2a30eee01
Andrew Hoskins <ahoskins@mesosphere.com>
2017-07-27 10:13:39 -0700

    test(Dashboard): better input validation for service id
    Check for leading slash, and service deployed directly into
    TEST_UUID group. Expected format: /TEST_UUID/service-name.
    
    Closes DCOS-17383.

--- system-tests/_support/index.js
+++ system-tests/_support/index.js
@@ -219,20 +219,34 @@ Cypress.addChildCommand("triggerHover", function(elements) {
 });
 
 /**
+ * Checks that a service id abides by format: /<test-UUID>/service-name
+ *
+ * @param {String} id - serviceId to validate
+ *
+ */
+function validateServiceId(id) {
+  if (id[0] !== "/") {
+    throw new Error("Must include leading slash in service id");
+  }
+  const idParts = id.split("/");
+  if (idParts[1] !== Cypress.env("TEST_UUID")) {
+    throw new Error("Service must be in the TEST_UUID group");
+  }
+  if (idParts.length !== 3) {
+    throw new Error("Must deploy service directly in TEST_UUID group");
+  }
+}
+
+/**
  * Launches a new service using the dcos CLI. Service must be created
- * in the TEST_UUID group.
+ * directly in the TEST_UUID group. Ex: /<TEST-UUID>/test-service
  *
  * @param {Object} serviceDefinition - The service JSON definition file
  *
  */
 export function createService(serviceDefinition) {
-  const idParts = serviceDefinition.id.split("/");
-  if (idParts.length !== 3) {
-    throw new Error("Must include leading slash in service id");
-  }
-  if (idParts[1] !== Cypress.env("TEST_UUID")) {
-    throw new Error("Service must be in the TEST_UUID group");
-  }
+  validateServiceId(serviceDefinition.id);
+  const serviceName = serviceDefinition.id.split("/").pop();
 
   cy.exec(
     `echo '${JSON.stringify(serviceDefinition)}' | dcos marathon app add`
@@ -242,13 +256,13 @@ export function createService(serviceDefinition) {
     .get(".page-body-content table", {
       timeout: Timeouts.SERVICE_DEPLOYMENT_TIMEOUT
     })
-    .contains(idParts[2], {
+    .contains(serviceName, {
       timeout: Timeouts.SERVICE_DEPLOYMENT_TIMEOUT
     })
     .should("exist");
   cy
     .get(".page-body-content table")
-    .getTableRowThatContains(idParts[2])
+    .getTableRowThatContains(serviceName)
     .contains("Running", { timeout: Timeouts.SERVICE_DEPLOYMENT_TIMEOUT })
     .should("exist");
 }
@@ -260,15 +274,10 @@ export function createService(serviceDefinition) {
  *
  */
 export function deleteService(serviceId) {
-  const idParts = serviceId.split("/");
-  if (idParts.length !== 3) {
-    throw new Error("Must include leading slash in service id");
-  }
-  if (idParts[1] !== Cypress.env("TEST_UUID")) {
-    throw new Error("Service must be in the TEST_UUID group");
-  }
+  validateServiceId(serviceId);
+  const serviceName = serviceId.split("/").pop();
 
   cy.exec(`dcos marathon app remove ${serviceId}`);
   cy.visitUrl(`services/overview/%2F${Cypress.env("TEST_UUID")}`);
-  cy.get(".page-body-content table").contains(idParts[2]).should("not.exist");
+  cy.get(".page-body-content table").contains(serviceName).should("not.exist");
 }
