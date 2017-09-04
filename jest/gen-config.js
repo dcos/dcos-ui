429ef8bfc16f2f4ff0e09a1f861dba2405c256a6
Julian Gieseke <jgieseke@mesosphere.com>
2017-09-04 14:32:27 +0200

    refactor(jest): adjust config to enable package specific mocks
    Adjust the config to dynamically generate the list of root directories
    to allow package specific mocks, as Jest only loads mocks from "root"
    (testPathDir) directories.
    
    Side Note: Jest also renamed `testPathDir` to `roots` in newer versions
    which better explains what this option is doing.

--- jest/gen-config.js
+++ jest/gen-config.js
@@ -3,14 +3,48 @@
  */
 var fs = require('fs');
 var path = require('path');
-var testPaths = ['src', 'plugins', 'tests', 'packages'];
 
+/**
+ * List of root (test path) directories
+ *
+ * @description Jest will only search for tests and
+ * load manual mocks (e.g. `src/__mocks__/fs.js`) within these directories.
+ *
+ * @type {Array.<string>}
+ */
+const roots = ['src', 'tests'];
+
+/**
+ * List of package directories
+ *
+ * @description Packages (subdirectories)  within these directories are
+ * going to be added as a "root" directory; this allows packages to roll
+ * their own "manual" mocks (e.g., `my-package/__mocks__/fs.js`),
+ * as Jest only loads them from root directories.
+ *
+ * @type {Array.<string>}
+ */
+const packages = ["plugins", "packages"];
+
+// Add `npm_config_externalplugins` and strip trailing slash
+// to make sure the paths are compatible.
 if (process.env.npm_config_externalplugins) {
-  testPaths.push(process.env.npm_config_externalplugins);
+  packages.push(process.env.npm_config_externalplugins.replace(/\/$/, ""));
 }
 
+// Traverse the subdirectories for every package directory and add the
+// directories to the list root directories.
+packages.forEach(function(packageDir) {
+  fs.readdirSync(packageDir).forEach(function(rootDir) {
+    const relativePath = packageDir + "/" + rootDir;
+    if (fs.statSync(relativePath).isDirectory()) {
+      roots.push(relativePath);
+    }
+  });
+});
+
 var config = {
-  'testPathDirs': testPaths,
+  'testPathDirs': roots,
   'globals': {
     '__DEV__': true
   },
