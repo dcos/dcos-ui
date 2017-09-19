5eab5c3509585c8d09668284500e22b627fd1737
Bill Stavroulakis <bstavroulakis@mesosphere.io>
2017-09-19 14:32:52 -0700

    fix(ServiceDeleteActions): remove service list in groups delete modal
    When you try to delete a group with frameworks we'll notify the user that he cannot delete that group. Instead of listing the frameworks we'll show a more generic message.

--- plugins/services/src/js/components/modals/GroupContainsFrameworksModal.js
+++ plugins/services/src/js/components/modals/GroupContainsFrameworksModal.js
@@ -2,37 +2,20 @@ import React from "react";
 import { Modal } from "reactjs-components";
 import ModalHeading from "#SRC/js/components/modals/ModalHeading";
 
-const getServices = function(services) {
-  if (services && services.length > 0) {
-    return services
-      .map(service => {
-        return service.getId();
-      })
-      .join(", ");
-  }
-
-  return;
-};
-
 const GroupContainsFrameworksModal = props => {
   const modalFooter = (
     <div className="text-align-center">
-      <button
-        className="button button-primary button-medium"
-        onClick={props.onClose}
-      >
-        Close
+      <button className="button button-medium" onClick={props.onClose}>
+        OK
       </button>
     </div>
   );
 
   const modalHeading = (
     <ModalHeading className="text-danger">
-      Group Delete
+      Delete Group
     </ModalHeading>
   );
-
-  const pluralEnding = props.services && props.services.length > 1 ? "s" : "";
 
   return (
     <Modal
@@ -44,14 +27,7 @@ const GroupContainsFrameworksModal = props => {
       showFooter={true}
       showHeader={true}
     >
-      Must delete
-      {" "}
-      <b>{getServices(props.services)}</b>
-      {" "}
-      service
-      {pluralEnding}
-      {" "}
-      before destroying the group
+      This group needs to be empty to delete it. Please delete any services in the group first.
     </Modal>
   );
 };
