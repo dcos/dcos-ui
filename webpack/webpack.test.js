b90aaf4bf0fecaad6b15e29ccbefd0d44aca7601
Dmitriy Rozhkov <drozhkov@mesosphere.io>
2017-10-05 16:15:48 +0200

    feat(babel): allow stage-3 syntax

--- webpack/webpack.test.js
+++ webpack/webpack.test.js
@@ -1,6 +1,6 @@
 // Transform our es6 webpack.dev.babel.js so we can use it in ../jest/preprocessor.js
 require("babel-register")({
-  presets: ["es2015"]
+  presets: ["es2015", "stage-3"]
 });
 
 module.exports = require("./webpack.dev.babel.js");
