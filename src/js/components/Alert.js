017413a0a65bbc5bce3a2d5169a459b568adb9fd
Lee Munroe <lee@leemunroe.com>
2017-10-20 16:32:32 -0700

    refactor(message): Add message icons back

--- src/js/components/Alert.js
+++ src/js/components/Alert.js
@@ -1,27 +1,48 @@
 import classNames from "classnames";
 import React from "react";
 
-const Alert = ({ children, flushBottom, type }) => {
+import Icon from "./Icon";
+
+const Alert = ({ children, flushBottom, showIcon, type }) => {
   const classes = classNames("message", {
     [`message-${type}`]: type != null,
     "flush-bottom": flushBottom === true
   });
+  let icon = null;
+
+  if (showIcon) {
+    const ids = {
+      danger: "yield",
+      success: "checkmark"
+    };
+
+    icon = (
+      <div className="message-icon">
+        <Icon id={ids[type]} size="mini" />
+      </div>
+    );
+  }
 
   return (
     <div className={classes}>
-      {children}
+      {icon}
+      <div className="message-content">
+        {children}
+      </div>
     </div>
   );
 };
 
 Alert.defaultProps = {
   flushBottom: false,
+  showIcon: true,
   type: "danger"
 };
 
 Alert.propTypes = {
   children: React.PropTypes.node.isRequired,
   flushBottom: React.PropTypes.bool,
+  showIcon: React.PropTypes.bool,
   type: React.PropTypes.oneOf(["danger", "success"])
 };
 
