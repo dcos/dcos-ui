f77dfcaf7369790c85bafdf9072945c66af3b2a2
Michael Lancaster <mlancaster@mesosphere.com>
2017-10-26 09:11:20 -0700

    feat(ServiceStatusIcon) include warning Include logic to display warning icon and it's tooltip when necessary
    Closes DCOS-18264

--- plugins/services/src/js/components/ServiceStatusIcon.js
+++ plugins/services/src/js/components/ServiceStatusIcon.js
@@ -1,20 +1,50 @@
 import React, { Component, PropTypes } from "react";
+import { Tooltip } from "reactjs-components";
 
 import StatusIcon from "#SRC/js/components//StatusIcon";
 import Pod from "../structs/Pod";
 import Service from "../structs/Service";
 import ServiceTree from "../structs/ServiceTree";
+import DeclinedOffersUtil from "../utils/DeclinedOffersUtil";
 
 class ServiceStatusIcon extends Component {
+  getTooltipContent() {
+    const { service } = this.props;
+    const serviceDebugPath = encodeURIComponent(service.getId());
+
+    return (
+      <div className="tooltip-line-item">
+        <span>
+          Waiting for resources to continue.
+          {" "}
+          <a href={`#/services/detail/${serviceDebugPath}/debug`}>
+            Debug information
+          </a>.
+        </span>
+      </div>
+    );
+  }
+
   render() {
     const { service } = this.props;
     const displayName = service.getServiceStatus().displayName.toUpperCase();
+    let iconState = displayName;
 
-    const iconState = displayName === "DEPLOYING" ||
+    if (
+      displayName === "DEPLOYING" ||
       displayName === "RECOVERING" ||
       displayName === "DELETING"
-      ? "TRANSITION"
-      : displayName;
+    ) {
+      iconState = "TRANSITION";
+    }
+
+    if (DeclinedOffersUtil.shouldDisplayDeclinedOffersWarning(service)) {
+      return (
+        <Tooltip interactive={true} content={this.getTooltipContent()}>
+          <StatusIcon state="WARNING" />
+        </Tooltip>
+      );
+    }
 
     return <StatusIcon state={iconState} />;
   }
