1d3e84e3ce40f7b04f7a016c69ef751ade7e1174
Dmitriy Rozhkov <drozhkov@mesosphere.io>
2017-10-26 11:32:09 +0200

    refactor(packages): extract packages from the source code

--- packages/connections/src/__tests__/AbstractConnection-test.js
+++
@@ -1,9 +0,0 @@
-import AbstractConnection from "../AbstractConnection.js";
-
-describe("AbstractConnection", () => {
-  it("throws Error on Init", () => {
-    expect(() => {
-      new AbstractConnection("foo.json");
-    }).toThrowError();
-  });
-});
--- src/js/core/MesosStream.js
+++ src/js/core/MesosStream.js
@@ -1,4 +1,4 @@
-import { stream } from "mesos-client";
+import { stream } from "@dcos/mesos-client";
 
 export const MesosStreamType = Symbol("MesosStreamType");
 export default stream({ type: "SUBSCRIBE" }).publishReplay().refCount();
