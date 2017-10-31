dcos/dcos-ui

commit 5e2d8320bb505284096e4a50104d5b7a60d39143

feat(DCOS-13054): Hover container panel purple outline and clickable

src/js/components/form/FormGroupContainer.js
@@ -24,7 +24,10 @@ const FormGroupContainer = props => {
   }
 
   return (
-    <div className="panel pod-short" onClick={props.onClick}>
+    <div
+      className="panel panel-interactive pod-short clickable"
+      onClick={props.onClick}
+    >
       <div className="pod-narrow pod-short">
         {removeButton}
         {props.children}

commit ab95c7997a90bf404fbf8fae177f960cfe465214

fix(FormGroupContainer): only apply hover styles if onClick
This was is fixing a regression from 5e2d832 which caused
hover on all FormGroupContainers. Now it will only apply the
outline and cursor pointer when a onClick prop is provided.
Currently, this only occurs for container pods when creating
a service.

Closes DCOS-18317

src/js/components/form/FormGroupContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Tooltip } from "reactjs-components";
+import classNames from "classnames";
 
 import Icon from "../Icon";
 import StringUtil from "../../utils/StringUtil";
@@ -23,11 +24,12 @@ const FormGroupContainer = props => {
     );
   }
 
+  const classes = classNames("panel pod-short", {
+    "panel-interactive clickable": props.onClick
+  });
+
   return (
-    <div
-      className="panel panel-interactive pod-short clickable"
-      onClick={props.onClick}
-    >
+    <div className={classes} onClick={props.onClick}>
       <div className="pod-narrow pod-short">
         {removeButton}
         {props.children}