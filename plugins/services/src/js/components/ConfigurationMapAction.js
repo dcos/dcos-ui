415b95cf7d857e72f41fb785b8b2f82d11c3b890
Andrew Hoskins <ahoskins@mesosphere.com>
2017-11-02 11:22:16 -0700

    feat(ConfigurationMapAction): add href prop
    Add an href prop to this component. When the href prop
    is provided it will render an anchor tag. When the href
    prop is not provided it will render a button tag with
    the onClick prop provided. This is more semantically correct
    HTML: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#Accessibility_recommendations
    
    Closes DCOS-19339.

--- plugins/services/src/js/components/ConfigurationMapAction.js
+++ plugins/services/src/js/components/ConfigurationMapAction.js
@@ -1,20 +1,29 @@
 import React from "react";
 import classNames from "classnames";
 
-const ConfigurationMapAction = ({ isHover, onClick, children }) => {
+const ConfigurationMapAction = ({ isHover, onClick, href, children }) => {
   const classes = classNames("button button-link flush", {
     "table-display-on-row-hover": isHover
   });
 
+  if (href) {
+    return (
+      <a className={classes} href={href}>
+        {children}
+      </a>
+    );
+  }
+
   return (
-    <a className={classes} onClick={onClick}>
+    <button className={classes} onClick={onClick}>
       {children}
-    </a>
+    </button>
   );
 };
 
 ConfigurationMapAction.propTypes = {
   onClick: React.PropTypes.func,
+  href: React.PropTypes.string,
   isHover: React.PropTypes.bool
 };
 
