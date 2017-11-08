10c76d6acf7509b0e788fd199d99f5a849bd8907
Andrew Hoskins <ahoskins@mesosphere.com>
2017-11-08 15:28:16 -0800

    feat(HighOrderServiceConfiguration): Framework config tab with cosmos
    Use the /cosmos/service/describe data to render the configuration
    tab on service detail when the service is Framework. This brings parity
    between this page and the review screen when editing the framework.
    
    Closes DCOS_OSS-1789.

--- plugins/services/src/js/components/HighOrderServiceConfiguration.js
+++ plugins/services/src/js/components/HighOrderServiceConfiguration.js
@@ -1,16 +1,23 @@
 import React from "react";
 
+import Framework from "#PLUGINS/services/src/js/structs/Framework";
 import Pod from "../structs/Pod";
 import PodConfigurationContainer
   from "../containers/pod-configuration/PodConfigurationContainer";
 import Service from "../structs/Service";
 import ServiceConfigurationContainer
   from "../containers/service-configuration/ServiceConfigurationContainer";
+import FrameworkConfigurationContainer
+  from "../containers/framework-configuration/FrameworkConfigurationContainer";
 
 const HighOrderServiceConfiguration = function(props) {
   const { errors, onEditClick, service } = props;
   if (service instanceof Pod) {
     return <PodConfigurationContainer pod={service} />;
+  }
+
+  if (service instanceof Framework) {
+    return <FrameworkConfigurationContainer service={service} />;
   }
 
   return (
---
+++ src/js/components/FrameworkConfigurationReviewScreen.js
@@ -0,0 +1,111 @@
+import React from "react";
+
+import HashMapDisplay from "#SRC/js/components/HashMapDisplay";
+import Util from "#SRC/js/utils/Util";
+import StringUtil from "#SRC/js/utils/StringUtil";
+import Icon from "#SRC/js/components/Icon";
+import RouterUtil from "#SRC/js/utils/RouterUtil";
+
+const METHODS_TO_BIND = ["reorderResolvedOptions", "getHashMapRenderKeys"];
+
+class FrameworkConfigurationReviewScreen extends React.Component {
+  constructor(props) {
+    super(props);
+
+    METHODS_TO_BIND.forEach(method => {
+      this[method] = this[method].bind(this);
+    });
+  }
+
+  reorderResolvedOptions(resolvedOptions, config) {
+    const order = Object.keys(config.properties);
+    const orderedResolvedOptions = {};
+    order.forEach(tab => {
+      orderedResolvedOptions[tab] = resolvedOptions[tab];
+    });
+
+    return orderedResolvedOptions;
+  }
+
+  getHashMapRenderKeys(formData) {
+    if (!Util.isObject(formData)) {
+      return {};
+    }
+
+    let renderKeys = {};
+    Object.keys(formData).forEach(key => {
+      renderKeys[key] = StringUtil.capitalizeEveryWord(key);
+      renderKeys = Object.assign(
+        renderKeys,
+        this.getHashMapRenderKeys(formData[key])
+      );
+    });
+
+    return renderKeys;
+  }
+
+  render() {
+    const { frameworkData, title, onEditClick } = this.props;
+
+    const fileName = "config.json";
+    const configString = JSON.stringify(frameworkData, null, 2);
+
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="column-4">
+            <h1 className="flush-top">{title}</h1>
+          </div>
+          <div className="column-8 text-align-right">
+            <button
+              className="button button-primary-link button-inline-flex"
+              onClick={onEditClick}
+            >
+              <Icon id="pencil" size="mini" family="system" />
+              <span className="form-group-heading-content">
+                {"Edit Config"}
+              </span>
+            </button>
+            <a
+              className="button button-primary-link flush-right"
+              download={fileName}
+              onClick={RouterUtil.triggerIEDownload.bind(
+                null,
+                fileName,
+                configString
+              )}
+              href={RouterUtil.getResourceDownloadPath(
+                "attachment/json",
+                fileName,
+                configString
+              )}
+            >
+              <Icon id="download" size="mini" family="system" />
+              <span className="form-group-heading-content">
+                {"Download Config"}
+              </span>
+            </a>
+          </div>
+        </div>
+        <HashMapDisplay
+          hash={frameworkData}
+          renderKeys={this.getHashMapRenderKeys(frameworkData)}
+          headlineClassName={"text-capitalize"}
+          emptyValue={"\u2014"}
+        />
+      </div>
+    );
+  }
+}
+
+FrameworkConfigurationReviewScreen.defaultProps = {
+  title: ""
+};
+
+FrameworkConfigurationReviewScreen.propTypes = {
+  onEditClick: React.PropTypes.func.isRequired,
+  frameworkData: React.PropTypes.object.isRequired,
+  title: React.PropTypes.string
+};
+
+module.exports = FrameworkConfigurationReviewScreen;
