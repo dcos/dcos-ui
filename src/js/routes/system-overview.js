6a9abb73af4ec22c91d3abf9aaf8992821d4bf4f
Bill Stavroulakis <bstavroulakis@mesosphere.io>
2017-11-13 17:12:51 -0800

    feat(cluster-linking): add linked clusters page and menu link
    Add linked clusters page and menu link.
    
    Closes DCOS-19345

--- src/js/routes/system-overview.js
+++ src/js/routes/system-overview.js
@@ -1,20 +1,30 @@
-import { Route, IndexRoute } from "react-router";
+import { Route, Redirect } from "react-router";
 
 import SystemOverviewPage from "../pages/SystemOverviewPage";
 import OverviewDetailTab from "../pages/system/OverviewDetailTab";
 
-const systemOverviewRoutes = {
-  type: Route,
-  path: "overview",
-  component: SystemOverviewPage,
-  category: "system",
-  isInSidebar: true,
-  children: [
-    {
-      type: IndexRoute,
-      component: OverviewDetailTab
-    }
-  ]
-};
+const systemOverviewRoutes = [
+  {
+    type: Redirect,
+    from: "/cluster",
+    to: "/cluster/overview"
+  },
+  {
+    type: Route,
+    id: "cluster",
+    path: "cluster",
+    component: SystemOverviewPage,
+    category: "system",
+    isInSidebar: true,
+    children: [
+      {
+        type: Route,
+        path: "overview",
+        component: OverviewDetailTab,
+        isInSidebar: true
+      }
+    ]
+  }
+];
 
 module.exports = systemOverviewRoutes;
