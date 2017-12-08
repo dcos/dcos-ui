c17dc403b922e73e39a7baf221a6d55c0bb13e42
John Ashenden <jaashenden@gmail.com>
2017-12-07 18:26:50 -0800

    refactor(table): Remove .table-simple classes from all instances
    Remove .table-simple table variant. The DC/OS UI Kit makes no distinction between .table and .table-simple. Remove unwanted variant.

--- plugins/services/src/js/service-configuration/ServiceNetworkingConfigSection.js
+++ plugins/services/src/js/service-configuration/ServiceNetworkingConfigSection.js
@@ -224,7 +224,7 @@ class ServiceNetworkingConfigSection extends ServiceConfigBaseSectionDisplay {
               </ConfigurationMapHeading>,
               <Table
                 key="service-endpoints"
-                className="table table-simple table-flush table-borderless-outer table-borderless-inner-columns vertical-align-top table-break-word table-fixed-layout flush-bottom"
+                className="table table-flush table-borderless-outer table-borderless-inner-columns vertical-align-top table-break-word table-fixed-layout flush-bottom"
                 columns={columns}
                 data={portDefinitions}
               />
