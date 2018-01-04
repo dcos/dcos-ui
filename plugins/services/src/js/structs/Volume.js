a6b7e7cd4847f5be4d384b55abb134ae8efe1dd1
Daniel Schmidt <dschmidt@mesosphere.com>
2018-01-04 16:54:48 +0100

    feat(VolumeTable): add volume type column
    This changes the volumes UI to show a volume
    type column.
    
    Closes DCOS-19585

--- plugins/services/src/js/structs/Volume.js
+++ plugins/services/src/js/structs/Volume.js
@@ -1,5 +1,6 @@
 import Item from "#SRC/js/structs/Item";
 import VolumeStatus from "../constants/VolumeStatus";
+import VolumeProfile from "../constants/VolumeProfile";
 
 class Volume extends Item {
   getContainerPath() {
@@ -16,6 +17,10 @@ class Volume extends Item {
 
   getMode() {
     return this.get("mode");
+  }
+
+  getProfile() {
+    return this.get("profileName") || VolumeProfile.UNAVAILABLE;
   }
 
   getStatus() {
