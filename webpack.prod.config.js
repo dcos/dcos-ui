7da38188dd55be4d6adb7b7b10f83f2e7f697643
Daniel Schmidt <dschmidt@mesosphere.com>
2018-02-02 16:16:09 +0100

    chore(webpack): add production config
    Relates to DCOS_OSS-1564

---
+++ webpack.prod.config.js
@@ -0,0 +1,37 @@
+const {
+  optimize: { ModuleConcatenationPlugin, UglifyJsPlugin }
+} = require("webpack");
+const CompressionPlugin = require("compression-webpack-plugin");
+
+const merge = require("webpack-merge");
+const common = require("./webpack.config.js");
+
+module.exports = merge(common, {
+  plugins: [
+    new ModuleConcatenationPlugin(),
+    new UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true
+      },
+      output: {
+        comments: false
+      }
+    }),
+    new CompressionPlugin({
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8
+    })
+  ]
+});
