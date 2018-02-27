4d11dd5b3188f3d51585b4f0f3f7783ea1df7f53
Daniel Schmidt <dschmidt@mesosphere.com>
2018-02-27 16:22:32 +0100

    chore(webpack): add html loader
    Support favicons in production build
    
    Relates to DCOS_OSS-1564

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -67,6 +67,15 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.html$/,
+        use: {
+          loader: "html-loader",
+          options: {
+            attrs: ["link:href"]
+          }
+        }
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
