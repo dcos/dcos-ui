f083f6271f9e533b9a29d4b531b02a87cc7aef06
Dmitriy Rozhkov <rojkov.dmitry@gmail.com>
2018-03-16 13:33:45 +0100

    feat(mesos-stream): repeat the stream
    in case Mesos closes the connection
    
    Closes DCOS-21701

--- src/js/core/MesosStream.js
+++ src/js/core/MesosStream.js
@@ -1,6 +1,8 @@
 import { stream } from "@dcos/mesos-client";
+import { ReplaySubject } from "rxjs/ReplaySubject";
 
 export const MesosStreamType = Symbol("MesosStreamType");
 export default stream({ type: "SUBSCRIBE" }, "/mesos/api/v1?subscribe")
-  .publishReplay()
+  .repeat()
+  .multicast(() => new ReplaySubject())
   .refCount();
