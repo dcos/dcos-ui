26cbaf0005f55b4190fdb970db392ebeb0ba8966
Fabs <fnascimento@mesosphere.com>
2018-03-28 22:27:10 +0200

    feat: add liveFetchRepositories
    After a mutation (update/delete) we want a stream that "magically"
    gets new data so we never out of sync with the server.

--- plugins/catalog/src/js/repositories/repositoriesStream.js
+++ plugins/catalog/src/js/repositories/repositoriesStream.js
@@ -1,15 +1,27 @@
 import { request } from "@dcos/http-service";
+import { Subject } from "rxjs/Subject";
 import Config from "#SRC/js/config/Config";
+import "rxjs/add/operator/do";
+import "rxjs/add/operator/concatMap";
 
-export const fetchRepositories = type =>
-  request(`${Config.rootUrl}${Config.cosmosAPIPrefix}/repository/list`, {
-    method: "POST",
-    body: JSON.stringify({ type }),
-    headers: {
-      Accept: "application/vnd.dcos.package.repository.list-response+json;charset=utf-8;version=v1",
-      "Content-Type": "application/vnd.dcos.package.repository.list-request+json;charset=utf-8;version=v1"
-    }
-  });
+const reloadSubject = new Subject();
+
+export const fetchRepositories = type => {
+  return reloadSubject.startWith(null).concatMap(() =>
+    request(`${Config.rootUrl}${Config.cosmosAPIPrefix}/repository/list`, {
+      method: "POST",
+      body: JSON.stringify({ type }),
+      headers: {
+        Accept: "application/vnd.dcos.package.repository.list-response+json;charset=utf-8;version=v1",
+        "Content-Type": "application/vnd.dcos.package.repository.list-request+json;charset=utf-8;version=v1"
+      }
+    })
+  );
+};
+
+export const liveFetchRepositories = type => {
+  return reloadSubject.startWith(null).concatMap(() => fetchRepositories(type));
+};
 
 export const addRepository = (name, uri, index) =>
   request(`${Config.rootUrl}${Config.cosmosAPIPrefix}/repository/add`, {
@@ -19,7 +31,7 @@ export const addRepository = (name, uri, index) =>
       Accept: "application/vnd.dcos.package.repository.add-response+json;charset=utf-8;version=v1",
       "Content-Type": "application/vnd.dcos.package.repository.add-request+json;charset=utf-8;version=v1"
     }
-  });
+  }).do(() => reloadSubject.next());
 
 export const deleteRepository = (name, uri) =>
   request(`${Config.rootUrl}${Config.cosmosAPIPrefix}/repository/delete`, {
@@ -29,4 +41,4 @@ export const deleteRepository = (name, uri) =>
       Accept: "application/vnd.dcos.package.repository.delete-response+json;charset=utf-8;version=v1",
       "Content-Type": "application/vnd.dcos.package.repository.delete-request+json;charset=utf-8;version=v1"
     }
-  });
+  }).do(() => reloadSubject.next());
