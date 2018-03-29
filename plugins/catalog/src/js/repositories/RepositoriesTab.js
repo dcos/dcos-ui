7cc385e430709676416809897b41b9304ae38886
Fabs <fnascimento@mesosphere.com>
2018-03-29 17:45:46 +0200

    fix: deprecate store methods and skip tests
    The Store way to mutate repositories work but the tests
    does not. We will remove that capability from the store
    until the end of this feature, so no point in fixing
    those tests.

--- plugins/catalog/src/js/repositories/RepositoriesTab.js
+++ plugins/catalog/src/js/repositories/RepositoriesTab.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { Observable } from "rxjs/Observable";
+import { BehaviorSubject } from "rxjs/BehaviorSubject";
+import "rxjs/add/operator/switchMap";
 
 // Recompose helps handle data down and event up as streams
-import {
-  createEventHandler,
-  componentFromStream
-} from "#PACKAGES/data-service/recomposeRx";
+import { componentFromStream } from "#PACKAGES/data-service/recomposeRx";
 
 // Our in-house solution to resolve graphql queries using streams
 import { graphqlObservable } from "#PACKAGES/data-service/graphqlObservable";
@@ -15,9 +14,7 @@ import { makeExecutableSchema } from "graphql-tools";
 import gql from "graphql-tag";
 
 // Streams we get our data from
-import {
-  liveFetchRepositories
-} from "#PLUGINS/catalog/src/js/repositories/repositoriesStream";
+import { liveFetchRepositories } from "#PLUGINS/catalog/src/js/repositories/repositoriesStream";
 import RepositoryList from "#SRC/js/structs/RepositoryList";
 
 // The graphql schema and resolvers for those streams;
@@ -35,9 +32,9 @@ const schema = makeExecutableSchema({
 });
 
 // 2. We declare which data our component needs in graphql (data down)
-const packageRepositoryQuery = gql`
+const packageRepositoryQuery = filter => gql`
   query {
-    packageRepository(filter: $filter) {
+    packageRepository(filter: "${filter}") {
       name
       uri
     }
@@ -45,39 +42,52 @@ const packageRepositoryQuery = gql`
 `;
 
 // 3. We create an event handler as a stream to deal with search (events up)
-const { handler: onSearch, stream: searchEvent$ } = createEventHandler();
-const searchTerm$ = searchEvent$.startWith("");
+const searchTerm$ = new BehaviorSubject("");
 
 // 4. We compose the result with the query, and massage it down the pipe until
 // we get the props for our component.
 // The idea is from an Observable of data and events, compose a stream o React.Components
-const components$ = graphqlObservable(packageRepositoryQuery, schema, {
-  query: liveFetchRepositories(),
-  filter: searchTerm$
-})
-  .combineLatest(searchTerm$, (result, searchTerm) => {
-    // Backwards compatible with the previous struct/RepositoryList for packages
-    return {
-      packageRepository: new RepositoryList({
+
+const packageRepository$ = liveFetchRepositories();
+
+const components$ = searchTerm$
+  .switchMap(searchTerm => {
+    const query = packageRepositoryQuery(searchTerm);
+    const liveQuery = graphqlObservable(query, schema, {
+      query: packageRepository$
+    }).map(result => {
+      // Backwards compatible with the previous struct/RepositoryList for packages
+      return new RepositoryList({
         items: result.packageRepository
-      }),
-      searchTerm
-    };
+      });
+    });
+
+    return liveQuery.map(packageRepository => {
+      return { packageRepository, searchTerm };
+    });
   })
   // We map over the data and return a component to render
   .map(data => {
+
     return (
       <RepositoriesTabUI
         repositories={data.packageRepository}
         searchTerm={data.searchTerm}
-        onSearch={onSearch}
+        // onSearch has arity 2 which prevents to pass `next` as reference
+        onSearch={(value) => {
+          searchTerm$.next(value);
+        }}
       />
     );
   })
   // The first component is the loading
   .startWith(<RepositoriesTabUI.Loading />)
   // If anything goes wrong, we render an error component
-  .catch(err => Observable.of(<RepositoriesTabUI.Error err={err} />));
+  .catch(err => {
+    console.error(err);
+
+    return Observable.of(<RepositoriesTabUI.Error err={err} />);
+  });
 
 // componentFromStream create a single component who render the latest emitted
 // component from the stream
