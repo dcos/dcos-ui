bd2d9e90c22cc8c77886de4f8a0a50c73dae3dfa
Daniel Schmidt <dschmidt@mesosphere.com>
2018-04-04 14:44:31 +0200

    fix(editservicemodal): render empty modal if the service is not loaded
    We accessed getLabel on the service without checking if the service is
    even there. Therefore reloading the edit page caused an error.
    
    By checking for a service and rendering an empty modal otherwise
    we make sure that we don't try to render before we have the data.
    
    Rendering the create form in these cases lead to a mutation of the data,
    which in return lead to the edit form never being rendered.
    Rendering null if there is no service lead to a dark flickering for the
    time before the service loads. An empty modal gives a nicer UX.
    
    Closes DCOS-21798

--- plugins/services/src/js/components/modals/EditServiceModal.js
+++ plugins/services/src/js/components/modals/EditServiceModal.js
@@ -5,12 +5,17 @@ import EditFrameworkConfiguration
 import CreateServiceModal
   from "#PLUGINS/services/src/js/components/modals/CreateServiceModal";
 import DCOSStore from "#SRC/js/stores/DCOSStore";
+import FullScreenModal from "#SRC/js/components/modals/FullScreenModal";
 
 class EditServiceModal extends Component {
   render() {
     const { id = "/" } = this.props.params;
     const serviceID = decodeURIComponent(id);
     const service = DCOSStore.serviceTree.findItemById(serviceID);
+
+    if (!service) {
+      return <FullScreenModal open={true} />;
+    }
 
     if (
       service.getLabels().DCOS_PACKAGE_DEFINITION != null ||
