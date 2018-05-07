4033a0183ba61cfc07c67709294bfa5f17d4da55
Fabs <fnascimento@mesosphere.com>
2018-05-07 16:19:30 +0200

    reafactor: organize methods of graphqlObservable
    This is a preliminary refactor, more should come, specially
    adding types with typescript.
    - breaks logic into smaller methods
    - reorder the methods according to Clean Code style
    - renames parameters and methods so patterns are more easy visible

--- packages/data-service/src/graphqlObservable.js
+++ packages/data-service/src/graphqlObservable.js
@@ -6,141 +6,145 @@ import "rxjs/add/operator/do";
 import "rxjs/add/operator/map";
 import "rxjs/add/operator/concatMap";
 
-// TODO: refactor this once it has a more comprehensive implementation
-// of the graphql api
+// TODO: add types with typescript
 
 // WARNING: This is NOT a spec complete graphql implementation
 // https://facebook.github.io/graphql/October2016/
 
-const translateOperation = {
-  query: "Query",
-  mutation: "Mutation"
-};
-
-function resolveFields(types, definition, context, parent) {
-  return definition.selectionSet.selections.reduce((acc, sel) => {
-    const result = resolveStep(types, sel, context, parent);
-
-    const merger = (acc, emitted) => {
-      const fieldName = (sel.alias || sel.name).value;
-
-      return { ...acc, [fieldName]: emitted };
-    };
-
-    return acc.combineLatest(result, merger);
-  }, Observable.of({}));
-}
-
-function resolveList(values, resolver, types, definition, context) {
-  return values.reduce((acc, result) => {
-    const refinedTypes = resolver.type.resolveType
-      ? resolver.type.resolveType(values).getFields()
-      : types;
-
-    const resultObserver = resolveFields(
-      refinedTypes,
-      definition,
-      context,
-      result
-    );
-
-    const merger = (acc, emitted) => {
-      return acc.concat(emitted);
-    };
-
-    return acc.combineLatest(resultObserver, merger);
-  }, Observable.of([]));
-}
-
-function resolveStep(types, definition, context, parent) {
-  if (definition.kind === "OperationDefinition") {
-    const nextTypeMap = types[
-      translateOperation[definition.operation]
-    ].getFields();
-
-    return resolveFields(nextTypeMap, definition, context, parent);
-  }
-
-  // Node Field
-  if (definition.kind === "Field" && definition.selectionSet !== undefined) {
-    const args = definition.arguments
-      .map(arg => {
-        return arg.value.kind === "Variable"
-          ? { [arg.name.value]: context[arg.value.name.value] }
-          : { [arg.name.value]: arg.value.value };
-      })
-      .reduce(Object.assign, {});
-
-    const resolver = types[definition.name.value];
-    if (!resolver) {
-      return Observable.throw(
-        new Error(
-          `graphqlObservable error: cant find resolver for ${definition.name.value}`
-        )
-      );
-    }
-
-    const resolvedObservable = resolver.resolve(
-      parent,
-      args,
-      context,
-      null // that would be the info
-    );
-
-    if (!resolvedObservable) {
-      return Observable.throw(
-        new Error("graphqlObservable error: empty resolution")
-      );
-    }
-
-    if (!(resolvedObservable instanceof Observable)) {
-      return Observable.throw(
-        new Error("graphqlObservable error: not an observable")
-      );
-    }
-
-    return resolvedObservable.concatMap(emittedResults => {
-      if (!emittedResults) {
-        return Observable.throw(
-          new Error("graphqlObservable error: result not emitted")
-        );
-      }
-
-      if (!emittedResults.map) {
-        const refinedTypes = resolver.type.resolveType
-          ? resolver.type.resolveType(emittedResults).getFields()
-          : types;
-
-        return resolveFields(refinedTypes, definition, context, emittedResults);
-      }
-
-      return resolveList(emittedResults, resolver, types, definition, context);
-    });
-  }
-
-  if (definition.kind === "Field") {
-    return Observable.of(parent[definition.name.value]);
-  }
-
-  return Observable.throw(
-    new Error(
-      `graphqlObservable error: kind ${definition.kind} is not supported`
-    )
-  );
-}
-
 // eslint-disable-next-line import/prefer-default-export
 export function graphqlObservable(doc, schema, context) {
   if (doc.definitions.length !== 1) {
-    return Observable.throw(
-      new Error("graphqlObservable error: unsupported multi root document")
-    );
+    return throwObservable("document root must have a single definition");
   }
 
-  return resolveStep(
+  return resolve(
     schema._typeMap,
     doc.definitions[0],
     context,
     null
   ).map(data => ({ data }));
 }
+
+function throwObservable(error) {
+  const graphqlErrorMessage = `graphqlObservable error: ${error}`;
+  const graphqlError = new Error(graphqlErrorMessage);
+
+  return Observable.throw(graphqlError);
+}
+
+function resolve(types, definition, context, parent) {
+  if (definition.kind === "OperationDefinition") {
+    return resolveOperation(types, definition, context);
+  }
+
+  if (definition.kind === "Field" && definition.selectionSet !== undefined) {
+    return resolveNode(types, definition, context, parent);
+  }
+
+  if (definition.kind === "Field") {
+    return resolveLeaf(types, definition, context, parent);
+  }
+
+  return throwObservable(`kind not supported "${definition.kind}".`);
+}
+
+function resolveOperation(types, definition, context) {
+  const translateOperation = {
+    query: "Query",
+    mutation: "Mutation"
+  };
+
+  const nextTypeMap = types[
+    translateOperation[definition.operation]
+  ].getFields();
+
+  return resolveResult(null, nextTypeMap, definition, context);
+}
+
+function resolveNode(types, definition, context, parent) {
+  const args = buildResolveArgs(definition, context);
+  const resolver = types[definition.name.value];
+
+  if (!resolver) {
+    return throwObservable(`missing resolver for ${definition.name.value}`);
+  }
+
+  const resolvedObservable = resolver.resolve(
+    parent,
+    args,
+    context,
+    null // that would be the info
+  );
+
+  if (!resolvedObservable) {
+    return throwObservable("resolver returns empty value");
+  }
+
+  if (!(resolvedObservable instanceof Observable)) {
+    return throwObservable("resolver does not return an observable");
+  }
+
+  return resolvedObservable.concatMap(emitted => {
+    if (!emitted) {
+      return throwObservable("resolver emitted empty value");
+    }
+
+    if (emitted instanceof Array) {
+      return resolveArrayResults(emitted, types, definition, context, resolver);
+    }
+
+    return resolveResult(emitted, types, definition, context, resolver);
+  });
+}
+
+function resolveLeaf(types, definition, context, parent) {
+  return Observable.of(parent[definition.name.value]);
+}
+
+function resolveResult(parent, types, definition, context, resolver) {
+  return definition.selectionSet.selections.reduce((acc, sel) => {
+    const refinedTypes = refineTypes(resolver, parent, types);
+    const result = resolve(refinedTypes, sel, context, parent);
+    const fieldName = (sel.alias || sel.name).value;
+
+    return acc.combineLatest(result, objectAppendWithKey(fieldName));
+  }, Observable.of({}));
+}
+
+function resolveArrayResults(parents, types, definition, context, resolver) {
+  return parents.reduce((acc, result) => {
+    const resultObserver = resolveResult(
+      result,
+      types,
+      definition,
+      context,
+      resolver
+    );
+
+    return acc.combineLatest(resultObserver, listAppend);
+  }, Observable.of([]));
+}
+
+function buildResolveArgs(definition, context) {
+  return definition.arguments
+    .map(arg => {
+      return arg.value.kind === "Variable"
+        ? { [arg.name.value]: context[arg.value.name.value] }
+        : { [arg.name.value]: arg.value.value };
+    })
+    .reduce(Object.assign, {});
+}
+
+function refineTypes(resolver, parent, types) {
+  return resolver && resolver.type.resolveType
+    ? resolver.type.resolveType(parent).getFields()
+    : types;
+}
+
+const objectAppendWithKey = key => {
+  return (destination, source) => ({ ...destination, [key]: source });
+};
+
+const listAppend = (destination, source) => {
+  return destination.concat(source);
+};
