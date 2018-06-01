9638b2e557f012f7d2375940c4cfaaebf1d613f3
Daniel Schmidt <dschmidt@mesosphere.com>
2018-06-01 12:38:35 +0200

    WIP

---
+++ src/js/config/Config.dev.ts
@@ -0,0 +1,38 @@
+// Configuration overrides
+
+export default {
+  analyticsKey: "39uhSEOoRHMw6cMR6st9tYXDbAL3JSaP",
+  rootUrl: "",
+  historyServer: "",
+  // Override cluster's uiConfiguration for development
+  uiConfigurationFixture: {
+    uiConfiguration: {
+      plugins: {
+        banner: {
+          enabled: false
+        },
+        mesos: {
+          "logging-strategy": "logrotate"
+        },
+        oauth: {
+          enabled: true,
+          authHost: "https://dcos.auth0.com"
+        },
+        "overview-detail": {
+          enabled: true
+        },
+        tracking: {
+          enabled: true
+        }
+      }
+    },
+    clusterConfiguration: {
+      firstUser: true,
+      id: "ui-fixture-cluster-id"
+    }
+  },
+  // Use fixtures to mock API requests
+  useFixtures: false,
+  // Use uiConfigurationFixture defined above
+  useUIConfigFixtures: false
+};
---
+++ src/js/pages/jobs/JobModel.ts
@@ -0,0 +1,169 @@
+import { Observable } from "rxjs";
+import { makeExecutableSchema } from "graphql-tools";
+
+interface ISchedule {
+  concurrencyPolicy: string;
+  cron: string;
+  enabled: boolean;
+  id: string;
+  nextRunAt: string;
+  startingDeadlineSeconds: number;
+  timezone: string;
+}
+
+interface IJobResponse {
+  id: string;
+  labels?: object;
+  run: {
+    cpus: number;
+    mem: number;
+    disk: number;
+    cmd: string;
+    env: object;
+    placement: {
+      constraints: any[];
+    };
+    artifacts: any[];
+  };
+  schedules: ISchedule[];
+  historySummary: {
+    failureCount: number;
+    lastFailureAt: string;
+    lastSuccessAt: string;
+    successCount: number;
+  };
+}
+
+interface IJobsArg {
+  filter?: string;
+  sortBy?: "id" | "status" | "lastRun";
+  sortDirection?: "ASC" | "DESC";
+}
+
+export const typeDefs = `
+  type JobRunSummary {
+    lastFailureAt: String
+    lastSuccessAt: String
+    status: JobRunStatus
+  }
+
+  type Job {
+    id: ID!
+    name: String!
+    status: JobStatus!
+    lastRun: JobRunSummary!
+  }
+
+  enum JobRunStatus {
+    NOT_AVAILABLE
+    SUCCESS
+    FAILED
+  }
+
+  enum JobStatus {
+    RUNNING
+    COMPLETED
+    SCHEDULED
+    UNSCHEDULED
+    FAILED
+    ACTIVE
+    STARTING
+    INITIAL
+  }
+
+  type Namespace {
+    id: ID!
+    items: MetronomeItem
+  }
+
+  union MetronomeItem = Job | Namespace
+
+  type SearchCount {
+    all: Int!
+    filtered: Int!
+  }
+
+  type MetronomeResult {
+    count: SearchCount!
+    items: [MetronomeItem]!
+  }
+
+  enum SortOption {
+    ID
+    STATUS
+    LAST_RUN
+  }
+
+  enum SortDirection {
+    ASC
+    DESC
+  }
+
+  type Query {
+    metronomeItems(filter: String, sortBy: SortOption, sortDirection: SortDirection): MetronomeResult
+    metronomeItem(id: ID!, filter: String, sortBy: SortOption, sortDirection: SortDirection): MetronomeResult
+  }
+`;
+
+function isNamespace(job: IJobResponse): boolean {
+  return job.id.split(".").length > 1;
+}
+
+function sortJobById(a: Job, b: Job): number {}
+
+export const resolvers = ({
+  fetchJobs,
+  pollingInterval
+}: {
+  fetchJobs: () => IJobResponse[];
+  pollingInterval: number;
+}) => ({
+  Query: {
+    metronomeItems(obj = {}, args: IJobsArg = {}, context = {}) {
+      const { sortBy = "id", sortDirection = "ASC" } = args;
+      const pollingInterval$ = Observable.interval(pollingInterval);
+      const responses$ = pollingInterval$.switchMap(fetchJobs);
+
+      // TODO: data mangling
+      // .map(response => ({ ...response, struct: new Job(response) }))
+      // .map(response => ({
+      //   id: response.id,
+      //   lastRun: {
+      //     status: response.struct.getLastRunStatus().status,
+      //     lastSuccessAt: response.historySummary.lastSuccessAt,
+      //     lastFailureAt: response.historySummary.lastFailureAt
+      //   },
+      //   name: response.struct.getName(),
+      //   schedules: response.struct.getSchedules(),
+      //   status: response.struct.getScheduleStatus()
+      // }));
+
+      return responses$.map(jobs =>
+        jobs.sort((a, b) => {
+          const direction = sortDirection === "ASC" ? 1 : -1;
+          const isANamespace = isNamespace(a);
+          const isBNamespace = isNamespace(b);
+
+          if (isANamespace && !isBNamespace) {
+            return -1;
+          }
+
+          if (!isANamespace && isBNamespace) {
+            return 1;
+          }
+
+          const result = a.id.localeCompare(b.id);
+          return result * direction;
+        })
+      );
+    },
+    metronomeItem(obj = {}, args = {}, context = {}) {
+      return Observable.of({});
+    }
+  }
+});
+
+export const defaultSchema = makeExecutableSchema({
+  typeDefs,
+  resolvers: {} // TODO: fill with real interval and API
+});
