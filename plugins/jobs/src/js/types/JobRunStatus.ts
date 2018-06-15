c46cb88459928f922ea1e041b4e9112e4d9bc471
Daniel Schmidt <dschmidt@mesosphere.com>
2018-06-15 17:55:20 +0200

    WIP

--- plugins/jobs/src/js/types/JobRunStatus.ts
+++ plugins/jobs/src/js/types/JobRunStatus.ts
@@ -7,3 +7,27 @@ enum JobRunStatus {
   Success
 }
 `;
+
+export interface JobRunStatusMap {
+  [state: string]: {
+    displayName: string;
+    sortOrder: number;
+  };
+}
+
+const statusMap: JobRunStatusMap = {
+  "N/A": {
+    displayName: "N/A",
+    sortOrder: 1
+  },
+  Success: {
+    displayName: "Success",
+    sortOrder: 2
+  },
+  Failed: {
+    displayName: "Failed",
+    sortOrder: 0
+  }
+};
+
+export default statusMap;
--- plugins/jobs/src/js/types/JobStatus.ts
+++ plugins/jobs/src/js/types/JobStatus.ts
@@ -20,3 +20,61 @@ enum JobStatus {
   UNSCHEDULED
 }
 `;
+
+/**
+ * Sort order is ordered by most important (lowest number, top of list)
+ * to least important (largest number, bottom of list)
+ */
+
+export interface JobStatusMap {
+  [state: string]: {
+    stateTypes: string[];
+    displayName: string;
+    sortOrder: number;
+  };
+}
+
+const states: JobStatusMap = {
+  INITIAL: {
+    stateTypes: ["active"],
+    displayName: "Starting",
+    sortOrder: 3
+  },
+  STARTING: {
+    stateTypes: ["active"],
+    displayName: "Starting",
+    sortOrder: 3
+  },
+  ACTIVE: {
+    stateTypes: ["active"],
+    displayName: "Running",
+    sortOrder: 4
+  },
+  FAILED: {
+    stateTypes: ["completed", "failure"],
+    displayName: "Failed",
+    sortOrder: 0
+  },
+  SUCCESS: {
+    stateTypes: ["success"],
+    displayName: "Success",
+    sortOrder: 6
+  },
+  COMPLETED: {
+    stateTypes: ["success"],
+    displayName: "Completed",
+    sortOrder: 5
+  },
+  SCHEDULED: {
+    stateTypes: [],
+    displayName: "Scheduled",
+    sortOrder: 2
+  },
+  UNSCHEDULED: {
+    stateTypes: [],
+    displayName: "Unscheduled",
+    sortOrder: 1
+  }
+};
+
+export default states;
