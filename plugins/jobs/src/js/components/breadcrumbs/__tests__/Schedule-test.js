2b401c7490c9236bf420b1f4cd818285d65d1fce
Daniel Schmidt <dschmidt@mesosphere.com>
2018-06-25 14:55:42 +0200

    fixup! feat(Breadcrumb): remove jobTree and job struct

---
+++ plugins/jobs/src/js/components/breadcrumbs/__tests__/Schedule-test.js
@@ -0,0 +1,34 @@
+import React from "react";
+import renderer from "react-test-renderer";
+
+import Schedule from "../Schedule";
+
+describe("Schedule", () => {
+  //   {
+  //     name: "with no schedule",
+  //     schedules: [],
+  //     jobTask: null
+  //   },
+  //   {
+  //     name: "with enabled schedules",
+  //     schedules: [
+  //       {
+  //         enabled: true,
+  //         cron: "0 0 20 * *"
+  //       }
+  //     ],
+  //     jobTask: null
+  //   },
+
+  // for (const pathCase of pathCases) {
+  //   it(`has ${pathCase.name}`, () => {
+  //     const item = {
+  //       path: pathCase.path,
+  //       name: "MyJob"
+  //     };
+  //     expect(renderer.create(<Schedule />).toJSON()).toMatchSnapshot();
+  //   });
+  // }
+
+  it("renders the schedule");
+});
---
+++ plugins/jobs/src/js/components/breadcrumbs/__tests__/Status-test.js
@@ -0,0 +1,8 @@
+import React from "react";
+import renderer from "react-test-renderer";
+
+import Status from "../Status";
+
+describe("Status", () => {
+  it("renders the status");
+});
