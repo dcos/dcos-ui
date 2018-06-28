ca0ec0418a232d174b607624d9736b8e9e209076
Daniel Schmidt <dschmidt@mesosphere.com>
2018-06-28 14:23:51 +0200

    refactor(JobConfiguration): move to functional components

--- plugins/jobs/src/js/pages/JobConfiguration.js
+++ plugins/jobs/src/js/pages/JobConfiguration.js
@@ -9,129 +9,105 @@ import ConfigurationMapSection from "#SRC/js/components/ConfigurationMapSection"
 import ConfigurationMapValue from "#SRC/js/components/ConfigurationMapValue";
 import HashMapDisplay from "#SRC/js/components/HashMapDisplay";
 
-export default class JobConfiguration extends React.Component {
-  static getSchedule(job) {
-    const lastSchedule = job.getSchedules()[0];
+const GeneralSection = ({ id, description, cpus, mem, disk, command }) => (
+  <ConfigurationMapSection>
+    <ConfigurationMapHeading>General</ConfigurationMapHeading>
+    <ConfigurationMapRow>
+      <ConfigurationMapLabel>ID</ConfigurationMapLabel>
+      <ConfigurationMapValue>{id}</ConfigurationMapValue>
+    </ConfigurationMapRow>
+    <ConfigurationMapRow>
+      <ConfigurationMapLabel>Description</ConfigurationMapLabel>
+      <ConfigurationMapValue>{description}</ConfigurationMapValue>
+    </ConfigurationMapRow>
+    <ConfigurationMapRow>
+      <ConfigurationMapLabel>CPUs</ConfigurationMapLabel>
+      <ConfigurationMapValue>{cpus}</ConfigurationMapValue>
+    </ConfigurationMapRow>
+    <ConfigurationMapRow>
+      <ConfigurationMapLabel>Memory (MiB)</ConfigurationMapLabel>
+      <ConfigurationMapValue>{mem}</ConfigurationMapValue>
+    </ConfigurationMapRow>
+    <ConfigurationMapRow>
+      <ConfigurationMapLabel>Disk Space (Mib)</ConfigurationMapLabel>
+      <ConfigurationMapValue>{disk}</ConfigurationMapValue>
+    </ConfigurationMapRow>
+    <ConfigurationMapRow>
+      <ConfigurationMapLabel>Command</ConfigurationMapLabel>
+      <ConfigurationMapValue>
+        <pre className="flush transparent wrap">{command}</pre>
+      </ConfigurationMapValue>
+    </ConfigurationMapRow>
+  </ConfigurationMapSection>
+);
 
-    if (!!lastSchedule && !!lastSchedule.cron) {
-      return lastSchedule.cron;
-    }
+const ScheduleSection = ({
+  id,
+  enabled,
+  cron,
+  timezone,
+  startingDeadlineSeconds
+}) => (
+  <ConfigurationMapSection>
+    <ConfigurationMapHeading>Schedule</ConfigurationMapHeading>
+    <ConfigurationMapRow>
+      <ConfigurationMapLabel>ID</ConfigurationMapLabel>
+      <ConfigurationMapValue>{id}</ConfigurationMapValue>
+    </ConfigurationMapRow>
+    <ConfigurationMapRow>
+      <ConfigurationMapLabel>Enabled</ConfigurationMapLabel>
+      <ConfigurationMapValue>{enabled}</ConfigurationMapValue>
+    </ConfigurationMapRow>
+    <ConfigurationMapRow>
+      <ConfigurationMapLabel>CRON Schedule</ConfigurationMapLabel>
+      <ConfigurationMapValue>{cron}</ConfigurationMapValue>
+    </ConfigurationMapRow>
+    <ConfigurationMapRow>
+      <ConfigurationMapLabel>Time Zone</ConfigurationMapLabel>
+      <ConfigurationMapValue>{timezone}</ConfigurationMapValue>
+    </ConfigurationMapRow>
+    <ConfigurationMapRow>
+      <ConfigurationMapLabel>Starting Deadline</ConfigurationMapLabel>
+      <ConfigurationMapValue>{startingDeadlineSeconds}</ConfigurationMapValue>
+    </ConfigurationMapRow>
+  </ConfigurationMapSection>
+);
 
-    return "No schedule available.";
-  }
+const DockerContainerSection = ({ image }) => (
+  <ConfigurationMapSection>
+    <ConfigurationMapHeading>Docker Container</ConfigurationMapHeading>
+    <ConfigurationMapRow>
+      <ConfigurationMapLabel>Image</ConfigurationMapLabel>
+      <ConfigurationMapValue>{image}</ConfigurationMapValue>
+    </ConfigurationMapRow>
+  </ConfigurationMapSection>
+);
 
-  getGeneralSection(job) {
-    return (
-      <ConfigurationMapSection>
-        <ConfigurationMapHeading>General</ConfigurationMapHeading>
-        <ConfigurationMapRow>
-          <ConfigurationMapLabel>ID</ConfigurationMapLabel>
-          <ConfigurationMapValue>{job.id}</ConfigurationMapValue>
-        </ConfigurationMapRow>
-        <ConfigurationMapRow>
-          <ConfigurationMapLabel>Description</ConfigurationMapLabel>
-          <ConfigurationMapValue>{job.description}</ConfigurationMapValue>
-        </ConfigurationMapRow>
-        <ConfigurationMapRow>
-          <ConfigurationMapLabel>CPUs</ConfigurationMapLabel>
-          <ConfigurationMapValue>{job.cpus}</ConfigurationMapValue>
-        </ConfigurationMapRow>
-        <ConfigurationMapRow>
-          <ConfigurationMapLabel>Memory (MiB)</ConfigurationMapLabel>
-          <ConfigurationMapValue>{job.mem}</ConfigurationMapValue>
-        </ConfigurationMapRow>
-        <ConfigurationMapRow>
-          <ConfigurationMapLabel>Disk Space (Mib)</ConfigurationMapLabel>
-          <ConfigurationMapValue>{job.disk}</ConfigurationMapValue>
-        </ConfigurationMapRow>
-        <ConfigurationMapRow>
-          <ConfigurationMapLabel>Command</ConfigurationMapLabel>
-          <ConfigurationMapValue>
-            <pre className="flush transparent wrap">{job.command}</pre>
-          </ConfigurationMapValue>
-        </ConfigurationMapRow>
-      </ConfigurationMapSection>
-    );
-  }
+const LabelsSection = ({ labels }) => (
+  <HashMapDisplay
+    hash={labels.reduce(
+      (memo, { key, value }) => ({ ...memo, [key]: value }),
+      {}
+    )}
+    headline="Labels"
+  />
+);
 
-  getScheduleSection(job) {
-    const [schedule] = job.schedules.nodes;
-    if (schedule == null) {
-      return null;
-    }
+export default function JobConfiguration({ job }) {
+  const [schedule] = job.schedules.nodes;
 
-    return (
-      <ConfigurationMapSection>
-        <ConfigurationMapHeading>Schedule</ConfigurationMapHeading>
-        <ConfigurationMapRow>
-          <ConfigurationMapLabel>ID</ConfigurationMapLabel>
-          <ConfigurationMapValue>{schedule.id}</ConfigurationMapValue>
-        </ConfigurationMapRow>
-        <ConfigurationMapRow>
-          <ConfigurationMapLabel>Enabled</ConfigurationMapLabel>
-          <ConfigurationMapValue>{schedule.enabled}</ConfigurationMapValue>
-        </ConfigurationMapRow>
-        <ConfigurationMapRow>
-          <ConfigurationMapLabel>CRON Schedule</ConfigurationMapLabel>
-          <ConfigurationMapValue>{schedule.cron}</ConfigurationMapValue>
-        </ConfigurationMapRow>
-        <ConfigurationMapRow>
-          <ConfigurationMapLabel>Time Zone</ConfigurationMapLabel>
-          <ConfigurationMapValue>{schedule.timezone}</ConfigurationMapValue>
-        </ConfigurationMapRow>
-        <ConfigurationMapRow>
-          <ConfigurationMapLabel>Starting Deadline</ConfigurationMapLabel>
-          <ConfigurationMapValue>
-            {schedule.startingDeadlineSeconds}
-          </ConfigurationMapValue>
-        </ConfigurationMapRow>
-      </ConfigurationMapSection>
-    );
-  }
-
-  getDockerContainerSection(job) {
-    const docker = job.docker;
-    if (docker == null || !docker.image) {
-      return null;
-    }
-
-    return (
-      <ConfigurationMapSection>
-        <ConfigurationMapHeading>Docker Container</ConfigurationMapHeading>
-        <ConfigurationMapRow>
-          <ConfigurationMapLabel>Image</ConfigurationMapLabel>
-          <ConfigurationMapValue>{docker.image}</ConfigurationMapValue>
-        </ConfigurationMapRow>
-      </ConfigurationMapSection>
-    );
-  }
-
-  getLabelSection(job) {
-    return (
-      <HashMapDisplay
-        hash={job.labels.reduce(
-          (memo, { key, value }) => ({ ...memo, [key]: value }),
-          {}
-        )}
-        headline="Labels"
-      />
-    );
-  }
-
-  render() {
-    const { job } = this.props;
-
-    return (
-      <div className="container">
-        <ConfigurationMap>
-          {this.getGeneralSection(job)}
-          {this.getScheduleSection(job)}
-          {this.getDockerContainerSection(job)}
-          {this.getLabelSection(job)}
-        </ConfigurationMap>
-      </div>
-    );
-  }
+  return (
+    <div className="container">
+      <ConfigurationMap>
+        <GeneralSection {...job} />
+        {schedule ? <ScheduleSection {...schedule} /> : null}
+        {job.docker && job.docker.image ? (
+          <DockerContainerSection {...job.docker} />
+        ) : null}
+        <LabelsSection labels={job.labels} />
+      </ConfigurationMap>
+    </div>
+  );
 }
 
 JobConfiguration.propTypes = {
