b82d4309fdebf83e7d14b729a3cfd929f9dbdf4b
Daniel Schmidt <dschmidt@mesosphere.com>
2018-06-28 14:36:32 +0200

    refactor(JobDetailPage): remove unused code

--- plugins/jobs/src/js/pages/JobDetailPage.js
+++ plugins/jobs/src/js/pages/JobDetailPage.js
@@ -1,5 +1,3 @@
-import classNames from "classnames";
-import prettycron from "prettycron";
 /* eslint-disable no-unused-vars */
 import React from "react";
 /* eslint-enable no-unused-vars */
@@ -7,13 +5,9 @@ import { routerShape } from "react-router";
 import mixin from "reactjs-mixin";
 
 import JobFormModalContainer from "#PLUGINS/jobs/src/js/JobFormModalContainer";
-import TaskStates from "#PLUGINS/services/src/js/constants/TaskStates";
 
-import Icon from "#SRC/js/components/Icon";
 import Page from "#SRC/js/components/Page";
-import TimeAgo from "#SRC/js/components/TimeAgo";
 import TabsMixin from "#SRC/js/mixins/TabsMixin";
-import StringUtil from "#SRC/js/utils/StringUtil";
 import Job from "#SRC/js/structs/Job";
 import JobConfiguration from "./JobConfiguration";
 import { DIALOGS } from "./JobDetailPageContainer";
@@ -39,88 +33,6 @@ class JobDetailPage extends mixin(TabsMixin) {
 
   getDestroyConfirmDialog() {
     return null;
-  }
-
-  getPrettySchedule(job) {
-    const schedules = job.schedules;
-    if (schedules == null || schedules.length === 0) {
-      return null;
-    }
-
-    const schedule = schedules[0];
-    if (!schedule.enabled) {
-      return null;
-    }
-
-    return prettycron.toString(schedule.cron);
-  }
-
-  getSubTitle(job) {
-    const nodes = [];
-    const scheduleText = this.getPrettySchedule(job);
-    let longestRunningTask = null;
-    const longestRunningActiveRun = job
-      .getActiveRuns()
-      .getLongestRunningActiveRun();
-
-    if (longestRunningActiveRun != null) {
-      longestRunningTask = longestRunningActiveRun
-        .getTasks()
-        .getLongestRunningTask();
-    }
-
-    if (longestRunningTask == null && scheduleText == null) {
-      return null;
-    }
-
-    if (scheduleText != null) {
-      nodes.push(
-        <p className="text-overflow flush" key="schedule-text">
-          <Icon
-            className="icon-margin-right"
-            key="schedule-icon"
-            color="grey"
-            id="repeat"
-            size="mini"
-          />
-          <span>Scheduled {StringUtil.lowercase(scheduleText)}</span>
-        </p>
-      );
-    }
-
-    if (longestRunningTask != null) {
-      const dateCompleted = longestRunningTask.getDateCompleted();
-
-      const status = TaskStates[longestRunningTask.getStatus()];
-      const statusClasses = classNames("job-details-header-status", {
-        "text-success":
-          status.stateTypes.includes("success") &&
-          !status.stateTypes.includes("failure"),
-        "text-danger": status.stateTypes.includes("failure")
-      });
-
-      let timePrefix = null;
-      const shouldSuppressRelativeTime = dateCompleted == null;
-
-      if (shouldSuppressRelativeTime) {
-        timePrefix = "for ";
-      }
-
-      nodes.push(
-        <span className={statusClasses} key="status-text">
-          {status.displayName}
-        </span>,
-        <span key="time-running">
-          <TimeAgo
-            prefix={timePrefix}
-            suppressSuffix={shouldSuppressRelativeTime}
-            time={longestRunningActiveRun.getDateCreated()}
-          />
-        </span>
-      );
-    }
-
-    return nodes;
   }
 
   renderConfigurationTabView(job) {
