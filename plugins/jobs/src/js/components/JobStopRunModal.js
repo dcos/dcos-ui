e6192aa3be7ee877609bb3111c857d85d1b64bda
Fabs <fnascimento@mesosphere.com>
2018-07-03 12:18:22 +0200

    refactor: bring stopRun to the mediator pattern

--- plugins/jobs/src/js/components/JobStopRunModal.js
+++ plugins/jobs/src/js/components/JobStopRunModal.js
@@ -1,55 +1,9 @@
 import { Confirm } from "reactjs-components";
-import mixin from "reactjs-mixin";
 import PropTypes from "prop-types";
 import React from "react";
-import { StoreMixin } from "mesosphere-shared-reactjs";
+import ModalHeading from "#SRC/js/components/modals/ModalHeading";
 
-import MetronomeStore from "../../stores/MetronomeStore";
-import ModalHeading from "../modals/ModalHeading";
-
-const METHODS_TO_BIND = ["handleButtonConfirm"];
-
-class JobStopRunModal extends mixin(StoreMixin) {
-  constructor() {
-    super(...arguments);
-
-    this.store_listeners = [
-      {
-        name: "metronome",
-        events: ["jobStopRunSuccess", "jobStopRunError"],
-        suppressUpdate: true
-      }
-    ];
-
-    this.state = {
-      pendingRequest: false
-    };
-
-    METHODS_TO_BIND.forEach(method => {
-      this[method] = this[method].bind(this);
-    });
-  }
-
-  handleButtonConfirm() {
-    const { selectedItems, jobID } = this.props;
-    // TODO DCOS-8763 introduce support for multiple job run IDs
-    if (selectedItems.length === 1) {
-      MetronomeStore.stopJobRun(jobID, selectedItems[0]);
-    }
-
-    this.setState({ pendingRequest: true });
-  }
-
-  onMetronomeStoreJobStopRunSuccess() {
-    this.setState({ pendingRequest: false });
-    this.props.onClose();
-    this.props.onSuccess();
-  }
-
-  onMetronomeStoreJobStopRunError() {
-    this.props.onClose();
-  }
-
+class JobStopRunModal extends React.Component {
   getContentHeader(selectedItems, selectedItemsLength) {
     let headerContent = ` ${selectedItemsLength} Job Runs`;
     if (selectedItemsLength === 1) {
@@ -98,7 +52,7 @@ class JobStopRunModal extends mixin(StoreMixin) {
     return (
       <Confirm
         closeByBackdropClick={true}
-        disabled={this.state.pendingRequest}
+        disabled={this.props.disabled}
         header={this.getContentHeader(selectedItems, selectedItemsLength)}
         open={open}
         onClose={onClose}
@@ -107,7 +61,7 @@ class JobStopRunModal extends mixin(StoreMixin) {
         leftButtonClassName="button button-primary-link"
         rightButtonText={rightButtonText}
         rightButtonClassName="button button-danger"
-        rightButtonCallback={this.handleButtonConfirm}
+        rightButtonCallback={this.props.onSuccess}
         showHeader={true}
       >
         {this.getModalContents()}
@@ -125,6 +79,7 @@ JobStopRunModal.propTypes = {
   onClose: PropTypes.func.isRequired,
   onSuccess: PropTypes.func,
   open: PropTypes.bool.isRequired,
+  disabled: PropTypes.bool.isRequired,
   selectedItems: PropTypes.array.isRequired
 };
 
