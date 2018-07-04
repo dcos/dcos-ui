4e894b465c97cd20ebca62cba976dd06d1bbdc07
Orlando Hohmeier <orlando@mesosphere.com>
2018-07-04 13:10:57 +0200

    fix(MesosStream): stop replay multicasting the stream
    Adjust `MesosStream` to stop multicasting the stream using the replay subject. This change allows the browser to garbage collect dependent/sliced strings created while processing the Mesos response.
    
    Please note that this is a temporary adjustment as it would result in multiple open connections if consumed by more than one component. We're going to revert this change once the `http-service`  to emit event objects instead of the response as well as changed the `mesos-client` to output parsed JSON.
    
    Closes DCOS-38339

--- src/js/core/MesosStream.js
+++ src/js/core/MesosStream.js
@@ -1,11 +1,10 @@
 import { stream } from "@dcos/mesos-client";
-import { ReplaySubject } from "rxjs/ReplaySubject";
 import { Observable } from "rxjs/Observable";
 
 const RECONNECTION_DELAY = 2000;
 
 export const MesosStreamType = Symbol("MesosStreamType");
-export default stream({ type: "SUBSCRIBE" }, "/mesos/api/v1?subscribe")
-  .repeatWhen(() => Observable.timer(RECONNECTION_DELAY))
-  .multicast(() => new ReplaySubject())
-  .refCount();
+export default stream(
+  { type: "SUBSCRIBE" },
+  "/mesos/api/v1?subscribe"
+).repeatWhen(() => Observable.timer(RECONNECTION_DELAY));
