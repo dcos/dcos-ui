c9deab939e9c237cc80de675fb4b0b06b71d369a
Julian Gieseke <jgieseke@mesosphere.com>
2018-07-08 23:48:04 +0200

    chore(types): add NodesList types

---
+++ src/js/structs/NodesList.d.ts
@@ -0,0 +1,10 @@
+import List from "./List";
+import Node from "./Node";
+
+export default class NodesList extends List {
+  constructor(props: any);
+  filter: any;
+  sumUsedResources: any;
+  sumResources: any;
+  getItems(): Array<Node>;
+}
