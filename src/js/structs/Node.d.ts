6c28b0474553ec78ed2e50abaedf39926a8d2870
Julian Gieseke <jgieseke@mesosphere.com>
2018-07-08 23:47:57 +0200

    chore(types): add Node types

---
+++ src/js/structs/Node.d.ts
@@ -0,0 +1,16 @@
+import Item from "./Item";
+
+export default class Node extends Item {
+  getID: () => any;
+  getServiceIDs: () => any;
+  isActive: () => any;
+  getDomain: () => any;
+  getRegionName: () => any;
+  getZoneName: () => any;
+  getUsageStats: (resource: any) => any;
+  getHostName: () => any;
+  getHealth: () => any;
+  getOutput: () => any;
+  sumTaskTypesByState: (state: any) => any;
+  getUsedResources: () => any;
+}
