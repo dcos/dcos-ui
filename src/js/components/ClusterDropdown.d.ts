fd94eb683b3b47e1ba1e3594dbf77a53e1bde387
Daniel Schmidt <dschmidt@mesosphere.com>
2018-07-13 16:59:22 +0200

    feat(HeaderBar): add cluster dropdown
    Closes DCOS-38941

---
+++ src/js/components/ClusterDropdown.d.ts
@@ -0,0 +1,9 @@
+import { ReactNode, Component } from "react";
+
+interface Props {}
+
+interface State {
+  isTextCopied: boolean;
+}
+
+export default class ClusterDropdown extends Component<Props, State> {}
