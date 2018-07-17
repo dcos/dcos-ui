57fbd7851f8450a43951a18f805f1df6d331a560
Georgi Todorov <gtodorov.c@mesosphere.io>
2018-07-17 11:48:08 +0300

    refactor(NodesTable): add two columns for disk
    Add two columns for disk usage - progress bar and percent value.
    
    Closes DCOS-38824

--- plugins/nodes/src/js/columns/NodesTableDiskBarColumn.tsx
+++ plugins/nodes/src/js/columns/NodesTableDiskBarColumn.tsx
@@ -3,12 +3,20 @@ import Node from "#SRC/js/structs/Node";
 // TODO: DCOS-39079
 // import { IWidthArgs as WidthArgs } from "@dcos/ui-kit/packages/table/components/Column";
 import { IWidthArgs as WidthArgs } from "#PLUGINS/nodes/src/js/types/IWidthArgs";
+import ProgressBar from "#SRC/js/components/ProgressBar";
 
 export function diskbarRenderer(data: Node): React.ReactNode {
-  // TODO: DCOS-38824
-  return <span>{data.get("used_resources").disk.toString()}</span>;
+  return (
+    <ProgressBar
+      data={[
+        { value: data.getUsageStats("disk").percentage, className: "color-1" }
+      ]}
+      total={100}
+    />
+  );
 }
+
 export function diskbarSizer(args: WidthArgs): number {
-  // TODO: DCOS-38824
-  return Math.max(100, args.width / args.totalColumns);
+  // TODO: DCOS-39147
+  return Math.min(60, Math.max(60, args.width / args.totalColumns));
 }
--- plugins/nodes/src/js/columns/NodesTableDiskColumn.tsx
+++ plugins/nodes/src/js/columns/NodesTableDiskColumn.tsx
@@ -7,11 +7,10 @@ import { IWidthArgs as WidthArgs } from "#PLUGINS/nodes/src/js/types/IWidthArgs"
 import { SortDirection } from "plugins/nodes/src/js/types/SortDirection";
 
 export function diskRenderer(data: Node): React.ReactNode {
-  // TODO: DCOS-38824
-  return <span>{data.get("used_resources").disk.toString()}</span>;
+  return <span>{data.getUsageStats("disk").percentage}%</span>;
 }
+
 export function diskSorter(data: Node[], sortDirection: SortDirection): Node[] {
-  // TODO: DCOS-38824
   // current implementation is a rough idea, not sure if it is the best one…
   const sortedData = data.sort((a, b) =>
     compareValues(
@@ -24,6 +23,6 @@ export function diskSorter(data: Node[], sortDirection: SortDirection): Node[] {
   return sortDirection === "ASC" ? sortedData : sortedData.reverse();
 }
 export function diskSizer(args: WidthArgs): number {
-  // TODO: DCOS-38824
-  return Math.max(100, args.width / args.totalColumns);
+  // TODO: DCOS-39147
+  return Math.min(60, Math.max(60, args.width / args.totalColumns));
 }
