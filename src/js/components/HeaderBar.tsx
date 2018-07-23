1c6351a4301863f5015afa819b8483199b86a887
Tommy Nguyen <tnguyen@mesosphere.com>
2018-07-23 11:19:43 +0200

    chore(HeaderBar): remove header bar from ui-kit-stage
    Removes the ui-kit-stage header bar component and use
    the dcos-ui component.
    
    Closes DCOS-39074

--- src/js/components/HeaderBar.tsx
+++ src/js/components/HeaderBar.tsx
@@ -1,24 +1,26 @@
 import * as React from "react";
 
 import { Link } from "react-router";
-import { HeaderBar as UIHeaderBar } from "ui-kit-stage/HeaderBar";
+
 import ClusterDropdown from "./ClusterDropdown";
 import AccountDropdown from "./AccountDropdown";
 import SidebarToggle from "./SidebarToggle";
 
-export default function HeaderBar() {
-  return (
-    <UIHeaderBar>
-      <div className="header-bar-left-align-wrapper">
-        <SidebarToggle />
-        <Link to="/dashboard" className="header-bar-logo-wrapper">
-          <span className="header-bar-logo" />
-        </Link>
+export class HeaderBar extends React.Component {
+  render() {
+    return (
+      <div className="header-bar">
+        <div className="header-bar-left-align-wrapper">
+          <SidebarToggle />
+          <Link to="/dashboard" className="header-bar-logo-wrapper">
+            <span className="header-bar-logo" />
+          </Link>
+        </div>
+        <div className="header-bar-right-align-wrapper">
+          <AccountDropdown />
+          <ClusterDropdown />
+        </div>
       </div>
-      <div className="header-bar-right-align-wrapper">
-        <AccountDropdown />
-        <ClusterDropdown />
-      </div>
-    </UIHeaderBar>
-  );
+    );
+  }
 }
