891569e3fb712ff520141494df8f57b3aabfe4e7
Daniel Schmidt <dschmidt@mesosphere.com>
2018-07-26 11:28:49 +0200

    fix(NodesTable): fix sorting of numeric values
    Closes DCOS-39112

--- plugins/nodes/src/js/columns/NodesTableMemColumn.tsx
+++ plugins/nodes/src/js/columns/NodesTableMemColumn.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { compareValues } from "#PLUGINS/nodes/src/js/utils/compareValues";
+import sort from "array-sort";
 import Node from "#SRC/js/structs/Node";
 // TODO: DCOS-39079
 // import { IWidthArgs as WidthArgs } from "@dcos/ui-kit/packages/table/components/Column";
@@ -7,25 +7,33 @@ import { IWidthArgs as WidthArgs } from "#PLUGINS/nodes/src/js/types/IWidthArgs"
 import { SortDirection } from "plugins/nodes/src/js/types/SortDirection";
 import { TextCell } from "@dcos/ui-kit";
 
+function getMemUsage(data: Node): number {
+  return data.getUsageStats("mem").percentage;
+}
+
 export function memRenderer(data: Node): React.ReactNode {
   return (
     <TextCell>
-      <span>{data.getUsageStats("mem").percentage}%</span>
+      <span>{getMemUsage(data)}%</span>
     </TextCell>
   );
 }
 
+function compareNodesByMemUsage(a: Node, b: Node): number {
+  return getMemUsage(a) - getMemUsage(b);
+}
+
+function compareNodesByHostname(a: Node, b: Node): number {
+  return a
+    .getHostName()
+    .toLowerCase()
+    .localeCompare(b.getHostName().toLowerCase());
+}
+
+const comparators = [compareNodesByMemUsage, compareNodesByHostname];
 export function memSorter(data: Node[], sortDirection: SortDirection): Node[] {
-  // current implementation is a rough idea, not sure if it is the best one…
-  const sortedData = data.sort((a, b) =>
-    compareValues(
-      a.get("used_resources").mem.toString(),
-      b.get("used_resources").mem.toString(),
-      a.getHostName().toLowerCase(),
-      b.getHostName().toLowerCase()
-    )
-  );
-  return sortDirection === "ASC" ? sortedData : sortedData.reverse();
+  const reverse = sortDirection !== "ASC";
+  return sort(data, comparators, { reverse });
 }
 
 export function memSizer(args: WidthArgs): number {
--- plugins/nodes/src/js/columns/NodesTableZoneColumn.tsx
+++ plugins/nodes/src/js/columns/NodesTableZoneColumn.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { compareValues } from "#PLUGINS/nodes/src/js/utils/compareValues";
+import sort from "array-sort";
 import Node from "#SRC/js/structs/Node";
 // TODO: DCOS-39079
 // import { IWidthArgs as WidthArgs } from "@dcos/ui-kit/packages/table/components/Column";
@@ -15,16 +15,25 @@ export function zoneRenderer(data: Node): React.ReactNode {
   );
 }
 
+function compareNodesByZone(a: Node, b: Node): number {
+  return a
+    .getZoneName()
+    .toLowerCase()
+    .localeCompare(b.getZoneName().toLowerCase());
+}
+
+function compareNodesByHostname(a: Node, b: Node): number {
+  return a
+    .getHostName()
+    .toLowerCase()
+    .localeCompare(b.getHostName().toLowerCase());
+}
+
+const comparators = [compareNodesByZone, compareNodesByHostname];
+
 export function zoneSorter(data: Node[], sortDirection: SortDirection): Node[] {
-  const sortedData = data.sort((a, b) =>
-    compareValues(
-      a.getZoneName().toLowerCase(),
-      b.getZoneName().toLowerCase(),
-      a.getHostName().toLowerCase(),
-      b.getHostName().toLowerCase()
-    )
-  );
-  return sortDirection === "ASC" ? sortedData : sortedData.reverse();
+  const reverse = sortDirection !== "ASC";
+  return sort(data, comparators, { reverse });
 }
 
 export function zoneSizer(args: WidthArgs): number {
