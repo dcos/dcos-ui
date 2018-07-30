7e65f7b4ef0e3f59fa9ffe08fcde9fa7313cfc0b
Daniel Schmidt <dschmidt@mesosphere.com>
2018-07-30 12:13:23 +0200

    chore(HealthTab): apply prettier to integration tests

--- tests/pages/nodes/node/HealthTab-cy.js
+++ tests/pages/nodes/node/HealthTab-cy.js
@@ -9,8 +9,12 @@ describe("Node Health Tab [0fa]", function() {
   context("Navigate to tab [0fb]", function() {
     it("navigates to health tab [0fc]", function() {
       cy.visitUrl({ url: "/nodes", identify: true, fakeAnalytics: true });
-      cy.get("tr a").eq(0).click({ force: true });
-      cy.get(".menu-tabbed-item").contains("Health").click();
+      cy.get("tr a")
+        .eq(0)
+        .click({ force: true });
+      cy.get(".menu-tabbed-item")
+        .contains("Health")
+        .click();
 
       cy.hash().should("match", /nodes\/[a-zA-Z0-9-]+/);
       cy.get(".page-body-content .h4").should(function($title) {
@@ -22,31 +26,40 @@ describe("Node Health Tab [0fa]", function() {
   context("Health Tab [0fd]", function() {
     beforeEach(function() {
       cy.visitUrl({ url: "/nodes", identify: true, fakeAnalytics: true });
-      cy.get("tr a").eq(0).click({ force: true });
-      cy.get(".menu-tabbed-item").contains("Health").click();
+      cy.get("tr a")
+        .eq(0)
+        .click({ force: true });
+      cy.get(".menu-tabbed-item")
+        .contains("Health")
+        .click();
 
-      cy
-        .get(".page-body-content .form-control input[type='text']")
-        .as("filterTextbox");
-      cy.get("button").contains("Health Checks").as("filterHealth");
+      cy.get(".page-body-content .form-control input[type='text']").as(
+        "filterTextbox"
+      );
+      cy.get("button")
+        .contains("Health Checks")
+        .as("filterHealth");
     });
 
     it("filters by health [0fe]", function() {
-      cy
-        .get(".page-body-content td .text-success")
-        .then(function($healthyRows) {
-          cy.get("@filterHealth").click();
+      cy.get(".page-body-content td .text-success").then(function(
+        $healthyRows
+      ) {
+        cy.get("@filterHealth").click();
 
-          cy.get(".dropdown-menu").find("li").contains("Healthy").click();
-          // Healthy rows should remain
-          cy.get(".page-body-content td .text-success").should(function($row) {
-            expect($row.length).to.equal($healthyRows.length);
-          });
-          // Unhealthy rows should not show
-          cy.get(".page-body-content td .text-danger").should(function($row) {
-            expect($row.length).to.equal(0);
-          });
+        cy.get(".dropdown-menu")
+          .find("li")
+          .contains("Healthy")
+          .click();
+        // Healthy rows should remain
+        cy.get(".page-body-content td .text-success").should(function($row) {
+          expect($row.length).to.equal($healthyRows.length);
         });
+        // Unhealthy rows should not show
+        cy.get(".page-body-content td .text-danger").should(function($row) {
+          expect($row.length).to.equal(0);
+        });
+      });
     });
 
     it("filters by health check name [0ff]", function() {
