70060a0504c855eb5f75f63fc1e7cbf7925b2a36
Fabs <fnascimento@mesosphere.com>
2018-08-07 13:49:13 +0200

    chore: improve code style

--- plugins/nodes/src/js/pages/NodesMasters.js
+++ plugins/nodes/src/js/pages/NodesMasters.js
@@ -4,15 +4,14 @@ import NodeBreadcrumbs from "../components/NodeBreadcrumbs";
 import MesosMasters from "../data/MesosMasters";
 
 const NodesMastersPage = ({ children }) => {
+  const tabs = [
+    { label: "Agents", routePath: "/nodes/agents" },
+    { label: "Masters", routePath: "/nodes/masters" }
+  ];
+
   return (
     <Page>
-      <Page.Header
-        breadcrumbs={<NodeBreadcrumbs />}
-        tabs={[
-          { label: "Agents", routePath: "/nodes/agents" },
-          { label: "Masters", routePath: "/nodes/masters" }
-        ]}
-      />
+      <Page.Header breadcrumbs={<NodeBreadcrumbs />} tabs={tabs} />
       {children}
     </Page>
   );
