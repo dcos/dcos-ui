59122f3ddc0b4a1b241efc647dd41f2c6fa7819c
Georgi Todorov <gtodorov.c@mesosphere.io>
2018-08-08 17:14:04 +0300

    Revert "broken-commit"
    This reverts commit 536011af085f17e3b074213b054bacf48f340ebd.

--- plugins/nodes/src/js/components/__tests__/NodesTable-test.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-unused-vars */
-const React = require("react");
-/* eslint-enable no-unused-vars */
-
-NodesTable = require("../NodesTable");
-SortableColumnHeader = require("../../../../../packages/ui-kit-stage/SortableColumnHeader");
