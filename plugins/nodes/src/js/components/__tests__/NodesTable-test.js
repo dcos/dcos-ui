536011af085f17e3b074213b054bacf48f340ebd
Georgi Todorov <gtodorov.c@mesosphere.io>
2018-08-08 16:25:53 +0300

    broken-commit

---
+++ plugins/nodes/src/js/components/__tests__/NodesTable-test.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-unused-vars */
+const React = require("react");
+/* eslint-enable no-unused-vars */
+
+NodesTable = require("../NodesTable");
+SortableColumnHeader = require("../../../../../packages/ui-kit-stage/SortableColumnHeader");
