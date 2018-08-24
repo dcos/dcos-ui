fb67ed4d7281bc4d97f97fa1c8b1fadbc7b578af
Armand Grillet <armand.grillet@outlook.com>
2018-08-24 18:07:22 +0200

    feat: update DC/OS CLI installation link to latest.
    The new DC/OS CLI is designed to work for any version of DC/OS >= 1.10.
    We can thus safely use the latest release for the current and upcoming
    versions of DC/OS.
    
    Closes DCOS-40900

--- src/js/components/modals/CliInstallModal.js
+++ src/js/components/modals/CliInstallModal.js
@@ -65,14 +65,9 @@ class CliInstallModal extends React.Component {
     }
     const clusterUrl = `${protocol}://${hostname}${port}`;
     const { selectedOS } = this.state;
-    let version = MetadataStore.parsedVersion;
-    // Prepend 'dcos-' to any version other than latest
-    if (version !== "latest") {
-      version = `dcos-${version}`;
-    }
     const downloadUrl = `https://downloads.dcos.io/binaries/cli/${
       osTypes[selectedOS]
-    }/x86-64/${version}/dcos`;
+    }/x86-64/latest/dcos`;
     if (selectedOS === "Windows") {
       return this.getWindowsInstallInstruction(clusterUrl, downloadUrl);
     }
