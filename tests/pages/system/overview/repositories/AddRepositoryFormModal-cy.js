ff1b6ea6b1327a28b210744e2d5a83b2d4973be0
Poltergeist <philipph@mesosphere.io>
2018-08-31 15:31:36 +0200

    fix(packagerepository): remove url validation on add
    This removes the regex validation of the url field and lets rather the backend decide if a url is
    valid or not.
    
    Close DCOS_OSS-526

--- tests/pages/system/overview/repositories/AddRepositoryFormModal-cy.js
+++ tests/pages/system/overview/repositories/AddRepositoryFormModal-cy.js
@@ -1,136 +1,125 @@
-describe("Add Repository Form Modal", function() {
+describe('Add Repository Form Modal', function() {
   beforeEach(function() {
     cy
       .configureCluster({
-        mesos: "1-task-healthy",
+        mesos: '1-task-healthy',
         universePackages: true
       })
-      .visitUrl({ url: "/settings/repositories" })
-      .get(".page-header-actions button")
+      .visitUrl({ url: '/settings/repositories' })
+      .get('.page-header-actions button')
       .click();
   });
 
-  it("displays modal for adding repository", function() {
-    cy.get(".modal h2").should("contain", "Add Repository");
+  it('displays modal for adding repository', function() {
+    cy.get('.modal h2').should('contain', 'Add Repository');
   });
 
-  it("displays three fields", function() {
-    cy.get(".modal input").should("to.have.length", 3);
+  it('displays three fields', function() {
+    cy.get('.modal input').should('to.have.length', 3);
   });
 
   it("should display error if both fields aren't filled out", function() {
     cy
-      .get(".modal .modal-footer .button.button-success")
-      .contains("Add")
+      .get('.modal .modal-footer .button.button-success')
+      .contains('Add')
       .click();
 
     cy
-      .get(".modal .form-control-feedback")
+      .get('.modal .form-control-feedback')
       .eq(0)
-      .should("contain", "Field cannot be empty.");
+      .should('contain', 'Field cannot be empty.');
 
     cy
-      .get(".modal .form-control-feedback")
+      .get('.modal .form-control-feedback')
       .eq(1)
-      .should("contain", "Must be a valid url with http:// or https://");
+      .should('contain', 'Field cannot be empty.');
   });
 
-  it("should display error if not a valid url", function() {
+  it('closes modal after add is successful', function() {
     cy
-      .get(".modal .modal-footer .button.button-success")
-      .contains("Add")
-      .click();
-
-    cy
-      .get(".modal .form-control-feedback")
-      .should("contain", "Must be a valid url with http:// or https://");
-  });
-
-  it("closes modal after add is successful", function() {
-    cy
-      .get(".modal input")
+      .get('.modal input')
       .eq(0)
-      .type("Here we go!")
-      .get(".modal input")
+      .type('Here we go!')
+      .get('.modal input')
       .eq(1)
-      .type("http://there-is-no-stopping.us")
-      .get(".modal input")
+      .type('http://there-is-no-stopping.us')
+      .get('.modal input')
       .eq(2)
-      .type("0")
-      .get(".modal .modal-footer .button.button-success")
-      .contains("Add")
+      .type('0')
+      .get('.modal .modal-footer .button.button-success')
+      .contains('Add')
       .click();
 
-    cy.get(".modal").should("exist");
+    cy.get('.modal').should('exist');
 
     // Clean up
     cy.clusterCleanup(function() {
       cy
-        .get(".page-body-content")
-        .contains("tr", "Here we go!")
-        .find(".button.button-link.button-danger")
-        .invoke("show")
+        .get('.page-body-content')
+        .contains('tr', 'Here we go!')
+        .find('.button.button-link.button-danger')
+        .invoke('show')
         .click({ force: true });
       cy
-        .get(".modal .modal-footer .button.button-danger")
-        .contains("Delete Repository")
+        .get('.modal .modal-footer .button.button-danger')
+        .contains('Delete Repository')
         .click();
     });
   });
 
-  it("displays error in modal after add causes an error", function() {
+  it('displays error in modal after add causes an error', function() {
     // We need to add a fixture for this test to pass.
-    var url = "http://there-is-no-stopping.us";
+    var url = 'http://there-is-no-stopping.us';
     cy
       .route({
-        method: "POST",
+        method: 'POST',
         url: /repository\/add/,
         status: 409,
         response: {
-          message: "Conflict with " + url
+          message: 'Conflict with ' + url
         }
       })
-      .get(".modal input")
+      .get('.modal input')
       .eq(0)
-      .type("Here we go!")
-      .get(".modal input")
+      .type('Here we go!')
+      .get('.modal input')
       .eq(1)
       .type(url)
-      .get(".modal input")
+      .get('.modal input')
       .eq(2)
-      .type("0");
+      .type('0');
     cy
-      .get(".modal .modal-footer .button.button-success")
-      .contains("Add")
+      .get('.modal .modal-footer .button.button-success')
+      .contains('Add')
       .click();
 
-    cy.get(".modal h4.text-danger").should("contain", url);
+    cy.get('.modal h4.text-danger').should('contain', url);
   });
 
   // TODO: Turn into unit test
-  it("displays generic error in modal if no message is provided", function() {
+  it('displays generic error in modal if no message is provided', function() {
     cy
       .route({
-        method: "POST",
+        method: 'POST',
         url: /repository\/add/,
         status: 400,
         response: {}
       })
-      .get(".modal input")
+      .get('.modal input')
       .eq(0)
-      .type("Here we go!");
+      .type('Here we go!');
     cy
-      .get(".modal input")
+      .get('.modal input')
       .eq(1)
-      .type("http://there-is-no-stopping.us")
-      .get(".modal input")
+      .type('http://there-is-no-stopping.us')
+      .get('.modal input')
       .eq(2)
-      .type("0");
+      .type('0');
     cy
-      .get(".modal .modal-footer .button.button-success")
-      .contains("Add")
+      .get('.modal .modal-footer .button.button-success')
+      .contains('Add')
       .click();
 
-    cy.get(".modal h4.text-danger").should("contain", "An error has occurred");
+    cy.get('.modal h4.text-danger').should('contain', 'An error has occurred');
   });
 });
