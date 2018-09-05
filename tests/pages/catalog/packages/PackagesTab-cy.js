4b12a499931f5dbb8891152c98dea5024f8ceb59
natmegs <nataliemegans@gmail.com>
2018-09-04 17:09:42 -0700

    refactor(PackagesTab-cy): fix prettier errors in packages tab integration test

--- tests/pages/catalog/packages/PackagesTab-cy.js
+++ tests/pages/catalog/packages/PackagesTab-cy.js
@@ -7,101 +7,83 @@ describe("Packages Tab", function() {
   });
 
   it("displays correct error message for invalid repo uri", function() {
-    cy
-      .route({
-        method: "POST",
-        url: /package\/search/,
-        status: 400,
-        response: {
-          type: "RepositoryUriSyntax",
-          name: "Invalid",
-          message: "The url for Invalid does not have correct syntax"
-        }
-      })
-      .visitUrl({ url: "/catalog/packages", logIn: true });
+    cy.route({
+      method: "POST",
+      url: /package\/search/,
+      status: 400,
+      response: {
+        type: "RepositoryUriSyntax",
+        name: "Invalid",
+        message: "The url for Invalid does not have correct syntax"
+      }
+    }).visitUrl({ url: "/catalog/packages", logIn: true });
 
-    cy
-      .get(".page-body-content .message")
-      .should(
-        "contain",
-        "The url for Invalid does not have correct syntax. You can go to the Repositories Settings page to change installed repositories."
-      );
+    cy.get(".page-body-content .message").should(
+      "contain",
+      "The url for Invalid does not have correct syntax. You can go to the Repositories Settings page to change installed repositories."
+    );
   });
 
   it("displays correct message for 'no index' error", function() {
-    cy
-      .route({
-        method: "POST",
-        url: /package\/search/,
-        status: 400,
-        response: {
-          type: "IndexNotFound",
-          name: "Invalid",
-          message: "The index file is missing in Invalid"
-        }
-      })
-      .visitUrl({ url: "/catalog", logIn: true });
+    cy.route({
+      method: "POST",
+      url: /package\/search/,
+      status: 400,
+      response: {
+        type: "IndexNotFound",
+        name: "Invalid",
+        message: "The index file is missing in Invalid"
+      }
+    }).visitUrl({ url: "/catalog", logIn: true });
 
-    cy
-      .get(".page-body-content .message")
-      .should(
-        "contain",
-        "The index file is missing in Invalid. You can go to the Repositories Settings page to change installed repositories."
-      );
+    cy.get(".page-body-content .message").should(
+      "contain",
+      "The index file is missing in Invalid. You can go to the Repositories Settings page to change installed repositories."
+    );
   });
 
   it("displays correct message for missing package file", function() {
-    cy
-      .route({
-        method: "POST",
-        url: /package\/search/,
-        status: 400,
-        response: {
-          type: "PackageFileMissing",
-          name: "Invalid",
-          message: "The package file is missing in Invalid"
-        }
-      })
-      .visitUrl({ url: "/catalog", logIn: true });
+    cy.route({
+      method: "POST",
+      url: /package\/search/,
+      status: 400,
+      response: {
+        type: "PackageFileMissing",
+        name: "Invalid",
+        message: "The package file is missing in Invalid"
+      }
+    }).visitUrl({ url: "/catalog", logIn: true });
 
-    cy
-      .get(".page-body-content .message")
-      .should(
-        "contain",
-        "The package file is missing in Invalid. You can go to the Repositories Settings page to change installed repositories."
-      );
+    cy.get(".page-body-content .message").should(
+      "contain",
+      "The package file is missing in Invalid. You can go to the Repositories Settings page to change installed repositories."
+    );
   });
 
   it("uses default repository name if not provided", function() {
-    cy
-      .route({
-        method: "POST",
-        url: /package\/search/,
-        status: 400,
-        response: {
-          type: "PackageFileMissing",
-          message: "The package file is missing in a repository"
-        }
-      })
-      .visitUrl({ url: "/catalog", logIn: true });
+    cy.route({
+      method: "POST",
+      url: /package\/search/,
+      status: 400,
+      response: {
+        type: "PackageFileMissing",
+        message: "The package file is missing in a repository"
+      }
+    }).visitUrl({ url: "/catalog", logIn: true });
 
-    cy
-      .get(".page-body-content .message")
-      .should(
-        "contain",
-        "The package file is missing in a repository. You can go to the Repositories Settings page to change installed repositories."
-      );
+    cy.get(".page-body-content .message").should(
+      "contain",
+      "The package file is missing in a repository. You can go to the Repositories Settings page to change installed repositories."
+    );
   });
 
   it("shows default error message for missing package file", function() {
-    cy
-      .route({
-        method: "POST",
-        url: /package\/search/,
-        status: 400,
-        response: { message: "Some other error" }
-      })
-      .visitUrl({ url: "/catalog", logIn: true });
+    cy.route({
+      method: "POST",
+      url: /package\/search/,
+      status: 400,
+      response: { message: "Some other error" }
+    }).visitUrl({ url: "/catalog", logIn: true });
 
     cy.get(".panel-content h2").should("contain", "An Error Occurred");
   });
@@ -113,9 +95,11 @@ describe("Packages Tab", function() {
     });
 
     it("hides certified panels", function() {
-      cy.get("h1").contains("Certified").should(function($certifiedHeading) {
-        expect($certifiedHeading.length).to.equal(0);
-      });
+      cy.get("h1")
+        .contains("Certified")
+        .should(function($certifiedHeading) {
+          expect($certifiedHeading.length).to.equal(0);
+        });
     });
 
     it("shows only cassandra in panels", function() {
@@ -128,8 +112,7 @@ describe("Packages Tab", function() {
   context("selected packages", function() {
     beforeEach(function() {
       cy.visitUrl({ url: "/catalog", logIn: true });
-      cy
-        .get("h1")
+      cy.get("h1")
         .contains("Certified")
         .closest(".pod")
         .find(".panel")
@@ -149,8 +132,12 @@ describe("Packages Tab", function() {
     });
 
     it("opens the modal when the panel button is clicked", function() {
-      cy.get(".panel").contains("arangodb").click();
-      cy.get(".button.button-primary").contains("Review & Run").click();
+      cy.get(".panel")
+        .contains("arangodb")
+        .click();
+      cy.get(".button.button-primary")
+        .contains("Review & Run")
+        .click();
 
       cy.get(".modal").should(function($modal) {
         expect($modal.length).to.equal(1);
@@ -158,7 +145,9 @@ describe("Packages Tab", function() {
     });
 
     it("doesn't open the modal when the panel is clicked", function() {
-      cy.get(".panel").contains("arangodb").click();
+      cy.get(".panel")
+        .contains("arangodb")
+        .click();
 
       cy.get(".modal").should(function($modal) {
         expect($modal.length).to.equal(0);
@@ -168,19 +157,18 @@ describe("Packages Tab", function() {
 
   context("respect DCOS version", function() {
     beforeEach(function() {
-      cy
-        .route(/dcos-version/, {
-          version: "1.6",
-          "dcos-image-commit": "null",
-          "bootstrap-id": "null"
-        })
-        .visitUrl({ url: "/catalog", logIn: true });
+      cy.route(/dcos-version/, {
+        version: "1.6",
+        "dcos-image-commit": "null",
+        "bootstrap-id": "null"
+      }).visitUrl({ url: "/catalog", logIn: true });
     });
 
     it("cant install package because of DCOS version", function() {
-      cy.get(".panel").contains("arangodb").click();
-      cy
-        .get(".button.button-primary")
+      cy.get(".panel")
+        .contains("arangodb")
+        .click();
+      cy.get(".button.button-primary")
         .contains("Review & Run")
         .should("be.disabled");
     });
