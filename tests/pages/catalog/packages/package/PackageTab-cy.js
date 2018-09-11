a939eb381dd4e97b1d7c668f768a42d23d186c67
natmegs <nataliemegans@gmail.com>
2018-09-10 21:22:07 -0700

    fix(PackageDetailTab): package detail links open in new tab
    Ensure links on package detail page rendered from markdown open in new tab.
    
    Closes DCOS-8601.

--- tests/pages/catalog/packages/package/PackageTab-cy.js
+++ tests/pages/catalog/packages/package/PackageTab-cy.js
@@ -1,11 +1,9 @@
 describe("Package Detail Tab", function() {
   beforeEach(function() {
-    cy
-      .configureCluster({
-        mesos: "1-task-healthy",
-        universePackages: true
-      })
-      .visitUrl({ url: "/catalog/packages/marathon?version=1.4.6" });
+    cy.configureCluster({
+      mesos: "1-task-healthy",
+      universePackages: true
+    }).visitUrl({ url: "/catalog/packages/marathon?version=1.4.6" });
   });
 
   context("Package Details", function() {
@@ -17,8 +15,7 @@ describe("Package Detail Tab", function() {
     it("displays marathon package details", function() {
       cy.get(".page-body-content .pod p").as("information");
 
-      cy
-        .get("@information")
+      cy.get("@information")
         .eq(0)
         .should(
           "contain",
@@ -44,53 +41,58 @@ describe("Package Detail Tab", function() {
         );
     });
 
+    it("contains _blank target for links in description", function() {
+      cy.get("h2")
+        .contains("Description")
+        .parent()
+        .find("a")
+        .should("have.attr", "target", "_blank");
+    });
+
+    it("contains _blank target for links in preinstall notes", function() {
+      cy.get(".pre-install-notes")
+        .find("a")
+        .should("have.attr", "target", "_blank");
+    });
+
     it("displays image in the image viewer", function() {
-      cy
-        .get(".media-object-item-fill-image.image-rounded-corners.clickable")
+      cy.get(".media-object-item-fill-image.image-rounded-corners.clickable")
         .eq(4)
         .click();
 
-      cy
-        .get(".modal.modal-image-viewer img")
-        .should(
-          "have.attr",
-          "src",
-          "http://www.clker.com/cliparts/0/f/d/b/12917289761851255679earth-map-huge.jpg"
-        );
+      cy.get(".modal.modal-image-viewer img").should(
+        "have.attr",
+        "src",
+        "http://www.clker.com/cliparts/0/f/d/b/12917289761851255679earth-map-huge.jpg"
+      );
     });
 
     it("changes image in the image viewer by clicking left arrow", function() {
-      cy
-        .get(".media-object-item-fill-image.image-rounded-corners.clickable")
+      cy.get(".media-object-item-fill-image.image-rounded-corners.clickable")
         .eq(4)
         .click();
 
       cy.get(".modal-image-viewer-arrow-container.clickable.backward").click();
 
-      cy
-        .get(".modal.modal-image-viewer img")
-        .should(
-          "have.attr",
-          "src",
-          "https://mesosphere.com/wp-content/themes/mesosphere/library/images/assets/marathon-0.6.0/mesosphere-marathon-app-list.png"
-        );
+      cy.get(".modal.modal-image-viewer img").should(
+        "have.attr",
+        "src",
+        "https://mesosphere.com/wp-content/themes/mesosphere/library/images/assets/marathon-0.6.0/mesosphere-marathon-app-list.png"
+      );
     });
 
     it("changes image in the image viewer by clicking right arrow", function() {
-      cy
-        .get(".media-object-item-fill-image.image-rounded-corners.clickable")
+      cy.get(".media-object-item-fill-image.image-rounded-corners.clickable")
         .eq(4)
         .click();
 
       cy.get(".modal-image-viewer-arrow-container.clickable.forward").click();
 
-      cy
-        .get(".modal.modal-image-viewer img")
-        .should(
-          "have.attr",
-          "src",
-          "https://mesosphere.com/wp-content/themes/mesosphere/library/images/assets/marathon-0.6.0/mesosphere-marathon-app-list.png"
-        );
+      cy.get(".modal.modal-image-viewer img").should(
+        "have.attr",
+        "src",
+        "https://mesosphere.com/wp-content/themes/mesosphere/library/images/assets/marathon-0.6.0/mesosphere-marathon-app-list.png"
+      );
     });
 
     it("dropdown display all versions available", function() {
@@ -100,7 +102,9 @@ describe("Package Detail Tab", function() {
 
     it("select available version on dropdown", function() {
       cy.get(".dropdown-toggle").click();
-      cy.get(".is-selectable").eq(3).click();
+      cy.get(".is-selectable")
+        .eq(3)
+        .click();
       cy.window().then(function(window) {
         const result = window.location.hash.includes("0.2.1");
         expect(result).to.equal(true);
@@ -110,24 +114,23 @@ describe("Package Detail Tab", function() {
 
   context("Package Configuration", function() {
     beforeEach(function() {
-      cy
-        .get(".page-body-content .package-action-buttons button")
+      cy.get(".page-body-content .package-action-buttons button")
         .contains("Review & Run")
         .click();
 
-      cy.get(".modal .modal-header button").contains("Review & Run").click();
+      cy.get(".modal .modal-header button")
+        .contains("Review & Run")
+        .click();
     });
 
     it("opens framework configuration when Review & Run clicked", function() {
-      cy
-        .get(".modal .configuration-map-label")
+      cy.get(".modal .configuration-map-label")
         .contains("Name")
         .should("to.have.length", 1);
     });
 
     it("shows preinstall notes on review screen", function() {
-      cy
-        .get(".modal .message.message-warning")
+      cy.get(".modal .message.message-warning")
         .contains(
           "We recommend a minimum of one node with at least 2 CPU shares and 1GB of RAM available for the Marathon DCOS Service."
         )
@@ -135,70 +138,71 @@ describe("Package Detail Tab", function() {
     });
 
     it("goes to form when back button is clicked", function() {
-      cy.get(".modal .modal-header button").contains("Back").click();
+      cy.get(".modal .modal-header button")
+        .contains("Back")
+        .click();
 
-      cy
-        .get('.modal .menu-tabbed-container input[name="group"]')
-        .type(`{selectall}group-1`);
+      cy.get('.modal .menu-tabbed-container input[name="group"]').type(
+        `{selectall}group-1`
+      );
     });
 
     it("changes cancel button to back when on review screen", function() {
-      cy.get(".modal button").contains("Edit Config").click();
+      cy.get(".modal button")
+        .contains("Edit Config")
+        .click();
 
-      cy
-        .get('.modal .menu-tabbed-container input[name="group"]')
-        .type(`{selectall}group-1`);
+      cy.get('.modal .menu-tabbed-container input[name="group"]').type(
+        `{selectall}group-1`
+      );
 
-      cy.get(".modal .modal-header button").contains("Review & Run").click();
+      cy.get(".modal .modal-header button")
+        .contains("Review & Run")
+        .click();
 
-      cy
-        .get(".modal .modal-header button")
+      cy.get(".modal .modal-header button")
         .contains("Back")
         .should("to.have.length", 1);
     });
 
     it("shows success dialog on successfully package install", function() {
-      cy
-        .get(".modal .modal-header button.button-primary")
+      cy.get(".modal .modal-header button.button-primary")
         .contains("Run Service")
         .click();
 
-      cy
-        .get(".modal.modal-small .modal-body")
+      cy.get(".modal.modal-small .modal-body")
         .contains("Success")
         .should("to.have.length", 1);
     });
 
     context("Framework: Placement", function() {
       beforeEach(function() {
-        cy.get(".modal button").contains("Edit Config").click();
+        cy.get(".modal button")
+          .contains("Edit Config")
+          .click();
       });
 
       context("Add Placement Constraint", function() {
         beforeEach(function() {
           cy.get(".menu-tabbed-view-container").as("tabView");
-          cy
-            .get(".button-primary-link")
+          cy.get(".button-primary-link")
             .contains("Add Placement Constraint")
             .click();
         });
 
         it('Should add rows when "Add Placement Constraint" link clicked', function() {
           // Field
-          cy
-            .get("@tabView")
+          cy.get("@tabView")
             .find('.form-control[name="constraints.0.fieldName"]')
             .should("exist");
 
           // operator
-          cy
-            .get("@tabView")
+          cy.get("@tabView")
             .find('[name="constraints.0.operator"]')
             .should("exist");
 
           // value
-          cy
-            .get("@tabView")
+          cy.get("@tabView")
             .find('.form-control[name="constraints.0.value"]')
             .should("exist");
         });
@@ -210,32 +214,30 @@ describe("Package Detail Tab", function() {
           });
 
           // Field
-          cy
-            .get("@tabView")
+          cy.get("@tabView")
             .find('.form-control[name="constraints.0.fieldName"]')
             .should("not.exist");
 
           // operator
-          cy
-            .get("@tabView")
+          cy.get("@tabView")
             .find('[name="constraints.0.operator"]')
             .should("not.exist");
 
           // value
-          cy
-            .get("@tabView")
+          cy.get("@tabView")
             .find('.form-control[name="constraints.0.value"]')
             .should("not.exist");
         });
 
         it("Should disable the field value when Unique is selected in operator dropdown", function() {
-          cy.get("@tabView").find(".button.dropdown-toggle").click();
+          cy.get("@tabView")
+            .find(".button.dropdown-toggle")
+            .click();
 
           cy.contains(".dropdown-select-item-title", "Unique").click();
 
           // value
-          cy
-            .get("@tabView")
+          cy.get("@tabView")
             .find('[name="constraints.0.value"]')
             .should("be.disabled");
         });
