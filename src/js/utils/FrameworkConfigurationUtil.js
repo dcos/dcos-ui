e086fbb7b2189027cb683509bdfeddc47b48d38d
Dmitriy Rozhkov <drozhkov@mesosphere.io>
2018-09-14 16:56:04 +0200

    fixup! fix: guard against invalid schema

--- src/js/utils/FrameworkConfigurationUtil.js
+++ src/js/utils/FrameworkConfigurationUtil.js
@@ -2,7 +2,6 @@ import Util from "#SRC/js/utils/Util";
 
 // eslint-disable-next-line import/prefer-default-export
 export function getFirstTabAndField(packageDetails) {
-  window.packageDetails = packageDetails;
   const schema = packageDetails.getConfig() || {};
 
   const [activeTab] = Object.keys(schema.properties || {});
