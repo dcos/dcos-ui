b9d0cc6c6ea61393c35ea79ab2154f64bd0b906f
Dmitriy Rozhkov <drozhkov@mesosphere.io>
2018-09-14 14:53:40 +0200

    fix: guard against invalid schema
    Closes DCOS_OSS-4118

---
+++ src/js/utils/FrameworkConfigurationUtil.js
@@ -0,0 +1,18 @@
+import Util from "#SRC/js/utils/Util";
+
+// eslint-disable-next-line import/prefer-default-export
+export function getFirstTabAndField(packageDetails) {
+  const schema = packageDetails.getConfig() || {};
+
+  const [activeTab] = Object.keys(schema.properties || {});
+  const activeTabProperties = Util.findNestedPropertyInObject(
+    schema,
+    `properties.${activeTab}.properties`
+  ) || {};
+  const [focusField] = Object.keys(activeTabProperties);
+
+  return {
+    activeTab,
+    focusField
+  };
+}
