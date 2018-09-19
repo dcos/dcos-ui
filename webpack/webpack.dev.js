73d93a81e76cf7ba83e8f870a76742e082d062ea
Brandon Croft <brandon.croft@gmail.com>
2018-09-19 15:16:39 -0600

    perf(Webpack): Add optional webpack loader profiling local dev
    Run `npm start` with PROFILE_WEBPACK=1 in order to get some useful
    output about how much time is being spent in each file type loader.

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -3,23 +3,30 @@ const merge = require("webpack-merge");
 
 const packageInfo = require("../package");
 const common = require("./webpack.config.js");
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
 
 const dependencies = Object.assign({}, packageInfo.dependencies);
 delete dependencies["canvas-ui"];
 delete dependencies["cnvs"];
 
-module.exports = merge(common, {
-  entry: {
-    index: "./src/js/index.js",
-    vendor: Object.keys(dependencies)
-  },
-  devtool: "cheap-module-eval-source-map",
-  plugins: [
-    new DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify("development"),
-        LATER_COV: false
-      }
-    })
-  ]
-});
+const maybeProfile = process.env.PROFILE_WEBPACK
+  ? new SpeedMeasurePlugin().wrap
+  : noop => noop;
+
+module.exports = maybeProfile(
+  merge(common, {
+    entry: {
+      index: "./src/js/index.js",
+      vendor: Object.keys(dependencies)
+    },
+    devtool: "cheap-module-eval-source-map",
+    plugins: [
+      new DefinePlugin({
+        "process.env": {
+          NODE_ENV: JSON.stringify("development"),
+          LATER_COV: false
+        }
+      })
+    ]
+  })
+);
