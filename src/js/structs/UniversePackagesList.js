562aa71c272d2d4f946a67cc3e5edfb6828505f7
Brandon Croft <brandon.croft@gmail.com>
2018-09-20 15:36:23 -0600

    feat(UniversePackagesList): When filtering by text, rank certified first
    Now that we are ordering lists by string score when filtering, catalog packages should be re-ranked according to certification.
    
    Closes DCOS-21232

--- src/js/structs/UniversePackagesList.js
+++ src/js/structs/UniversePackagesList.js
@@ -20,6 +20,31 @@ class UniversePackagesList extends List {
     super(options, ...Array.prototype.slice(arguments, 1));
   }
 
+  reRankCertified(items) {
+    // Can't use array sort here since sorting on a boolean value will
+    // reorder nearly everything and we want to preserve the previous ordering
+    let certIndex = 0;
+
+    return items.reduce((acc, item) => {
+      if (item.isCertified()) {
+        acc.splice(certIndex, 0, item);
+        certIndex++;
+      } else {
+        acc.push(item);
+      }
+
+      return acc;
+    }, []);
+  }
+
+  filterItemsByText(filterText) {
+    const items = List.prototype.filterItemsByText
+      .call(this, filterText)
+      .getItems();
+
+    return new this.constructor({ items: this.reRankCertified(items) });
+  }
+
   getSelectedAndNonSelectedPackages() {
     const selectedPackages = [];
     const nonSelectedPackages = [];
