99c2f7f00485ac053f804109399c93195ca7fa7d
Brandon Croft <brandon.croft@gmail.com>
2018-09-20 13:17:55 -0600

    perf(Webpack): Enable babel-loader caching
    Decreases successive rebuild time be avoiding transpilation on unchanged modules.
    
    Caches by default to node_modules/.cache/babel-loader. Disk utilization was 10MB.

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -98,6 +98,7 @@ module.exports = {
           {
             loader: "babel-loader",
             options: {
+              cacheDirectory: true,
               presets: requireAll([
                 "babel-preset-es2015",
                 "babel-preset-stage-3",
