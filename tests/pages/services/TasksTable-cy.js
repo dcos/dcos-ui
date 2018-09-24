0783615658ed7aae902ac52e4d3a51e800c84d97
Daniel Schmidt <dschmidt@mesosphere.com>
2018-09-24 13:51:51 +0200

    style(TasksTable): apply prettier

--- tests/pages/services/TasksTable-cy.js
+++ tests/pages/services/TasksTable-cy.js
@@ -18,8 +18,7 @@ describe("Tasks Table", function() {
         cy.visitUrl({
           url: "/services/detail/%2Fcassandra/tasks/server-0_10a"
         });
-        cy
-          .get(".page-header-navigation .menu-tabbed-item")
+        cy.get(".page-header-navigation .menu-tabbed-item")
           .contains("Files")
           .click();
       });
@@ -31,9 +30,9 @@ describe("Tasks Table", function() {
       });
 
       it("shows directories as well as files", function() {
-        cy
-          .get(".page-body-content  .table-virtual-list")
-          .contains("jre1.7.0_76");
+        cy.get(".page-body-content  .table-virtual-list").contains(
+          "jre1.7.0_76"
+        );
       });
     });
   });
@@ -49,8 +48,7 @@ describe("Tasks Table", function() {
 
     context("Running task without healthcheck", function() {
       beforeEach(function() {
-        cy
-          .get("table tr")
+        cy.get("table tr")
           .contains("broker-0__3c7ab984-a9b9-41fb-bb73-0569f88c657e")
           .closest("tr")
           .find("td")
@@ -58,19 +56,23 @@ describe("Tasks Table", function() {
       });
 
       it("correctly shows status", function() {
-        cy.get("@tds").eq(6).contains("Running");
+        cy.get("@tds")
+          .eq(6)
+          .contains("Running");
       });
 
       it("correctly shows health", function() {
-        cy.get("@tds").eq(7).find(".dot").trigger("mouseover");
+        cy.get("@tds")
+          .eq(7)
+          .find(".dot")
+          .trigger("mouseover");
         cy.get(".tooltip").contains("No health checks available");
       });
     });
 
     context("Running task with healthcheck", function() {
       beforeEach(function() {
-        cy
-          .get("table tr")
+        cy.get("table tr")
           .contains("confluent-kafka.825e1e2e-d6a6-11e6-a564-8605ecf0a9df")
           .closest("tr")
           .find("td")
@@ -78,11 +80,16 @@ describe("Tasks Table", function() {
       });
 
       it("correctly shows status", function() {
-        cy.get("@tds").eq(6).contains("Running");
+        cy.get("@tds")
+          .eq(6)
+          .contains("Running");
       });
 
       it("correctly shows health", function() {
-        cy.get("@tds").eq(7).find(".dot").trigger("mouseover");
+        cy.get("@tds")
+          .eq(7)
+          .find(".dot")
+          .trigger("mouseover");
         cy.get(".tooltip").contains("Healthy");
       });
     });
@@ -94,7 +101,9 @@ describe("Tasks Table", function() {
         mesos: "1-service-with-executor-task"
       });
       cy.visitUrl({ url: "/services/detail/%2Fcassandra/tasks?_k=rh67gf" });
-      cy.get("table tr").find(".form-element-checkbox").as("checkboxes");
+      cy.get("table tr")
+        .find(".form-element-checkbox")
+        .as("checkboxes");
     });
 
     function assertCheckboxLength() {
@@ -102,44 +111,58 @@ describe("Tasks Table", function() {
     }
 
     function assertActionButtons() {
-      cy
-        .get(".filter-bar .button-collection .button-primary-link > span")
+      cy.get(".filter-bar .button-collection .button-primary-link > span")
         .eq(0)
         .contains("Restart");
-      cy
-        .get(".button-collection .button-primary-link > span")
+      cy.get(".button-collection .button-primary-link > span")
         .eq(1)
         .contains("Stop");
     }
 
     it("Select all tasks available and confirm action buttons exist", function() {
       assertCheckboxLength();
-      cy.get("@checkboxes").eq(0).click();
-      cy.get("@checkboxes").eq(0).find("input").should(function($checkbox) {
-        expect($checkbox[0].name).to.equal("headingCheckbox");
-        expect($checkbox[0].checked).to.equal(true);
-      });
-      cy.get("@checkboxes").eq(1).find("input").should(function($checkbox) {
-        expect($checkbox[0].name).to.equal(
-          "cassandra.f3c25eea-da3d-11e5-af84-0242fa37187c"
-        );
-        expect($checkbox[0].checked).to.equal(true);
-      });
+      cy.get("@checkboxes")
+        .eq(0)
+        .click();
+      cy.get("@checkboxes")
+        .eq(0)
+        .find("input")
+        .should(function($checkbox) {
+          expect($checkbox[0].name).to.equal("headingCheckbox");
+          expect($checkbox[0].checked).to.equal(true);
+        });
+      cy.get("@checkboxes")
+        .eq(1)
+        .find("input")
+        .should(function($checkbox) {
+          expect($checkbox[0].name).to.equal(
+            "cassandra.f3c25eea-da3d-11e5-af84-0242fa37187c"
+          );
+          expect($checkbox[0].checked).to.equal(true);
+        });
       assertActionButtons();
     });
 
     it("Select first task available and confirm action buttons exist", function() {
       assertCheckboxLength();
-      cy.get("@checkboxes").eq(1).find("input").should(function($checkbox) {
-        expect($checkbox[0].name).to.equal(
-          "cassandra.f3c25eea-da3d-11e5-af84-0242fa37187c"
-        );
-        expect($checkbox[0].checked).to.equal(false);
-      });
-      cy.get("@checkboxes").eq(1).click();
-      cy.get("@checkboxes").eq(1).find("input").should(function($checkbox) {
-        expect($checkbox[0].checked).to.equal(true);
-      });
+      cy.get("@checkboxes")
+        .eq(1)
+        .find("input")
+        .should(function($checkbox) {
+          expect($checkbox[0].name).to.equal(
+            "cassandra.f3c25eea-da3d-11e5-af84-0242fa37187c"
+          );
+          expect($checkbox[0].checked).to.equal(false);
+        });
+      cy.get("@checkboxes")
+        .eq(1)
+        .click();
+      cy.get("@checkboxes")
+        .eq(1)
+        .find("input")
+        .should(function($checkbox) {
+          expect($checkbox[0].checked).to.equal(true);
+        });
       assertActionButtons();
     });
   });
@@ -194,7 +217,9 @@ describe("Tasks Table", function() {
 
     it("Shows the correct region", function() {
       cy.visitUrl({ url: "/services/detail/%2Fcassandra/tasks?_k=rh67gf" });
-      cy.get("td.task-table-column-zone-address").eq(1).contains("N/A");
+      cy.get("td.task-table-column-zone-address")
+        .eq(1)
+        .contains("N/A");
     });
   });
 
@@ -209,15 +234,15 @@ describe("Tasks Table", function() {
       cy.visitUrl({ url: "/services/detail/%2Fsleep/tasks" });
       cy.get("th.task-table-column-host-address").click();
 
-      cy
-        .get(":nth-child(2) > .task-table-column-host-address")
-        .contains("dcos-01");
-      cy
-        .get(":nth-child(3) > .task-table-column-host-address")
-        .contains("dcos-02");
-      cy
-        .get(":nth-child(4) > .task-table-column-host-address")
-        .contains("dcos-03");
+      cy.get(":nth-child(2) > .task-table-column-host-address").contains(
+        "dcos-01"
+      );
+      cy.get(":nth-child(3) > .task-table-column-host-address").contains(
+        "dcos-02"
+      );
+      cy.get(":nth-child(4) > .task-table-column-host-address").contains(
+        "dcos-03"
+      );
     });
 
     it("sorts ASC by hostname", function() {
@@ -225,15 +250,15 @@ describe("Tasks Table", function() {
       cy.get("th.task-table-column-host-address").click();
       cy.get("th.task-table-column-host-address").click();
 
-      cy
-        .get(":nth-child(2) > .task-table-column-host-address")
-        .contains("dcos-03");
-      cy
-        .get(":nth-child(3) > .task-table-column-host-address")
-        .contains("dcos-02");
-      cy
-        .get(":nth-child(4) > .task-table-column-host-address")
-        .contains("dcos-01");
+      cy.get(":nth-child(2) > .task-table-column-host-address").contains(
+        "dcos-03"
+      );
+      cy.get(":nth-child(3) > .task-table-column-host-address").contains(
+        "dcos-02"
+      );
+      cy.get(":nth-child(4) > .task-table-column-host-address").contains(
+        "dcos-01"
+      );
     });
 
     it("sorts DESC by region", function() {
@@ -241,15 +266,15 @@ describe("Tasks Table", function() {
 
       cy.get("th.task-table-column-region-address").click();
 
-      cy
-        .get(":nth-child(2) > .task-table-column-region-address")
-        .contains("ap-northeast-1");
-      cy
-        .get(":nth-child(3) > .task-table-column-region-address")
-        .contains("eu-central-1");
-      cy
-        .get(":nth-child(4) > .task-table-column-region-address")
-        .contains("eu-central-1");
+      cy.get(":nth-child(2) > .task-table-column-region-address").contains(
+        "ap-northeast-1"
+      );
+      cy.get(":nth-child(3) > .task-table-column-region-address").contains(
+        "eu-central-1"
+      );
+      cy.get(":nth-child(4) > .task-table-column-region-address").contains(
+        "eu-central-1"
+      );
     });
 
     it("sorts ASC by region", function() {
@@ -258,15 +283,15 @@ describe("Tasks Table", function() {
       cy.get("th.task-table-column-region-address").click();
       cy.get("th.task-table-column-region-address").click();
 
-      cy
-        .get(":nth-child(2) > .task-table-column-region-address")
-        .contains("eu-central-1");
-      cy
-        .get(":nth-child(3) > .task-table-column-region-address")
-        .contains("eu-central-1");
-      cy
-        .get(":nth-child(4) > .task-table-column-region-address")
-        .contains("ap-northeast-1");
+      cy.get(":nth-child(2) > .task-table-column-region-address").contains(
+        "eu-central-1"
+      );
+      cy.get(":nth-child(3) > .task-table-column-region-address").contains(
+        "eu-central-1"
+      );
+      cy.get(":nth-child(4) > .task-table-column-region-address").contains(
+        "ap-northeast-1"
+      );
     });
 
     it("sorts DESC by zone", function() {
@@ -274,15 +299,15 @@ describe("Tasks Table", function() {
 
       cy.get("th.task-table-column-zone-address").click();
 
-      cy
-        .get(":nth-child(2) > .task-table-column-zone-address")
-        .contains("ap-northeast-1a");
-      cy
-        .get(":nth-child(3) > .task-table-column-zone-address")
-        .contains("eu-central-1b");
-      cy
-        .get(":nth-child(4) > .task-table-column-zone-address")
-        .contains("eu-central-1c");
+      cy.get(":nth-child(2) > .task-table-column-zone-address").contains(
+        "ap-northeast-1a"
+      );
+      cy.get(":nth-child(3) > .task-table-column-zone-address").contains(
+        "eu-central-1b"
+      );
+      cy.get(":nth-child(4) > .task-table-column-zone-address").contains(
+        "eu-central-1c"
+      );
     });
 
     it("sorts ASC by zone", function() {
@@ -290,15 +315,15 @@ describe("Tasks Table", function() {
 
       cy.get("th.task-table-column-zone-address").click();
       cy.get("th.task-table-column-zone-address").click();
-      cy
-        .get(":nth-child(2) > .task-table-column-zone-address")
-        .contains("eu-central-1c");
-      cy
-        .get(":nth-child(3) > .task-table-column-zone-address")
-        .contains("eu-central-1b");
-      cy
-        .get(":nth-child(4) > .task-table-column-zone-address")
-        .contains("ap-northeast-1a");
+      cy.get(":nth-child(2) > .task-table-column-zone-address").contains(
+        "eu-central-1c"
+      );
+      cy.get(":nth-child(3) > .task-table-column-zone-address").contains(
+        "eu-central-1b"
+      );
+      cy.get(":nth-child(4) > .task-table-column-zone-address").contains(
+        "ap-northeast-1a"
+      );
     });
   });
 });
