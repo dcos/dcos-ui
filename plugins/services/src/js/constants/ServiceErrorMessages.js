2e43075fc6a2fc753422a43c44ca1925f353e4bd
Rodney Norris <rnorris@mesosphere.com>
2018-10-12 16:03:14 -0500

    feat(ServiceErrorMessages): mark messages for translation

--- plugins/services/src/js/constants/ServiceErrorMessages.js
+++ plugins/services/src/js/constants/ServiceErrorMessages.js
@@ -1,43 +1,44 @@
+import { i18nMark } from "@lingui/react";
 import DefaultErrorMessages from "#SRC/js/constants/DefaultErrorMessages";
 
 const ServiceErrorMessages = [
   {
     path: /^id$/,
     type: "PROP_IS_MISSING",
-    message: "Must be defined"
+    message: i18nMark("Must be defined")
   },
   {
     path: /^id$/,
     type: "ALREADY_EXISTS",
-    message: "Already exists"
+    message: i18nMark("Already exists")
   },
   {
     path: /^id$/,
     type: "STRING_PATTERN",
-    message:
-      "May only contain digits (0-9), dashes (-), " +
-      "dots (.),lowercase letters (a-z), and slashes (/) e.g. /group/my-service"
+    message: i18nMark(
+      "May only contain digits (0-9), dashes (-), dots (.),lowercase letters (a-z), and slashes (/) e.g. /group/my-service"
+    )
   },
   {
     path: /.*/,
     type: "SERVICE_DEPLOYING",
-    message:
-      "The service is currently locked by one or more deployments. " +
-      "Press again to force this operation."
+    message: i18nMark(
+      "The service is currently locked by one or more deployments. Press again to force this operation."
+    )
   },
   {
     path: /^container\.docker\.portMappings\.[0-9]+\.name$/,
     type: "STRING_PATTERN",
-    message:
-      "May only contain digits (0-9), dashes (-) and " +
-      "lowercase letters (a-z) e.g. web-server"
+    message: i18nMark(
+      "May only contain digits (0-9), dashes (-) and lowercase letters (a-z) e.g. web-server"
+    )
   },
   {
     path: /^portDefinitions\.[0-9]+\.name$/,
     type: "STRING_PATTERN",
-    message:
-      "May only contain digits (0-9), dashes (-) and " +
-      "lowercase letters (a-z) e.g. web-server"
+    message: i18nMark(
+      "May only contain digits (0-9), dashes (-) and lowercase letters (a-z) e.g. web-server"
+    )
   }
 ].concat(DefaultErrorMessages);
 
