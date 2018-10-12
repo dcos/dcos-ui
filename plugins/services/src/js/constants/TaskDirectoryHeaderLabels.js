98487de7a9338b31fbd22b95151f372f02727e3b
Rodney Norris <rnorris@mesosphere.com>
2018-10-12 14:36:24 -0500

    feat(TaskDirectoryHeaderLabels): mark labels for translation

--- plugins/services/src/js/constants/TaskDirectoryHeaderLabels.js
+++ plugins/services/src/js/constants/TaskDirectoryHeaderLabels.js
@@ -1,9 +1,11 @@
-var TaskDirectoryHeaderLabels = {
-  path: "Name",
-  mode: "Permissions",
-  uid: "Owner",
-  size: "Size",
-  mtime: "Modified"
+import { i18nMark } from "@lingui/react";
+
+const TaskDirectoryHeaderLabels = {
+  path: i18nMark("Name"),
+  mode: i18nMark("Permissions"),
+  uid: i18nMark("Owner"),
+  size: i18nMark("Size"),
+  mtime: i18nMark("Modified")
 };
 
 module.exports = TaskDirectoryHeaderLabels;
