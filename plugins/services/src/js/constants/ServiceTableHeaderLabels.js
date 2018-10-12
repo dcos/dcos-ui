b6142fe7a5cb8bacfa70c409bf3bf6124ec544b5
Rodney Norris <rnorris@mesosphere.com>
2018-10-12 14:34:38 -0500

    feat(ServiceTableHeaderLabels): mark for translation

--- plugins/services/src/js/constants/ServiceTableHeaderLabels.js
+++ plugins/services/src/js/constants/ServiceTableHeaderLabels.js
@@ -1,13 +1,15 @@
-var ServiceTableHeaderLabels = {
-  cpus: "CPU",
-  disk: "Disk",
-  gpus: "GPU",
-  mem: "Mem",
-  name: "Name",
-  status: "Status",
-  version: "Version",
-  instances: "Instances",
-  regions: "Region"
+import { i18nMark } from "@lingui/react";
+
+const ServiceTableHeaderLabels = {
+  cpus: i18nMark("CPU"),
+  disk: i18nMark("Disk"),
+  gpus: i18nMark("GPU"),
+  mem: i18nMark("Mem"),
+  name: i18nMark("Name"),
+  status: i18nMark("Status"),
+  version: i18nMark("Version"),
+  instances: i18nMark("Instances"),
+  regions: i18nMark("Region")
 };
 
 module.exports = ServiceTableHeaderLabels;
