61cf719b9b21776ad36a5fb5dc630021141e79d8
Rodney Norris <rnorris@mesosphere.com>
2018-10-15 13:14:58 -0500

    feat(VolumeProfile): mark strings for translation

--- plugins/services/src/js/constants/VolumeProfile.js
+++ plugins/services/src/js/constants/VolumeProfile.js
@@ -1,5 +1,7 @@
-var VolumeProfile = {
-  UNAVAILABLE: "Not Available"
+import { i18nMark } from "@lingui/react";
+
+const VolumeProfile = {
+  UNAVAILABLE: i18nMark("Not Available")
 };
 
 module.exports = VolumeProfile;
