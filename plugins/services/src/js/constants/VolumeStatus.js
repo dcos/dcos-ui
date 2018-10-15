6e2e57d15c499416bc5eac4dbf1a5c29914e41f7
Rodney Norris <rnorris@mesosphere.com>
2018-10-15 13:16:19 -0500

    feat(VolumeStatus): mark strings for translation

--- plugins/services/src/js/constants/VolumeStatus.js
+++ plugins/services/src/js/constants/VolumeStatus.js
@@ -1,7 +1,9 @@
-var VolumeStatus = {
-  ATTACHED: "Attached",
-  DETACHED: "Detached",
-  UNAVAILABLE: "Unavailable"
+import { i18nMark } from "@lingui/react";
+
+const VolumeStatus = {
+  ATTACHED: i18nMark("Attached"),
+  DETACHED: i18nMark("Detached"),
+  UNAVAILABLE: i18nMark("Unavailable")
 };
 
 module.exports = VolumeStatus;
