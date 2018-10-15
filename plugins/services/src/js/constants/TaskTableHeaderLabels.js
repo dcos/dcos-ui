7cc2fb37e7de0bde2ccaa78bb8b7ffd043b0ec61
Rodney Norris <rnorris@mesosphere.com>
2018-10-15 09:24:14 -0500

    feat(TaskTableHeaderLabels): mark labels for translation

--- plugins/services/src/js/constants/TaskTableHeaderLabels.js
+++ plugins/services/src/js/constants/TaskTableHeaderLabels.js
@@ -1,17 +1,19 @@
-var TaskTableHeaderLabels = {
-  cpus: "CPU",
-  gpus: "GPU",
-  disk: "Disk",
-  host: "Host",
-  zone: "Zone",
-  region: "Region",
-  mem: "Mem",
-  id: "ID",
-  name: "Name",
-  status: "Status",
-  health: "Health",
-  updated: "Updated",
-  version: "Version"
+import { i18nMark } from "@lingui/react";
+
+const TaskTableHeaderLabels = {
+  cpus: i18nMark("CPU"),
+  gpus: i18nMark("GPU"),
+  disk: i18nMark("Disk"),
+  host: i18nMark("Host"),
+  zone: i18nMark("Zone"),
+  region: i18nMark("Region"),
+  mem: i18nMark("Mem"),
+  id: i18nMark("ID"),
+  name: i18nMark("Name"),
+  status: i18nMark("Status"),
+  health: i18nMark("Health"),
+  updated: i18nMark("Updated"),
+  version: i18nMark("Version")
 };
 
 module.exports = TaskTableHeaderLabels;
