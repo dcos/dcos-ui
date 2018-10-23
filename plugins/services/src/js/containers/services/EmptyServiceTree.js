fe23a3f29ef590b1f8cf17ef3064d7b400f60e2b
natmegs <nataliemegans@gmail.com>
2018-10-22 20:43:01 -0700

    feat(EmptyServiceTree): translate component text

--- plugins/services/src/js/containers/services/EmptyServiceTree.js
+++ plugins/services/src/js/containers/services/EmptyServiceTree.js
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import React from "react";
+import { Trans } from "@lingui/macro";
 
 import AlertPanel from "#SRC/js/components/AlertPanel";
 import AlertPanelHeader from "#SRC/js/components/AlertPanelHeader";
@@ -7,21 +8,32 @@ import AlertPanelHeader from "#SRC/js/components/AlertPanelHeader";
 const EmptyServiceTree = function({ onCreateGroup, onCreateService }) {
   const footer = (
     <div className="button-collection flush-bottom">
-      <button className="button button-primary-link" onClick={onCreateGroup}>
+      <Trans
+        render={
+          <button
+            className="button button-primary-link"
+            onClick={onCreateGroup}
+          />
+        }
+      >
         Create a Group
-      </button>
-      <button className="button button-primary" onClick={onCreateService}>
+      </Trans>
+      <Trans
+        render={
+          <button className="button button-primary" onClick={onCreateService} />
+        }
+      >
         Run a Service
-      </button>
+      </Trans>
     </div>
   );
 
   return (
     <AlertPanel>
-      <AlertPanelHeader>No running services</AlertPanelHeader>
-      <p className="tall">
+      <Trans render={<AlertPanelHeader />}>No running services</Trans>
+      <Trans render="p" className="tall">
         Run a new service or create a new group to help organize your services.
-      </p>
+      </Trans>
       {footer}
     </AlertPanel>
   );
