2a2abacca59a7489a1f02f2cdf642843c2315a15
Daniel Schmidt <dschmidt@mesosphere.com>
2018-11-14 12:02:21 +0100

    WIP - add previous pairing implementation

--- packages/data-service/src/graphqlObservable.js
+++ packages/data-service/src/graphqlObservable.js
@@ -55,7 +55,15 @@ function resolveOperation(types, definition, context) {
     translateOperation[definition.operation]
   ].getFields();
 
-  return resolveResult(null, nextTypeMap, definition, context);
+  // TODO: separate current types and all types
+  const resolver = nextTypeMap[definition.name.value];
+
+  return resolveResult(
+    null,
+    definition,
+    context,
+    refineTypes(resolver, null, nextTypeMap)
+  );
 }
 
 function resolveNode(types, definition, context, parent) {
@@ -66,12 +74,7 @@ function resolveNode(types, definition, context, parent) {
     return throwObservable(`missing resolver for ${definition.name.value}`);
   }
 
-  const resolvedObservable = resolver.resolve(
-    parent,
-    args,
-    context,
-    null // that would be the info
-  );
+  const resolvedObservable = resolver.resolve(parent, args);
 
   if (!resolvedObservable) {
     return throwObservable("resolver returns empty value");
@@ -81,16 +84,30 @@ function resolveNode(types, definition, context, parent) {
     return throwObservable("resolver does not return an observable");
   }
 
+  const whatever = getFieldsFromResolvedType(types, resolver);
+  // TODO: this puts type into our main type map that are field resolvers, we should have another list for them
+  const newTypes = { ...types, ...whatever };
+
   return resolvedObservable.concatMap(emitted => {
     if (!emitted) {
       return throwObservable("resolver emitted empty value");
     }
 
+    // TODO: change these types to resemble
+    // See: https://facebook.github.io/graphql/June2018/#ResolveFieldValue()
+    // ResolveFieldValue(objectValue, argumentValues)
+    // objectValue: whatever the endpoint gives you
+    // argumentValues: unclear in the spec, would assume it's the last args given to the system
+    const resolverArgs = [
+      sel => resolve(newTypes, sel, context, emitted),
+      definition
+    ];
+
     if (emitted instanceof Array) {
-      return resolveArrayResults(emitted, types, definition, context, resolver);
+      return resolveArrayResults(...resolverArgs);
     }
 
-    return resolveResult(emitted, types, definition, context, resolver);
+    return resolveResult(...resolverArgs);
   });
 }
 
@@ -98,24 +115,20 @@ function resolveLeaf(types, definition, context, parent) {
   return Observable.of(parent[definition.name.value]);
 }
 
-function resolveResult(parent, types, definition, context, resolver) {
+function resolveResult(resolveThunk, definition) {
   return definition.selectionSet.selections.reduce((acc, sel) => {
-    const refinedTypes = refineTypes(resolver, parent, types);
-    const result = resolve(refinedTypes, sel, context, parent);
     const fieldName = (sel.alias || sel.name).value;
 
-    return acc.combineLatest(result, objectAppendWithKey(fieldName));
+    return acc.combineLatest(resolveThunk(sel), objectAppendWithKey(fieldName));
   }, Observable.of({}));
 }
 
 function resolveArrayResults(parents, types, definition, context, resolver) {
   return parents.reduce((acc, result) => {
     const resultObserver = resolveResult(
-      result,
-      types,
-      definition,
-      context,
-      resolver
+      sel =>
+        resolve(refineTypes(resolver, result, types), sel, context, result),
+      definition
     );
 
     return acc.combineLatest(resultObserver, listAppend);
@@ -130,6 +143,17 @@ function buildResolveArgs(definition, context) {
         : { [arg.name.value]: arg.value.value };
     })
     .reduce(Object.assign, {});
+}
+
+function getFieldsFromResolvedType(types, resolver) {
+  if (!resolver.type.ofType) {
+    return {};
+  }
+  const typeName = resolver.type.ofType.name
+    ? resolver.type.ofType.name
+    : resolver.type.ofType.ofType.name;
+
+  return types[typeName] ? types[typeName]._fields : {};
 }
 
 function refineTypes(resolver, parent, types) {
