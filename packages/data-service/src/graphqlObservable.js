cb2aac5205775f0ad5fe64810f8b115982ea954f
Daniel Schmidt <dschmidt@mesosphere.com>
2018-11-14 14:10:23 +0100

    Revert "WIP - add previous pairing implementation"
    This reverts commit 2a2abacca59a7489a1f02f2cdf642843c2315a15.

--- packages/data-service/src/graphqlObservable.js
+++ packages/data-service/src/graphqlObservable.js
@@ -61,15 +61,7 @@ function resolveOperation(types, definition, context) {
     translateOperation[definition.operation]
   ].getFields();
 
-  // TODO: separate current types and all types
-  const resolver = nextTypeMap[definition.name.value];
-
-  return resolveResult(
-    null,
-    definition,
-    context,
-    refineTypes(resolver, null, nextTypeMap)
-  );
+  return resolveResult(null, nextTypeMap, definition, context);
 }
 
 function resolveNode(types, definition, context, parent) {
@@ -80,7 +72,12 @@ function resolveNode(types, definition, context, parent) {
     return throwObservable(`missing resolver for ${definition.name.value}`);
   }
 
-  const resolvedObservable = resolver.resolve(parent, args);
+  const resolvedObservable = resolver.resolve(
+    parent,
+    args,
+    context,
+    null // that would be the info
+  );
 
   if (!resolvedObservable) {
     return throwObservable("resolver returns empty value");
@@ -90,30 +87,16 @@ function resolveNode(types, definition, context, parent) {
     return throwObservable("resolver does not return an observable");
   }
 
-  const whatever = getFieldsFromResolvedType(types, resolver);
-  // TODO: this puts type into our main type map that are field resolvers, we should have another list for them
-  const newTypes = { ...types, ...whatever };
-
   return resolvedObservable.concatMap(emitted => {
     if (!emitted) {
       return throwObservable("resolver emitted empty value");
     }
 
-    // TODO: change these types to resemble
-    // See: https://facebook.github.io/graphql/June2018/#ResolveFieldValue()
-    // ResolveFieldValue(objectValue, argumentValues)
-    // objectValue: whatever the endpoint gives you
-    // argumentValues: unclear in the spec, would assume it's the last args given to the system
-    const resolverArgs = [
-      sel => resolve(newTypes, sel, context, emitted),
-      definition
-    ];
-
     if (emitted instanceof Array) {
-      return resolveArrayResults(...resolverArgs);
+      return resolveArrayResults(emitted, types, definition, context, resolver);
     }
 
-    return resolveResult(...resolverArgs);
+    return resolveResult(emitted, types, definition, context, resolver);
   });
 }
 
@@ -121,20 +104,24 @@ function resolveLeaf(types, definition, context, parent) {
   return Observable.of(parent[definition.name.value]);
 }
 
-function resolveResult(resolveThunk, definition) {
+function resolveResult(parent, types, definition, context, resolver) {
   return definition.selectionSet.selections.reduce((acc, sel) => {
+    const refinedTypes = refineTypes(resolver, parent, types);
+    const result = resolve(refinedTypes, sel, context, parent);
     const fieldName = (sel.alias || sel.name).value;
 
-    return acc.combineLatest(resolveThunk(sel), objectAppendWithKey(fieldName));
+    return acc.combineLatest(result, objectAppendWithKey(fieldName));
   }, Observable.of({}));
 }
 
 function resolveArrayResults(parents, types, definition, context, resolver) {
   return parents.reduce((acc, result) => {
     const resultObserver = resolveResult(
-      sel =>
-        resolve(refineTypes(resolver, result, types), sel, context, result),
-      definition
+      result,
+      types,
+      definition,
+      context,
+      resolver
     );
 
     return acc.combineLatest(resultObserver, listAppend);
@@ -149,17 +136,6 @@ function buildResolveArgs(definition, context) {
         : { [arg.name.value]: arg.value.value };
     })
     .reduce(Object.assign, {});
-}
-
-function getFieldsFromResolvedType(types, resolver) {
-  if (!resolver.type.ofType) {
-    return {};
-  }
-  const typeName = resolver.type.ofType.name
-    ? resolver.type.ofType.name
-    : resolver.type.ofType.ofType.name;
-
-  return types[typeName] ? types[typeName]._fields : {};
 }
 
 function refineTypes(resolver, parent, types) {
