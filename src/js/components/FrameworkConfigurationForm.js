7d22a399459893b197fbaf21257a4ac0ac46bb8d
Rodney Norris <rnorris@mesosphere.com>
2018-12-07 15:10:25 -0600

    fix(FrameworkConfigurationForm): writeErrors schema
    When attempting to `writeErrors` we check that the `formData` is an object,
    but if so we assume the schema has properties. But if the `formData` is an
    Array then its an object and the schema does not have any properties.
    
    Added a check that the schema has properties before iterating over
    `formData` keys. To resolve this issue.

--- src/js/components/FrameworkConfigurationForm.js
+++ src/js/components/FrameworkConfigurationForm.js
@@ -159,7 +159,7 @@ class FrameworkConfigurationForm extends Component {
     if (schema == null) {
       return;
     }
-    if (Util.isObject(formData)) {
+    if (Util.isObject(formData) && schema.properties) {
       Object.keys(formData).forEach(property => {
         this.writeErrors(
           formData[property],
