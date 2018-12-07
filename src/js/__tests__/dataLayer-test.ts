4cca6602d94d7abca512ee4e35418f4dfeebddcb
Daniel Schmidt <dschmidt@mesosphere.com>
2018-12-07 17:02:39 +0100

    test(data-layer): extensibility of schemas

--- src/js/__tests__/dataLayer-test.ts
+++ src/js/__tests__/dataLayer-test.ts
@@ -7,41 +7,50 @@ import { injectable, ContainerModule } from "inversify";
 import gql from "graphql-tag";
 import { marbles } from "rxjs-marbles/jest";
 
+const JobsSymbol = Symbol("Jobs");
 @injectable()
 class JobsExtension implements DataLayerExtensionInterface {
-  id = Symbol("Jobs");
+  id = JobsSymbol;
 
   getMutationTypeDefinitions() {
-    return ``;
+    return `deleteJob(id: String): Boolean`;
   }
   getQueryTypeDefinitions() {
     return `jobs: Job`;
   }
 
-  getResolvers() {
+  getResolvers(activeExtensions: symbol[]) {
+    const hasJobs = activeExtensions.includes(TasksSymbol);
+
+    const additionalJobData = hasJobs ? { taskId: "foo-task" } : {};
+
     return {
       Query: {
         jobs() {
-          return [{ id: "foo", docker: "nginx" }];
+          return [{ id: "foo", docker: "nginx", ...additionalJobData }];
         }
       }
     };
   }
 
-  getTypeDefinitions() {
+  getTypeDefinitions(activeExtensions: symbol[]) {
+    const hasJobs = activeExtensions.includes(TasksSymbol);
+
     return `
     type Job {
       id: String!
       docker: String!
+      ${hasJobs ? "taskId: String!" : ""}
     }
     `;
   }
 }
 
+const TasksSymbol = Symbol("Tasks");
 // tslint:disable-next-line
 @injectable()
 class TasksExtension implements DataLayerExtensionInterface {
-  id = Symbol("Tasks");
+  id = TasksSymbol;
 
   getMutationTypeDefinitions() {
     return `
@@ -69,6 +78,14 @@ class TasksExtension implements DataLayerExtensionInterface {
         deleteTask(id: string) {
           return id === "deletable-task";
         }
+      },
+      Job: {
+        task(parent: { id: string }) {
+          return {
+            id: parent.id + "task",
+            log: parent.id + "tasklog"
+          };
+        }
       }
     };
   }
@@ -78,6 +95,10 @@ class TasksExtension implements DataLayerExtensionInterface {
     type Task {
       id: String!
       log: String!
+    }
+
+    extend type Job {
+      task: Task
     }
     `;
   }
@@ -143,7 +164,159 @@ describe("DataLayer", () => {
     })
   );
 
-  it("allows to extend the schema based on active extensions");
-  it("provides an extended schema with nested types");
+  it(
+    "provides an extended schema with nested types",
+    marbles(m => {
+      m.bind();
+      const query = gql`
+        query {
+          jobs {
+            id
+            docker
+            task {
+              id
+              log
+            }
+          }
+          task(id: "foo") {
+            id
+            log
+          }
+        }
+      `;
+
+      container
+        .bind(DataLayerExtension)
+        .to(JobsExtension)
+        .inSingletonScope();
+      container
+        .bind(DataLayerExtension)
+        .to(TasksExtension)
+        .inSingletonScope();
+      const dl: DataLayer = container.get<DataLayer>(DataLayerSymbol);
+
+      const expected$ = m.cold("(a|)", {
+        a: {
+          data: {
+            jobs: [
+              {
+                id: "foo",
+                docker: "nginx",
+                task: { id: "footask", log: "footasklog" }
+              }
+            ],
+            task: { id: "foo", log: "foolog" }
+          }
+        }
+      });
+
+      m.expect(dl.query(query, null).take(1)).toBeObservable(expected$);
+    })
+  );
+
+  it(
+    "provides an extended schema with nested types independent of import order",
+    marbles(m => {
+      m.bind();
+      const query = gql`
+        query {
+          jobs {
+            id
+            docker
+            task {
+              id
+              log
+            }
+          }
+          task(id: "foo") {
+            id
+            log
+          }
+        }
+      `;
+
+      container
+        .bind(DataLayerExtension)
+        .to(TasksExtension)
+        .inSingletonScope();
+      container
+        .bind(DataLayerExtension)
+        .to(JobsExtension)
+        .inSingletonScope();
+
+      const dl: DataLayer = container.get<DataLayer>(DataLayerSymbol);
+
+      const expected$ = m.cold("(a|)", {
+        a: {
+          data: {
+            jobs: [
+              {
+                id: "foo",
+                docker: "nginx",
+                task: { id: "footask", log: "footasklog" }
+              }
+            ],
+            task: { id: "foo", log: "foolog" }
+          }
+        }
+      });
+
+      m.expect(dl.query(query, null).take(1)).toBeObservable(expected$);
+    })
+  );
+
+  it.skip(
+    "allows to conditionally extend the schema based on active extensions",
+    marbles(m => {
+      m.bind();
+      const query = gql`
+        query {
+          jobs {
+            id
+            docker
+            taskId
+          }
+          task(id: "foo") {
+            id
+            log
+          }
+        }
+      `;
+
+      container
+        .bind(DataLayerExtension)
+        .to(JobsExtension)
+        .inSingletonScope();
+      const dl: DataLayer = container.get<DataLayer>(DataLayerSymbol);
+      const expectedBeforeBind$ = m.cold(
+        "#",
+        {},
+        new Error(
+          "graphqlObservable error: field was not of the right type. Given type: Job"
+        )
+      );
+      m.expect(dl.query(query, null).take(1)).toBeObservable(
+        expectedBeforeBind$
+      );
+
+      container
+        .bind(DataLayerExtension)
+        .to(TasksExtension)
+        .inSingletonScope();
+
+      const expectedAfterBind$ = m.cold("(a|)", {
+        a: {
+          data: {
+            jobs: [{ id: "foo", docker: "nginx", taskId: "foo-task" }],
+            task: { id: "foo", log: "foolog" }
+          }
+        }
+      });
+
+      m.expect(dl.query(query, null).take(1)).toBeObservable(
+        expectedAfterBind$
+      );
+    })
+  );
   it("extends a schema while a query is running");
 });
