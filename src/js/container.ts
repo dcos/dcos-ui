e46af268213e7f69550933e1d49e4bf97bd68c8e
Daniel Schmidt <dschmidt@mesosphere.com>
2019-01-03 23:33:13 +0100

    feat(data-layer): add data layer extension to container

--- src/js/container.ts
+++ src/js/container.ts
@@ -1,9 +1,14 @@
 import "reflect-metadata"; // Needed for inversify
-import { Container } from "@extension-kid/core";
+import { Container, bindExtensionProvider } from "@extension-kid/core";
 import notificationServiceFactory from "@extension-kid/notification-service";
 import toastsExtensionFactory from "@extension-kid/toast-notifications";
 
 import mesosStream, { MesosStreamType } from "./core/MesosStream";
+import { ContainerModule } from "inversify";
+import DataLayer, {
+  DataLayerExtensionSymbol,
+  DataLayerSymbol
+} from "./dataLayer";
 
 const container = new Container();
 
@@ -17,5 +22,15 @@ if (toastsExtension) {
   // tslint:disable-next-line
   console.error("Could not load toasts extension, please check export");
 }
+
+const dataLayerModule = new ContainerModule(bind => {
+  bindExtensionProvider(bind, DataLayerExtensionSymbol);
+});
+
+container.load(dataLayerModule);
+
+container.bindAsync(DataLayerSymbol, bts => {
+  bts.to(DataLayer).inSingletonScope();
+});
 
 export default container;
