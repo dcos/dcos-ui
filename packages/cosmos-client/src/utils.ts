e40d096946cf6e62abe752b322d7b77d9674da2f
Rodney Norris <rnorris@mesosphere.com>
2019-02-06 08:41:10 -0600

    refactor(cosmos-client): pr feedback & tweaks
    - removed the api prefix as a parameter to the cosmos client so we can
    support other prefixes for cosmos in the future
    - moved error handling to the cosmos client to better possible error
    messages from the response. It seemed right to have the logic in the
    client on further review.

--- packages/cosmos-client/src/utils.ts
+++ packages/cosmos-client/src/utils.ts
@@ -1,3 +1,5 @@
+import { RequestResponse } from "@dcos/http-service";
+
 export function buildRequestHeader(
   action: string,
   actionType: string,
@@ -5,4 +7,23 @@ export function buildRequestHeader(
   version: string
 ) {
   return `application/vnd.dcos.${entity}.${action}-${actionType}+json;charset=utf-8;version=${version}`;
+}
+
+export function getErrorMessage(reqResp: RequestResponse<any>): string {
+  const { response, message } = reqResp;
+  if (typeof response === "string") {
+    return response;
+  } else if (typeof response === "object") {
+    if (response.description) {
+      return response.description;
+    }
+    if (response.message) {
+      return response.message;
+    }
+  }
+  if (message) {
+    return message;
+  }
+
+  return "An error has occurred.";
 }
