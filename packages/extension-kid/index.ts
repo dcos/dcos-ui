6d2bf5de24429d79b9f551552db051f405e5529b
Dmitriy Rozhkov <drozhkov@mesosphere.io>
2019-02-07 19:48:22 +0100

    refactor: use extension-kid from npm

--- packages/extension-kid/index.ts
+++
@@ -1,7 +0,0 @@
-import {
-  bindExtensionProvider,
-  ExtensionProvider
-} from "./src/ExtensionProvider";
-import Container from "./src/Container";
-
-export { Container, ExtensionProvider, bindExtensionProvider };
