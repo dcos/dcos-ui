34ac2b8e4eccdc030bf8afa372c1dde3c820fd93
Pierre Beitz <pierrebeitz@gmail.com>
2019-02-10 11:43:27 +0100

    refactor(cosmos-data-layer): let CosmosClient know about its rootUrl
    It's more a matter of taste. Feel free to ignore this commit completely!
    
    I just played around with the code and thought that static stuff would be
    sufficient.
    
    As `rootUrl` seems to be set in Config per environment everything should work
    the same as before.

--- packages/cosmos-client/src/CosmosClient.ts
+++ packages/cosmos-client/src/CosmosClient.ts
@@ -1,4 +1,4 @@
-import { Observable } from "rxjs";
+import Config from "#SRC/js/config/Config";
 import { map } from "rxjs/operators";
 import { request, RequestResponse } from "@dcos/http-service";
 
@@ -6,43 +6,38 @@ import { buildRequestHeader, getErrorMessage } from "./utils";
 
 import { PackageVersionsResponse } from "../";
 
-export class CosmosClient {
-  readonly rootUrl: string;
+export const CosmosClient = {
+  listPackageVersions: (packageName: string) =>
+    request<PackageVersionsResponse>(
+      `${Config.rootUrl || "/"}package/list-versions`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          includePackageVersions: true,
+          packageName
+        }),
+        headers: {
+          Accept: buildRequestHeader(
+            "list-versions",
+            "response",
+            "package",
+            "v1"
+          ),
 
-  constructor(rootUrl: string) {
-    this.rootUrl = rootUrl;
-  }
-
-  listPackageVersions(
-    packageName: string
-  ): Observable<RequestResponse<PackageVersionsResponse>> {
-    return request(`${this.rootUrl || "/"}package/list-versions`, {
-      method: "POST",
-      body: JSON.stringify({
-        includePackageVersions: true,
-        packageName
-      }),
-      headers: {
-        Accept: buildRequestHeader(
-          "list-versions",
-          "response",
-          "package",
-          "v1"
-        ),
-        "Content-type": buildRequestHeader(
-          "list-versions",
-          "request",
-          "package",
-          "v1"
-        )
+          "Content-type": buildRequestHeader(
+            "list-versions",
+            "request",
+            "package",
+            "v1"
+          )
+        }
       }
-    }).pipe(
+    ).pipe(
       map((reqResp: RequestResponse<PackageVersionsResponse>) => {
         if (reqResp.code < 300) {
           return reqResp;
         }
         throw new Error(getErrorMessage(reqResp));
       })
-    );
-  }
-}
+    )
+};
