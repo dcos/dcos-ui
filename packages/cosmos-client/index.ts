50bcd34fb5a42083a1c8898fce7620caa274748f
Pierre Beitz <pierrebeitz@gmail.com>
2019-02-12 16:01:50 +0100

    refactor(cosmos-client): internalize ListVersionsResult

--- packages/cosmos-client/index.ts
+++ packages/cosmos-client/index.ts
@@ -1,9 +1,5 @@
 export { CosmosClient } from "./src/CosmosClient";
 
-export interface ListVersionsResults {
-  [key: string]: string;
-}
-
 export interface PackageVersionsResponse {
-  results: ListVersionsResults;
+  results: Record<string, string>;
 }
