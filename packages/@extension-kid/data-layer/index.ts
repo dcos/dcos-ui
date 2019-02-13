2ed4d55c58117ad1be144e7ad5aaada7e81ce331
Daniel Schmidt <dschmidt@mesosphere.com>
2019-02-13 11:20:28 +0100

    refactor(DataLayer): expose convenient way to build an extension module

--- packages/@extension-kid/data-layer/index.ts
+++ packages/@extension-kid/data-layer/index.ts
@@ -8,11 +8,20 @@ import DataLayer, {
 } from "./dataLayer";
 
 export {
+  getExtensionModule,
   DataLayerExtensionType,
   DataLayerType,
   DataLayerExtensionInterface,
   DataLayer
 };
+
+function getExtensionModule<T>(extension: { new (...args: any[]): T }) {
+  return new ContainerModule(bind => {
+    bind(DataLayerExtensionType)
+      .to(extension)
+      .inSingletonScope();
+  });
+}
 
 export default (_context = {}) =>
   new ContainerModule(bind => {
