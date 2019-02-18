0e7623a1fbe5b697719ca4798e8ad6ed36e3b29e
Poltergeist <philipph@mesosphere.io>
2019-02-18 23:26:37 +0100

    feat(publicips): add node table wrapper component
    Change data flow to merge data with network information just above the table.

--- plugins/nodes/src/js/columns/NodesTablePublicIPColumn.tsx
+++ plugins/nodes/src/js/columns/NodesTablePublicIPColumn.tsx
@@ -1,61 +1,27 @@
-import * as React from "react";
 import { TextCell } from "@dcos/ui-kit";
+import * as React from "react";
+import { Tooltip } from "reactjs-components";
+import { Trans } from "@lingui/macro";
 
-import { default as schema } from "../data/NodesResolver";
-import { NodeNetwork } from "../data/NodesNetworkClient";
-import graphqlObservable from "reactive-graphql";
-import gql from "graphql-tag";
-import { map, switchMap, startWith } from "rxjs/operators";
-import { componentFromStream } from "@dcos/data-service";
-import { Observable } from "rxjs";
-import { Trans } from "@lingui/react";
+import Node from "src/js/structs/Node";
 
-const getGraphQl = (privateIP: string) =>
-  graphqlObservable<{ node: { network: NodeNetwork } }>(
-    gql`
-      query {
-        node(privateIP: $privateIP) {
-          network {
-            public_ips
-          }
-        }
-      }
-    `,
-    schema,
-    { privateIP }
-  ).pipe(map(response => response.data));
-
-const Column = componentFromStream<{ privateIP: string }>(
-  (props$): Observable<React.ReactNode> => {
-    return (props$ as Observable<{ privateIP: string }>).pipe(
-      switchMap(({ privateIP }) => getGraphQl(privateIP)),
-      map(({ node }: { node: { network: { public_ips: string[] } } }) => {
-        if (
-          node.network == null ||
-          node.network.public_ips == null ||
-          node.network.public_ips.length === 0
-        ) {
-          return (
-            <TextCell>
-              <Trans>N/A</Trans>
-            </TextCell>
-          );
-        }
-        return <TextCell>{node.network.public_ips.join(", ")}</TextCell>;
-      }),
-      startWith(
-        <TextCell>
-          <Trans>Loading</Trans>
-        </TextCell>
-      )
+const NodesTablePublicIpColumn = (item: Node) => {
+  const publicIps = item.getPublicIps();
+  if (publicIps.length === 0) {
+    return (
+      <TextCell>
+        <Trans>N/A</Trans>
+      </TextCell>
     );
   }
-);
+  if (publicIps.length === 1) {
+    return <TextCell>{publicIps[0]}</TextCell>;
+  }
+  return (
+    <TextCell>
+      <Tooltip content={publicIps.join(", ")}>{publicIps[0]}</Tooltip>
+    </TextCell>
+  );
+};
 
-export function publicIPCellWidth(): number {
-  return 125;
-}
-
-export default function({ hostname }: { hostname: string }) {
-  return <Column privateIP={hostname} />;
-}
+export { NodesTablePublicIpColumn as default };
