bcc1ccb9709c45d1af7a4aa7563bb47b962f1539
natmegs <nataliemegans@gmail.com>
2019-02-19 18:17:58 -0800

    fix(Job Form): separate form and output data
    Change data flow in the jobs form and modal so that output job data and form data are separately updated. Change update functions so that they return new state instead of mutating existing state. Remove JobFormHistory (redundant).

---
+++ plugins/jobs/src/js/components/form/reducers/JobReducers.ts
@@ -0,0 +1,122 @@
+import { deepCopy } from "#SRC/js/utils/Util";
+
+import {
+  JobSpec,
+  Action,
+  JobOutput,
+  JobFormActionType
+} from "../helpers/JobFormData";
+
+const jsonReducers = {
+  [JobFormActionType.Override]: (value: JobOutput, state: JobSpec): JobSpec => {
+    const stateCopy = deepCopy(state);
+    const valueCopy = deepCopy(value);
+    const cmdOnly = !(valueCopy.job.run.docker || valueCopy.job.run.ucr);
+    const container = valueCopy.job.run.docker ? "docker" : "ucr";
+    const newState = {
+      ...stateCopy,
+      ...valueCopy,
+      cmdOnly,
+      container
+    };
+    if (!newState.job.run.docker) {
+      newState.job.run.docker = stateCopy.job.run.docker;
+      if (newState.job.run.ucr) {
+        newState.job.run.docker.image = newState.job.run.ucr.image.id;
+      }
+    }
+    if (!newState.job.run.ucr) {
+      newState.job.run.ucr = stateCopy.job.run.ucr;
+      if (newState.job.run.docker) {
+        newState.job.run.ucr.image.id = newState.job.run.docker.image;
+      }
+    }
+    return newState;
+  }
+};
+
+const containerImageReducers = {
+  [JobFormActionType.Set]: (value: string, state: JobSpec) => {
+    const stateCopy = deepCopy(state);
+    if (stateCopy.job.run.ucr) {
+      const prevUcrImage = { ...stateCopy.job.run.ucr.image };
+      const newUcrImage = {
+        id: value
+      };
+      stateCopy.job.run.ucr.image = {
+        ...prevUcrImage,
+        ...newUcrImage
+      };
+    }
+    if (stateCopy.job.run.docker) {
+      stateCopy.job.run.docker.image = value;
+    }
+    return stateCopy;
+  }
+};
+
+const cmdOnlyReducers = {
+  [JobFormActionType.Set]: (value: "true" | "false", state: JobSpec) => {
+    const stateCopy = deepCopy(state);
+    if (value === "true") {
+      stateCopy.cmdOnly = true;
+    }
+    if (value === "false") {
+      stateCopy.cmdOnly = false;
+    }
+    return stateCopy;
+  }
+};
+
+const defaultReducer = {
+  [JobFormActionType.Set]: (value: any, state: JobSpec, path: string[]) => {
+    const stateCopy = deepCopy(state);
+    const assignProp = path.pop();
+    if (assignProp) {
+      path.reduce((acc: any, current: string) => {
+        return acc[current];
+      }, stateCopy)[assignProp] = value;
+    }
+
+    return stateCopy;
+  }
+};
+
+const combinedReducers = {
+  json: jsonReducers,
+  containerImage: containerImageReducers,
+  cmdOnly: cmdOnlyReducers
+};
+
+const isFunction = (func: any): boolean => {
+  return typeof func === "function";
+};
+
+export function jobFormOutputToSpecReducer(
+  action: Action,
+  state: JobSpec
+): JobSpec {
+  if (!action.path) {
+    return state;
+  }
+  const arrayPath = action.path.split(".");
+  const propToChange = arrayPath[arrayPath.length - 1];
+  const reducerSet = combinedReducers[propToChange];
+  const defaultSet = defaultReducer;
+  let updateFunction;
+
+  if (reducerSet) {
+    updateFunction = reducerSet[action.type];
+    return (
+      updateFunction &&
+      isFunction(updateFunction) &&
+      updateFunction(action.value, state)
+    );
+  }
+  updateFunction = defaultSet[action.type];
+  if (updateFunction && isFunction(updateFunction)) {
+    return updateFunction(action.value, state, arrayPath);
+  }
+
+  return state;
+}
