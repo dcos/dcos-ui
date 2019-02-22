9bf59257e9e67594cfeb9b07cbc9fcde158fa137
Pierre Beitz <pierrebeitz@gmail.com>
2019-02-22 23:58:49 +0100

    fix(types): nodes grid
    introducing a service-type just for the sake of reducing the number of type
    errors. this definition is neither supposed to be complete nor guaranteed to be
    correct - it's just a step to re-enable project wide type checking again - so
    feel free to change it as you like!

--- plugins/nodes/src/js/components/NodesGridView.tsx
+++ plugins/nodes/src/js/components/NodesGridView.tsx
@@ -10,6 +10,14 @@ import NodesGridDials from "./NodesGridDials";
 const MAX_SERVICES_TO_SHOW = 32;
 const OTHER_SERVICES_COLOR = 32;
 
+// This interface has just been crafted to reenable type checking.
+// It may very well be that it needs extension.
+interface Service {
+  id: string;
+  name: string;
+  getId(): string;
+}
+
 interface NodesGridViewProps {
   hasLoadingError?: boolean;
   hiddenServices?: string[];
@@ -19,7 +27,7 @@ interface NodesGridViewProps {
   resourcesByFramework: ResourcesByFramework;
   selectedResource: string;
   serviceColors: ServiceColors;
-  services: object[]; // TODO TS: Framework[] `plugins/services/src/js/structs/Framework.js`
+  services: Service[]; // TODO TS: Framework[] `plugins/services/src/js/structs/Framework.js`
 }
 
 export default class NodesGridView extends React.PureComponent<
@@ -110,7 +118,7 @@ export default class NodesGridView extends React.PureComponent<
         <div className={classSet}>{this.getServicesList()}</div>
 
         <NodesGridDials
-          hosts={hosts.getItems()}
+          hosts={(hosts as any).getItems()}
           resourcesByFramework={resourcesByFramework}
           selectedResource={selectedResource}
           serviceColors={serviceColors}
