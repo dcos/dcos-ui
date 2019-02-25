236c87d3cc3c3ec8dcf32de90605571f888206a1
natmegs <nataliemegans@gmail.com>
2019-02-25 13:36:07 -0800

    fix(test-jobs): fix jobs system tests
    Fix jobs system tests.

--- system-tests/jobs/test-jobs.js
+++ system-tests/jobs/test-jobs.js
@@ -20,10 +20,14 @@ describe("Jobs", function() {
     cy.contains("Create a Job").click();
 
     // Wait for the 'New Job' dialog to appear
-    cy.get(".modal-header").contains("New Job").should("exist");
+    cy.get(".modal-header")
+      .contains("New Job")
+      .should("exist");
 
     // Fill-in the input elements
-    cy.root().getFormGroupInputFor("ID *").type(`{selectall}${fullJobName}`);
+    cy.root()
+      .getFormGroupInputFor("Job ID *")
+      .type(`{selectall}${fullJobName}`);
     //
     // TODO: Due to a bug in cypress you cannot type values with dots
     // cy
@@ -31,44 +35,49 @@ describe("Jobs", function() {
     //   .getFormGroupInputFor('CPUs')
     //   .type('{selectall}0.1');
     //
-    cy.root().getFormGroupInputFor("Mem (MiB)").type("{selectall}32");
-    cy.root().getFormGroupInputFor("Command").type(cmdline);
+    cy.root()
+      .getFormGroupInputFor("Mem (MiB) *")
+      .type("{selectall}32");
+    cy.root()
+      .getFormGroupInputFor("Command *")
+      .type(cmdline);
 
     // Click create job
-    cy.contains("Create Job").click();
+    cy.contains("Submit").click();
 
     // Switch to the group that will contain the service
     cy.visitUrl(`jobs/overview/${Cypress.env("TEST_UUID")}`);
 
     // Wait for the table and the service to appear
-    cy
-      .get(".page-body-content table")
+    cy.get(".page-body-content table")
       .contains(jobName, { timeout: Timeouts.JOB_DEPLOYMENT_TIMEOUT })
       .get("a.table-cell-link-primary")
       .contains(`${jobName}`)
       .click();
 
     // open edit screen
-    cy
-      .get(".page-header-actions .dropdown")
+    cy.get(".page-header-actions .dropdown")
       .click()
       .get(".dropdown-menu-items")
       .contains("Edit")
       .click();
 
     // Check the input elements
-    cy
-      .root()
-      .getFormGroupInputFor("ID *")
+    cy.root()
+      .getFormGroupInputFor("Job ID *")
       .should("have.value", `${fullJobName}`);
 
-    cy.root().getFormGroupInputFor("Mem (MiB)").should("have.value", "32");
+    cy.root()
+      .getFormGroupInputFor("Mem (MiB) *")
+      .should("have.value", "32");
 
-    cy.root().getFormGroupInputFor("Command").contains(`${cmdline}`);
+    cy.root()
+      .getFormGroupInputFor("Command *")
+      .contains(`${cmdline}`);
   });
 
-  it("creates a job with docker config", function() {
-    const jobName = "job-with-docker-config";
+  it("creates a job with default ucr config", function() {
+    const jobName = "job-with-ucr-config";
     const fullJobName = `${Cypress.env("TEST_UUID")}.${jobName}`;
     const cmdline = "while true; do echo 'test' ; sleep 100 ; done";
 
@@ -80,10 +89,14 @@ describe("Jobs", function() {
     cy.get(".button.button-primary-link.button-narrow").click();
 
     // Wait for the 'New Job' dialog to appear
-    cy.get(".modal-header").contains("New Job").should("exist");
+    cy.get(".modal-header")
+      .contains("New Job")
+      .should("exist");
 
     // Fill-in the input elements
-    cy.root().getFormGroupInputFor("ID *").type(`{selectall}${fullJobName}`);
+    cy.root()
+      .getFormGroupInputFor("Job ID *")
+      .type(`{selectall}${fullJobName}`);
     //
     // TODO: Due to a bug in cypress you cannot type values with dots
     // cy
@@ -91,45 +104,47 @@ describe("Jobs", function() {
     //   .getFormGroupInputFor('CPUs')
     //   .type('{selectall}0.5');
     //
-    cy.root().getFormGroupInputFor("Mem (MiB)").type("{selectall}32");
-    cy.root().getFormGroupInputFor("Command").type(cmdline);
+    cy.root()
+      .getFormGroupInputFor("Mem (MiB) *")
+      .type("{selectall}32");
+    cy.root()
+      .getFormGroupInputFor("Command *")
+      .type(cmdline);
 
-    // Select `Docker Container`
-    cy
-      .root()
-      .get(".multiple-form-modal-sidebar-menu-item")
-      .contains("Docker Container")
+    // Select `Container Image` radio button
+    cy.root()
+      .get("label")
+      .contains("Container Image")
       .click();
 
     // Fill-in image
-    cy.root().getFormGroupInputFor("Image").type("python:3");
+    cy.root()
+      .getFormGroupInputFor("Container Image *")
+      .type("nginx");
 
     // Click crate job
-    cy.contains("Create Job").click();
+    cy.contains("Submit").click();
 
     // Switch to the group that will contain the service
     cy.visitUrl(`jobs/overview/${Cypress.env("TEST_UUID")}`);
 
     // Wait for the table and the service to appear
-    cy
-      .get(".page-body-content table")
+    cy.get(".page-body-content table")
       .contains(jobName, { timeout: Timeouts.JOB_DEPLOYMENT_TIMEOUT })
       .get("a.table-cell-link-primary")
       .contains(`${jobName}`)
       .click();
 
     // open edit screen
-    cy
-      .get(".page-header-actions .dropdown")
+    cy.get(".page-header-actions .dropdown")
       .click()
       .get(".dropdown-menu-items")
       .contains("Edit")
       .click();
 
     // Fill-in the input elements
-    cy
-      .root()
-      .getFormGroupInputFor("ID *")
+    cy.root()
+      .getFormGroupInputFor("Job ID *")
       .should("have.value", `${fullJobName}`);
     //
     // TODO: Due to a bug in cypress you cannot type values with dots
@@ -138,257 +153,17 @@ describe("Jobs", function() {
     //   .getFormGroupInputFor('CPUs')
     //   .type('{selectall}0.5');
     //
-    cy.root().getFormGroupInputFor("Mem (MiB)").should("have.value", "32");
-    cy.root().getFormGroupInputFor("Command").contains(cmdline);
-
-    // Select `Docker Container`
-    cy
-      .root()
-      .get(".multiple-form-modal-sidebar-menu-item")
-      .contains("Docker Container")
-      .click();
+    cy.root()
+      .getFormGroupInputFor("Mem (MiB) *")
+      .should("have.value", "32");
+    cy.root()
+      .getFormGroupInputFor("Command")
+      .contains(cmdline);
 
     // Fill-in image
-    cy.root().getFormGroupInputFor("Image").should("have.value", "python:3");
-  });
-
-  it("creates a job with labels", function() {
-    const jobName = "job-with-labels";
-    const fullJobName = `${Cypress.env("TEST_UUID")}.${jobName}`;
-    const cmdline = "while true; do echo 'test' ; sleep 100 ; done";
-
-    // Visit jobs
-    cy.visitUrl("jobs/overview");
-
-    // Click 'Create a job'
-    // Note: The current group contains the previous jobs
-    cy.get(".button.button-primary-link.button-narrow").click();
-
-    // Wait for the 'New Job' dialog to appear
-    // Note: The current group contains the previous two jobs
-    cy.get(".modal-header").contains("New Job").should("exist");
-
-    // Fill-in the input elements
-    cy.root().getFormGroupInputFor("ID *").type(`{selectall}${fullJobName}`);
-    //
-    // TODO: Due to a bug in cypress you cannot type values with dots
-    // cy
-    //   .root()
-    //   .getFormGroupInputFor('CPUs')
-    //   .type('{selectall}0.5');
-    //
-    cy.root().getFormGroupInputFor("Mem (MiB)").type("{selectall}32");
-    cy.root().getFormGroupInputFor("Command").type(cmdline);
-
-    // Select `Labels`
-    cy
-      .root()
-      .get(".multiple-form-modal-sidebar-menu-item")
-      .contains("Labels")
-      .click();
-
-    // Fill-in the first label
-    cy.root().getFormGroupInputFor("Label Name").eq(0).type("camelCase");
-    cy.root().getFormGroupInputFor("Label Value").eq(0).type("test");
-
-    // Add an additional label
-    cy.get(".clickable").contains("Add Label").click({ force: true });
-
-    // Fill-in the second label
-    cy.root().getFormGroupInputFor("Label Name").eq(1).type("snake_case");
-    cy.root().getFormGroupInputFor("Label Value").eq(1).type("test");
-
-    // Add an additional label
-    cy.get(".clickable").contains("Add Label").click({ force: true });
-
-    // Fill-in the third label
-    cy.root().getFormGroupInputFor("Label Name").eq(2).type("lowercase");
-    cy.root().getFormGroupInputFor("Label Value").eq(2).type("test");
-
-    // Add an additional label
-    cy.get(".clickable").contains("Add Label").click({ force: true });
-
-    // Fill-in the fourth label
-    cy.root().getFormGroupInputFor("Label Name").eq(3).type("UPPERCASE");
-    cy.root().getFormGroupInputFor("Label Value").eq(3).type("test");
-
-    // Click crate job
-    cy.contains("Create Job").click();
-
-    // Switch to the group that will contain the service
-    cy.visitUrl(`jobs/overview/${Cypress.env("TEST_UUID")}`);
-
-    // Wait for the table and the service to appear
-    cy
-      .get(".page-body-content table")
-      .contains(jobName, { timeout: Timeouts.JOB_DEPLOYMENT_TIMEOUT })
-      .get("a.table-cell-link-primary")
-      .contains(`${jobName}`)
-      .click();
-
-    // open edit screen
-    cy
-      .get(".page-header-actions .dropdown")
-      .click()
-      .get(".dropdown-menu-items")
-      .contains("Edit")
-      .click();
-
-    // Fill-in the input elements
-    cy
-      .root()
-      .getFormGroupInputFor("ID *")
-      .should("have.value", `${fullJobName}`);
-    cy.root().getFormGroupInputFor("Mem (MiB)").should("have.value", "32");
-    cy.root().getFormGroupInputFor("Command").contains(cmdline);
-
-    // Select `Labels`
-    cy
-      .root()
-      .get(".multiple-form-modal-sidebar-menu-item")
-      .contains("Labels")
-      .click();
-
-    // Fill-in the first label
-    cy
-      .root()
-      .getFormGroupInputFor("Label Name")
-      .eq(0)
-      .should("have.value", "camelCase");
-    cy
-      .root()
-      .getFormGroupInputFor("Label Value")
-      .eq(0)
-      .should("have.value", "test");
-
-    // Fill-in the second label
-    cy
-      .root()
-      .getFormGroupInputFor("Label Name")
-      .eq(1)
-      .should("have.value", "snake_case");
-    cy
-      .root()
-      .getFormGroupInputFor("Label Value")
-      .eq(1)
-      .should("have.value", "test");
-
-    // Fill-in the third label
-    cy
-      .root()
-      .getFormGroupInputFor("Label Name")
-      .eq(2)
-      .should("have.value", "lowercase");
-    cy
-      .root()
-      .getFormGroupInputFor("Label Value")
-      .eq(2)
-      .should("have.value", "test");
-
-    // Fill-in the fourth label
-    cy
-      .root()
-      .getFormGroupInputFor("Label Name")
-      .eq(3)
-      .should("have.value", "UPPERCASE");
-    cy
-      .root()
-      .getFormGroupInputFor("Label Value")
-      .eq(3)
-      .should("have.value", "test");
-  });
-
-  it("creates a job with schedule", function() {
-    const jobName = "job-with-schedule";
-    const fullJobName = `${Cypress.env("TEST_UUID")}.${jobName}`;
-    const cmdline = "while true; do echo 'test' ; sleep 100 ; done";
-
-    // Visit jobs
-    cy.visitUrl("jobs/overview");
-
-    // Click 'Create a job'
-    // Note: The current group contains the previous jobs
-    cy.get(".button.button-primary-link.button-narrow").click();
-
-    // Wait for the 'New Job' dialog to appear
-    cy.get(".modal-header").contains("New Job").should("exist");
-
-    // Fill-in the input elements
-    cy.root().getFormGroupInputFor("ID *").type(`{selectall}${fullJobName}`);
-    //
-    // TODO: Due to a bug in cypress you cannot type values with dots
-    // cy
-    //   .root()
-    //   .getFormGroupInputFor('CPUs')
-    //   .type('{selectall}0.1');
-    //
-    cy.root().getFormGroupInputFor("Mem (MiB)").type("{selectall}32");
-    cy.root().getFormGroupInputFor("Command").type(cmdline);
-
-    // Select `Schedule`
-    cy
-      .root()
-      .get(".multiple-form-modal-sidebar-menu-item")
-      .contains("Schedule")
-      .click();
-
-    // Check 'Run on a schedule'
-    cy.contains("Run on a schedule").click();
-
-    // Specify a schedule
-    cy.root().getFormGroupInputFor("Cron Schedule *").type("* * * * *");
-
-    // Enable schedule
-    cy.contains("Enable").click();
-
-    // Click crate job
-    cy.contains("Create Job").click();
-
-    // Switch to the group that will contain the service
-    cy.visitUrl(`jobs/overview/${Cypress.env("TEST_UUID")}`);
-
-    // Wait for the table and the service to appear
-    cy
-      .get(".page-body-content table")
-      .contains(jobName, { timeout: Timeouts.JOB_DEPLOYMENT_TIMEOUT })
-      .get("a.table-cell-link-primary")
-      .contains(`${jobName}`)
-      .click();
-
-    // open edit screen
-    cy
-      .get(".page-header-actions .dropdown")
-      .click()
-      .get(".dropdown-menu-items")
-      .contains("Edit")
-      .click();
-
-    // Fill-in the input elements
-    cy
-      .root()
-      .getFormGroupInputFor("ID *")
-      .should("have.value", `${fullJobName}`);
-    //
-    // TODO: Due to a bug in cypress you cannot type values with dots
-    // cy
-    //   .root()
-    //   .getFormGroupInputFor('CPUs')
-    //   .type('{selectall}0.1');
-    //
-    cy.root().getFormGroupInputFor("Mem (MiB)").should("have.value", "32");
-    cy.root().getFormGroupInputFor("Command").contains(cmdline);
-
-    // Select `Schedule`
-    cy
-      .root()
-      .get(".multiple-form-modal-sidebar-menu-item")
-      .contains("Schedule")
-      .click();
-
-    cy
-      .root()
-      .getFormGroupInputFor("Cron Schedule *")
-      .should("have.value", "* * * * *");
+    cy.root()
+      .getFormGroupInputFor("Container Image *")
+      .should("have.value", "nginx");
   });
 
   it("removes job from table when deleted", function() {
@@ -400,41 +175,49 @@ describe("Jobs", function() {
     // Visit jobs
     cy.visitUrl("jobs/overview");
     cy.get(".button.button-primary-link.button-narrow").click();
-    cy.get(".modal-header").contains("New Job").should("exist");
+    cy.get(".modal-header")
+      .contains("New Job")
+      .should("exist");
 
     // Fill-in the input elements
-    cy.root().getFormGroupInputFor("ID *").type(`{selectall}${fullJobName}`);
-    cy.root().getFormGroupInputFor("Command").type(cmdline);
+    cy.root()
+      .getFormGroupInputFor("Job ID *")
+      .type(`{selectall}${fullJobName}`);
+    cy.root()
+      .getFormGroupInputFor("Command")
+      .type(cmdline);
 
     // Click create job
-    cy.contains("Create Job").click();
+    cy.contains("Submit").click();
 
     // Switch to the group that will contain the service
     cy.visitUrl(`jobs/overview/${Cypress.env("TEST_UUID")}`);
 
     // Wait for the table and the service to appear
-    cy
-      .get(".page-body-content table")
+    cy.get(".page-body-content table")
       .contains(jobName, { timeout: Timeouts.JOB_DEPLOYMENT_TIMEOUT })
       .get("a.table-cell-link-primary")
       .contains(`${jobName}`)
       .click();
 
     // open edit screen
-    cy
-      .get(".page-header-actions .dropdown")
+    cy.get(".page-header-actions .dropdown")
       .click()
       .get(".dropdown-menu-items")
       .contains("Delete")
       .click();
 
     // click delete
-    cy.get(".modal .button-danger").contains("Delete Job").click();
+    cy.get(".modal .button-danger")
+      .contains("Delete Job")
+      .click();
 
     // Switch to the group that will contain the service
     cy.visitUrl(`jobs/overview/${Cypress.env("TEST_UUID")}`);
 
     // The job should no longer be in the table
-    cy.get(".page-body-content table").contains(jobName).should("not.exist");
+    cy.get(".page-body-content table")
+      .contains(jobName)
+      .should("not.exist");
   });
 });
