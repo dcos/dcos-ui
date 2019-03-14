da90e76394c47a3e03592f762e90f8b04e7990c0
Rodney Norris <rnorris@mesosphere.com>
2019-03-14 08:40:49 -0500

    refactor(ui-update): improve notification streams

--- plugins/ui-update/notifications.ts
+++ plugins/ui-update/notifications.ts
@@ -1,6 +1,6 @@
 import { i18nMark } from "@lingui/react";
 import * as semver from "semver";
-import { filter } from "rxjs/operators";
+import { filter, map, tap } from "rxjs/operators";
 import { NotificationService } from "@extension-kid/notification-service";
 import {
   ToastAppearance,
@@ -25,16 +25,6 @@ function removeActiveNotification(id: string) {
   const notificationIndex = activeNotifications.indexOf(id);
   if (notificationIndex !== -1) {
     activeNotifications.splice(notificationIndex, 1);
-  }
-}
-
-function displayNotification(
-  notificationService: NotificationService,
-  notification: ToastNotification
-) {
-  if (!activeNotifications.includes(notification.id)) {
-    notificationService.push(notification);
-    activeNotifications.push(notification.id);
   }
 }
 
@@ -86,34 +76,44 @@ function dismissNotificationCallback(
 }
 
 function setupUIUpdatedNotification(notificationService: NotificationService) {
-  getUiMetadata$().subscribe(uiMetadata => {
-    const { clientBuild, serverBuild } = uiMetadata;
-    const coercedClientBuild = semver.coerce(clientBuild || "");
-    const coercedServerBuild = semver.coerce(serverBuild || "");
-    if (
-      coercedClientBuild !== null &&
-      coercedServerBuild !== null &&
-      coercedClientBuild.raw !== coercedServerBuild.raw &&
-      coercedClientBuild.raw !== LOCAL_DEV_VERSION
-    ) {
-      const displayVersion = coercedServerBuild.raw;
-      const description = {
-        id: i18nMark(
-          "DC/OS UI has been updated to {version}. Reload the UI for the updated version."
-        ),
-        values: {
-          version: displayVersion
-        }
-      };
-      const notification = new ToastNotification(i18nMark("New UI Available"), {
-        appearance: ToastAppearance.Success,
-        description,
-        primaryActionText: i18nMark("Reload"),
-        callback: updateAvailableCallback
-      });
-      displayNotification(notificationService, notification);
-    }
-  });
+  getUiMetadata$()
+    .pipe(
+      filter(uiMetadata => {
+        const { clientBuild, serverBuild } = uiMetadata;
+        const coercedClientBuild = semver.coerce(clientBuild || "");
+        const coercedServerBuild = semver.coerce(serverBuild || "");
+        return (
+          coercedClientBuild !== null &&
+          coercedServerBuild !== null &&
+          coercedClientBuild.raw !== coercedServerBuild.raw &&
+          coercedClientBuild.raw !== LOCAL_DEV_VERSION
+        );
+      }),
+      map(uiMetadata => {
+        //@ts-ignore
+        const displayVersion = semver.coerce(uiMetadata.serverBuild);
+        const description = {
+          id: i18nMark(
+            "DC/OS UI has been updated to {version}. Reload the UI for the updated version."
+          ),
+          values: {
+            // @ts-ignore
+            version: displayVersion.raw
+          }
+        };
+        return new ToastNotification(i18nMark("New UI Available"), {
+          appearance: ToastAppearance.Success,
+          description,
+          primaryActionText: i18nMark("Reload"),
+          callback: updateAvailableCallback
+        });
+      }),
+      filter(
+        toastNotification => !activeNotifications.includes(toastNotification.id)
+      ),
+      tap(toastNotification => activeNotifications.push(toastNotification.id))
+    )
+    .subscribe(notificationService.push);
 }
 
 function setupUpdateFailedNotification(
@@ -122,41 +122,49 @@ function setupUpdateFailedNotification(
   getAction$()
     .pipe(
       filter(
-        action =>
-          action.action === UIActions.Error &&
-          action.type === UIActionType.Update
-      )
+        uiAction =>
+          uiAction.action === UIActions.Error &&
+          uiAction.type === UIActionType.Update
+      ),
+      map(uiAction => {
+        let notification: ToastNotification;
+        const title = i18nMark("UI Upgrade Failure");
+        const description = i18nMark(
+          "The UI upgrade has failed due to an error."
+        );
+        if (uiAction.value.data !== undefined) {
+          const upgradeVersion: string = uiAction.value.data;
+          const tryAgainCallback = (
+            callbackType: ToastCallbackType,
+            toastNotification: ToastNotification
+          ) => {
+            updateFailedCallback(
+              callbackType,
+              toastNotification,
+              upgradeVersion
+            );
+          };
+          notification = new ToastNotification(title, {
+            appearance: ToastAppearance.Danger,
+            description,
+            primaryActionText: i18nMark("Try Again"),
+            callback: tryAgainCallback
+          });
+        } else {
+          notification = new ToastNotification(title, {
+            appearance: ToastAppearance.Danger,
+            description,
+            callback: dismissNotificationCallback
+          });
+        }
+        return notification;
+      }),
+      filter(
+        toastNotification => !activeNotifications.includes(toastNotification.id)
+      ),
+      tap(toastNotification => activeNotifications.push(toastNotification.id))
     )
-    .subscribe(action => {
-      let notification: ToastNotification;
-      const title = i18nMark("UI Upgrade Failure");
-      const description = i18nMark(
-        "The UI upgrade has failed due to an error."
-      );
-      if (action.value.data !== undefined) {
-        const upgradeVersion: string = action.value.data;
-        const tryAgainCallback = (
-          callbackType: ToastCallbackType,
-          toastNotification: ToastNotification
-        ) => {
-          updateFailedCallback(callbackType, toastNotification, upgradeVersion);
-        };
-        notification = new ToastNotification(title, {
-          appearance: ToastAppearance.Danger,
-          description,
-          primaryActionText: i18nMark("Try Again"),
-          callback: tryAgainCallback
-        });
-      } else {
-        notification = new ToastNotification(title, {
-          appearance: ToastAppearance.Danger,
-          description,
-          callback: dismissNotificationCallback
-        });
-      }
-
-      displayNotification(notificationService, notification);
-    });
+    .subscribe(notificationService.push);
 }
 
 function setupRollbackFailedNotification(
@@ -165,23 +173,27 @@ function setupRollbackFailedNotification(
   getAction$()
     .pipe(
       filter(
-        action =>
-          action.action === UIActions.Error &&
-          action.type === UIActionType.Reset
-      )
+        uiAction =>
+          uiAction.action === UIActions.Error &&
+          uiAction.type === UIActionType.Reset
+      ),
+      map(
+        () =>
+          new ToastNotification(i18nMark("UI Rollback Failure"), {
+            appearance: ToastAppearance.Danger,
+            description: i18nMark(
+              "The UI rollback has failed due to an error."
+            ),
+            primaryActionText: i18nMark("Try Again"),
+            callback: rollbackFailedCallback
+          })
+      ),
+      filter(
+        toastNotification => !activeNotifications.includes(toastNotification.id)
+      ),
+      tap(toastNotification => activeNotifications.push(toastNotification.id))
     )
-    .subscribe(() => {
-      const notification = new ToastNotification(
-        i18nMark("UI Rollback Failure"),
-        {
-          appearance: ToastAppearance.Danger,
-          description: i18nMark("The UI rollback has failed due to an error."),
-          primaryActionText: i18nMark("Try Again"),
-          callback: rollbackFailedCallback
-        }
-      );
-      displayNotification(notificationService, notification);
-    });
+    .subscribe(notificationService.push);
 }
 
 export {
