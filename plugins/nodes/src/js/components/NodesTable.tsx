5f034032569da9fc2fa23d3c7e3602a5ef6129e8
Mike Perrotti <mperrotti573@gmail.com>
2019-03-14 18:43:45 -0400

    fix(NodesTable): no content-based column width sizing on nodes table
    Closes DCOS-49714

--- plugins/nodes/src/js/components/NodesTable.tsx
+++ plugins/nodes/src/js/components/NodesTable.tsx
@@ -29,10 +29,6 @@ import { cpuSorter, cpuRenderer } from "../columns/NodesTableCPUColumn";
 import { memSorter, memRenderer } from "../columns/NodesTableMemColumn";
 import { diskSorter, diskRenderer } from "../columns/NodesTableDiskColumn";
 import { gpuSorter, gpuRenderer } from "../columns/NodesTableGPUColumn";
-import {
-  spacingSizer,
-  spacingRenderer
-} from "../columns/NodesTableSpacingColumn";
 
 import PublicIPColumn from "../columns/NodesTablePublicIPColumn";
 
@@ -185,6 +181,7 @@ export default class NodesTable extends React.Component<
               />
             }
             cellRenderer={ipRenderer}
+            minWidth={120}
           />
 
           <Column
@@ -196,6 +193,8 @@ export default class NodesTable extends React.Component<
               />
             }
             cellRenderer={healthRenderer}
+            minWidth={80}
+            maxWidth={100}
           />
 
           <Column
@@ -205,6 +204,7 @@ export default class NodesTable extends React.Component<
               </HeaderCell>
             }
             cellRenderer={PublicIPColumn}
+            minWidth={125}
           />
 
           <Column
@@ -216,6 +216,7 @@ export default class NodesTable extends React.Component<
               />
             }
             cellRenderer={typeRenderer}
+            maxWidth={70}
           />
 
           <Column
@@ -227,6 +228,7 @@ export default class NodesTable extends React.Component<
               />
             }
             cellRenderer={this.regionRenderer}
+            minWidth={170}
           />
 
           <Column
@@ -238,6 +240,7 @@ export default class NodesTable extends React.Component<
               />
             }
             cellRenderer={zoneRenderer}
+            minWidth={100}
           />
 
           <Column
@@ -250,6 +253,8 @@ export default class NodesTable extends React.Component<
               />
             }
             cellRenderer={tasksRenderer}
+            minWidth={60}
+            maxWidth={80}
           />
 
           <Column
@@ -261,6 +266,8 @@ export default class NodesTable extends React.Component<
               />
             }
             cellRenderer={cpuRenderer}
+            growToFill={true}
+            minWidth={110}
           />
 
           <Column
@@ -272,6 +279,8 @@ export default class NodesTable extends React.Component<
               />
             }
             cellRenderer={memRenderer}
+            growToFill={true}
+            minWidth={110}
           />
 
           <Column
@@ -283,6 +292,8 @@ export default class NodesTable extends React.Component<
               />
             }
             cellRenderer={diskRenderer}
+            growToFill={true}
+            minWidth={110}
           />
 
           <Column
@@ -294,12 +305,8 @@ export default class NodesTable extends React.Component<
               />
             }
             cellRenderer={gpuRenderer}
-          />
-
-          <Column
-            header={<span title="Spacing" />}
-            cellRenderer={spacingRenderer}
-            width={spacingSizer}
+            growToFill={true}
+            minWidth={110}
           />
         </Table>
       </div>
