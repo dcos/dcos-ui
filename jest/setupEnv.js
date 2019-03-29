7a3af182e2f525e0d8a5e00893443341dd8d37ad
Pierre Beitz <pierrebeitz@gmail.com>
2019-03-29 12:01:51 +0100

    chore: remove localStorage from setupEnv
    ...because removing does not break anything

--- jest/setupEnv.js
+++ jest/setupEnv.js
@@ -2,9 +2,6 @@ require("babel-polyfill");
 require("reflect-metadata");
 var EventEmitter = require("events").EventEmitter;
 
-// jsdom doesn't have support for localStorage at the moment
-global.localStorage = require("localStorage");
-
 // Tests should just mock responses for the json API
 // so let's just default to a noop
 const RequestUtil = require("mesosphere-shared-reactjs").RequestUtil;
