3c5ed8f88bc8b314a86dc15e3e24223eee088187
Pierre Beitz <pierrebeitz@gmail.com>
2019-03-29 11:52:34 +0100

    chore(jest): use glob to express our intent concisely
    glob handles all the slashing and the globbing and the isDirectory'ing
    we implemented ourselfes before.

--- jest.config.js
+++ jest.config.js
@@ -1,54 +1,30 @@
-const fs = require("fs");
+const glob = require("glob");
+
+// Return directories within the given path.
+const dirs = path => (path ? glob.sync(`${path}/*/`) : []);
 
 /**
  * List of root (test path) directories
  *
- * @description Jest will only search for tests and
- * load manual mocks (e.g. `src/__mocks__/fs.js`) within these directories.
- *
- * @type {Array.<string>}
+ * Jest will only search for tests and load manual mocks
+ * (e.g. `src/__mocks__/fs.js`) within these directories.
  */
-const roots = ["./src", "./tests"];
-
-/**
- * List of package directories
- *
- * @description Packages (subdirectories)  within these directories are
- * going to be added as a "root" directory; this allows packages to roll
- * their own "manual" mocks (e.g., `my-package/__mocks__/fs.js`),
- * as Jest only loads them from root directories.
- *
- * @type {Array.<string>}
- */
-const packages = ["plugins", "packages"];
-
-// Add `npm_config_externalplugins` and strip trailing slash
-// to make sure the paths are compatible.
-if (process.env.npm_config_externalplugins) {
-  packages.push(process.env.npm_config_externalplugins.replace(/\/$/, ""));
-}
-
-// Traverse the subdirectories for every package directory and add the
-// directories to the list root directories.
-packages.forEach(function(packageDir) {
-  fs.readdirSync(packageDir).forEach(function(rootDir) {
-    const relativePath = packageDir + "/" + rootDir;
-    if (fs.statSync(relativePath).isDirectory()) {
-      roots.push("./" + relativePath);
-    }
-  });
-});
+const roots = ["./src", "./tests"].concat(
+  dirs("./plugins"),
+  dirs("./packages"),
+  dirs(process.env.npm_config_externalplugins)
+);
 
 module.exports = {
   verbose: true,
   testURL: "http://localhost/",
-  roots,
   globals: {
     __DEV__: true,
     "ts-jest": {
       useBabelrc: true
     }
   },
+  roots,
   transform: {
     "^.+\\.jsx?$": "babel-jest",
     "^.+\\.tsx?$": "ts-jest",
