a32051596cf0f55f44aa6e84f7434ea9bdc6f7f4
Pierre Beitz <pierrebeitz@gmail.com>
2019-03-29 12:04:15 +0100

    chore(jest): remove an obsolete polyfill
    because nothing seems to break if we remove it.

--- jest/setupEnv.js
+++ jest/setupEnv.js
@@ -8,39 +8,6 @@ const { RequestUtil } = require("mesosphere-shared-reactjs");
 
 RequestUtil.json = _ => {};
 
-// jsdom doesn't have support for requestAnimationFrame so we polyfill it.
-// https://gist.github.com/paulirish/1579671
-(function() {
-  var lastTime = 0;
-  var vendors = ["ms", "moz", "webkit", "o"];
-
-  for (var x = 0; x < vendors.length && !global.requestAnimationFrame; ++x) {
-    global.requestAnimationFrame = global[vendors[x] + "RequestAnimationFrame"];
-    global.cancelAnimationFrame =
-      global[vendors[x] + "CancelAnimationFrame"] ||
-      global[vendors[x] + "CancelRequestAnimationFrame"];
-  }
-
-  if (!global.requestAnimationFrame) {
-    global.requestAnimationFrame = function(callback) {
-      var currTime = new Date().getTime();
-      var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-      var id = global.setTimeout(function() {
-        callback(currTime + timeToCall);
-      }, timeToCall);
-
-      lastTime = currTime + timeToCall;
-
-      return id;
-    };
-  }
-  if (!global.cancelAnimationFrame) {
-    global.cancelAnimationFrame = function(id) {
-      clearTimeout(id);
-    };
-  }
-})();
-
 /*
  * Mock the EventSource using the functionality of EventEmitter,
  * which mimics the functionality of the EventTarget interface.
