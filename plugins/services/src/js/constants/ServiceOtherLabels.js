6e095c149d913f651b6f4ec922bbe9110dd87a82
Pierre Beitz <pierrebeitz@gmail.com>
2019-04-08 19:04:52 +0200

    chore: remove dead code

--- plugins/services/src/js/constants/ServiceOtherLabels.js
+++
@@ -1,9 +0,0 @@
-import { i18nMark } from "@lingui/react";
-
-const ServiceOtherLabels = {
-  CATALOG: i18nMark("Catalog"),
-  VOLUMES: i18nMark("Volumes"),
-  PODS: i18nMark("Pods")
-};
-
-module.exports = ServiceOtherLabels;
