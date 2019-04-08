7ee968a9d329a78cf3f5acb5a33da252fefdadb4
Pierre Beitz <pierrebeitz@gmail.com>
2019-04-08 20:55:23 +0200

    chore: es6ify some exports

--- plugins/services/src/js/constants/DefaultPod.js
+++ plugins/services/src/js/constants/DefaultPod.js
@@ -1,17 +1,13 @@
-const DEFAULT_POD_RESOURCES = { cpus: 0.1, mem: 128 };
-const DEFAULT_POD_SCALING = { kind: "fixed", instances: 1 };
 const DEFAULT_POD_CONTAINER = {
   name: "container-1",
-  resources: DEFAULT_POD_RESOURCES
+  resources: { cpus: 0.1, mem: 128 }
 };
 const DEFAULT_POD_SPEC = {
   containers: [DEFAULT_POD_CONTAINER],
-  scaling: DEFAULT_POD_SCALING
+  scaling: { kind: "fixed", instances: 1 }
 };
 
 module.exports = {
   DEFAULT_POD_CONTAINER,
-  DEFAULT_POD_RESOURCES,
-  DEFAULT_POD_SCALING,
   DEFAULT_POD_SPEC
 };
