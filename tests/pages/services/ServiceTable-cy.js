685c381b338b1b4c89168620dbd6606c6fafc176
Rodney Norris <rnorris@mesosphere.com>
2019-04-09 15:05:43 -0500

    chore: add integration test for group aggregate status tooltip

--- tests/pages/services/ServiceTable-cy.js
+++ tests/pages/services/ServiceTable-cy.js
@@ -385,4 +385,22 @@ describe("Service Table", function() {
       });
     });
   });
+
+  context("Groups", function() {
+    beforeEach(function() {
+      cy.configureCluster({
+        mesos: "1-sdk-service",
+        nodeHealth: true
+      });
+
+      cy.visitUrl({ url: "/services/overview" });
+    });
+
+    it("shows service status counts in group tooltip", function() {
+      cy.get('.service-status-icon-wrapper > .tooltip-wrapper')
+        .eq(1)
+        .trigger("mouseover");
+      cy.get(".tooltip").contains("3 Running");
+    })
+  });
 });
