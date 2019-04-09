5d092726352eee9d0fb37eedd8e4b40aa2611d0c
Rodney Norris <rnorris@mesosphere.com>
2019-04-09 15:25:46 -0500

    chore: add integration test for group aggregate status text

--- tests/pages/services/ServiceTable-cy.js
+++ tests/pages/services/ServiceTable-cy.js
@@ -396,6 +396,12 @@ describe("Service Table", function() {
       cy.visitUrl({ url: "/services/overview" });
     });
 
+    it("group status is an aggregate of children", function() {
+      cy.get('.status-bar-text')
+        .eq(1)
+        .contains("Running (3 of 3)")
+    });
+
     it("shows service status counts in group tooltip", function() {
       cy.get('.service-status-icon-wrapper > .tooltip-wrapper')
         .eq(1)
