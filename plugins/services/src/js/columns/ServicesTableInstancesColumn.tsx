6846ea910eca86c9ca848150c9700d36719dac26
Daniel Schmidt <dschmidt@mesosphere.com>
2019-04-10 12:34:25 +0200

    perf(services): avoid rerenders for instances column
    Closes DCOS-51377

--- plugins/services/src/js/columns/ServicesTableInstancesColumn.tsx
+++ plugins/services/src/js/columns/ServicesTableInstancesColumn.tsx
@@ -11,34 +11,49 @@ import StringUtil from "#SRC/js/utils/StringUtil";
 import { SortDirection } from "plugins/services/src/js/types/SortDirection";
 import ServiceTableUtil from "../utils/ServiceTableUtil";
 
+const ServiceInstances = React.memo(
+  ({
+    instancesCount,
+    runningInstances
+  }: {
+    instancesCount: number;
+    runningInstances: number;
+  }) => {
+    const overview =
+      runningInstances === instancesCount
+        ? ` ${runningInstances}`
+        : ` ${runningInstances}/${instancesCount}`;
+
+    const content = !Number.isInteger(instancesCount)
+      ? EmptyStates.CONFIG_VALUE
+      : overview;
+
+    // L10NTODO: Pluralize
+    const tooltipContent = (
+      <Trans render="span">
+        {runningInstances} {StringUtil.pluralize("instance", runningInstances)}{" "}
+        running out of {instancesCount}
+      </Trans>
+    );
+
+    return (
+      <NumberCell>
+        <Tooltip content={tooltipContent}>
+          <span>{content}</span>
+        </Tooltip>
+      </NumberCell>
+    );
+  }
+);
+
 export function instancesRenderer(
   service: Service | Pod | ServiceTree
 ): React.ReactNode {
-  const instancesCount = service.getInstancesCount();
-  const runningInstances = service.getRunningInstancesCount();
-  const overview =
-    runningInstances === instancesCount
-      ? ` ${runningInstances}`
-      : ` ${runningInstances}/${instancesCount}`;
-
-  const content = !Number.isInteger(instancesCount)
-    ? EmptyStates.CONFIG_VALUE
-    : overview;
-
-  // L10NTODO: Pluralize
-  const tooltipContent = (
-    <Trans render="span">
-      {runningInstances} {StringUtil.pluralize("instance", runningInstances)}{" "}
-      running out of {instancesCount}
-    </Trans>
-  );
-
   return (
-    <NumberCell>
-      <Tooltip content={tooltipContent}>
-        <span>{content}</span>
-      </Tooltip>
-    </NumberCell>
+    <ServiceInstances
+      instancesCount={service.getInstancesCount()}
+      runningInstances={service.getRunningInstancesCount()}
+    />
   );
 }
 
