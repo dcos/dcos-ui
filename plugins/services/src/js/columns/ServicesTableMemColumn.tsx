52fe525ade5c672d9c7a9d9a327922074483e3a8
Pierre Beitz <pierrebeitz@gmail.com>
2019-04-12 16:17:24 +0200

    refactor(ServicesTable): remove Util, inline sorting
    sorting feels very much like the responsibility of the table and the indirection
    that's been happening before revealed that the sorting for regions is not even
    implemented.
    now that we don't have the double indirection (getting sorter from Column-file,
    determining sorter in Util) a mismatch should be more obvious.
    also we had sorting for `tasks` before. that's been removed, because i did not
    find an according tasks-column.
    
    the tests for sorting have been rewritten, because i was not clever enough to
    understand the old ones (i made sure that the data stood the same though).
    
    also a Version-Util has been factored out, as it seemed natural to have some
    place to stuff the version-things to outside of the table.

--- plugins/services/src/js/columns/ServicesTableMemColumn.tsx
+++ plugins/services/src/js/columns/ServicesTableMemColumn.tsx
@@ -5,8 +5,6 @@ import Pod from "../structs/Pod";
 import Service from "../structs/Service";
 import ServiceTree from "../structs/ServiceTree";
 import Units from "#SRC/js/utils/Units";
-import { SortDirection } from "plugins/services/src/js/types/SortDirection";
-import ServiceTableUtil from "../utils/ServiceTableUtil";
 
 export const ServiceMem = React.memo(({ resource }: { resource: string }) => (
   <NumberCell>
@@ -18,11 +16,4 @@ export function memRenderer(
   service: Service | Pod | ServiceTree
 ): React.ReactNode {
   return <ServiceMem resource={service.getResources()[`mem`]} />;
-}
-
-export function memSorter(
-  data: Array<Service | Pod | ServiceTree>,
-  sortDirection: SortDirection
-): any {
-  return ServiceTableUtil.sortData(data, sortDirection, "mem");
 }
