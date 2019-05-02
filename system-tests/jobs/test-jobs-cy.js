9ffe3896a4209ce651aeea41a81bcb697bd3ea65
Poltergeist <philipph@mesosphere.io>
2019-05-02 16:20:03 +0200

    fix: check for development flag

--- system-tests/jobs/test-jobs-cy.js
+++ system-tests/jobs/test-jobs-cy.js
@@ -71,83 +71,83 @@ describe("Jobs", function() {
       .getFormGroupInputFor("Command *")
       .should("have.value", `${cmdline}`);
   });
-  // it("creates a job with default ucr config", function() {
-  //   const jobName = "job-with-ucr-config";
-  //   const fullJobName = `${Cypress.env("TEST_UUID")}-${jobName}`;
-  //   const cmdline = "while true; do echo 'test' ; sleep 100 ; done";
-  //   // Visit jobs
-  //   cy.visitUrl("jobs/overview");
-  //   // Click 'Create a job'
-  //   // Note: The current group contains the previous job
-  //   cy.get(".button.button-primary-link.button-narrow").click();
-  //   // Wait for the 'New Job' dialog to appear
-  //   cy.get(".modal-header")
-  //     .contains("New Job")
-  //     .should("exist");
-  //   // Fill-in the input elements
-  //   cy.root()
-  //     .getFormGroupInputFor("Job ID *")
-  //     .type(`{selectall}${fullJobName}`);
-  //   //
-  //   // TODO: Due to a bug in cypress you cannot type values with dots
-  //   // cy
-  //   //   .root()
-  //   //   .getFormGroupInputFor('CPUs')
-  //   //   .type('{selectall}0.5');
-  //   //
-  //   cy.root()
-  //     .getFormGroupInputFor("Mem (MiB) *")
-  //     .type("{selectall}32");
-  //   // Select `Container Image` radio button
-  //   cy.root()
-  //     .get("label")
-  //     .contains("Container Image")
-  //     .click();
-  //   cy.root()
-  //     .getFormGroupInputFor("Command")
-  //     .type(cmdline);
-  //   // Fill-in image
-  //   cy.root()
-  //     .getFormGroupInputFor("Container Image *")
-  //     .type("nginx");
-  //   // Click crate job
-  //   cy.contains("Submit").click();
-  //   // Switch to the group that will contain the service
-  //   cy.visitUrl(`jobs/overview/${Cypress.env("TEST_UUID")}`);
-  //   // Wait for the table and the service to appear
-  //   cy.get(".page-body-content table")
-  //     .contains(jobName, { timeout: Timeouts.JOB_DEPLOYMENT_TIMEOUT })
-  //     .get("a.table-cell-link-primary")
-  //     .contains(`${jobName}`)
-  //     .click();
-  //   // open edit screen
-  //   cy.get(".page-header-actions .dropdown")
-  //     .click()
-  //     .get(".dropdown-menu-items")
-  //     .contains("Edit")
-  //     .click();
-  //   // Fill-in the input elements
-  //   cy.root()
-  //     .getFormGroupInputFor("Job ID *")
-  //     .should("have.value", `${fullJobName}`);
-  //   //
-  //   // TODO: Due to a bug in cypress you cannot type values with dots
-  //   // cy
-  //   //   .root()
-  //   //   .getFormGroupInputFor('CPUs')
-  //   //   .type('{selectall}0.5');
-  //   //
-  //   cy.root()
-  //     .getFormGroupInputFor("Mem (MiB) *")
-  //     .should("have.value", "32");
-  //   cy.root()
-  //     .getFormGroupInputFor("Command")
-  //     .should("have.value", `${cmdline}`);
-  //   // Fill-in image
-  //   cy.root()
-  //     .getFormGroupInputFor("Container Image *")
-  //     .should("have.value", "nginx");
-  // });
+  it("creates a job with default ucr config", function() {
+    const jobName = "job-with-ucr-config";
+    const fullJobName = `${Cypress.env("TEST_UUID")}-${jobName}`;
+    const cmdline = "while true; do echo 'test' ; sleep 100 ; done";
+    // Visit jobs
+    cy.visitUrl("jobs/overview");
+    // Click 'Create a job'
+    // Note: The current group contains the previous job
+    cy.get(".button.button-primary-link.button-narrow").click();
+    // Wait for the 'New Job' dialog to appear
+    cy.get(".modal-header")
+      .contains("New Job")
+      .should("exist");
+    // Fill-in the input elements
+    cy.root()
+      .getFormGroupInputFor("Job ID *")
+      .type(`{selectall}${fullJobName}`);
+    //
+    // TODO: Due to a bug in cypress you cannot type values with dots
+    // cy
+    //   .root()
+    //   .getFormGroupInputFor('CPUs')
+    //   .type('{selectall}0.5');
+    //
+    cy.root()
+      .getFormGroupInputFor("Mem (MiB) *")
+      .type("{selectall}32");
+    // Select `Container Image` radio button
+    cy.root()
+      .get("label")
+      .contains("Container Image")
+      .click();
+    cy.root()
+      .getFormGroupInputFor("Command")
+      .type(cmdline);
+    // Fill-in image
+    cy.root()
+      .getFormGroupInputFor("Container Image *")
+      .type("nginx");
+    // Click crate job
+    cy.contains("Submit").click();
+    // Switch to the group that will contain the service
+    cy.visitUrl(`jobs/overview/${Cypress.env("TEST_UUID")}`);
+    // Wait for the table and the service to appear
+    cy.get(".page-body-content table")
+      .contains(jobName, { timeout: Timeouts.JOB_DEPLOYMENT_TIMEOUT })
+      .get("a.table-cell-link-primary")
+      .contains(`${jobName}`)
+      .click();
+    // open edit screen
+    cy.get(".page-header-actions .dropdown")
+      .click()
+      .get(".dropdown-menu-items")
+      .contains("Edit")
+      .click();
+    // Fill-in the input elements
+    cy.root()
+      .getFormGroupInputFor("Job ID *")
+      .should("have.value", `${fullJobName}`);
+    //
+    // TODO: Due to a bug in cypress you cannot type values with dots
+    // cy
+    //   .root()
+    //   .getFormGroupInputFor('CPUs')
+    //   .type('{selectall}0.5');
+    //
+    cy.root()
+      .getFormGroupInputFor("Mem (MiB) *")
+      .should("have.value", "32");
+    cy.root()
+      .getFormGroupInputFor("Command")
+      .should("have.value", `${cmdline}`);
+    // Fill-in image
+    cy.root()
+      .getFormGroupInputFor("Container Image *")
+      .should("have.value", "nginx");
+  });
   // it("runs, stops and deletes a job", function() {
   //   // first create a simple job
   //   const jobName = "job-to-delete";
