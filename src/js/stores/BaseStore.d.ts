56bf974b45427c07cdda5c809a11bce9916538c8
Pierre Beitz <pierrebeitz@gmail.com>
2019-05-02 14:49:07 +0200

    chore: convert BaseStore to TS

--- src/js/stores/BaseStore.d.ts
+++
@@ -1,6 +0,0 @@
-import { EventEmitter } from "events";
-
-export default class BaseStore extends EventEmitter {
-  addChangeListener: (eventName: string, callback: (...any) => any) => void;
-  removeChangeListener: (eventName: string, callback: (...any) => any) => void;
-}
