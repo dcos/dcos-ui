50d7d4def7fedd68ac6ff52c67180f16d60ba979
Georgi Todorov <gtodorov.c@mesosphere.io>
2019-05-10 13:28:58 +0300

    feat(Logs): wrap text
    Wrap the text in logs to hide the vertical scrollbar
    and enable users to find information more efficiently.
    
    Closes DCOS_OSS-1577

--- tests/pages/nodes/node/tasks/NodesTaskDetailPage-cy.js
+++ tests/pages/nodes/node/tasks/NodesTaskDetailPage-cy.js
@@ -7,17 +7,36 @@ describe("Nodes Task Detail Page", function() {
   });
 
   context("Navigate to node task detail page", function() {
-    it("navigates to node task detail page", function() {
-      cy.visitUrl({ url: "/nodes", identify: true });
-      cy.get("a.table-cell-link-primary")
-        .eq(0)
-        .click({ force: true });
-      cy.get("a.table-cell-link-secondary")
-        .eq(0)
-        .click();
-      cy.hash().should("match", /nodes\/[a-zA-Z0-9-]+\/tasks\/[a-zA-Z0-9-]+/);
+    context("Navigate to the tabs", function() {
+      beforeEach(function() {
+        cy.visitUrl({ url: "/nodes", identify: true, fakeAnalytics: true });
+        cy.get("a.table-cell-link-primary")
+          .eq(0)
+          .click({ force: true });
+        cy.get("a.table-cell-link-secondary")
+          .eq(0)
+          .click();
+        cy.hash().should("match", /nodes\/[a-zA-Z0-9-]+\/tasks\/[a-zA-Z0-9-]+/);
+      });
 
-      cy.get("h1.configuration-map-heading").contains("Configuration");
+      it("navigates to node task detail page", function() {
+        cy.get("h1.configuration-map-heading").contains("Configuration");
+      });
+
+      it("navigates to node task files page", function() {
+        cy.get(".menu-tabbed-item-label")
+          .contains("Files")
+          .click();
+        cy.get(".control-group").contains("Working Directory");
+        cy.get("table.table").should("exist");
+      });
+
+      it("navigates to node task logs page", function() {
+        cy.get(".menu-tabbed-item-label")
+          .contains("Logs")
+          .click();
+        cy.get("pre.prettyprint").should("exist");
+      });
     });
 
     it("loads page with data after hard reload", function() {
