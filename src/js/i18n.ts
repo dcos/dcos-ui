afef338e53dcdd5fa89575aaa47a6a60a36d60b0
Brandon Croft <brandon.croft@gmail.com>
2019-05-13 16:07:45 +0200

    feat: split ee lingui message catalogs

--- src/js/i18n.ts
+++ src/js/i18n.ts
@@ -5,7 +5,38 @@ import UserLanguageStore from "./stores/UserLanguageStore";
 import en from "#LOCALE/en/messages";
 import zh from "#LOCALE/zh/messages";
 
-const catalogs = { en, zh };
+let enEE = { languageData: {}, messages: {} };
+let zhEE = { languageData: {}, messages: {} };
+
+try {
+  enEE = require("#EXTERNAL_PLUGINS/locale/en/messages").default;
+  zhEE = require("#EXTERNAL_PLUGINS/locale/zh/messages").default;
+} catch {
+  // No enterprise plugins
+}
+
+const catalogs = {
+  en: {
+    languageData: {
+      ...en.languageData,
+      ...enEE.languageData
+    },
+    messages: {
+      ...en.messages,
+      ...enEE.messages
+    }
+  },
+  zh: {
+    languageData: {
+      ...zh.languageData,
+      ...zhEE.languageData
+    },
+    messages: {
+      ...zh.messages,
+      ...zhEE.messages
+    }
+  }
+};
 const language = UserLanguageStore.get();
 const i18n = setupI18n({ language, catalogs });
 
