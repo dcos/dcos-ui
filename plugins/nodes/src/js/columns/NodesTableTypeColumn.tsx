01fb90ff191a218603e2bf1e734daa8ec410ea18
Pierre Beitz <pierrebeitz@gmail.com>
2019-06-27 16:28:26 +0200

    refactor(NodesTable): reduce the noise of width-preferences
    introduce `hasCustomWidth` and `customWidthFor` to save a lot of indirection

--- plugins/nodes/src/js/columns/NodesTableTypeColumn.tsx
+++ plugins/nodes/src/js/columns/NodesTableTypeColumn.tsx
@@ -2,11 +2,8 @@ import * as React from "react";
 import sort from "array-sort";
 import { Trans } from "@lingui/macro";
 import { TextCell } from "@dcos/ui-kit";
-import { WidthArgs } from "@dcos/ui-kit/dist/packages/table/components/Column";
 
 import Node from "#SRC/js/structs/Node";
-import TableColumnResizeStore from "#SRC/js/stores/TableColumnResizeStore";
-import { columnWidthsStorageKey } from "../components/NodesTable";
 import { SortDirection } from "plugins/nodes/src/js/types/SortDirection";
 
 const NodeType = React.memo(({ isPublic }: { isPublic: boolean }) => {
@@ -41,8 +38,4 @@ const comparators = [compareNodesByType, compareNodesByHostname];
 export function typeSorter(data: Node[], sortDirection: SortDirection): Node[] {
   const reverse = sortDirection !== "ASC";
   return sort(data, comparators, { reverse });
-}
-
-export function typeWidth(_: WidthArgs) {
-  return TableColumnResizeStore.get(columnWidthsStorageKey).type;
 }
