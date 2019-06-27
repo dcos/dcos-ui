dbc87af33ac2cf9aa304f19f5cda419b6fee8aff
Rodney Norris <rnorris@mesosphere.com>
2019-06-27 14:56:41 -0500

    feat: display quota tab for services overview (FFd)

--- plugins/services/src/js/containers/services/ServicesContainer.js
+++ plugins/services/src/js/containers/services/ServicesContainer.js
@@ -146,7 +146,9 @@ const METHODS_TO_BIND = [
   "restartService",
   "onStoreChange",
   "getResetDelaySuccessToast",
-  "getResetDelayErrorToast"
+  "getResetDelayErrorToast",
+  "getServiceTree",
+  "getServiceQuota"
 ];
 
 class ServicesContainer extends React.Component {
@@ -617,6 +619,10 @@ class ServicesContainer extends React.Component {
     );
   }
 
+  getServiceQuota(item) {
+    return this.getServiceTree(item);
+  }
+
   getResetDelaySuccessToast(serviceName) {
     const title = i18nTranslate(i18nMark("Reset Delay Successful"));
     const description = i18nTranslate(
@@ -688,6 +694,10 @@ class ServicesContainer extends React.Component {
       return this.getServiceTree(item);
     }
 
+    if (currentRoutePath.startsWith("/services/quota")) {
+      return this.getServiceQuota(item);
+    }
+
     if (item instanceof Pod) {
       return this.getPodDetail(item);
     }
@@ -729,7 +739,7 @@ ServicesContainer.contextTypes = {
 ServicesContainer.routeConfig = {
   label: i18nMark("Services"),
   icon: <Icon shape={ProductIcons.ServicesInverse} size={iconSizeS} />,
-  matches: /^\/services\/(detail|overview)/
+  matches: /^\/services\/(detail|overview|quota)/
 };
 
 module.exports = ServicesContainer;
