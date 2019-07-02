4b46631427906755bed476feaac5666d37b9565f
Pierre Beitz <pierrebeitz@gmail.com>
2019-07-02 14:10:31 +0200

    feat(Maintenance): add i18n

--- plugins/nodes/src/js/types/Status.ts
+++ plugins/nodes/src/js/types/Status.ts
@@ -2,17 +2,17 @@ import { i18nMark } from "@lingui/react";
 import StatusIcon from "#SRC/js/constants/StatusIcon";
 import Node from "#SRC/js/structs/Node";
 
-const build = (priority: number, icon: StatusIcon, name: string): Status => ({
-  priority,
-  icon,
-  displayName: i18nMark(name)
-});
+const build = (
+  priority: number,
+  icon: StatusIcon,
+  displayName: string
+): Status => ({ priority, icon, displayName });
 
-const active = build(2, StatusIcon.SUCCESS, "Active");
-const deactivated = build(3, StatusIcon.STOPPED, "Deactivated");
-const drained = build(5, StatusIcon.STOPPED, "Drained");
-const draining = build(4, StatusIcon.LOADING, "Draining");
-const unknown = build(1, StatusIcon.ERROR, "Unknown");
+const active = build(2, StatusIcon.SUCCESS, i18nMark("Active"));
+const deactivated = build(3, StatusIcon.STOPPED, i18nMark("Deactivated"));
+const drained = build(5, StatusIcon.STOPPED, i18nMark("Drained"));
+const draining = build(4, StatusIcon.LOADING, i18nMark("Draining"));
+const unknown = build(1, StatusIcon.ERROR, i18nMark("Unknown"));
 
 const fromNode = (n: Node) => {
   if (!n.isDeactivated()) {
