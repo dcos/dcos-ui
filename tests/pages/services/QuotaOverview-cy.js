5dd6e09bb32f851f2d65a7a86ac75b3fd3ddcdce
Georgi Todorov <gtodorov.c@mesosphere.io>
2019-07-09 11:52:14 +0300

    feat: update integration tests

--- tests/pages/services/QuotaOverview-cy.js
+++ tests/pages/services/QuotaOverview-cy.js
@@ -39,118 +39,204 @@ describe("Quota Tab", function() {
       cy.visitUrl({ url: "/services/quota" });
     });
 
-    function getFirstRowName(name) {
-      cy.get(".ReactVirtualized__Grid")
-        .eq(2)
-        .children()
-        .first()
-        .children()
-        .first()
-        .contains(name);
-    }
+    context("Quota table", function() {
+      function getFirstRowName(name) {
+        cy.get(".ReactVirtualized__Grid")
+          .eq(2)
+          .children()
+          .first()
+          .children()
+          .first()
+          .contains(name);
+      }
 
-    function getSecondRowName(name) {
-      cy.get(".ReactVirtualized__Grid")
-        .eq(2)
-        .children()
-        .first()
-        .children()
-        .last()
-        .contains(name);
-    }
+      function getSecondRowName(name) {
+        cy.get(".ReactVirtualized__Grid")
+          .eq(2)
+          .children()
+          .first()
+          .children()
+          .last()
+          .contains(name);
+      }
 
-    function clickHeading(name) {
-      cy.get(".TopRightGrid_ScrollWrapper")
-        .contains(name)
-        .click();
-    }
+      function clickHeading(name) {
+        cy.get(".TopRightGrid_ScrollWrapper")
+          .contains(name)
+          .click();
+      }
 
-    it("Shows the quota table", function() {
-      cy.get(".quota-table").should("exist");
+      it("Shows the quota table", function() {
+        cy.get(".quota-table").should("exist");
+      });
+
+      it("Shows the correct headings", function() {
+        cy.get(".ReactVirtualized__Grid")
+          .eq(0)
+          .contains("Name")
+          .should("exist");
+        cy.get(".TopRightGrid_ScrollWrapper")
+          .contains("CPU Consumed")
+          .should("exist");
+        cy.get(".TopRightGrid_ScrollWrapper")
+          .contains("Memory Consumed")
+          .should("exist");
+        cy.get(".TopRightGrid_ScrollWrapper")
+          .contains("Disk Consumed")
+          .should("exist");
+        cy.get(".TopRightGrid_ScrollWrapper")
+          .contains("GPU Consumed")
+          .should("exist");
+      });
+
+      it("Shows the correct number of rows", function() {
+        cy.get(".ReactVirtualized__Grid")
+          .eq(2)
+          .children()
+          .first()
+          .children()
+          .should("to.have.length", 2);
+      });
+
+      it("Sorts the table by name", function() {
+        cy.get(".ReactVirtualized__Grid")
+          .eq(0)
+          .contains("Name")
+          .click();
+        getFirstRowName("10000_apps");
+        getSecondRowName("10_apps");
+
+        cy.get(".ReactVirtualized__Grid")
+          .eq(0)
+          .contains("Name")
+          .click();
+        getFirstRowName("10_apps");
+        getSecondRowName("10000_apps");
+      });
+
+      it("Sorts the table by CPU consumed", function() {
+        clickHeading("CPU Consumed");
+        getFirstRowName("10000_apps");
+        getSecondRowName("10_apps");
+
+        clickHeading("CPU Consumed");
+        getFirstRowName("10_apps");
+        getSecondRowName("10000_apps");
+      });
+
+      it("Sorts the table by Memory consumed", function() {
+        clickHeading("Memory Consumed");
+        getFirstRowName("10000_apps");
+        getSecondRowName("10_apps");
+
+        clickHeading("Memory Consumed");
+        getFirstRowName("10_apps");
+        getSecondRowName("10000_apps");
+      });
+
+      it("Sorts the table by Disk consumed", function() {
+        clickHeading("Disk Consumed");
+        getFirstRowName("10_apps");
+        getSecondRowName("10000_apps");
+
+        clickHeading("Disk Consumed");
+        getFirstRowName("10000_apps");
+        getSecondRowName("10_apps");
+      });
+
+      it("Sorts the table by GPU consumed", function() {
+        clickHeading("GPU Consumed");
+        getFirstRowName("10000_apps");
+        getSecondRowName("10_apps");
+
+        clickHeading("GPU Consumed");
+        getFirstRowName("10_apps");
+        getSecondRowName("10000_apps");
+      });
     });
 
-    it("Shows the correct headings", function() {
-      cy.get(".ReactVirtualized__Grid")
-        .eq(0)
-        .contains("Name")
-        .should("exist");
-      cy.get(".TopRightGrid_ScrollWrapper")
-        .contains("CPU Consumed")
-        .should("exist");
-      cy.get(".TopRightGrid_ScrollWrapper")
-        .contains("Memory Consumed")
-        .should("exist");
-      cy.get(".TopRightGrid_ScrollWrapper")
-        .contains("Disk Consumed")
-        .should("exist");
-      cy.get(".TopRightGrid_ScrollWrapper")
-        .contains("GPU Consumed")
-        .should("exist");
-    });
+    context("Quota Detail", function() {
+      beforeEach(function() {
+        cy.get(".table-cell-link-primary")
+          .contains("10_apps")
+          .click();
+      });
 
-    it("Shows the correct number of rows", function() {
-      cy.get(".ReactVirtualized__Grid")
-        .eq(2)
-        .children()
-        .first()
-        .children()
-        .should("to.have.length", 2);
-    });
+      it("Opens the quota detail page when we click on a group", function() {
+        cy.get(".breadcrumb__content--text")
+          .contains("10_apps")
+          .should("exist");
+        cy.get(".quota-details").should("exist");
+      });
 
-    it("Sorts the table by name", function() {
-      cy.get(".ReactVirtualized__Grid")
-        .eq(0)
-        .contains("Name")
-        .click();
-      getFirstRowName("10000_apps");
-      getSecondRowName("10_apps");
+      it("Open the quota detail tab when we visit the correct url", function() {
+        cy.visitUrl({ url: "/services/quota/%2F10000_apps" });
+        cy.get(".breadcrumb__content--text")
+          .contains("10000_apps")
+          .should("exist");
+        cy.get(".quota-details").should("exist");
+      });
 
-      cy.get(".ReactVirtualized__Grid")
-        .eq(0)
-        .contains("Name")
-        .click();
-      getFirstRowName("10_apps");
-      getSecondRowName("10000_apps");
-    });
+      it("Shows the correct number of cards", function() {
+        cy.get(".quota-card").should("have.length", 4);
+      });
 
-    it("Sorts the table by CPU consumed", function() {
-      clickHeading("CPU Consumed");
-      getFirstRowName("10000_apps");
-      getSecondRowName("10_apps");
+      it("Shows the correct card titles", function() {
+        cy.get(".quota-card-title")
+          .contains("CPU")
+          .should("exist");
+        cy.get(".quota-card-title")
+          .contains("Memory")
+          .should("exist");
+        cy.get(".quota-card-title")
+          .contains("Disk")
+          .should("exist");
+        cy.get(".quota-card-title")
+          .contains("GPU")
+          .should("exist");
+      });
 
-      clickHeading("CPU Consumed");
-      getFirstRowName("10_apps");
-      getSecondRowName("10000_apps");
-    });
+      it("Shows percent or N/A for each card", function() {
+        cy.get(".quota-card-main")
+          .eq(0)
+          .contains("25%")
+          .should("exist");
+        cy.get(".quota-card-main")
+          .eq(1)
+          .contains("12.5%")
+          .should("exist");
+        cy.get(".quota-card-main")
+          .eq(2)
+          .contains("50%")
+          .should("exist");
+        cy.get(".quota-card-main")
+          .eq(3)
+          .contains("N/A")
+          .should("exist");
+      });
 
-    it("Sorts the table by Memory consumed", function() {
-      clickHeading("Memory Consumed");
-      getFirstRowName("10000_apps");
-      getSecondRowName("10_apps");
+      it("Shows consumed of limit or no limit for each card", function() {
+        cy.get(".quota-card-label")
+          .eq(0)
+          .contains("0.5 of 2 Cores")
+          .should("exist");
+        cy.get(".quota-card-label")
+          .eq(1)
+          .contains("128 of 1024 MiB")
+          .should("exist");
+        cy.get(".quota-card-label")
+          .eq(2)
+          .contains("5 of 10 MiB")
+          .should("exist");
+        cy.get(".quota-card-label")
+          .eq(3)
+          .contains("No Limit")
+          .should("exist");
+      });
 
-      clickHeading("Memory Consumed");
-      getFirstRowName("10_apps");
-      getSecondRowName("10000_apps");
-    });
-
-    it("Sorts the table by Disk consumed", function() {
-      clickHeading("Disk Consumed");
-      getFirstRowName("10_apps");
-      getSecondRowName("10000_apps");
-
-      clickHeading("Disk Consumed");
-      getFirstRowName("10000_apps");
-      getSecondRowName("10_apps");
-    });
-
-    it("Sorts the table by GPU consumed", function() {
-      clickHeading("GPU Consumed");
-      getFirstRowName("10000_apps");
-      getSecondRowName("10_apps");
-
-      clickHeading("GPU Consumed");
-      getFirstRowName("10_apps");
-      getSecondRowName("10000_apps");
+      it("Shows progress bars", function() {
+        cy.get(".quota-progress-bar").should("have.length", 4);
+      });
     });
   });
 });
