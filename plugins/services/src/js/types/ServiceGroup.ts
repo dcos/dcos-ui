12e29f6f885c063cdbf7d2ef4e721d8fcd0b37e8
Rodney Norris <rnorris@mesosphere.com>
2019-07-19 14:16:09 -0500

    refactor(QuotaUtil): moved limit status strings to constant
    Moved the Quota Limit statuses to a constant object and removed type to
    ensure lingui doesn't lose the ids. There may be a better way to resolve
    this issue and I'm happy to adjust this as we lost the benefit of the
    type in the switch case in the column renderer.

--- plugins/services/src/js/types/ServiceGroup.ts
+++ plugins/services/src/js/types/ServiceGroup.ts
@@ -1,4 +1,5 @@
 import { findNestedPropertyInObject } from "#SRC/js/utils/Util";
+import { i18nMark } from "@lingui/react";
 
 export type QuotaResources = {
   guarantee?: number;
@@ -12,15 +13,21 @@ export type ServiceGroupQuotaRoles = {
   groupRoleCount: number;
 };
 
-export type ServiceGroupQuotaLimit =
-  | "Enforced"
-  | "Not Enforced"
-  | "Partially Enforced"
-  | "N/A";
+export const QuotaLimitStatuses: {
+  enforced: string;
+  notEnforced: string;
+  partiallyEnforced: string;
+  na: string;
+} = {
+  enforced: i18nMark("Enforced"),
+  notEnforced: i18nMark("Not Enforced"),
+  partiallyEnforced: i18nMark("Partially Enforced"),
+  na: i18nMark("N/A")
+};
 
 export type ServiceGroupQuota = {
   enforced: boolean;
-  limitStatus: ServiceGroupQuotaLimit;
+  limitStatus: string;
   serviceRoles?: ServiceGroupQuotaRoles;
   cpus?: QuotaResources;
   memory?: QuotaResources;
