e2a392bea3b6fc2ddf682f77e5c9a75458b8f516
Pierre Beitz <pierrebeitz@gmail.com>
2019-07-22 11:34:39 +0200

    chore: remove dead code
    filterButtonContent seems to be never used/rendered.

--- plugins/nodes/src/js/pages/nodes-overview/HostsPageContent.js
+++ plugins/nodes/src/js/pages/nodes-overview/HostsPageContent.js
@@ -243,7 +243,6 @@ class HostsPageContent extends React.PureComponent {
 
 HostsPageContent.propTypes = {
   byServiceFilter: PropTypes.string,
-  filterButtonContent: PropTypes.func,
   filterInputText: PropTypes.node,
   filterItemList: PropTypes.array.isRequired,
   filteredNodeCount: PropTypes.number.isRequired,
