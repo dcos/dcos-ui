44c30d92684b1c56649ea8326661a2d134c48c9e
Rodney Norris <rnorris@mesosphere.com>
2019-07-31 16:50:44 -0500

    fix(ui-update): setup notification when logged in
    Updated the ui-update plugin to wait until a user is logged in to setup
    notifications. Also added code to unsubscribe notification streams when
    a user is logged out or redirected to login
    
    - Closes DCOS_OSS-5279

--- plugins/ui-update/hooks.js
+++ plugins/ui-update/hooks.js
@@ -4,21 +4,62 @@ import container from "#SRC/js/container";
 
 import UIDetails from "./components/UIDetails";
 import { loadNotifications, UIUpdateNotificationsType } from "./notifications";
+import { getSDK } from "./SDK";
+
+const SDK = getSDK();
 
 module.exports = {
+  actions: ["userLoginSuccess", "userLogoutSuccess", "redirectToLogin"],
+  notifications: {
+    updated: null,
+    updateFailed: null,
+    rollbackFailed: null
+  },
+
   initialize() {
-    loadNotifications(container);
+    this.actions.forEach(action => {
+      SDK.Hooks.addAction(action, this[action].bind(this));
+    });
 
     MountService.MountService.registerComponent(
       UIDetails,
       "UISettings:UIDetails:Content"
     );
 
+    loadNotifications(container);
+  },
+  registerNotifications() {
+    console.log("notifications being registered");
     const notifications = container.get(UIUpdateNotificationsType);
-    if (notifications) {
-      notifications.setupUIUpdatedNotification();
-      notifications.setupUpdateFailedNotification();
-      notifications.setupRollbackFailedNotification();
+    if (!notifications) {
+      return;
     }
+    if (this.notifications.updated === null) {
+      this.notifications.updated = notifications.setupUIUpdatedNotification();
+    }
+    if (this.notifications.updateFailed === null) {
+      this.notifications.updateFailed = notifications.setupUpdateFailedNotification();
+    }
+    if (this.notifications.rollbackFailed === null) {
+      this.notifications.rollbackFailed = notifications.setupRollbackFailedNotification();
+    }
+  },
+  unregisterNotifications() {
+    for (const key in this.notifications) {
+      if (this.notifications[key] !== null) {
+        this.notifications[key].unsubscribe();
+        this.notifications[key] = null;
+      }
+    }
+  },
+
+  userLoginSuccess() {
+    this.registerNotifications();
+  },
+  userLogoutSuccess() {
+    this.unregisterNotifications();
+  },
+  redirectToLogin() {
+    this.unregisterNotifications();
   }
 };
