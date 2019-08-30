f9e53c46407399cb80d612f5d6b0a0f2001c55fb
Pierre Beitz <pierrebeitz@gmail.com>
2019-08-30 14:47:01 +0200

    refactor: constants

--- src/js/constants/AuthConstants.js
+++
@@ -1,5 +0,0 @@
-const AuthConstants = {
-  userCookieKey: "dcos-acs-info-cookie"
-};
-
-module.exports = AuthConstants;
--- src/js/utils/CookieUtils.js
+++ src/js/utils/CookieUtils.js
@@ -1,13 +1,13 @@
 import cookie from "cookie";
 
-import { userCookieKey } from "../constants/AuthConstants";
+const USER_COOKIE_KEY = "dcos-acs-info-cookie";
 
 const Utils = {
   getUserMetadata() {
-    return cookie.parse(global.document.cookie)[userCookieKey];
+    return cookie.parse(global.document.cookie)[USER_COOKIE_KEY];
   },
   emptyCookieWithExpiry(date) {
-    return cookie.serialize(userCookieKey, "", { expires: date });
+    return cookie.serialize(USER_COOKIE_KEY, "", { expires: date });
   }
 };
 
