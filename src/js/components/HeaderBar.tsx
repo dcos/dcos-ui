08f437d08b5413558398d60b304904179f77cfca
Pierre Beitz <pierrebeitz@gmail.com>
2019-11-08 15:39:45 +0100

    chore: inline AccountHeader-Mount

--- src/js/components/HeaderBar.tsx
+++ src/js/components/HeaderBar.tsx
@@ -1,10 +1,9 @@
 import * as React from "react";
-
 import { Link } from "react-router";
+import { MountService } from "foundation-ui";
 
 import ClusterHeader from "./ClusterHeader";
 import SidebarToggle from "./SidebarToggle";
-import AccountHeader from "#SRC/js/components/AccountHeader";
 
 export default class HeaderBar extends React.Component {
   render() {
@@ -17,7 +16,7 @@ export default class HeaderBar extends React.Component {
           </Link>
         </div>
         <div className="header-bar-right-align-wrapper">
-          <AccountHeader />
+          <MountService.Mount type="Header:UserAccountDropdown" />;
           <ClusterHeader />
         </div>
       </div>
