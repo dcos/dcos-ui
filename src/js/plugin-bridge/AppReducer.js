3b06e86e456c4ecafbea74994d88a39da51c61b0
Pierre Beitz <pierrebeitz@gmail.com>
2019-11-13 15:25:37 -0800

    chore: inline some constants

--- src/js/plugin-bridge/AppReducer.js
+++ src/js/plugin-bridge/AppReducer.js
@@ -2,10 +2,6 @@ import { APPLICATION } from "#SRC/js/constants/PluginConstants";
 import { APP_STORE_CHANGE } from "../constants/EventTypes";
 import StructUtil from "../utils/StructUtil";
 
-const initialState = {};
-// List of plugins with permissions to alter this state
-const PERMISSIONS_LIST = [APPLICATION, "authentication", "organization"];
-
 // Compute new state based on action
 function updateState(prevState, action) {
   // Peel away Structs and assign to State tree rooted at storeID
@@ -14,12 +10,12 @@ function updateState(prevState, action) {
   return prevState;
 }
 
-function allowedToProceed(action) {
-  return PERMISSIONS_LIST.indexOf(action.__origin) > -1;
-}
+// List of plugins with permissions to alter this state
+const allowedToProceed = action =>
+  [APPLICATION, "authentication", "organization"].includes(action.__origin);
 
 // Clones state from application stores and maps it into the OmniStore
-export default (state = initialState, action) => {
+export default (state = {}, action) => {
   // Return early if the action didn't come from Application
   //  or plugins with permission
   if (!allowedToProceed(action)) {
