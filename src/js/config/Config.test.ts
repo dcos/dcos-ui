6355667cdfaa07b79fee1529aa2e2034678de321
Pierre Beitz <pierrebeitz@gmail.com>
2019-11-21 01:54:46 +0100

    chore: remove fake page-load-delay
    it could not be seen on most installations anyway anymore as the delay has
    gradually been tweaked down to one second.

--- src/js/config/Config.test.ts
+++ src/js/config/Config.test.ts
@@ -1,6 +1,5 @@
 // put in your config overrides here
 export default {
-  applicationRenderDelay: 100,
   rootUrl: "",
   historyServer: "",
   analyticsKey: "39uhSEOoRHMw6cMR6st9tYXDbAL3JSaP",
--- src/js/utils/__tests__/ApplicationUtil-test.js
+++ src/js/utils/__tests__/ApplicationUtil-test.js
@@ -1,38 +1,8 @@
 const ApplicationUtil = require("../ApplicationUtil");
-const Config = require("#SRC/js/config/Config").default;
 const EventTypes = require("../../constants/EventTypes");
 const MesosSummaryStore = require("../../stores/MesosSummaryStore");
 
 describe("ApplicationUtil", () => {
-  describe("#invokeAfterPageLoad", () => {
-    it("calls callback right away", () => {
-      const handler = jasmine.createSpy("handler");
-      const now = Date.now();
-
-      global.getPageLoadedTime = () => now - Config.applicationRenderDelay;
-
-      ApplicationUtil.invokeAfterPageLoad(handler);
-
-      jest.runAllTimers();
-      expect(handler).toHaveBeenCalled();
-    });
-
-    it("calls after time has elapsed", () => {
-      const handler = jasmine.createSpy("handler");
-      const now = Date.now();
-
-      global.getPageLoadedTime = () => now;
-
-      ApplicationUtil.invokeAfterPageLoad(handler);
-
-      // Ensure that it's not called before the time has elapsed, tick the
-      // timers and then check if it was called
-      expect(handler).not.toHaveBeenCalled();
-      jest.advanceTimersByTime(Config.applicationRenderDelay);
-      expect(handler).toHaveBeenCalled();
-    });
-  });
-
   describe("#beginTemporaryPolling", () => {
     it("calls callback once event is emitted", () => {
       const handler = jasmine.createSpy("handler");
