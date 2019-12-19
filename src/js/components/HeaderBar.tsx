95d156bc81f7fa7ef63c9c62ec394ec09b741d51
Poltergeist <philipph@mesosphere.io>
2019-12-20 00:32:06 +0100

    feat(refreshratee): add refreshrate to the header
    This will add the refresh rate to the header if it deviates from the default setting.
    
    CLOSES DCOS-60820

--- src/js/components/HeaderBar.tsx
+++ src/js/components/HeaderBar.tsx
@@ -1,21 +1,45 @@
 import * as React from "react";
 import { Link } from "react-router";
 import { MountService } from "foundation-ui";
+import { Tooltip } from "@dcos/ui-kit";
+
+import { Trans } from "@lingui/macro";
 
 import ClusterHeader from "./ClusterHeader";
 import SidebarToggle from "./SidebarToggle";
+import Config from "../config/Config";
 
-export default () => (
-  <div className="header-bar">
-    <div className="header-bar-left-align-wrapper">
-      <SidebarToggle />
-      <Link to="/dashboard" className="header-bar-logo-wrapper">
-        <span className="header-bar-logo" />
-      </Link>
+export default () => {
+  const refreshRate = Config.getRefreshRate() / 1000;
+  const refreshRateHint =
+    Config.getRefreshRate() !== Config.defaultRefreshRate ? (
+      <span>
+        <Tooltip
+          id="refreshRate"
+          trigger={`Refresh Rate: ${refreshRate} sec`}
+          maxWidth={150}
+        >
+          <Trans
+            id="Data will only be requested every {refreshRate} seconds."
+            values={{ refreshRate }}
+          />
+        </Tooltip>
+      </span>
+    ) : null;
+
+  return (
+    <div className="header-bar">
+      <div className="header-bar-left-align-wrapper">
+        <SidebarToggle />
+        <Link to="/dashboard" className="header-bar-logo-wrapper">
+          <span className="header-bar-logo" />
+        </Link>
+      </div>
+      <div className="header-bar-right-align-wrapper">
+        {refreshRateHint}
+        <MountService.Mount type="Header:UserAccountDropdown" />
+        <ClusterHeader />
+      </div>
     </div>
-    <div className="header-bar-right-align-wrapper">
-      <MountService.Mount type="Header:UserAccountDropdown" />
-      <ClusterHeader />
-    </div>
-  </div>
-);
+  );
+};
