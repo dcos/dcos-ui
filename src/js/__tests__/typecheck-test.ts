e5d7fb070ab6ff55d4cbf2b810c4a73879b92738
Pierre Beitz <pbeitz@d2iq.com>
2019-12-23 18:28:23 +0100

    chore: remove the non helpful errors in typecheck.snap
    with the need to sort the output of tsc some messages have been shuffled to the
    beginning of the snapshot without maintaining their link to the original path of
    the errors. we don't need those lines.
    
    maybe we can revamp the snapshots some day to be represented by some object that
    groups errors by path and sorts smarter (in a way that does not delocate the
    lines removed here).

--- src/js/__tests__/typecheck-test.ts
+++ src/js/__tests__/typecheck-test.ts
@@ -7,7 +7,10 @@ test("TypeScript snapshot should be up to date", async () => {
       // we're having trouble with paths here. on CI the paths given by that
       // error message are // project-relative. on my machine they are absolute.
       // we ignoring those for now.
-      .filter((line: string) => !line.includes("has no exported member"))
+      .filter(
+        (line: string) =>
+          !line.includes("has no exported member") && !line.startsWith(" ")
+      )
       // strip exact error position to reduce noise in the snapshots and detect actual errors more easily:
       //    src/File.tsx(230,24): error TS7030: Not all code paths return a value.
       // -> src/File.tsx: error TS7030: Not all code paths return a value.
