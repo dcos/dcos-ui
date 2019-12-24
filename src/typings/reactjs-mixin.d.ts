b9667caf8d01b06aa164543c112df8dedcd37de6
Pierre Beitz <pbeitz@d2iq.com>
2019-12-24 22:08:50 +0100

    chore: remove some lying types

--- src/typings/reactjs-mixin.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "reactjs-mixin" {
-  import { Component } from "react";
-
-  // Had to go with any.
-  // This should be the way of declaring it properly https://github.com/Microsoft/TypeScript/pull/13743
-  export default function mixin(...mixin: object[]): any;
-}
