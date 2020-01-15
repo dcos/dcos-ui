35e62618141540dbbfff71ac7c134da2f1a6a059
Pierre Beitz <pbeitz@d2iq.com>
2020-01-15 17:46:59 +0100

    chore: refactor FilterInputText

--- plugins/services/src/js/constants/ServiceFilterTypes.ts
+++
@@ -1,9 +0,0 @@
-const ServiceFilterTypes = {
-  HEALTH: "filterHealth",
-  OTHER: "filterOther",
-  STATUS: "filterStatus",
-  LABELS: "filterLabels",
-  TEXT: "searchString"
-};
-
-export default ServiceFilterTypes;
