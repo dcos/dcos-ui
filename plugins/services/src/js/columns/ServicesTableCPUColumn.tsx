aece18f43a4b68f5c44cf13c5df7fd63b58835ed
Poltergeist <philipph@mesosphere.io>
2020-01-28 14:09:50 +0100

    fixup! feat(virtualbursting): add limits to the service table

--- plugins/services/src/js/columns/ServicesTableCPUColumn.tsx
+++ plugins/services/src/js/columns/ServicesTableCPUColumn.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
-import { NumberCell } from "@dcos/ui-kit";
+import { Trans } from "@lingui/macro";
+import { NumberCell, Tooltip } from "@dcos/ui-kit";
 
 import Pod from "../structs/Pod";
 import Service from "../structs/Service";
@@ -7,16 +8,44 @@ import ServiceTree from "../structs/ServiceTree";
 import Units from "#SRC/js/utils/Units";
 
 export const ServiceCPU = React.memo(
-  ({ resource, limit }: { resource: string; limit?: number | string }) => (
-    <NumberCell>
-      <span>
-        {Units.formatResource("cpus", resource)}
-        {limit != null && limit !== 0 ? (
-          <React.Fragment> / {limit}</React.Fragment>
-        ) : null}
-      </span>
-    </NumberCell>
-  )
+  ({
+    resource,
+    limit,
+    id
+  }: {
+    resource: string;
+    limit?: number | string;
+    id: string;
+  }) => {
+    if (limit != null && limit !== 0) {
+      return (
+        <NumberCell>
+          <Tooltip
+            id={`cpu${id}`}
+            trigger={`${Units.formatResource(
+              "cpus",
+              resource
+            )} / ${Units.formatResource("cpus", limit)}`}
+            maxWidth={150}
+          >
+            <Trans
+              id="{resource} cpus are being used with a limit of {limit} cpus"
+              render="span"
+              values={{
+                resource: Units.formatResource("cpus", resource),
+                limit: Units.formatResource("cpus", limit)
+              }}
+            />
+          </Tooltip>
+        </NumberCell>
+      );
+    }
+    return (
+      <NumberCell>
+        <span>{Units.formatResource("cpus", resource)}</span>
+      </NumberCell>
+    );
+  }
 );
 
 export function cpuRenderer(
@@ -24,8 +53,9 @@ export function cpuRenderer(
 ): React.ReactNode {
   return (
     <ServiceCPU
-      resource={service.getResources()[`cpus`]}
-      limit={service.getResourceLimits()[`cpus`]}
+      id={service.getId()}
+      resource={service.getResources().cpus}
+      limit={service.getResourceLimits().cpus}
     />
   );
 }
