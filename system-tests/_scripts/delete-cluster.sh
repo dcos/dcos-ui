3ab8c335e0e6c437ecf418182cde7e178527d1df
Pierre Beitz <pbeitz@d2iq.com>
2020-01-31 12:01:21 +0100

    chore: parallelize system-tests for oss and ee
    by telling apart the proxies, dcos-clis and files that store important data
    (/tmp/cluster_url_$VARIANT.txt for example), we should now be able to parallely
    start clusters have their running.

--- system-tests/_scripts/delete-cluster.sh
+++ system-tests/_scripts/delete-cluster.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-CLUSTER_INFO=/tmp/cluster-info.json
+CLUSTER_INFO=/tmp/cluster_info_$VARIANT.json
 
 if ! dcos-launch delete -i ${CLUSTER_INFO} 1>&2; then
   echo "Cluster deletion failed."
@@ -8,4 +8,3 @@ if ! dcos-launch delete -i ${CLUSTER_INFO} 1>&2; then
 fi
 
 rm ${CLUSTER_INFO};
-exit 0
