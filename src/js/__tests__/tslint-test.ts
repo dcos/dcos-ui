0227eda4ad59fe66dbf8f0f8c04913bb8a356e9c
Pierre Beitz <pbeitz@d2iq.com>
2020-02-03 12:25:02 +0100

    chore: consume tslint via file

--- src/js/__tests__/tslint-test.ts
+++ src/js/__tests__/tslint-test.ts
@@ -1,13 +1,33 @@
-import { getOutput } from "./helper";
+import fs from "fs";
+import execa from "execa";
+import path from "path";
+
+const projectRoot = path.resolve(__dirname, "../../../");
+const replaceRoot = new RegExp(projectRoot, "g");
 
 test("tslint snapshot should be up to date", async () => {
+  await execa("tslint", [
+    "--project",
+    "tsconfig.json",
+    "-o",
+    "/tmp/tslint"
+  ]).catch(() => {
+    return "that's ok!";
+  });
+
+  const file = fs.readFileSync("/tmp/tslint", "utf-8");
   expect(
-    (await getOutput("tslint", ["--project", "tsconfig.json"])).replace(
-      // strip exact error position to reduce noise in the snapshots and detect actual errors more easily
-      //   ERROR: /plugins/NodesTable.tsx:123:42 - Multiline JS expressions inside JSX are forbidden
-      // ->ERROR: /plugins/NodesTable.tsx - Multiline JS expressions inside JSX are forbidden
-      /ERROR: (\S*):\d*:\d* -/gm,
-      "$1 -"
-    )
+    file
+      .replace(replaceRoot, "")
+      .replace(
+        // strip exact error position to reduce noise in the snapshots and detect actual errors more easily
+        //   ERROR: /plugins/NodesTable.tsx:123:42 - Multiline JS expressions inside JSX are forbidden
+        // ->ERROR: /plugins/NodesTable.tsx - Multiline JS expressions inside JSX are forbidden
+        /ERROR: (\S*):\d*:\d* -/gm,
+        "$1 -"
+      )
+      .split("\n")
+      .sort()
+      .join("\n")
   ).toMatchSnapshot();
 }, 120000);
