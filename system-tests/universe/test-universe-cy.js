2fa43580a69ea219292d36ba422cfe979dfe254f
Pierre Beitz <pbeitz@d2iq.com>
2020-03-04 12:34:04 +0100

    chore: fix a flake
    moving the plan-inspection test down a little for the service to get up and
    running. also cleaning up stuff to not have multiple visitURLs one after
    another.

--- system-tests/universe/test-universe-cy.js
+++ system-tests/universe/test-universe-cy.js
@@ -1,219 +1,221 @@
 require("../_support/utils/ServicesUtil");
 
 describe("Universe", () => {
-  beforeEach(() => {
-    cy.visitUrl("catalog/packages");
+  describe("on catalog/packages", () => {
+    beforeEach(() => {
+      cy.visitUrl("catalog/packages");
+    });
+
+    it("installs a certified package", () => {
+      const packageName = "confluent-kafka";
+
+      // Click 'Find a the kafka package'
+      cy.contains(packageName).click();
+
+      // Check that this package is certified
+      cy.contains("Certified");
+
+      // Click the Review & Run button
+      cy.contains("Review & Run").click();
+
+      // Move to the review screen
+      cy.contains("Review & Run").click();
+
+      // Click the Run Service button
+      cy.contains("Run Service").click();
+
+      // Go to the root services page
+      cy.visitUrl("services/overview");
+
+      // Check that it appears in the service list
+      cy.get(".page-body-content .service-table").contains(packageName);
+    });
+
+    it("fails to install a package with the same name", () => {
+      const packageName = "confluent-kafka";
+
+      // Click 'Find a the kafka package'
+      cy.contains(packageName).click();
+
+      // Check that this package is certified
+      cy.contains("Certified");
+
+      // Click the Review & Run button
+      cy.contains("Review & Run").click();
+
+      // Move to the review screen
+      cy.contains("Review & Run").click();
+
+      // Click the Run Service button
+      cy.contains("Run Service").click();
+
+      // Should give error that package already installed
+      cy.get(".cosmosErrorMsg").contains(
+        "A service with the same name already exists. Try a different name."
+      );
+    });
+
+    it("installs a community package", () => {
+      cy.get("button")
+        .contains("Community")
+        .click();
+
+      cy.contains("bitbucket").click();
+
+      // Check that this package is not certified
+      cy.contains("Community");
+
+      // Move to the config screen
+      cy.contains("Review & Run").click();
+
+      // Click the Continue button
+      cy.get(".button-primary")
+        .contains("Continue")
+        .click();
+
+      // Move to the review screen
+      cy.contains("Review & Run").click();
+
+      cy.contains("Run Service").click();
+
+      cy.contains("Open Service").click();
+
+      // Go to the root services page
+      cy.visitUrl("services/overview");
+
+      // Check that it appears in the service list
+      cy.get(".page-body-content .service-table").contains("bitbucket");
+    });
+
+    it("uses advanced install to deploy a certified package", () => {
+      const packageName = "confluent-kafka";
+      const serviceName = `${Cypress.env("TEST_UUID")}-${packageName}`;
+
+      // Click 'Find a the kafka package'
+      cy.contains(packageName).click();
+
+      // Check that this package is certified
+      cy.contains("Certified");
+
+      // Click the Review & Run button
+      cy.contains("Review & Run").click();
+
+      // Click Edit Config button
+      cy.contains("Edit Config").click();
+
+      // Find name input
+      cy.get(".modal input[name=name]").retype(serviceName);
+
+      // Wait for the new service to deploy
+      cy.get(".modal")
+        .contains("Review & Run")
+        .click();
+      cy.get(".modal")
+        .contains("Run Service")
+        .click();
+      cy.get(".modal.modal-small")
+        .contains("Open Service")
+        .click();
+
+      // Go to the root services page
+      cy.visitUrl("services/overview");
+
+      // Check that it appears in the service list
+      cy.get(".page-body-content .service-table").contains(serviceName);
+    });
+
+    it("uses advanced install to deploy a community package", () => {
+      const packageName = "bitbucket";
+      const serviceName = `${Cypress.env("TEST_UUID")}-${packageName}`;
+
+      cy.get("button")
+        .contains("Community")
+        .click();
+
+      cy.contains(packageName).click();
+
+      // Check that this package is certified
+      cy.contains("Community");
+
+      cy.contains("Review & Run").click();
+
+      // Click the Continue button
+      cy.get(".button-primary")
+        .contains("Continue")
+        .click();
+
+      // Click Edit Config button
+      cy.contains("Edit Config").click();
+
+      // Find name input
+      cy.get(".modal input[name=name]").retype(serviceName);
+
+      // Wait for the new service to deploy
+      cy.get(".modal")
+        .contains("Review & Run")
+        .click();
+      cy.get(".modal")
+        .contains("Run Service")
+        .click();
+      cy.get(".modal.modal-small")
+        .contains("Open Service")
+        .click();
+
+      // Go to the root services page
+      cy.visitUrl("services/overview");
+
+      // Check that it appears in the service list
+      cy.get(".page-body-content .service-table").contains(serviceName);
+    });
   });
 
-  it("installs a certified package", () => {
-    const packageName = "confluent-kafka";
+  describe("on services/overview", () => {
+    beforeEach(() => {
+      cy.visitUrl("services/overview");
+    });
 
-    // Click 'Find a the kafka package'
-    cy.contains(packageName).click();
+    it("plan is displayed", () => {
+      const packageName = "confluent-kafka";
+      // Check that it appears in the service list
+      cy.get(".page-body-content .service-table")
+        .contains(packageName)
+        .click();
 
-    // Check that this package is certified
-    cy.contains("Certified");
+      cy.get(".page-header-navigation-tabs")
+        .contains("Plans")
+        .click();
 
-    // Click the Review & Run button
-    cy.contains("Review & Run").click();
+      cy.contains("broker (serial)");
+    });
 
-    // Move to the review screen
-    cy.contains("Review & Run").click();
+    it("deletes an already installed package", () => {
+      const packageName = "bitbucket";
+      const serviceName = `${Cypress.env("TEST_UUID")}-${packageName}`;
 
-    // Click the Run Service button
-    cy.contains("Run Service").click();
+      // Click on the name of the package to delete
+      cy.get(".page-body-content .service-table")
+        .contains(serviceName)
+        .get("a.table-cell-link-primary")
+        .contains(serviceName)
+        .click({ force: true });
 
-    // Go to the root services page
-    cy.visitUrl("services/overview");
+      // Click delete in the dropdown
+      cy.get(".page-header-actions .dropdown")
+        .click()
+        .get(".dropdown-menu-items")
+        .scrollIntoView()
+        .contains("Delete")
+        .click();
 
-    // Check that it appears in the service list
-    cy.get(".page-body-content .service-table").contains(packageName);
-  });
+      // Confirm the deletion
+      cy.get(".modal.modal-small input").type(serviceName);
+      cy.get(".modal.modal-small button.button-danger")
+        .contains("Delete Service")
+        .click();
 
-  it("fails to install a package with the same name", () => {
-    const packageName = "confluent-kafka";
-
-    // Click 'Find a the kafka package'
-    cy.contains(packageName).click();
-
-    // Check that this package is certified
-    cy.contains("Certified");
-
-    // Click the Review & Run button
-    cy.contains("Review & Run").click();
-
-    // Move to the review screen
-    cy.contains("Review & Run").click();
-
-    // Click the Run Service button
-    cy.contains("Run Service").click();
-
-    // Should give error that package already installed
-    cy.get(".cosmosErrorMsg").contains(
-      "A service with the same name already exists. Try a different name."
-    );
-  });
-
-  it("plan is displayed", () => {
-    const packageName = "confluent-kafka";
-
-    // Go to the root services page
-    cy.visitUrl("services/overview");
-
-    // Check that it appears in the service list
-    cy.get(".page-body-content .service-table")
-      .contains(packageName)
-      .click();
-
-    cy.get(".page-header-navigation-tabs")
-      .contains("Plans")
-      .click();
-
-    cy.contains("broker (serial)");
-  });
-
-  it("installs a community package", () => {
-    cy.get("button")
-      .contains("Community")
-      .click();
-
-    cy.contains("bitbucket").click();
-
-    // Check that this package is not certified
-    cy.contains("Community");
-
-    // Move to the config screen
-    cy.contains("Review & Run").click();
-
-    // Click the Continue button
-    cy.get(".button-primary")
-      .contains("Continue")
-      .click();
-
-    // Move to the review screen
-    cy.contains("Review & Run").click();
-
-    cy.contains("Run Service").click();
-
-    cy.contains("Open Service").click();
-
-    // Go to the root services page
-    cy.visitUrl("services/overview");
-
-    // Check that it appears in the service list
-    cy.get(".page-body-content .service-table").contains("bitbucket");
-  });
-
-  it("uses advanced install to deploy a certified package", () => {
-    const packageName = "confluent-kafka";
-    const serviceName = `${Cypress.env("TEST_UUID")}-${packageName}`;
-
-    // Click 'Find a the kafka package'
-    cy.contains(packageName).click();
-
-    // Check that this package is certified
-    cy.contains("Certified");
-
-    // Click the Review & Run button
-    cy.contains("Review & Run").click();
-
-    // Click Edit Config button
-    cy.contains("Edit Config").click();
-
-    // Find name input
-    cy.get(".modal input[name=name]").retype(serviceName);
-
-    // Wait for the new service to deploy
-    cy.get(".modal")
-      .contains("Review & Run")
-      .click();
-    cy.get(".modal")
-      .contains("Run Service")
-      .click();
-    cy.get(".modal.modal-small")
-      .contains("Open Service")
-      .click();
-
-    // Go to the root services page
-    cy.visitUrl("services/overview");
-
-    // Check that it appears in the service list
-    cy.get(".page-body-content .service-table").contains(serviceName);
-  });
-
-  it("uses advanced install to deploy a community package", () => {
-    const packageName = "bitbucket";
-    const serviceName = `${Cypress.env("TEST_UUID")}-${packageName}`;
-
-    cy.get("button")
-      .contains("Community")
-      .click();
-
-    cy.contains(packageName).click();
-
-    // Check that this package is certified
-    cy.contains("Community");
-
-    cy.contains("Review & Run").click();
-
-    // Click the Continue button
-    cy.get(".button-primary")
-      .contains("Continue")
-      .click();
-
-    // Click Edit Config button
-    cy.contains("Edit Config").click();
-
-    // Find name input
-    cy.get(".modal input[name=name]").retype(serviceName);
-
-    // Wait for the new service to deploy
-    cy.get(".modal")
-      .contains("Review & Run")
-      .click();
-    cy.get(".modal")
-      .contains("Run Service")
-      .click();
-    cy.get(".modal.modal-small")
-      .contains("Open Service")
-      .click();
-
-    // Go to the root services page
-    cy.visitUrl("services/overview");
-
-    // Check that it appears in the service list
-    cy.get(".page-body-content .service-table").contains(serviceName);
-  });
-
-  it("deletes an already installed package", () => {
-    const packageName = "bitbucket";
-    const serviceName = `${Cypress.env("TEST_UUID")}-${packageName}`;
-
-    cy.visitUrl("services/overview");
-
-    // Click on the name of the package to delete
-    cy.get(".page-body-content .service-table")
-      .contains(serviceName)
-      .get("a.table-cell-link-primary")
-      .contains(serviceName)
-      .click({ force: true });
-
-    // Click delete in the dropdown
-    cy.get(".page-header-actions .dropdown")
-      .click()
-      .get(".dropdown-menu-items")
-      .scrollIntoView()
-      .contains("Delete")
-      .click();
-
-    // Confirm the deletion
-    cy.get(".modal.modal-small input").type(serviceName);
-    cy.get(".modal.modal-small button.button-danger")
-      .contains("Delete Service")
-      .click();
-
-    cy.get(".page-body-content .service-table")
-      .contains(serviceName)
-      .should("not.exist");
+      cy.get(".page-body-content .service-table")
+        .contains(serviceName)
+        .should("not.exist");
+    });
   });
 });
