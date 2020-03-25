6091b60f919f36ae0e91bcd076794bc5fd785319
Pierre Beitz <pbeitz@d2iq.com>
2020-03-25 14:57:20 +0100

    chore: remove getAPIResponse
    cause... it causes WAY more noise than it helps.

--- tests/_support/utils/tests/router-test.js
+++ tests/_support/utils/tests/router-test.js
@@ -18,17 +18,6 @@ describe("router", () => {
     global.cy = this.originalCy;
   });
 
-  describe("#clearRoutes", () => {
-    it("clears previously defined routes", (done) => {
-      router.route(/foo/, "fx:bar");
-      router.clearRoutes();
-      router.getAPIResponse("foo", (foundFixture) => {
-        expect(foundFixture).to.equal(null);
-        done();
-      });
-    });
-  });
-
   describe("#route", () => {
     beforeEach(function () {
       this.fooRegEx = /foo/;
@@ -48,23 +37,6 @@ describe("router", () => {
 
     it("returns an instance of the router util", function () {
       expect(this.returnValue).to.equal(router);
-    });
-  });
-
-  describe("#getAPIResponse", () => {
-    it("calls the callback with the fixture when found", (done) => {
-      router.route(/foo/, "fx:bar");
-      router.getAPIResponse("foo", (foundFixture) => {
-        expect(foundFixture).to.equal("bar");
-        done();
-      });
-    });
-
-    it("calls the callback with null when the fixture is not found", (done) => {
-      router.getAPIResponse("baz", (foundFixture) => {
-        expect(foundFixture).to.equal(null);
-        done();
-      });
     });
   });
 });
