d9e5cce37a46ccbbd6a961ee31a775cf84c3f13c
Pierre Beitz <pbeitz@d2iq.com>
2020-04-21 15:01:17 +0200

    chore: remove obsolete code from PodSpec

--- plugins/services/src/js/structs/PodSpec.ts
+++ plugins/services/src/js/structs/PodSpec.ts
@@ -2,6 +2,10 @@ import * as ValidatorUtil from "#SRC/js/utils/ValidatorUtil";
 import ServiceSpec from "./ServiceSpec";
 
 export default class PodSpec extends ServiceSpec {
+  scaling?: {
+    instances: number;
+  };
+
   getContainers() {
     return this.get("containers") || [];
   }
@@ -10,20 +14,8 @@ export default class PodSpec extends ServiceSpec {
     return this.getContainers().find((container) => container.name === name);
   }
 
-  getContainerCount() {
-    return this.getContainers().length;
-  }
-
-  getEnvironment() {
-    return this.get("environment") || {};
-  }
-
   getLabels() {
     return this.get("labels") || {};
-  }
-
-  getNetworks() {
-    return this.get("networks") || [];
   }
 
   getResources() {
@@ -35,32 +27,13 @@ export default class PodSpec extends ServiceSpec {
 
         return resources;
       },
-      {
-        cpus: 0,
-        mem: 0,
-        gpus: 0,
-        disk: 0,
-      }
+      { cpus: 0, mem: 0, gpus: 0, disk: 0 }
     );
   }
 
-  getScaling() {
-    return this.get("scaling") || {};
-  }
-
   getScalingInstances() {
-    const scaling = this.get("scaling") || { kind: "fixed", instances: 1 };
-    if (process.env.NODE_ENV !== "production") {
-      if (scaling.kind !== "fixed") {
-        throw new TypeError("Unknown scaling type (expecting fixed)");
-      }
-    }
-
-    if (!ValidatorUtil.isNumber(scaling.instances)) {
-      return 1;
-    }
-
-    return scaling.instances;
+    const scaling = this.scaling || { kind: "fixed", instances: 1 };
+    return ValidatorUtil.isNumber(scaling.instances) ? scaling.instances : 1;
   }
 
   getVersion() {
