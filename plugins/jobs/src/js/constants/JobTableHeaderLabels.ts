ca79460df40e60db65ea6bdc5c3f69c8747e0f6d
Pierre Beitz <pbeitz@d2iq.com>
2020-08-17 17:51:53 +0200

    refactor: use ui-kit's table for JobsOverview
    whilst addressing COPS-4665, the JobsOverviewTable got an overhaul.
    the main point is that we're now using ui-kits new Table component instead of
    the legacy reactjs-components' version.

--- plugins/jobs/src/js/constants/JobTableHeaderLabels.ts
+++
@@ -1,9 +0,0 @@
-import { i18nMark } from "@lingui/react";
-
-const JobTableHeaderLabels = {
-  name: i18nMark("Name"),
-  status: i18nMark("Status"),
-  lastRun: i18nMark("Last Run"),
-};
-
-export default JobTableHeaderLabels;
