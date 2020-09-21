afa8e688257a8f1367c92ea4e52ffbeae557fd9a
Pierre Beitz <pbeitz@d2iq.com>
2020-09-21 16:02:02 +0200

    chore: reorganize JobActions test
    * we don't need that PUT-stub.
    * we don't need a nested describe.
    * we don't need separate test cases for those tiny assertions.
    * we don't need `nodeHealth: true`.
    
    and we don't need that console.log i introduced accidentally. :blush:

--- tests/pages/jobs/JobActions-cy.js
+++ tests/pages/jobs/JobActions-cy.js
@@ -1,46 +1,24 @@
-describe("Job Actions", () => {
-  context("Edit Action", () => {
-    beforeEach(() => {
-      cy.configureCluster({
-        jobDetails: true,
-        mesos: "1-for-each-health",
-        nodeHealth: true,
-      });
-      cy.visitUrl({ url: "/jobs/detail/foo" });
+describe("Edit Job", () => {
+  beforeEach(() => {
+    cy.configureCluster({ jobDetails: true, mesos: "1-for-each-health" });
+    cy.route({ method: "PUT", url: /metronome\/v1\/jobs\/foo/, response: [] });
 
-      cy.get(".page-header-actions .dropdown").click();
-      cy.get(".dropdown-menu-items").contains("Edit").click();
-    });
+    cy.visitUrl({ url: "/jobs/detail/foo" });
+    cy.get(".page-header-actions .dropdown").click();
+    cy.get(".dropdown-menu-items").contains("Edit").click();
+  });
 
-    it("opens the correct jobs edit modal", () => {
-      cy.getFormGroupInputFor("Job ID *").should("have.value", "foo");
-    });
+  it("opens and edits the correct job", () => {
+    cy.getFormGroupInputFor("Job ID *").should("have.value", "foo");
+    cy.get("input[name='job.id']").should("be.disabled");
 
-    it("disables the job ID input", () => {
-      cy.get("input[name='job.id']").should("be.disabled");
-    });
+    cy.get(".modal .button-primary").contains("Submit").click();
+    cy.get(".modal").should("have.length", 0);
+  });
 
-    it("closes modal on successful API request", () => {
-      cy.route({
-        method: "PUT",
-        url: /metronome\/v1\/jobs\/foo/,
-        response: [],
-        delay: 0,
-      });
-      cy.route({
-        method: "PUT",
-        url: /metronome\/v1\/jobs\/foo\/schedules\/every-once-in-a-while/,
-        response: [],
-        delay: 0,
-      });
-      cy.get(".modal .button-primary").contains("Submit").click();
-      cy.get(".modal").should("have.length", 0);
-    });
-
-    it("closes modal on secondary button click", () => {
-      cy.get(".modal .button").contains("Cancel").click();
-      cy.get(".modal .button").contains("Discard").click();
-      cy.get(".modal").should("have.length", 0);
-    });
+  it("closes modal on secondary button click", () => {
+    cy.get(".modal .button").contains("Cancel").click();
+    cy.get(".modal .button").contains("Discard").click();
+    cy.get(".modal").should("have.length", 0);
   });
 });
