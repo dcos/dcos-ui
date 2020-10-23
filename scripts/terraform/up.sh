4820edf2479709235e8f0e7e72a8881157880894
Pierre Beitz <pbeitz@d2iq.com>
2020-10-23 11:28:07 +0200

    chore: slickify terraform script
    thanks @fatz!

--- scripts/terraform/up.sh
+++ scripts/terraform/up.sh
@@ -6,16 +6,11 @@ ssh-keygen -t rsa -N "" -f ~/.ssh/$TF_VAR_variant
 ssh-add ~/.ssh/$TF_VAR_variant
 
 echo "Running terraform init"
-for i in {1..3}; do terraform init && break; done
+for i in {1..3}; do terraform init --upgrade && break; done
 
 echo "Running terraform apply. This may take a while. Expect 15 minutes."
 # we try to recover a couple times in case the first try did not work - which currently happens frequently.
-for i in {1..3}; do terraform apply -auto-approve > ./log && break; done
-
-cat log
-
-# something like `cluster-address = dcos-ui-system-tests-1371554912.us-west-2.elb.amazonaws.com`
-line=$(grep -o "cluster-address = \S*" ./log | head -n1)
+for i in {1..3}; do terraform apply -auto-approve && break; done
 
 # print that cluster url
-echo http://$(echo $line | cut -d" " -f3)
+echo http://$(terraform output cluster-address)
